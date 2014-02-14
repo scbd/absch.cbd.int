7d6cd43c65ffc636b419a2a3293680e8d9dbe630
sbilodeau <bilodeaustephane@gmail.com>
2014-02-14 11:41:49 -0500

    Added resource CF

--- app/views/forms/edit/edit-resource.directive.js
+++ app/views/forms/edit/edit-resource.directive.js
@@ -1,13 +1,11 @@
 require("app").directive("editResource", [ function () {
 
 	return {
-		restrict   : 'EAC',
-		templateUrl: '/app/views/forms/edit/edit-abs-permit.resource.directive.html',
+		restrict   : "EAC",
+		templateUrl: "/app/views/forms/edit/edit-resource.directive.html",
 		replace    : true,
 		transclude : false,
-		scope: {
-			cmsParams : "&"
-		},
+		scope      : {},
 		controller : ["$scope", "$q", "authHttp", "Enumerable", "$filter", "guid", "IStorage", "authentication", "editFormUtility", "Thesaurus", function ($scope, $q, $http, Enumerable, $filter, guid, storage, authentication, editFormUtility, thesaurus)
 		{
 			$scope.status   = "";
@@ -15,54 +13,124 @@ require("app").directive("editResource", [ function () {
 			$scope.document = null;
 			$scope.review   = { locale: "en" };
 			$scope.options  = {
-				libraries     : function() { return $http.get("/api/v2013/thesaurus/domains/cbdLibraries/terms",                         { cache: true }).then(function(o){ return Enumerable.From(o.data).Where("$.identifier!='cbdLibrary:bch'").ToArray();})},
-				languages     : function() { return $http.get("/api/v2013/thesaurus/domains/52AFC0EE-7A02-4EFA-9277-8B6C327CE21F/terms", { cache: true }).then(function(o){ return $filter('orderBy')(o.data, 'name'); }); },
-				resourceTypes : function() { return $http.get("/api/v2013/thesaurus/domains/83BA4728-A843-442B-9664-705F55A8EC52/terms", { cache: true }).then(function(o){ return thesaurus.buildTree(o.data) }); },
-				cbdSubjects   : function() { return $http.get("/api/v2013/thesaurus/domains/CBD-SUBJECTS/terms",                         { cache: true }).then(function(o){ return o.data; }); },
-				aichiTargets  : function() { return $http.get("/api/v2013/thesaurus/domains/AICHI-TARGETS/terms",                        { cache: true }).then(function(o){ return o.data; }); },
+				libraries     : function() { return $http.get("/api/v2013/thesaurus/domains/cbdLibraries/terms",                         { cache: true }).then(function(o){ return Enumerable.from(o.data).where("$.identifier!='cbdLibrary:bch'").toArray();});},
+				languages     : function() { return $http.get("/api/v2013/thesaurus/domains/52AFC0EE-7A02-4EFA-9277-8B6C327CE21F/terms", { cache: true }).then(function(o){ return $filter("orderBy")(o.data, "name"); }); },
+				resourceTypes : function() { return $http.get("/api/v2013/thesaurus/domains/83BA4728-A843-442B-9664-705F55A8EC52/terms", { cache: true }).then(function(o){ return thesaurus.buildTree(o.data); }); },
 				absSubjects   : function() { return $http.get("/api/v2013/thesaurus/domains/CA9BBEA9-AAA7-4F2F-B3A3-7ED180DE1924/terms", { cache: true }).then(function(o){ return o.data; }); },
-				bchSubjects   : function() { return $http.get("/api/v2013/thesaurus/domains/043C7F0D-2226-4E54-A56F-EE0B74CCC984/terms", { cache: true }).then(function(o){ return o.data; }); },
-				bchRaSubjects : function() { return $http.get("/api/v2013/thesaurus/domains/69B43BB5-693B-4ED9-8FE0-95895E144142/terms", { cache: true }).then(function(o){ return o.data; }); },
-				ebsaSubjects  : function() { return []; },
-				regions       : function() { return $q.all([$http.get("/api/v2013/thesaurus/domains/countries/terms", { cache: true }), 
-														    $http.get("/api/v2013/thesaurus/domains/regions/terms",   { cache: true })]).then(function(o) {
-														    	return Enumerable.from($filter('orderBy')(o[0].data, 'name')).union(
-																	   Enumerable.from($filter('orderBy')(o[1].data, 'name'))).toArray();
-														   }) }
+				regions       : function() { return $q.all([$http.get("/api/v2013/thesaurus/domains/countries/terms", { cache: true }),
+															$http.get("/api/v2013/thesaurus/domains/regions/terms",   { cache: true })]).then(function(o) {
+																return Enumerable.from($filter("orderBy")(o[0].data, "name")).union(
+																	   Enumerable.from($filter("orderBy")(o[1].data, "name"))).toArray();
+											   });
+											}
+			};
 
 			//==================================
 			//
 			//==================================
-			$scope.isInLibrary = function(name, document) {
-				document = document || $scope.document;
+			function load(identifier) {
 
-				if (!document || !document.libraries)
-					return false;
+				$scope.status = "loading";
 
-				var qLibraries = Enumerable.From(document.libraries);
+				var qDocument = null;
 
-				if(name=="chm"  ) return qLibraries.Any(function(o){ return o.identifier == "cbdLibrary:chm"    });
-				if(name=="absch") return qLibraries.Any(function(o){ return o.identifier == "cbdLibrary:abs-ch" });
-				if(name=="bch"  ) return qLibraries.Any(function(o){ return o.identifier == "cbdLibrary:bch"    });
-				if(name=="ebsa" ) return qLibraries.Any(function(o){ return o.identifier == "cbdLibrary:ebsa"   });
+				if(identifier) {
+					qDocument = editFormUtility.load(identifier, "resource");
+				}
+				else {
+					qDocument = {
+						header: {
+							identifier: guid(),
+							schema   : "resource",
+							languages: ["en"]
+						},
+						government: $scope.userGovernment() ? { identifier: $scope.userGovernment() } : undefined,
+						libraries: [{ identifier: "cbdLibrary:abs-ch" }]
+					};
+				}
 
-				return false;
+				$q.when(qDocument).then(function(doc) {
+
+					$scope.tab    = "edit";
+					$scope.status = "ready";
+					$scope.document = doc;
+
+				}).catch(function(err) {
+
+					$scope.onError(err.data, err.status);
+					throw err;
+
+				});
 			}
 
 			//==================================
 			//
 			//==================================
-			$scope.IsBchRa = function(document) {
+			$scope.$on("loadDocument", function(evt, info) {
+
+				if(info.schema!="resource")
+					return;
+
+				load(info.identifier);
+			});
+
+			//==================================
+			//
+			//==================================
+			$scope.$on("documentInvalid", function(){
+				$scope.tab = "review";
+			});
+
+			//==================================
+			//
+			//==================================
+			$scope.$watch("tab", function(tab) {
+
+				if(tab == "edit")   { $scope.prevTab = "edit";  $scope.nextTab = "absch"; }
+				if(tab == "absch")  { $scope.prevTab = "edit";  $scope.nextTab = "review"; }
+				if(tab == "review") { $scope.prevTab = "absch"; $scope.nextTab = "review"; }
+
+				if(tab == "review")
+					validate();
+			});
+
+			//==================================
+			//
+			//==================================
+			$scope.getCleanDocument = function(document) {
+
 				document = document || $scope.document;
 
-				if (!document || !document.bchSubjects)
-					return false;
+				if (!document)
+					return undefined;
 
-				var qLibraries = Enumerable.from(document.bchSubjects);
+				document = angular.fromJson(angular.toJson(document));
 
-				return qLibraries.any(function(o) {
-					return o.identifier == "FBAF958B-14BF-45DD-BC6D-D34A9953BCEF"  //Risk assessment
-					    || o.identifier == "6F28D3FB-7CCE-4FD0-8C29-FB0306C52BD0"; //Risk assessment and risk management
+				if (/^\s*$/g.test(document.notes))
+					document.notes = undefined;
+
+				return document;
+			};
+
+			//==================================
+			//
+			//==================================
+			function validate() {
+
+				$scope.validationReport = null;
+
+				var oDocument = $scope.reviewDocument = $scope.getCleanDocument();
+
+				return storage.documents.validate(oDocument).then(function(success) {
+				
+					$scope.validationReport = success.data;
+					return !!(success.data && success.data.errors && success.data.errors.length);
+
+				}).catch(function(error) {
+
+					$scope.onError(error.data);
+					return true;
+
 				});
 			}
 
@@ -71,222 +139,31 @@ require("app").directive("editResource", [ function () {
 			//==================================
 			$scope.isLoading = function() {
 				return $scope.status=="loading";
-			}
+			};
 
 			//==================================
 			//
 			//==================================
 			$scope.hasError = function() {
-				return $scope.error!=null;
-			}
+				return $scope.error!==null;
+			};
 
 			//==================================
 			//
 			//==================================
 			$scope.userGovernment = function() {
-				return authentication.user().government;
+				return authentication.getUser().government;
 			};
-
-			//==================================
-			//
-			//==================================
-			$scope.init = function() {
-				if ($scope.document)
-					return;
-
-				$scope.status = "loading";
-
-				var identifier = URI().search(true).uid;
-				var promise = null;
-
-				if(identifier)
-					promise = editFormUtility.load(identifier, "resource");
-				else
-					promise = $q.when({
-						header: {
-							identifier: guid(),
-							schema   : "resource",
-							languages: ["en"]
-						}
-					}).then(function(doc){
-						if ($scope.cmsParams() && $scope.cmsParams().defaultLibrary)
-							doc.libraries = [{ identifier: $scope.cmsParams().defaultLibrary }];
-						return doc;
-					});
-
-
-				promise.then(
-					function(doc) {
-						$scope.status = "ready";
-						$scope.document = doc;
-					}).then(null, 
-					function(err) {
-						$scope.onError(err.data, err.status)
-						throw err;
-					});
-			}
-
-			//==================================
-			//
-			//==================================
-			$scope.tab = function(tab, show) {
-
-				var oTabNames    = [];
-				var sActiveTab   = $('.tab-content:first > .tab-pane.active').attr("id");
-				var qActiveTab   = $('#editFormPager a[data-toggle="tab"]:not(:first):not(:last)').filter('[href="#'+sActiveTab+'"]');
-
-				if (tab == "-") tab = (qActiveTab.prevAll(":not(:hidden):not(:last)").attr("href")||"").replace("#", "");
-				if (tab == "+") tab = (qActiveTab.nextAll(":not(:hidden):not(:last)").attr("href")||"").replace("#", "");
-
-				if(!tab)
-					return undefined;
-
-				if (show)
-					$('#editFormPager a[data-toggle="tab"][href="#review"]:first').tab('show');
-
-				return {
-					'name' : tab,
-					'active': sActiveTab == tab
-				}
-			}
-
-			//==================================
-			//
-			//==================================
-			$scope.onTab  = function(tab) {
-				var fn = function() {
-					if (tab == 'review')
-						$scope.validate();
-
-					if (!$('body').is(":animated"))
-						$('body').stop().animate({ scrollTop: 0 }, 600);
-				};
-
-				if ($scope.$root.$$phase == '$apply' || $scope.$root.$$phase == '$digest')
-					fn();
-				else
-					$scope.$apply(fn);
-			}
-
-			//==================================
-			//
-			//==================================
-			$scope.onPreSaveDraft = function() {
-				return $scope.cleanUp();
-			}
-
-			//==================================
-			//
-			//==================================
-			$scope.onPrePublish = function() {
-				return $scope.validate(false).then(function(hasError) {
-					if (hasError)
-						$scope.tab("review", true)
-					return hasError;
-				});
-			}
-
-			//==================================
-			//
-			//==================================
-			$scope.onPostWorkflow = function(data) {
-				window.location = "/managementcentre/my-pending-items";
-			};
-
-			//==================================
-			//
-			//==================================
-			$scope.onPostPublish = function(data) {
-				window.location = "/database/record?documentID="+data.documentID;
-			};
-
-			//==================================
-			//
-			//==================================
-			$scope.onPostSaveDraft = function(data) {
-				window.location = "/managementcentre/edit-draft/";
-			};
-
-			//==================================
-			//
-			//==================================
-			$scope.onPostClose = function() {
-				window.location = "/managementcentre/";
-			};
-
-			//==================================
-			//
-			//==================================
-			$scope.cleanUp = function(document) {
-				document = document || $scope.document;
-
-				if (!document)
-				return $q.when(true);
-
-				if (!$scope.isInLibrary("absch", document)) {
-					document.absSubjects = undefined;
-				}
-
-				if (!$scope.isInLibrary("bch", document)) {
-					document.bchSubjects       = undefined;
-					document.bchRaRecommend    = undefined;
-					document.bchRaSubjects     = undefined;
-					document.bchRaForLmo       = undefined;
-					document.organisms         = undefined;
-					document.genes             = undefined;
-					document.modifiedOrganisms = undefined;
-				}
-
-				if (!$scope.isInLibrary("chm", document)) {
-					document.cbdSubjects  = undefined;
-					document.aichiTargets = undefined;
-				}
-
-				if (!$scope.isInLibrary("ebsa", document)) {
-					document.ebsaSubjects = undefined;
-				}
-
-				if (/^\s*$/g.test(document.notes))
-					document.notes = undefined;
-
-				return $q.when(false);
-			};
-
-			//==================================
-			//
-			//==================================
-			$scope.validate = function(clone) {
-
-				$scope.validationReport = null;
-
-				var oDocument = $scope.document;
-
-				if (clone !== false)
-					oDocument = angular.fromJson(angular.toJson(oDocument));
-
-				return $scope.cleanUp(oDocument).then(function(cleanUpError) {
-					return storage.documents.validate(oDocument).then(
-						function(success) {
-							$scope.validationReport = success.data;
-							return cleanUpError || !!(success.data && success.data.errors && success.data.errors.length);
-						},
-						function(error) {
-							$scope.onError(error.data);
-							return true;
-						}
-					);
-				});
-			}
 
 			//==================================
 			//
 			//==================================
 			$scope.isFieldValid = function(field) {
 				if (field && $scope.validationReport && $scope.validationReport.errors)
-					return !Enumerable.from($scope.validationReport.errors).any(function(x){return x.property==field})
+					return !Enumerable.from($scope.validationReport.errors).any(function(x){return x.property==field;});
 
 				return true;
-			}
+			};
 
 			//==================================
 			//
@@ -308,10 +185,10 @@ require("app").directive("editResource", [ function () {
 					$scope.error  = "Record type is invalid.";
 				}
 				else if (error.Message)
-					$scope.error = error.Message
+					$scope.error = error.Message;
 				else
 					$scope.error = error;
-			}
+			};
 			
 			//==================================
 			//
@@ -327,11 +204,11 @@ require("app").directive("editResource", [ function () {
 						}, function(e) {
 							if (e.status == 404) {
 								storage.drafts.get(identifier, { info: "" })
-									.then(function(r) { deferred.resolve(r.data)},
-										  function(e) { deferred.reject (e)});
+									.then(function(r) { deferred.resolve(r.data);},
+										  function(e) { deferred.reject (e);});
 							}
 							else {
-								deferred.reject (e)
+								deferred.reject(e);
 							}
 						});
 					return deferred.promise;
@@ -341,14 +218,14 @@ require("app").directive("editResource", [ function () {
 
 				var sQuery = "type eq '" + encodeURI(schema) + "'";
 
-				return $q.all([storage.documents.query(sQuery, null, { cache: true }), 
+				return $q.all([storage.documents.query(sQuery, null, { cache: true }),
 							   storage.drafts   .query(sQuery, null, { cache: true })])
 					.then(function(results) {
 						var qResult = Enumerable.from (results[0].data.Items)
 												.union(results[1].data.Items, "$.identifier");
 						return qResult.toArray();
 					});
-			}
+			};
 		}]
-	}
+	};
 }]);
