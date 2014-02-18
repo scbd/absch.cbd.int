7b95776084c6019836dbff328a89835ecca6cd41
sbilodeau <bilodeaustephane@gmail.com>
2014-02-18 13:01:39 -0500

    Connect to api.cbd.int using https

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+/* jshint node:true */
 var fs = require('fs');
 var http = require('http');
-var https = require('https');
 var express = require('express');
 var httpProxy = require('http-proxy');
 
@@ -8,6 +8,7 @@ var httpProxy = require('http-proxy');
 
 var app = express();
 var server = http.createServer(app);
+var oneDay = 24*60*60*1000;
 
 app.configure(function() {
     app.set('port', process.env.PORT || 2010, '127.0.0.1');
@@ -15,25 +16,26 @@ app.configure(function() {
     app.use(express.logger('dev'));
     app.use(express.compress());
     app.use('/app', express.static(__dirname + '/app'));
-    app.use('/favicon.ico', express.static(__dirname + '/favicon.ico'));
+    app.use('/favicon.ico', express.static(__dirname + '/favicon.ico', { maxAge: oneDay }));
 });
 
 // Configure routes
 
-var proxy = new httpProxy.RoutingProxy();
+var proxy = httpProxy.createProxyServer({});
 
 app.get   ('/app/*'   , function(req, res) { res.send('404', 404); } );
+app.get   ('/public/*', function(req, res) { res.send('404', 404); } );
 
-app.get   ('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'api.cbd.int', port: 80 }); } );
-app.put   ('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'api.cbd.int', port: 80 }); } );
-app.post  ('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'api.cbd.int', port: 80 }); } );
-app.delete('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'api.cbd.int', port: 80 }); } );
+app.get   ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
+app.put   ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
+app.post  ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
+app.delete('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
 
 // Configure index.html
 
 app.get('/*', function(req, res) {
-	fs.readFile(__dirname + '/app/template.html', 'utf8', function (error, text) { 
-		res.send(text); 
+	fs.readFile(__dirname + '/app/template.html', 'utf8', function (error, text) {
+		res.send(text);
 	});
 });
 
