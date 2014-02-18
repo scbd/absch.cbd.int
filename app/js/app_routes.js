96199a454f10e7d3ad1a254885ec01e9ead18a6f
Matthew Dias <matthew.dias@cbd.int>
2014-02-18 12:42:08 -0500

    added fake country profile

--- app/js/app_routes.js
+++ app/js/app_routes.js
@@ -11,12 +11,13 @@ define(['app', 'authentication', 'services', 'filters', 'storage', 'workflows'],
         $locationProvider.hashPrefix('!');
     
         $routeProvider.
-            when('/',                           { templateUrl: '/app/views/index.html'                      , resolve: { user: resolveUser }}).
-            when('/find',                       { templateUrl: '/app/views/find.html'                       , resolve: { user: resolveUser }}).
-            when('/register',                   { templateUrl: '/app/views/register.html'                   , resolve: { user: resolveUser }}).
+            when('/',                            { templateUrl: '/app/views/index.html'                      , resolve: { user: resolveUser }}).
+            when('/find',                        { templateUrl: '/app/views/find.html'                       , resolve: { user: resolveUser }}).
+            when('/register',                    { templateUrl: '/app/views/register.html'                   , resolve: { user: resolveUser }}).
             when('/countries',                   { templateUrl: '/app/views/countries.html'                   , resolve: { user: resolveUser }}).
+            when('/countries/:code',             { templateUrl: '/app/views/profiles.html'                     , resolve: { user: resolveUser }}).
             when('/database/records/:id',        { templateUrl: '/app/views/forms/view/records-id.html'     , resolve: { user: resolveUser }}).
-            when('/oauth2/callback',            { templateUrl: '/app/views/oauth2/callback.html'            , resolve: { user: resolveUser }}).
+            when('/oauth2/callback',             { templateUrl: '/app/views/oauth2/callback.html'            , resolve: { user: resolveUser }}).
             otherwise({redirectTo:'/help/404'});
     }]);
 });
