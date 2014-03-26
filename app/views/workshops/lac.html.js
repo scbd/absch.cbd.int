14420975784df85ec28a6f25845890d803d7f844
Matthew Dias <matthew.dias@cbd.int>
2014-03-26 10:34:13 -0300

    updated workshops folder

---
+++ app/views/workshops/lac.html.js
@@ -0,0 +1,8 @@
+require("app").controller("LacController", ["$scope", "$http", function ($scope, $http) {
+
+
+$scope.photos24 = [{'id':'8903'},{'id':'8924'},{'id':'8950'},{'id':'8952'},{'id':'8954'},{'id':'8961'},{'id':'8963'}, {'id':'8968'},  {'id':'8970'},  {'id':'8971'}];
+
+
+
+}]);
