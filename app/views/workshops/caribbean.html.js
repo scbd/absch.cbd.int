27b5ad61b73c03085795e4303e2c72df0d8eb97c
Matthew Dias <matthew.dias@cbd.int>
2014-05-22 17:24:16 -0400

    UI updates

--- app/views/workshops/caribbean.html.js
+++ app/views/workshops/caribbean.html.js
@@ -2,6 +2,8 @@ define(['app'], function (app) {
 
 app.controller("CaribbeanController", ["$scope", "$http", function ($scope, $http) {
 
+$scope.photoSet = [{'id':'header.jpg'},{'id':'IMG_0359.jpg'},{'id':'IMG_0360.jpg'},{'id':'IMG_0366.jpg'},{'id':'IMG_0368.jpg'},{'id':'IMG_0369.jpg'},{'id':'photo1.jpg'},{'id':'photo2.jpg'}];
 
+}]);
 
 });
