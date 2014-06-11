c486e1784234154dc29fbf929be30b8055c05412
unknown <blaise.fonseca@Fonseca.biodiv.org>
2014-06-11 19:52:11 -0400

    Conact fix

--- app/views/forms/edit/edit-absPermit.html.js
+++ app/views/forms/edit/edit-absPermit.html.js
@@ -1,6 +1,6 @@
 define(['app', '/app/views/forms/edit/edit.js'], function (app) {
 
-  app.controller("editAbsPermit", ["$scope", "authHttp", "guid", "$filter", "Thesaurus", "$q", "$location", "IStorage", "authentication", "Enumerable", "editFormUtility", "$routeParams", "$controller", function ($scope, $http, guid, $filter, thesaurus, $q, $location, storage, authentication, Enumerable, editFormUtility, $routeParams, $controller) {
+  app.controller("editAbsPermit", ["$scope", "authHttp", "guid", "$filter", "Thesaurus", "$q", "$location", "IStorage", "authentication", "Enumerable", "editFormUtility", "$routeParams", "$controller", function ($scope, $http, guid, $filter, Thesaurus, $q, $location, storage, authentication, Enumerable, editFormUtility, $routeParams, $controller) {
     $controller('editController', {$scope: $scope});
 
     $scope.options  = {
