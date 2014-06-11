4edc5a7fc2310ed60678ba2453b633d1f626efc9
Jason McCarrell <jason.abz@gmail.com>
2014-06-11 17:09:17 -0400

    added back the elements of measure checkboxes directive.

--- app/views/forms/edit/edit-measure.html.js
+++ app/views/forms/edit/edit-measure.html.js
@@ -68,4 +68,172 @@ define(['app', '/app/views/forms/edit/edit.js'], function (app) {
     };
 
   }]);
+
+
+	//============================================================
+	//
+	//
+	//============================================================
+	app.directive("editMeasureAbs", function () {
+		return {
+			restrict: "EAC",
+			templateUrl: "/app/views/forms/edit/edit-measure.directive/abs",
+			replace: true,
+			transclude: false,
+			require : "?ngModel",
+			scope: {
+				binding     : "=ngModel",
+				locales     : "=",
+				termsFn     : "&terms",
+				required    : "@",
+				layout      : "@"
+			},
+			link: function ($scope, $element, $attr, ngModelController) 
+			{
+				$scope.identifiers = null;
+				$scope.sections    = null;
+				$scope.terms       = null;
+				$scope.rootTerms   = [];
+
+				$scope.$watch("terms",        $scope.onTerms);
+				$scope.$watch("binding",      $scope.load);
+				$scope.$watch("binding", function() {
+					ngModelController.$setViewValue($scope.binding);
+				});
+
+				$scope.$watch(function(){return angular.toJson($scope.identifiers) }, $scope.save); //use tojson to detect changes
+				$scope.$watch(function(){return angular.toJson($scope.sections   ) }, $scope.save);
+
+				$scope.init();
+	
+				if(!$attr["class"])
+					$element.addClass("list-unstyled");
+
+			},
+			controller: ["$scope", "$q", "Thesaurus", "Enumerable", function ($scope, $q, thesaurus, Enumerable) 
+			{
+				//==============================
+				//
+				//==============================
+				$scope.init = function() {
+					$scope.setError(null);
+					$scope.__loading = true;
+
+					$q.when($scope.termsFn(), 
+						function(data) { // on success
+							$scope.__loading = false;
+							$scope.terms     = data;
+						}, function(error) { // on error
+							$scope.__loading = false;
+							$scope.setError(error);
+						});
+				}
+
+				//==============================
+				//
+				//==============================
+				$scope.load = function() 
+				{
+					if (!$scope.terms) // Not initialized
+						return;
+
+					var oNewIdentifiers = {};
+					var oNewSections    = {};
+
+					if(!$.isArray($scope.terms))
+						throw "Type must be array";
+
+					if($scope.binding) {
+
+						if(!$.isArray($scope.binding))
+							throw "Type must be array";
+
+						for(var i=0; i<$scope.binding.length; ++i)
+						{
+							oNewIdentifiers[$scope.binding[i].identifier] = true;
+							oNewSections   [$scope.binding[i].identifier] = $scope.binding[i].section
+						}
+					}
+
+					if (!angular.equals(oNewIdentifiers,  $scope.identifiers)) $scope.identifiers = oNewIdentifiers;
+					if (!angular.equals(oNewSections,     $scope.sections   )) $scope.sections    = oNewSections;
+				}
+
+				//==============================
+				//
+				//==============================
+				$scope.save = function() 
+				{
+					if(!$scope.identifiers)
+						return;
+
+					var oNewBinding = [];
+
+					angular.forEach($scope.terms, function(term, i) 
+					{
+						if(term==undefined) return //IE8 BUG
+
+						if($scope.identifiers[term.identifier])
+						{
+							var oTerm = { identifier : term.identifier };
+
+							if($scope.sections[term.identifier])
+								oTerm.section = $scope.sections[term.identifier];
+
+							oNewBinding.push(oTerm);
+						}
+					});
+
+					if($.isEmptyObject(oNewBinding))
+						oNewBinding = undefined;
+
+					if(!angular.equals(oNewBinding, $scope.binding))
+						$scope.binding = oNewBinding;
+				}
+
+				//==============================
+				//
+				//==============================
+				$scope.isRequired = function()
+				{
+					return $scope.required!=undefined 
+						&& $.isEmptyObject($scope.binding);
+				}
+
+				//==============================
+				//
+				//==============================
+				$scope.onTerms = function(refTerms) {
+
+					$scope.rootTerms = [];
+
+					if(refTerms)
+					{
+						if (($scope.layout||"tree") == "tree") //Default layout
+							$scope.rootTerms = thesaurus.buildTree(refTerms);
+						else
+							$scope.rootTerms = Enumerable.from(refTerms).Select("o=>{identifier : o.identifier, name : o.name, title : o.title, description : o.description}").ToArray();
+					}
+
+					$scope.load();
+				}
+
+				//==============================
+				//
+				//==============================
+				$scope.setError = function(error) {
+					if (!error) {
+						$scope.error = null;
+						return;
+					}
+
+					if (error.status == 404) $scope.error = "Terms not found";
+					else                     $scope.error = error.data || "unkown error";
+				}
+
+				$scope.$emit("getDocumentInfo", {});
+			}]
+		}
+	});
+
 });
