30d9532385c83a3a3375b820bd2bfbc6ab25579b
Blaise Fonseca <blaisefonseca@hotmail.com>
2014-06-11 11:12:02 -0400

    show facets for newly added filters on search page

--- app/views/find_schemas.partial.html.js
+++ app/views/find_schemas.partial.html.js
@@ -13,8 +13,8 @@ app.directive('searchFilterSchemas', function ($http) {
         link: function ($scope, element, attrs, ngModelController)
         {
         },
-        controller : ['$scope', '$element', '$location', 'Thesaurus', "IStorage", "guid", "$q", "Enumerable", "$filter",
-         function ($scope, $element, $location, Thesaurus, storage, guid, $q, Enumerable, $filter)
+        controller : ['$scope', '$element', '$location', 'Thesaurus', "IStorage", "guid", "$q", "Enumerable", "$filter","underscore",
+         function ($scope, $element, $location, Thesaurus, storage, guid, $q, Enumerable, $filter,_)
         {
             $scope.showNationalFilters = true;
             $scope.showReferenceFilters = true;
@@ -32,16 +32,33 @@ app.directive('searchFilterSchemas', function ($http) {
             $scope.permitExpiryDate= '*:*'
 
             $scope.options  = {
-               jurisdiction             : function () { return $http.get("/api/v2013/thesaurus/domains/7A56954F-7430-4B8B-B733-54B8A5E7FF40/terms",  { cache: true }).then(function(o){ return o.data; }); },
-               status                   : function () { return $http.get("/api/v2013/thesaurus/domains/ED7CDBD8-7762-4A84-82DD-30C01458A799/terms",  { cache: true }).then(function(o){ return o.data; }); },
-               typeOfDocuments          : function () { return $http.get("/api/v2013/thesaurus/domains/144CF550-7629-43F3-817E-CACDED34837E/terms",  { cache: true }).then(function(o){ return o.data; }); },
-               absJurisdictions         : function () { return $http.get("/api/v2013/thesaurus/domains/51A113E9-071F-440A-83DC-E3499B7C646D/terms", { cache: true }).then(function (o) { return o.data; }); },
-               absGeneticResourceTypes  : function () { return $http.get("/api/v2013/thesaurus/domains/1A22EAAB-9BBC-4543-890E-DEF913F59E98/terms", { cache: true }).then(function (o) { return Thesaurus.buildTree(o.data); }); },
-               keywords                 : function () { return $http.get("/api/v2013/thesaurus/domains/1A22EAAB-9BBC-4543-890E-DEF913F59E98/terms", { cache: true }).then(function (o) { return Thesaurus.buildTree(o.data); }); },
-               usage                    : function () { return $http.get("/api/v2013/thesaurus/domains/A7B77788-8C90-4849-9327-E181E9522F3A/terms", { cache: true }).then(function (o) { return o.data; }); },
-               cpjurisdictions          : function () { return $http.get("/api/v2013/thesaurus/domains/D7BD5BDE-A6B9-4261-B788-16839CCC4F7E/terms", { cache: true }).then(function(o){ return o.data; }); },
+               jurisdiction             : function () { return $http.get("/api/v2013/thesaurus/domains/7A56954F-7430-4B8B-B733-54B8A5E7FF40/terms",  { cache: true })
+                                                                    .then(function(o){ return $scope.updateFacets($scope.measure, 'msrJurisdiction',o.data); }); },
+               status                   : function () { return $http.get("/api/v2013/thesaurus/domains/ED7CDBD8-7762-4A84-82DD-30C01458A799/terms",  { cache: true })
+                                                                    .then(function(o){ return $scope.updateFacets($scope.measure,'msrStatus',o.data) }); },
+               typeOfDocuments          : function () { return $http.get("/api/v2013/thesaurus/domains/144CF550-7629-43F3-817E-CACDED34837E/terms",  { cache: true })
+                                                                    .then(function(o){ return $scope.updateFacets($scope.measure,'msrType',o.data) }); },
+               cnaJurisdictions         : function () { return $http.get("/api/v2013/thesaurus/domains/D7BD5BDE-A6B9-4261-B788-16839CCC4F7E/terms", { cache: true })
+                                                                    .then(function (o) { return $scope.updateFacets($scope.authority,'cnaJurisdiction',o.data) }); },
+               absGeneticResourceTypes  : function () { return $http.get("/api/v2013/thesaurus/domains/1A22EAAB-9BBC-4543-890E-DEF913F59E98/terms", { cache: true })
+                                                                    .then(function (o) { 
+
+                                                                       return $scope.updateFacets($scope.authority,'cnaGeneticResourceTypes',Thesaurus.buildTree(o.data)) ;
+                                                                         }); },
+               keywords                 : function () { return $http.get("/api/v2013/thesaurus/domains/1A22EAAB-9BBC-4543-890E-DEF913F59E98/terms", { cache: true })
+                                                                    .then(function (o) {return $scope.updateFacets($scope.absPermit,'permitkeywords',Thesaurus.buildTree(o.data)) ; }); },
+               usage                    : function () { return $http.get("/api/v2013/thesaurus/domains/A7B77788-8C90-4849-9327-E181E9522F3A/terms", { cache: true })
+                                                                    .then(function (o) { return $scope.updateFacets($scope.absPermit,'permitusage',o.data); }); },
+               cpJurisdictions         : function () { return $q.all([$http.get("/api/v2013/thesaurus/domains/D7BD5BDE-A6B9-4261-B788-16839CCC4F7E/terms", { cache: true }), 
+                                                               $http.get("/api/v2013/thesaurus/terms/5B6177DD-5E5E-434E-8CB7-D63D67D5EBED",   { cache: true })]).then(function(o) {
+                                                                var data = o[0].data;
+                                                                data.push(o[1].data)
+                                                                return $scope.updateFacets($scope.absCheckpoint,'cpJurisdiction',data);
+                                                              }); },
+               resourceTypes            : function () { return $http.get("/api/v2013/thesaurus/domains/83BA4728-A843-442B-9664-705F55A8EC52/terms", { cache: true })
+                                                                    .then(function(o){ return $scope.updateFacets($scope.resource,'vlrresourceTypes',Thesaurus.buildTree(o.data)); }); },
+
                countries                : function () { return $http.get("/api/v2013/thesaurus/domains/countries/terms", { cache: true }).then(function (o) { return $filter("orderBy")(o.data, "name"); }); },
-               resourceTypes            : function () { return $http.get("/api/v2013/thesaurus/domains/83BA4728-A843-442B-9664-705F55A8EC52/terms", { cache: true }).then(function(o){ return Thesaurus.buildTree(o.data); }); },
                regions                  : function () { return $q.all([$http.get("/api/v2013/thesaurus/domains/countries/terms", { cache: true }),
                                                             $http.get("/api/v2013/thesaurus/domains/regions/terms",   { cache: true })]).then(function(o) {
                                                                             return Enumerable.from($filter("orderBy")(o[0].data, "name")).union(
@@ -175,6 +192,60 @@ app.directive('searchFilterSchemas', function ($http) {
                     }
                 });                 
  
+            }            
+            $scope.updateFacets = function(schema,fieldName,data){
+                
+                var facets = _.where(schema.subFilters,{name:fieldName})[0]||[]; 
+                console.log(data);
+                if(!facets.facets)
+                    return data;
+
+                facets = facets.facets;
+                for (var i = 0; i < facets.length; i+=2) {
+                   var item =  _.where(data,{identifier:facets[i]});
+                   console.log(item);
+                   if(item.length>0){
+                        item[0].title.en += ' (' + facets[i+1] + ')';
+                        console.log(facets[i] + ":" + facets[i+1]);
+                    }
+                };
+                return data;
+            }
+
+            $scope.loadSchemaFacets=function(schema){
+
+                if($scope[schema].subFilters){
+
+                    var facetFields = [];
+                    $scope[schema].subFilters.forEach(function(filter){
+                        if(filter.type=='multiselect' && !filter.facets)
+                            facetFields.push(filter.field);
+                    });
+                    if(facetFields.length<=0)
+                        return;
+                    var queryFacetsParameters = {
+                            'q': 'realm_ss:absch AND NOT version_s:* AND schema_s:' + schema,
+                            'fl': '',       //fields for results.
+                            'wt': 'json',
+                            'rows': 0,      //limit
+                            'facet': true,  //get counts back
+                            'facet.field': facetFields,
+                            'facet.limit': 512
+                        };
+
+                        $http.get('/api/v2013/index/select', { params: queryFacetsParameters })
+                        .success(function (data) {                          
+                            console.log(data);
+                            $scope[schema].subFilters.forEach(function(filter){
+                                if(filter.type=='multiselect'){
+                                    filter.facets = data.facet_counts.facet_fields[filter.field];                                                                ;
+                                }
+                            });
+                        })
+                        .error(function (error) { 
+                            console.log('onerror'); console.log(error); 
+                        });
+                }
             }
 
 
@@ -266,7 +337,7 @@ app.directive('searchFilterSchemas', function ($http) {
 
             $scope.$watch('items', onWatch_items);
  
-            $scope.refresh = function(selected){
+            $scope.refresh = function(selected, schema){
                 
                 if(selected)
                     $scope.expandSearch++;
@@ -274,6 +345,8 @@ app.directive('searchFilterSchemas', function ($http) {
                     $scope.expandSearch--;
 
                 buildQuery();
+
+                $scope.loadSchemaFacets(schema);
             }
             
             buildQuery();
