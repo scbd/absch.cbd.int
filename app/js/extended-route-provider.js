e786a14046b3df9d62d49331999c96b2b5edbd7b
Blaise Fonseca <blaisefonseca@hotmail.com>
2014-08-18 10:02:56 -0400

    fix register navigation issue
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/js/extended-route-provider.js
+++ app/js/extended-route-provider.js
@@ -69,14 +69,14 @@ define(['app', 'angular', 'authentication'], function(app, angular) {
             return ['$q', '$route', function($q, $route) {
 
                 var deferred = $q.defer();
-     
+
                 var controllers = [];
                 controllers.push($route.current.$$route.templateUrl + '.js');
 
                 //TODO: I'm not sure if this is the most elegant approach... reconsider
                 //NOTE: for some reason the subTemplareUrl is staying as the old one, not as the newly defined one. Yet the document_type is being changed.
                 if($route.current.$$route.subTemplateUrl && $route.current.$$route.subTemplateUrl.slice(-1) == '-')
-                  $route.current.$$route.subTemplateUrlFull = $route.current.$$route.subTemplateUrl.slice(0, -1) + $route.current.params.document_type + '.html';
+                  $route.current.$$route.subTemplateUrlFull = $route.current.$$route.subTemplateUrl + $route.current.params.document_type + '.html';
                 else
                   $route.current.$$route.subTemplateUrlFull = $route.current.$$route.subTemplateUrl;
 
