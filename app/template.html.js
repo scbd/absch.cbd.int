500f657489c0f382f5d554904bb443783629cc96
matthew dias <matthew.dias@cbd.int>
2014-09-16 12:08:34 -0400

    updates

--- app/template.html.js
+++ app/template.html.js
@@ -24,11 +24,16 @@ define(['app'], function (app) {
         //
         //============================================================
         $scope.development_env = false;
+        $scope.env_name = "ABS-CH PILOT PHASE";
         $scope.development_env = $location.absUrl().toLowerCase().indexOf("://absch.cbd.int");
-        if(!$scope.development_env || $scope.development_env == -1)
+        if(!$scope.development_env || $scope.development_env == -1){
             $scope.development_env = true;
-        else
+            $scope.env_name = "DEVELOPMENT";
+        }
+        else{
             $scope.development_env = false;
+            $scope.env_name = "ABS-CH PILOT PHASE";
+        }
 
         //============================================================
         //
