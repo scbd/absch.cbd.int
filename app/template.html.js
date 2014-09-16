f57073b9e31d1fb81e8d51b4f42205ce78199dc1
matthew dias <matthew.dias@cbd.int>
2014-09-16 12:02:35 -0400

    update

--- app/template.html.js
+++ app/template.html.js
@@ -23,8 +23,8 @@ define(['app'], function (app) {
         //
         //
         //============================================================
+        $scope.development_env = false;
         $scope.development_env = $location.absUrl().toLowerCase().indexOf("://absch.cbd.int");
-
         if(!$scope.development_env || $scope.development_env == -1)
             $scope.development_env = true;
         else
