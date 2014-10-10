7993a38bd3e922feb8a24cb46e76d56dab7d85bf
Blaise Fonseca <blaisefonseca@hotmail.com>
2014-10-10 17:40:57 -0400

    fix permit UID on CPC
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/absPDFViewer/absCheckpointCommunique.html.js
+++ app/absPDFViewer/absCheckpointCommunique.html.js
@@ -222,7 +222,7 @@ app.filter("uniqueID", ['$filter', '$q','$http', function( $filter, $q, $http) {
 
                 var unique = 'ABSCH-' + $filter("schemaShortName")($filter("lowercase")(document.type)) +
                         (government != '' ? '-' + $filter("uppercase")(government) : '') +
-                        '-' + document.documentID + '-' + document.revision;
+                        '-' + document.documentID;
 				cacheMap[term.identifier] = unique;
 
 				return unique;
