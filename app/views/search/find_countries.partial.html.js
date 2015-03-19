5cd6f75bf899bbb64dab853614e772071fd46c9a
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-03-19 15:47:54 -0400

    remove inbetween party from country filter
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/search/find_countries.partial.html.js
+++ app/views/search/find_countries.partial.html.js
@@ -13,8 +13,8 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
         externalFilter: '=externalFilter'
       },
       link: function($scope, element, attrs, ngModelController) {},
-      controller: ['$scope', '$element', '$location', 'commonjs', '$q','$routeParams',
-        function($scope, $element, $location, commonjs, $q, $routeParams) {
+      controller: ['$scope', '$element', '$location', 'commonjs', '$q','$routeParams','$timeout',
+        function($scope, $element, $location, commonjs, $q, $routeParams, $timeout) {
           $scope.alphabet = ['All', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
             'W', 'X', 'Y', 'Z'
           ];
@@ -96,7 +96,7 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
           function buildConditions(conditions, items) {
             items.forEach(function(item) {
               if (item.selected)
-                conditions.push($scope.field + ':' + item.identifier);
+                conditions.push($scope.field + ':' + item.code.toLowerCase());
             });
 
             if ($scope.externalFilter && $scope.externalFilter.length > 0) {
@@ -112,7 +112,7 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
             var dictionary = [];
             items.forEach(function(item) {
               item.selected = false;
-              dictionary[item.identifier] = item;
+              dictionary[item.code.toLowerCase()] = item;
             });
             return dictionary;
           }
@@ -122,7 +122,7 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
           };
           $scope.filterCountries = function(entity) {
             return $scope.countryFilter == 'All' ||
-              (entity && entity.title.en.indexOf($scope.countryFilter) == 0);
+              (entity && entity.name.en.indexOf($scope.countryFilter) == 0);
             // ||$scope.countryFilter == 'isPartyToCBD' && !entity.isSignatory && !entity.isRatified
             // ||$scope.countryFilter == 'isSignatory' && entity.isSignatory
             // ||$scope.countryFilter == 'isRatified' && entity.isRatified;
@@ -130,7 +130,11 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
           };
 
           $scope.filterByType = function(entity) {
-            return $scope.typeFilter == 'All' || $scope.typeFilter == 'isPartyToCBD' && !entity.isSignatory && !entity.isRatified || $scope.typeFilter == 'isSignatory' && entity.isSignatory || $scope.typeFilter == 'isRatified' && entity.isRatified || $scope.typeFilter == 'isInbetween' && entity.isInbetween;
+            return $scope.typeFilter == 'All' ||
+            $scope.typeFilter == 'isPartyToCBD' && ((!entity.isSignatory && !entity.isRatified) || entity.isInbetween)||
+            $scope.typeFilter == 'isSignatory' && entity.isSignatory ||
+            $scope.typeFilter == 'isRatified' && entity.isRatified && !entity.isInbetween || 
+            $scope.typeFilter == 'isInbetween' && entity.isInbetween;
 
           };
           $scope.selectedCount = function() {
@@ -161,7 +165,7 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
             if(oldValue && newValue && oldValue.length > newValue.length){ //i.e filter had been removed.
                 var removedFilter = _.without(oldValue, newValue);
                 console.log(removedFilter);
-                var country = _.where($scope.terms, {identifier: removedFilter[0]})
+                var country = _.where($scope.terms, {code: removedFilter[0]})
                 country[0].selected = false;
             }
             buildQuery();
@@ -182,7 +186,7 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
               if($scope.externalFilter && $scope.externalFilter.length>0 && $scope.terms.length > 0){
                   $scope.externalFilter.forEach(function(item){
                       var cd = _.where($scope.terms, {
-                          identifier: item
+                          code: item
                       })
                       cd[0].selected = true;
                   });
@@ -191,8 +195,8 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
 
           function loadContryDetails(value) {
             if (value && $scope.terms.length == 0)
-              $http.get('/api/v2013/thesaurus/domains/countries/terms').success(function(data) {
-
+              //$http.get('/api/v2013/thesaurus/domains/countries/terms').success(function(data) {
+              $q.when(commonjs.getCountries(), function(data) {
                 $scope.terms = data;
                 $scope.termsx = dictionarize($scope.terms);
                 onWatch_items($scope.items);
@@ -202,30 +206,49 @@ define(['app', 'underscore', '/app/js/common.js'], function(app, _) {
                 $scope.ratifiedCount = 0;
                 $scope.inbetweenCount = 0;
 
-                $q.when(commonjs.getCountries(), function(countries) {
-
+                // $q.when(commonjs.getCountries(), function(countries) {
+                //
                   $scope.terms.forEach(function(country) {
-                    var cd = _.where(countries, {
-                      code: country.identifier.substring(0, 2).toUpperCase()
-                    })
-                    if (cd.length > 0) {
-                      country.isParty = cd[0].isParty;
-                      country.isSignatory = cd[0].isSignatory;
-                      country.isRatified = cd[0].isRatified;
-                      country.isInbetween = cd[0].isInbetweenParty;
+                    // var cd = _.where(countries, {
+                    //   code: country.identifier.substring(0, 2).toUpperCase()
+                    // })
+                    // if (cd.length > 0) {
+                      country.isParty =country.isParty;
+                      country.isSignatory = country.isSignatory;
+                      country.isRatified = country.isRatified;
+                      country.isInbetween = country.isInbetweenParty;
 
                       if (country.isSignatory)
                         $scope.signatoryCount++;
-                      if (!country.isSignatory && !country.isRatified)
+
+                      if (country.isInbetween || !country.isRatified)
                         $scope.nonRatifiedCount++;
-                      if (country.isRatified)
+
+                      if (country.isRatified && !country.isInbetween)
                         $scope.ratifiedCount++;
+
                       if (country.isInbetween)
                         $scope.inbetweenCount++;
-                    }
-                  });
+                    // }
 
-                });
+                  });
+                //
+                //  var cout =
+                //   _.reject(countries, function(country){
+                //       var cd = _.find($scope.terms, function(cot){
+                //           return cot.identifier.toUpperCase() == country.code.toUpperCase();
+                //         }                      )
+                //         if(!cd)
+                //         console.log(cd,country)
+                //         if(cd && cd.length>0)
+                //             return cd[0];
+                //
+                //   })
+                //   console.log(cout);
+                //
+                //   console.log($scope.terms)
+                //
+                // });
               });
           }
 
