5e48c238428f5f696e00f452e32168bffae6abf0
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-03-23 16:31:33 -0400

    load documents history on demand
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/forms/view/view-history-directive.html.js
+++ app/views/forms/view/view-history-directive.html.js
@@ -1,139 +1,141 @@
-define(['app'], function (app) {
+define(['app'], function(app) {
 
-app.directive("viewHistory", [function () {
-	return {
-		restrict   : "EAC",
-		templateUrl: "/app/views/forms/view/view-history-directive.html",
-		replace    : true,
-		transclude : false,
-		scope: {
-			documentID: "=documentId",
-			loadDocuments: "=loadDocuments",
-			government:  "=government"
-		},
-		link: function($scope, $element) {
-			// $element.find("[data-toggle='tooltip']").tooltip({trigger:'hover'});
-		},
-		controller: ['$scope', 'IStorage', "$q","$route","$filter","$timeout",
-			function ($scope, storage, $q,$route, $filter,$timeout) {
+    app.directive("viewHistory", [function() {
+        return {
+            restrict: "EAC",
+            templateUrl: "/app/views/forms/view/view-history-directive.html",
+            replace: true,
+            transclude: false,
+            scope: {
+                documentID: "=documentId",
+                loadDocuments: "=loadDocuments",
+                government: "=government"
+            },
+            link: function($scope, $element) {
+                // $element.find("[data-toggle='tooltip']").tooltip({trigger:'hover'});
+            },
+            controller: ['$scope', 'IStorage', "$q", "$route", "$filter", "$timeout",
+                function($scope, storage, $q, $route, $filter, $timeout) {
 
-			$scope.$watch('documentID', function(value){
-				if(value && !$scope.documents){
-					$scope.load(value);
-				}
-				else
-					$scope.loading=false;
-			});
+                    //$scope.$watch('documentID', function(value) {
+                        // if (value && !$scope.documents) {
+                        //     $scope.load(value);
+                        // } else
+                        //     $scope.loading = false;
+                    //});
 
-			// $scope.$watch('loadDocuments', function(value){
-			// 	if(value && !$scope.documents){
-			// 		$scope.load($scope.documentID);
-			// 	}
-			// 	else
-			// 		$scope.loading=false;
-			// });
-
-
-			//==================================
-			//
-			//==================================
-			$scope.load = function (identifier) {
-
-				$scope.error = undefined;
-
-				if($scope.loading)
-					return
-
-				if(!identifier)
-				{
-					$scope.loading=false;
-					return;
-				}
-
-				$scope.loading=true;
-				var qDocument     = storage.documentVersions.get(identifier,{body:true});
-				// .then(function(result) { return result.data || result });
-
-				$q.when(qDocument).then(function(results) {
-
-						$scope.documents     = results.data.Items;
-						if($scope.documents && $scope.documents.length >0 && ($scope.documents[0].type=="absPermit" || $scope.documents[0].type=="absCheckpointCommunique"))
-						 	$scope.showFile = true;
-
-						if(results.data.Items && results.data.Items.length >0 && $scope.documents[0].type=="absPermit"){
-							loadPermitCPC(identifier, results.data.Items);
-							$scope.isPermit = true;
-						}
-						$scope.loading=false;
-						// $timeout(function(){
-						// 	console.log($("[data-toggle='tooltip']"));
-						// 	$("[data-toggle='tooltip']").tooltip({trigger:'hover'});
-						// },200);
-
-				}).then(null, function(error) {
-					$scope.error = "error loading document history";
-					 console.log( $scope.error );
-				});
-
-			};
+                    // $scope.$watch('loadDocuments', function(value){
+                    // 	if(value && !$scope.documents){
+                    // 		$scope.load($scope.documentID);
+                    // 	}
+                    // 	else
+                    // 		$scope.loading=false;
+                    // });
 
 
-	      function loadPermitCPC(identifier, documents) {
+                    //==================================
+                    //
+                    //==================================
+                    $scope.load = function(identifier) {
 
-			if(!documents)
-				return;
+                        $scope.error = undefined;
 
-	        if(!identifier)
-	        {
-	          $scope.loading=false;
-	          return;
-	        }
-	        var filter = ["type eq 'absCheckpointCommunique'"];
-	        var query = [[{permit : identifier}]];
+                        if ($scope.loading)
+                            return
 
-	        var qDocument     = storage.documentQuery.body(filter,query,{body:true});
-	         $q.when(qDocument).then(function(results) {
-			    $q.when(results.data.forEach(function(cpc){
-					 var qDoc = storage.documentVersions.get(cpc.identifier,{body:true});
-					  $q.when(qDoc).then(function(results) {
-						  results.data.Items.forEach(function(history){
-								$scope.documents.push(history);
-							});
-					});
-				}));
-			});
-		  };
+                        if (!identifier) {
+                            $scope.loading = false;
+                            return;
+                        }
 
-		$scope.$watch("documents", function(){
-			$scope.column = 'createdOn';
-		});
+                        $scope.loading = true;
+                        var qDocument = storage.documentVersions.get(identifier, {
+                            body: true
+                        });
+                        // .then(function(result) { return result.data || result });
 
-		$scope.formatEmails = function(emailList){
-			var emailString = '';
-			// console.log(angular.isArray(emailList),emailList)
-			if(angular.isArray(emailList))
-				emailList.forEach(function(email){
-					//console.log(email.emails.join(',') +'(' + email.firstName + ' ' + email.lastName + ')');
-					if(email.emails){
-						emailString += email.emails.join(',');
+                        $q.when(qDocument).then(function(results) {
 
-						if(email.firstName || email.lastName){
-								emailString += '(';
-							if(email.firstName)
-								emailString += email.firstName + ' ';
-							if(email.lastName)
-								emailString += email.lastName;
+                            $scope.documents = results.data.Items;
+                            if ($scope.documents && $scope.documents.length > 0 && ($scope.documents[0].type == "absPermit" || $scope.documents[0].type == "absCheckpointCommunique"))
+                                $scope.showFile = true;
 
-							emailString += '); ';
-						}
-					}
-				});
-// console.log(emailString)
-			return emailString;
-		}
+                            if (results.data.Items && results.data.Items.length > 0 && $scope.documents[0].type == "absPermit") {
+                                loadPermitCPC(identifier, results.data.Items);
+                                $scope.isPermit = true;
+                            }
+
+                        }).then(null, function(error) {
+                            $scope.error = "error loading document history";
+                        }).finally(function(){$scope.loading = false;});
+
+                    };
 
 
-		}]
-	};
-}]);
+                    function loadPermitCPC(identifier, documents) {
+
+                        if (!documents)
+                            return;
+
+                        if (!identifier) {
+                            $scope.loading = false;
+                            return;
+                        }
+                        var filter = ["type eq 'absCheckpointCommunique'"];
+                        var query = [
+                            [{
+                                permit: identifier
+                            }]
+                        ];
+
+                        var qDocument = storage.documentQuery.body(filter, query, {
+                            body: true
+                        });
+                        $q.when(qDocument).then(function(results) {
+                            $q.when(results.data.forEach(function(cpc) {
+                                var qDoc = storage.documentVersions.get(cpc.identifier, {
+                                    body: true
+                                });
+                                $q.when(qDoc).then(function(results) {
+                                    results.data.Items.forEach(function(history) {
+                                        $scope.documents.push(history);
+                                    });
+                                });
+                            }));
+                        });
+                    };
+
+                    $scope.$watch("documents", function() {
+                        $scope.column = 'createdOn';
+                    });
+
+                    $scope.formatEmails = function(emailList) {
+                        var emailString = '';
+                        // console.log(angular.isArray(emailList),emailList)
+                        if (angular.isArray(emailList))
+                            emailList.forEach(function(email) {
+                                //console.log(email.emails.join(',') +'(' + email.firstName + ' ' + email.lastName + ')');
+                                if (email.emails) {
+                                    emailString += email.emails.join(',');
+
+                                    if (email.firstName || email.lastName) {
+                                        emailString += '(';
+                                        if (email.firstName)
+                                            emailString += email.firstName + ' ';
+                                        if (email.lastName)
+                                            emailString += email.lastName;
+
+                                        emailString += '); ';
+                                    }
+                                }
+                            });
+                        // console.log(emailString)
+                        return emailString;
+                    }
+
+
+                }
+            ]
+        };
+    }]);
 });
