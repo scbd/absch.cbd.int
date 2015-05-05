2999d5b63a323ed0b4aa7fbddbcc3d1f31fa1da6
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-05-05 17:23:06 -0400

    call authentication.signIn instead of local http call
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/directives/login.directive.html.js
+++ app/views/directives/login.directive.html.js
@@ -24,38 +24,21 @@ app.directive('login', function ($http) {
                 $scope.errorInvalid = false;
                     $scope.errorTimeout = false;
                     $scope.waiting      = true;
-
-                    var credentials = { 'email': $scope.email, 'password': $scope.password };
-
-                    $http.post('/api/v2013/authentication/token', credentials).then(function onsuccess(success) {
-
-                      $browser.cookies().authenticationToken = success.data.authenticationToken;
-                      $browser.cookies().email = $scope.rememberMe ? $scope.email : '';
-                      var response = { type: 'setAuthenticationToken', authenticationToken: $browser.cookies().authenticationToken, setAuthenticationEmail: $browser.cookies().email||'' };
-
-                      var authenticationFrame = angular.element(document.querySelector('#authenticationFrame'))[0];
-                        authenticationFrame.contentWindow.postMessage(JSON.stringify(response), 'https://accounts.cbd.int');
-
-                        authentication.getUser(true).then(function (user) {
-                            if(typeof $scope.onAuthorization =='function'){
-                                $scope.onAuthorization();
-                            }                            
-                            $rootScope.$broadcast('signIn', user);
-                            return $rootScope.user;
-                        })
-                        .finally(function(){
-                            $scope.waiting      = false;
-                            $scope.password = '';
-                        });
-
-                    }, function onerror(error) {
-                      console.log(error);
-                      $scope.password     = "";
+                    
+                    authentication.signIn($scope.email, $scope.password)
+                    .then(function(user){
+                        if(typeof $scope.onAuthorization =='function'){
+                            $scope.onAuthorization();
+                        }
+                    })
+                    .catch(function onerror(error) {
+                        console.log(error);
+                        $scope.password     = "";
                         $scope.errorInvalid = error.status == 403;
                         $scope.errorTimeout = error.status != 403;
                         $scope.waiting      = false;
                     });
-              }
+              };
 
              $scope.actionSignup = function () {
                   var redirect_uri = encodeURIComponent($location.protocol()+'://'+$location.host()+':'+$location.port()+'/');
