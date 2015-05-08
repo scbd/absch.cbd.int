966be581652c61fc22117ac62eebed3d38005d74
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-05-08 11:04:14 -0400

    Revert "Revert "add user role list to dropdown""
    This reverts commit 23aba9166632641912d8969247bcb1e236686284.
    
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

---
+++ app/views/help/releases.html.js
@@ -0,0 +1,12 @@
+define(['app'], function (app) {
+
+"use strict";
+//require("app", "dragAndDrop")
+
+app.controller("presentationController",
+	["$rootScope", "$scope", "$q", "underscore",'$http',function ($rootScope, $scope, $q, _, $http) {	
+	
+			
+
+   }]);
+});
