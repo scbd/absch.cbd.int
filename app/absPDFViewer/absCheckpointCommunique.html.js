8688fbe2a6735c4089f8376968a55c2c3e9d499e
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-05-21 18:25:26 -0400

    check name exists to skip empty contact box
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/absPDFViewer/absCheckpointCommunique.html.js
+++ app/absPDFViewer/absCheckpointCommunique.html.js
@@ -23,7 +23,7 @@ app.controller('printPermit', ['$scope','$http','$location','$sce','$filter','$q
 			$scope.documentInfo = result[1].data;
 			$scope.documentVersion = result[2].data;
 			$scope.realm = $scope.documentInfo.Realm;
-			
+
 			if($scope.document.permit){
 				$scope.document.permit.forEach(function(item){
 					$http.get('/api/v2013/documents/' +  item.identifier, { info:true})
@@ -48,7 +48,7 @@ app.controller('printPermit', ['$scope','$http','$location','$sce','$filter','$q
 		$scope.emailList = [];
 		if(document.permit){
 				angular.forEach(document.permit, function(permit){
-					if(permit.document.authority)
+					if(permit.document.authority  && (permit.document.authority.title_t || permit.document.authority.firstName))
 						$scope.emailList.push(permit.document.authority);
 				})
 		}
