45c73443e6ff8e12ef8b42bec9cc13c45c8a3b8f
matthew dias <matthew.dias@cbd.int>
2015-09-23 20:28:58 -0400

    update faqs

--- app/views/help/faq.html.js
+++ app/views/help/faq.html.js
@@ -1,6 +1,6 @@
-define(['app','underscore','ngMaterial','ngAria','angular-animate','scbd-angularjs-services','scbd-angularjs-filters','scbd-angularjs-controls'], function (app, _) {
+define(['app','underscore','ngSmoothScroll','ngMaterial','ngAria','angular-animate','scbd-angularjs-services','scbd-angularjs-filters','scbd-angularjs-controls'], function (app, _) {
 app.controller("faqController",
-	["$rootScope", "$scope", "$q", "underscore",'$http','commonjs',function ($rootScope, $scope, $q, _, $http, commonjs) {	
+	["$rootScope", "$scope", "$q", "underscore",'$http','commonjs','smoothScroll', '$element', function ($rootScope, $scope, $q, _, $http, commonjs, smoothScroll, $element) {	
 				
 		 $q.when( $http.get('/api/v2015/help-faqs'))
           .then(function(response){
@@ -12,6 +12,30 @@ app.controller("faqController",
 				commonjs.isUserInRole($rootScope.getRoleName('Administrator'))
 
 			};
-        
+			
+	
+		$scope.getTitle = function(id){
+			for(var i=0;i<$scope.faqs.length;i++){
+				if($scope.faqs[i]._id == id)
+					return $scope.faqs[i].title;	
+			}
+			return "";
+		}
+
+		
+		$scope.scrollTo = function(rel){
+			var element = document.getElementById(rel);
+			var options = {
+				duration: 700,
+				easing: 'easeInQuad',
+				offset: 60,
+			}
+			
+			smoothScroll(element, options);
+		}
+		
+	
+		
+			
    }]);
 });
