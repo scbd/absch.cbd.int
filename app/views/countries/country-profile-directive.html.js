0731899d3716d28b48e1680848c98fe01a90157e
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-09-25 17:11:37 -0400

    open MSR section if route contains measure-matrix
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/countries/country-profile-directive.html.js
+++ app/views/countries/country-profile-directive.html.js
@@ -194,6 +194,12 @@
                         });
                         $scope.getFacets($scope.absch_nfp);
                         $('[data-toggle="tooltip"]').tooltip()
+                        
+                        if($routeParams.countryCode && $routeParams.documentType){
+                            if($routeParams.documentType.toLowerCase()=='matrix'){
+                                $scope.showMSR = true;
+                            }
+                        }
                     });
                     }
                 }
