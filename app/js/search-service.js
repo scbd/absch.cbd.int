8c2b97bac59b6865cb9f054f1814c9ea3ada28a6
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-09-30 11:00:22 -0400

    implement group and list search in serive
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/js/search-service.js
+++ app/js/search-service.js
@@ -3,25 +3,67 @@ define(['app', 'underscore'], function (app, _) {
     app.factory('searchService', ['$http', '$q', function ($http, $q) {
         return new function () {
             
-            this.list = function(searchQuery){
+            var searchDefaults = {
+                currentPage     : 0,
+                rowsPerPage     : 25,
+                sort            : 'createdDate_dt desc',
+                fields          : 'title_t, description_t',
+                query           : '*:*',
                 
+                groupSort       :   'government_EN_t',
+                groupField      :   'government_EN_t asc'
             }
             
-            this.group = function(searchQuery){
+            this.list = function(searchQuery, queryCanceler){
+               
+               _.defaults(searchQuery, searchDefaults);
+               
+               var queryListParameters = {
+                    'q': searchQuery.query,
+                    'sort': searchQuery.sort,
+                    'fl': searchQuery.fields,
+                    'wt': 'json',
+                    'start': searchQuery.currentPage * searchQuery.rowsPerPage,
+                    'rows': searchQuery.rowsPerPage,
+                };
                 
+                return $http.get('/api/v2013/index/select', { params: queryListParameters, timeout: queryCanceler });
+            }
+            
+            this.group = function(searchQuery, queryCanceler){
+                
+               _.defaults(searchQuery, searchDefaults);
+               
+               var queryGroupParameters = {
+                    'q': searchQuery.query + ' AND government_s:*',
+                    'sort': searchQuery.sort,
+                    'fl': searchQuery.fields,
+                    'wt': 'json',
+                    'start': searchQuery.currentPage * searchQuery.rowsPerPage,
+                    'rows': searchQuery.rowsPerPage,
+                    'group': true,
+                    'group.ngroups' : true,
+                    'group.field': searchQuery.groupField,
+                    'group.limit': 1000,
+                    'group.sort': searchQuery.groupSort
+                };
+                
+                return $http.get('/api/v2013/index/select', { params: queryGroupParameters, timeout: queryCanceler });
             }
             
             this.facets = function(facetQuery){
                 
+               _.defaults(facetQuery, searchDefaults);
+               
                 if(facetQuery) {
                     
                     var queryFacetsParameters = {
-                        'q'             : facetQuery.q,
+                        'q'             : facetQuery.query,
                         'fl'            : '', 
                         'wt'            : 'json',
                         'rows'          : 0,
                         'facet'         : true,
-                        'facet.field'   :facetQuery.f,
+                        'facet.field'   :facetQuery.fields,
                         'facet.limit'   : 512,
                         'facet.mincount': 1
                     };
@@ -29,7 +71,7 @@ define(['app', 'underscore'], function (app, _) {
                    $q.when(queryAction)
                     .then(function (data) {
                         var facets = {};
-                        _.each(facetQuery.f, function(facet){
+                        _.each(facetQuery.fl, function(facet){
                             facets[facet] = readFacets2(data.data.facet_counts.facet_fields[facet]);
                         });
                         console.log(facets);
