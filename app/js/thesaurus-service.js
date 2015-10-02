6835312599e3e041ab6ceef695cded06e2d39610
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-10-02 11:03:58 -0400

    cleanup changes
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/js/thesaurus-service.js
+++ app/js/thesaurus-service.js
@@ -2,31 +2,49 @@ define(['app', 'underscore'], function (app, _) {
 
     app.factory('thesaurusService', ['$http', '$q', function ($http, $q) {
         return new function () {
-            
-            
+
+
             this.getDomains = function(domainIdentifier){
-               
+
                 return $http.get('/api/v2013/thesaurus/domains/' + domainIdentifier );
-            }
-            
+            };
+
             this.getDomainTerms = function(termIdentifier, includeOthers){
-                
+                return fn_getDomainTerms(termIdentifier, includeOthers);
+            };
+
+            function fn_getDomainTerms(termIdentifier, includeOthers){
                 if(!termIdentifier)
                     throw "Domain term is missing";
-                
-                var url = '/api/v2013/thesaurus/domains/' + termIdentifier + '/terms'                    
-                
+
+                var url = '/api/v2013/thesaurus/domains/' + domainTerms[termIdentifier] + '/terms';
+
                 if(includeOthers){
-                    $http.get(url)
+                    var urlOther = '/api/v2013/thesaurus/terms/' + domainTerms['others'];
+                    return $q.all([$http.get(url),$http.get(urlOther)])
+                                .then(function(termData){
+                                    var data = termData[0].data;
+                                    data.push(termData[1].data);
+                                    return data;
+                                });
                 }
-               
-                return $http.get(url);
+                if(termIdentifier == 'others'){
+                    url = '/api/v2013/thesaurus/terms/' + domainTerms.others;
+                    return $q.when($http.get(url))
+                             .then(function(data){
+                                return data.data;
+                             });
+                }
+
+                return $q.when($http.get(url))
+                            .then(function(data){
+                                return data.data;
+                            });
             }
-            
-            
-            var domainTerms = {    
+            var domainTerms = {
                 countries                : "countries",
                 regions                  : "regions",
+                others                   : "5B6177DD-5E5E-434E-8CB7-D63D67D5EBED",
                 jurisdiction             : "7A56954F-7430-4B8B-B733-54B8A5E7FF40",
                 status                   : "ED7CDBD8-7762-4A84-82DD-30C01458A799",
                 typeOfDocuments          : "144CF550-7629-43F3-817E-CACDED34837E",
@@ -39,7 +57,7 @@ define(['app', 'underscore'], function (app, _) {
                 resourceTypes            : "83BA4728-A843-442B-9664-705F55A8EC52",
                 absSubjects              : "CA9BBEA9-AAA7-4F2F-B3A3-7ED180DE1924",
                 mccResourceTypes         : "840427E5-E5AC-4578-B238-C81EAEEDBDD8"
-                        
+
             }
         }
     }]);
