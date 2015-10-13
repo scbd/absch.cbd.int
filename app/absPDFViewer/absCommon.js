a9e5adebfdf5edee8d3e072056c56e608067ceb3
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-10-13 16:45:13 -0400

    harmonize new UID format and links to its realms
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/absPDFViewer/absCommon.js
+++ app/absPDFViewer/absCommon.js
@@ -227,7 +227,7 @@ app.filter("uniqueID", ['$filter', '$q','$http', function( $filter, $q, $http) {
 				else if(document.body && document.body.government)
 					government = document.body.government.identifier;
 
-				var unique = 'ABSCH-' + $filter("schemaShortName")($filter("lowercase")(document.type)) +
+				var unique = 'ABSCH' + (document.realm.toUpperCase().replace('ABS','').replace('-','')) + '-' + $filter("schemaShortName")($filter("lowercase")(document.type)) +
 						(government != '' ? '-' + $filter("uppercase")(government) : '') +
 						'-' + document.documentID + (revision ? ('-' + document.revision) : '');
 
