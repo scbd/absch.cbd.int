c7131377bfe514de2c98c2a03b687e1a1293f1dc
matthew dias <matthew.dias@cbd.int>
2015-11-10 16:29:40 -0500

    update to forums

--- app/views/forums/forum-list-view.html.js
+++ app/views/forums/forum-list-view.html.js
@@ -4,6 +4,7 @@ define(['app', 'cbd-forums'], function(app) {
 
             //TODO : need a API to get facets
             var iac = $http.get('/api/v2014/discussions/forums/17415/threads');
+            var jointIac = $http.get('/api/v2014/discussions/forums/17446/threads');
             var vlr = $http.get('/api/v2014/discussions/forums/17384/threads');
             var car = $http.get('/api/v2014/discussions/forums/17378/threads');
             var art13 = $http.get('/api/v2014/discussions/forums/17316/threads');
@@ -11,9 +12,17 @@ define(['app', 'cbd-forums'], function(app) {
             //
             if($scope.training_env || $scope.development_env)
                 iac = $http.get('/api/v2014/discussions/forums/17433/threads');
+                jointIac = $http.get('/api/v2014/discussions/forums/17446/threads');
 
     	     $q.when(iac).then(function(response) {
                  $scope.iacThreads = response.data.length;
+             })
+             .catch(function(error){
+                console.log(error);
+            });
+            
+            $q.when(jointIac).then(function(response) {
+                 $scope.jointIacThreads = response.data.length;
              })
              .catch(function(error){
                 console.log(error);
