87254d53333924d277351cd1cb23ff36f60a2fd0
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-01-06 10:38:01 -0500

    humanize number filter
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/js/filters.js
+++ app/js/filters.js
@@ -380,5 +380,35 @@ define(["app",'/app/js/common.js'], function (app) {
 		console.log(text);
 			return $sce.trustAsHtml(text)
 		}
-	})
+	});
+
+//https://github.com/HubSpot/humanize/blob/master/public/src/humanize.js
+	app.filter('humanizeNumber', function() {
+		return function(value) {
+		    var end, leastSignificant, number, specialCase;
+		    number = parseInt(value, 10);
+		    if (number === 0) {
+		      return value;
+		    }
+		    specialCase = number % 100;
+		    if (specialCase === 11 || specialCase === 12 || specialCase === 13) {
+		      return "" + number + "th";
+		    }
+		    leastSignificant = number % 10;
+		    switch (leastSignificant) {
+		      case 1:
+		        end = "st";
+		        break;
+		      case 2:
+		        end = "nd";
+		        break;
+		      case 3:
+		        end = "rd";
+		        break;
+		      default:
+		        end = "th";
+		    }
+		    return "" + number + end;
+		};
+	});
 });
