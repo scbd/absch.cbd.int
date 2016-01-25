b758ebee4fb28c1be333e331e8942e7602432b25
matthew dias <matthew.dias@cbd.int>
2016-01-25 13:40:47 -0500

    update to the template

--- app/views/directives/nav/cbd-header.js
+++ app/views/directives/nav/cbd-header.js
@@ -1,89 +1,64 @@
-<<<<<<< HEAD
-define(['app', 'underscore', , 'angular-localizer', 'scbd-angularjs-services', 'scbd-angularjs-filters',
+define(['app', 'underscore','angular-localizer','scbd-angularjs-services','scbd-angularjs-filters',
     '/app/views/directives/login.directive.html.js',
     '/app/views/directives/xuser-notifications.js',
     'ngAria', 'angular-animate',
-], function(app, _) {
-    app.directive('cbdHeader', function() {
+], function (app, _) {
+        app.directive('cbdHeader', function () {
         return {
             restrict: 'EAC',
             replace: true,
+            // transclude: true,
             templateUrl: '/app/views/directives/nav/cbd-header.html',
             scope: {
-                uid: '@',
-                user : '='
+                    uid: '@',
             },
-            link: ['$scope', '$q', '$element', function($scope, $q, $element) {
+            link: ['$scope', '$q', '$element', function ($scope, $q, $element) {
 
-            }],
-            controller: ['$scope', '$rootScope', '$window', '$location', 'authentication', '$browser', 'realmConfiguration', 'underscore', 'IUserNotifications', '$timeout', '$filter', '$anchorScroll',
-                function($scope, $rootScope, $window, $location, authentication, $browser, realmConfiguration, _, userNotifications, $timeout, $filter) {
-
-                    //============================================================
-                    //
-                    //
-                    //============================================================
-                    $scope.actionSignOut = function() {
-                        authentication.signOut();
-                    };
-
-                    //============================================================
-                    //
-                    //
-                    //============================================================
-                    $scope.actionSignup = function() {
-                        var redirect_uri = encodeURIComponent($location.protocol() + '://' + $location.host() + ':' + $location.port() + '/');
-                        $window.location.href = 'https://accounts.cbd.int/signup?redirect_uri=' + redirect_uri;
-                    };
-
-                    //============================================================
-                    //
-                    //
-                    //============================================================
-                    $scope.actionPassword = function() {
-                        var redirect_uri = encodeURIComponent($location.protocol() + '://' + $location.host() + ':' + $location.port() + '/');
-                        $window.location.href = 'https://accounts.cbd.int/password?redirect_uri=' + redirect_uri;
-                    };
-
-                    //============================================================
-                    //
-                    //
-                    //============================================================
-                    $scope.actionProfile = function() {
-                        var redirect_uri = encodeURIComponent($location.protocol() + '://' + $location.host() + ':' + $location.port() + '/');
-                        $window.location.href = 'https://accounts.cbd.int/profile?redirect_uri=' + redirect_uri;
-
-                    };
-
-                    $scope.closePopup = function() {
-                        $('#loginDialog').modal('hide')
-                    };
+            }]
+            , controller: ['$scope', '$rootScope', '$window', '$location', 'authentication', '$browser', 'realmConfiguration', 'underscore', 'IUserNotifications', '$timeout','$filter','$anchorScroll',
+            function($scope, $rootScope, $window, $location, authentication, $browser, realmConfiguration, _, userNotifications, $timeout, $filter) {
 
 
-                    //$scope.user = $rootScope.user;
+            //============================================================
+            //
+            //
+            //============================================================
+            $scope.actionSignOut = function() {
+                authentication.signOut();
+            };
 
+            //============================================================
+            //
+            //
+            //============================================================
+            $scope.actionSignup = function() {
+                var redirect_uri = encodeURIComponent($location.protocol() + '://' + $location.host() + ':' + $location.port() + '/');
+                $window.location.href = 'https://accounts.cbd.int/signup?redirect_uri=' + redirect_uri;
+            };
 
-                }
-            ]
-        };
+            //============================================================
+            //
+            //
+            //============================================================
+            $scope.actionPassword = function() {
+                var redirect_uri = encodeURIComponent($location.protocol() + '://' + $location.host() + ':' + $location.port() + '/');
+                $window.location.href = 'https://accounts.cbd.int/password?redirect_uri=' + redirect_uri;
+            };
 
-    });
-=======
-define(['app', 'underscore', ], function (app, _) {
-        app.directive('cbdHeader', function () {
-                return {
-                        restrict: 'EAC',
-                        replace: true,
-                        // transclude: true,
-                        templateUrl: '/app/views/directives/nav/cbd-header.html',
-                        scope: {
-                                uid: '@',
-                        },
-                        link: ['$scope', '$q', '$element', function ($scope, $q, $element) {
+            //============================================================
+            //
+            //
+            //============================================================
+            $scope.actionProfile = function() {
+                var redirect_uri = encodeURIComponent($location.protocol() + '://' + $location.host() + ':' + $location.port() + '/');
+                $window.location.href = 'https://accounts.cbd.int/profile?redirect_uri=' + redirect_uri;
 
-                        }]
-                        , controller: ['$scope','$rootScope', '$q','$element','$http', '$filter',
-                                function ($scope, $rootScope, $q, $element, $http, $filter) {
+            };
+
+            $scope.closePopup = function() {
+                $('#loginDialog').modal('hide')
+            };
+
 
 
 
@@ -91,5 +66,4 @@ define(['app', 'underscore', ], function (app, _) {
                 };
 
         });
->>>>>>> update to home and template
 });
