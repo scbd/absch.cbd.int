20df193fefde79ddcb79821a5cdc6d5b63b41a05
matthew dias <matthew.dias@cbd.int>
2016-02-22 22:15:42 -0500

    update to about

--- app/views/about/about.html.js
+++ app/views/about/about.html.js
@@ -1,8 +1,11 @@
 define(['app','underscore','ngSmoothScroll','ngMaterial','ngAria','angular-animate','scbd-angularjs-services','scbd-angularjs-filters','scbd-angularjs-controls'], function (app, _) {
 app.controller("AboutController",
 	["$rootScope", "$scope", "$q", "underscore",'$http','commonjs','smoothScroll', '$element', function ($rootScope, $scope, $q, _, $http, commonjs, smoothScroll, $element) {
-
-		
+     
+    //  $scope.showAbout = false;
+    //  $scope.toggleAbout = function(){
+    //      $scope.showAbout = !$scope.showAbout;
+    //  }
 
    }]);
 });
