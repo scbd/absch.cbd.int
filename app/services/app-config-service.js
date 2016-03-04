e572c07881cf6f6e87defabbd2ee2f8cf338b3de
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-03-04 17:02:54 -0500

    add scbd schemas property
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/services/app-config-service.js
+++ app/services/app-config-service.js
@@ -2,8 +2,10 @@ define(['app', 'underscore'], function (app, _) { 'use strict';
 
 	app.factory('appConfigService',  ["$location", function($location) {
 
-        var nationalSchemas = ["absPermit", "absCheckpoint", "absCheckpointCommunique", "authority", "measure", "database", "focalPoint"];
-        var referenceSchemas = ["resource", "meeting", "notification", "pressRelease", "statement", "news", "modelContractualClause", "communityProtocol", "capacityBuildingInitiative", "capacityBuildingResource"];
+        var nationalSchemas 	= [ "absPermit", "absCheckpoint", "absCheckpointCommunique", "authority", "measure", "database", "focalPoint"];
+        var referenceSchemas 	= [ "modelContractualClause", "communityProtocol", "capacityBuildingInitiative", "capacityBuildingResource"];
+		var scbdSchemas			= [ "resource", "meeting", "notification", "pressRelease", "statement", "news" ];
+
         var realmConfigurations = [
             //Production
             {
@@ -162,6 +164,7 @@ define(['app', 'underscore'], function (app, _) { 'use strict';
 		return {
 			nationalSchemas     :   nationalSchemas,
 			referenceSchemas    :   referenceSchemas,
+			scbdSchemas			:	scbdSchemas,
 
 			currentRealm        :   currentRealm,
 			setCurrentRealm  	:   setCurrentRealm,
