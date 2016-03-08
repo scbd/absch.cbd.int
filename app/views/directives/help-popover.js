8bde1840b5e45cba714c34e01f6fc0c7ee8b0261
matthew dias <matthew.dias@cbd.int>
2016-03-08 11:20:47 -0500

    update

--- app/views/directives/help-popover.js
+++ app/views/directives/help-popover.js
@@ -1,13 +1,26 @@
-define(['app'], function (app) {
+define(['app', $], function (app) {
 	app.directive('helpPopover', function(){
 		return{
 			restrict: 'EAC',
 			replace:true,
 			templateUrl: '/app/views/directives/help-popover.html',
-			link: ['$scope','element', 'attrs', function($scope, element, attrs){
+			link: function($scope, element, attrs){
+                       
+                $scope.showPopover = function(element){   
+                    
+                    console.log("click");
+                    
+                    $(element).popover({
+                        trigger: 'click',
+                        html: true,
+                        title: attrs.title,
+                        content: attrs.content,
+                        placement: attrs.placement
+                    });
+                };
                         
-              
-			}]
+			},
+            
 		};
 
 	});
