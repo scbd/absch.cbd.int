c4f50216ce2241ac999fa50b52849500dd7df74a
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-03-10 15:32:21 -0500

    switch to api server url
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/admin/reported-records.html.js
+++ app/views/admin/reported-records.html.js
@@ -3,7 +3,7 @@ define(['app', '/app/js/common.js',
     ], function (app) {
 	app.controller('adminReportedRecordsCtrl', ['$scope', '$http','commonjs','$element',
         function($scope, $http, commonjs, $element){
-                $http.get('http://localhost:8000/api/v2015/report-records')
+                $http.get('/api/v2015/report-records')
                 .then(function(data){
                     $scope.reportRecords = data.data
                 });
