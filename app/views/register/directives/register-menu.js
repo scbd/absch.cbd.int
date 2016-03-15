58ed0cde466aae314b1dff3f7b444dc47f32c147
matthew dias <matthew.dias@cbd.int>
2016-03-15 19:38:29 -0400

    hide links for roles

--- app/views/register/directives/register-menu.js
+++ app/views/register/directives/register-menu.js
@@ -41,9 +41,26 @@ define(['app', 'underscore', '/app/services/role-service.js'], function(app, _) 
 
                     
                 },
-                controller : ['$scope', '$element', 'roleService', '$rootScope', '$compile', '$filter', '$routeParams',
-                    function($scope, $element, roleService, $rootScope, $compile, $filter, $routeParams){
+                controller : ['$scope', '$element', 'roleService', '$rootScope', '$compile', '$filter', '$routeParams','authentication',
+                    function($scope, $element, roleService, $rootScope, $compile, $filter, $routeParams, authentication){
 
+
+                     $scope.user = $rootScope.user;
+
+                      if ($scope.user.isAuthenticated) {
+                            $scope.roles = {
+                                isAbsPublishingAuthority : roleService.isAbsPublishingAuthority(),
+                                isAbsNationalFocalPoint  : roleService.isAbsNationalFocalPoint(),
+                                isAbsAdministrator       : roleService.isAbsAdministrator(),
+                                isAdministrator          : roleService.isAdministrator(),
+                                isAbsNationalAuthorizedUser : roleService.isAbsNationalAuthorizedUser(),
+                                isUser                      : roleService.isUser()
+                            };
+                        
+                        }
+              
+                       
+                    
                     if ($rootScope.user && $rootScope.user.isAuthenticated && roleService.hasAbsRoles()) {
 
                         require(['/app/views/register/directives/national-records-menu.html.js'], function(menu) {
