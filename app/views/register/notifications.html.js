290a3e319b66f20ab8c261917ddc769db4d150b7
matthew dias <matthew.dias@cbd.int>
2016-03-17 10:02:57 -0400

    update notificaitons

--- app/views/register/notifications.html.js
+++ app/views/register/notifications.html.js
@@ -1,5 +1,7 @@
 define(['app',
- '/app/services/role-service.js', ], function(app) {
+ '/app/services/role-service.js',
+  'scbd-branding/directives/xuser-notifications-panel',
+  ], function(app) {
     "use strict";
     app.controller("NotificationsController", ["$rootScope", "$scope", "underscore", "roleService", "IWorkflows", "realm", "$q",
                     "$routeParams", '$location', "$filter", "$http", "$element","$timeout",
@@ -25,6 +27,8 @@ define(['app',
                     $scope.userCountry = {identifier:$scope.user.government };
 
             }
+            
+            $scope.realm = realm.value;
         }
     ]);
 });
