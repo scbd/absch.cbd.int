8e921786a5182236f371c4608425868c740af235
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-04-03 13:50:33 -0400

    default Development branch in docker dev deploy
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-docker build -t localhost:5000/dev-absch-cbd-int git@github.com:scbd/absch.cbd.int.git
+docker build -t localhost:5000/dev-absch-cbd-int git@github.com:scbd/absch.cbd.int.git#Development
 docker push     localhost:5000/dev-absch-cbd-int
