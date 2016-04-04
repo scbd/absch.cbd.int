f958b982c1f3f086e69dd9d2676de10e41373fda
matthew dias <matthew.dias@cbd.int>
2016-04-04 12:09:46 -0400

    hide filters when none

--- app/views/search-new/search-filters/keyword-filter.js
+++ app/views/search-new/search-filters/keyword-filter.js
@@ -11,14 +11,9 @@ define(['app'],  function(app, _) {
             link: function($scope, $element, $attrs, searchDirectiveCtrl) {
               
                $scope.keywordSearchFilters = searchDirectiveCtrl.getSearchFilters("keyword");
-               
-               
+                              
                $scope.keywordSearchFiltersNoDups =   searchDirectiveCtrl.getSearchFilters("keyword");//filter this for unique object.id so the all keywords only shows the unique fitlers
-            
-               
-                      
-              
-               
+
                $scope.relatedFilters = {};
 
             },// link
