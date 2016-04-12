26039041b08206bed7daa33ba962750d3f98343a
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-04-12 19:48:28 -0400

    load documents from document api for display
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/forms/edit/document-selector.html.js
+++ app/views/forms/edit/document-selector.html.js
@@ -1,12 +1,12 @@
-define(['app', 
-'/app/js/common.js', 
+define(['app',
+'/app/js/common.js',
 '/app/views/directives/search-filter-dates.partial.html.js',
-'/app/views/search-new/search-results/result-default.js', 
+'/app/views/search-new/search-results/result-default.js',
 '/app/services/search-service.js',
 '/app/services/app-config-service.js'], function (app, commonjs) { // jshint ignore:line
 
-app.directive("documentSelector", ["$http",'$rootScope', "$filter", "underscore", "$q", "searchService", "appConfigService",
-function ($http, $rootScope, $filter, _,  $q, searchService, appConfigService) {
+app.directive("documentSelector", ["$http",'$rootScope', "$filter", "underscore", "$q", "searchService", "appConfigService", "IStorage",
+function ($http, $rootScope, $filter, _,  $q, searchService, appConfigService, IStorage) {
 
 	return {
 		restrict   : "EA",
@@ -25,14 +25,14 @@ function ($http, $rootScope, $filter, _,  $q, searchService, appConfigService) {
             filter : "@filter"
 		},
 		link : function($scope) {
-            
+
             $scope.rawDocuments = [];
 			$scope.areVisible = false;
             $scope.userGov = $scope.$root.user.government;
-            
+
             if(!$scope.type) $scope.type = "checkbox";
-            
-      
+
+
 
             //==================================
             //
@@ -53,43 +53,48 @@ function ($http, $rootScope, $filter, _,  $q, searchService, appConfigService) {
                     {
                         if(!$scope.model)
                             $scope.model=[];
-                            
+
                         $scope.model.push({identifier: doc.identifier_s});
                     }
                 });
-                 
+				 $scope.syncDocuments();
 
 				$('#'+$scope.question).modal('hide');
 				$scope.areVisible = true;
 			};
-            
-            
+
+
              //==================================
             //
             //==================================
 			$scope.syncDocuments = function(){
-                    
+
                 _.forEach($scope.rawDocuments.docs, function (doc) {
                     doc.__checked = false;
                 });
-                
+
                 if ($scope.model){
-                    _.forEach($scope.model, function (mod) {
-                        _.forEach($scope.rawDocuments.docs, function (doc) {
-                              if(mod.identifier === doc.identifier_s)
-                                    doc.__checked = true;
-                        });
+					var docs = []
+                    _.each($scope.model, function (mod) {
+						docs.push(IStorage.documents.get(mod.identifier));
                     });
-                    
+					$q.all(docs)
+					.then(function(results){
+
+						var documents = _.map(results, function(result){
+											return result.data || {};
+										});
+						console.log(documents);
+					})
                     if($scope.model.length === 0 )
                         $scope.model = undefined;
                 }
-               
+
 
 				$('#'+$scope.question).modal('hide');
 				$scope.areVisible = true;
 			};
-            
+
             //==================================
             //
             //==================================
@@ -98,69 +103,69 @@ function ($http, $rootScope, $filter, _,  $q, searchService, appConfigService) {
                     return item;
                 }
 			};
-            
+
             //==================================
             //
             //==================================
 			$scope.selectDoc = function(document){
-                 
+
                  _.forEach($scope.rawDocuments.docs, function (doc) {
                     doc.__checked = false;
-                    
+
                     if(doc.identifier_s === document.identifier_s ){
                         doc.__checked = true;
                     }
                 });
 			};
-            
+
 
             //==================================
             //
             //==================================
 			$scope.removeDocument = function(document){
-               
+
                  _.forEach($scope.rawDocuments.docs, function (doc) {
                     if(doc.identifier_s === document.identifier_s ){
                         doc.__checked = false;
                     }
                 });
-                
+
                $scope.model =  _.filter($scope.model, function (doc) {
                     if(doc.identifier !== document.identifier_s ){
                      return doc;
                     }
                 });
-                
+
                 if($scope.model){
                     if($scope.model.length===0)
                         $scope.model = undefined;
                 }
-                
+
 			};
-          
+
              //==================================
             //
             //==================================
             function getDocs () {
                 var searchOperation;
-                
+
                 var schema = "*";
                 if ($scope.schema)
                     schema = $scope.schema;
-                
+
                 var q  = "schema_s:"+ schema;
-                
+
                 if($scope.government)
                     q  = q + " AND government_s:" + $scope.government.identifier;
                 if(!$scope.government &&  $scope.userGov)
-                    q  = q + " AND government_s:" + $scope.userGov;  
-                      
+                    q  = q + " AND government_s:" + $scope.userGov;
+
                 var queryParameters = {
                     'query'    : q,
                     'currentPage' : 0,
                     'rowsPerPage': 1000
                 };
-                
+
                 searchOperation = searchService.list(queryParameters, null);
 
                 $q.when(searchOperation)
@@ -172,22 +177,22 @@ function ($http, $rootScope, $filter, _,  $q, searchService, appConfigService) {
                     .finally(function(){
                         $scope.isLoading = false;
                     });
-                    
+
                 console.log(q);
             }
-            
+
            //==================================
             //
             //==================================
             this.load = function () {
-               
+
                 if(!$scope.rawDocuments || _.isEmpty($scope.rawDocuments))
                 {
                     getDocs();
                     return;
                 }
             };
-            
+
             load();
 
 			//==================================
@@ -197,12 +202,12 @@ function ($http, $rootScope, $filter, _,  $q, searchService, appConfigService) {
 		        if(newValue != oldValue){
 
                      $scope.syncDocuments();
-                     
-                            
+
+
 		        }
 		    });
-            
-            
+
+
 
 		},
 
