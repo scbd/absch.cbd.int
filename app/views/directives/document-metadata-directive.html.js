67162d0856a4a4ce921a0d70706c7349ad08828c
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-04-25 16:23:18 -0400

    fix uid issue
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/directives/document-metadata-directive.html.js
+++ app/views/directives/document-metadata-directive.html.js
@@ -11,16 +11,39 @@ define(['app', '/app/js/common.js'
 					if(!document)
 						return;
                     var uid
-					if(!document.id)
-						uid = $filter("uniqueIDWithoutRevision")(document.info);
-					else
-						uid = $filter("uniqueID")(document);
+					// if(!document.id)
+					// 	uid = $filter("uniqueIDWithoutRevision")(document.info);
+					// else
+					uid = $filter("uniqueID")(document);
 
                     if(!uid)
                      return "unique identifier not yet assigned ";
 
                     return uid;
 				}
+
+                $scope.getUniqueIDForUrl = function(document){
+                    if(!document)
+						return;
+                    var uid
+
+                    if(!document.id)
+						uid = $filter("uniqueIDWithoutRevision")(document);
+					else
+					    uid = $filter("uniqueID")(document);
+
+                    if(document.revision || document.info && document.info.revision){
+                        var revision = (document.revision || document.info.revision);
+                        //temporary till revision data is migrated
+                        var tempUId = $filter("uniqueID")(document);
+                        if(tempUId == uid+'-'+revision)
+                            return uid
+
+                        return  uid + '/' + revision;
+                    }
+
+                    return  uid;
+                }
 
 				$scope.loadReportRecord = function(schema, identifier){
 
