2177bccaf38a762a9ba9b373435166f85ec1d9e5
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-05-04 17:13:03 -0400

    display country name in breadcrumbs
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/countries/country-profile.html.js
+++ app/views/countries/country-profile.html.js
@@ -1,13 +1,13 @@
 define(['app','underscore',
   'scbd-map/zoom-map', '/app/js/common.js',
-  'scbd-angularjs-services/locale',
+  'scbd-angularjs-services/locale','ng-breadcrumbs',
   'css!/app/libs/flag-icon-css/css/flag-icon.min.css',
   'css!./country-profile','./country-profile-directive.html.js'
 ], function(app, _) {
 
   app.controller("countryProfileController",
-  ["$scope","$route", "$http", "$timeout", "$location","locale", 'ammap3Service', 'commonjs', '$q',
-    function($scope,$route, $http, $timeout, $location,locale, ammap3Service,commonjs, $q) {
+  ["$scope","$route", "$http", "$timeout", "$location","locale", 'ammap3Service', 'commonjs', '$q', 'breadcrumbs',
+    function($scope,$route, $http, $timeout, $location,locale, ammap3Service,commonjs, $q, breadcrumbs) {
       $scope.code      = $route.current.params.code;
       $scope.$on('$destroy', function(){
           ammap3Service.clear('zoom-map-country');
@@ -19,6 +19,7 @@ define(['app','underscore',
           $scope.country.code = $scope.country.code.toLowerCase();
           $scope.country.name = $scope.country.name[locale];
           $scope.country.cssClass='flag-icon-'+$scope.country.code;
+          breadcrumbs.options = { 'Country Profile': $scope.country.name };
       });
       $q.when(commonjs.getCountries()).then(function(countries){
 
