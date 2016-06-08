01ee763a84c9555f6fea8dd6449db37a4091101c
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-06-08 10:33:03 -0400

    delete workflow
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/directives/task-id-directive.html.js
+++ app/views/directives/task-id-directive.html.js
@@ -1,102 +1,111 @@
-define(['app', 'ngDialog',
-	'../forms/view/record-loader.directive.html.js', 'toastr'], function (app) {
+define(['app',
+	'../forms/view/record-loader.directive.html.js', 'toastr', , 'ngDialog',
+	'/app/views/directives/document-reference-history.html.js'], function (app) {
 
-		app.directive('taskId', function () {
-			return {
-				restrict: 'EAC',
-				templateUrl: '/app/views/directives/task-id-directive.html?',
-				replace: true,
-				scope: {
-					loadTaskData: '=',
-					workflowTaskId: '@',
-					onActivityUpdate: '&', //used in case if the directive parent needs to be refreshed else the workflow details will be fetched.
-					showDetails: "="
-				},
-				controller: ["$scope", "$timeout", "$http", "$route", "IStorage", "IWorkflows", "authentication", "underscore", '$element', 'toastr', '$window', 'ngDialog',
-					function ($scope, $timeout, $http, $route, IStorage, IWorkflows, authentication, _, $element, toastr, $window, ngDialog) {
+app.directive('taskId', function () {
+        return {
+            restrict: 'EAC',
+            templateUrl: '/app/views/directives/task-id-directive.html?',
+            replace: true,
+            scope: {
+                loadTaskData : '=',
+                workflowTaskId : '@',
+                onActivityUpdate : '&', //used in case if the directive parent needs to be refreshed else the workflow details will be fetched.
+                showDetails: "="
+            },
+            controller: [ "$scope", "$timeout", "$http", "$route", "IStorage", "IWorkflows", "authentication", "underscore",'$element', 'toastr','$window', 'ngDialog', 
+					 function ($scope, $timeout, $http, $route, IStorage, IWorkflows, authentication, _, $element, toastr, $window, ngDialog)
+					{
+						var rejectRecordModal = $element.find("#rejectModal");
 						//==================================================
 						//
 						//
 						//==================================================
 						function load() {
 
-							var activityName = $route.current.params.activity;
-							if ($scope.workflowTaskId != undefined && $scope.loadTaskData != undefined) {
-								IWorkflows.get($scope.workflowTaskId).then(function (workflow) {
+								var activityName = $route.current.params.activity;
+								if($scope.workflowTaskId != undefined && $scope.loadTaskData != undefined)
+								{
+									IWorkflows.get($scope.workflowTaskId).then(function(workflow){
 
-									$scope.workflow = workflow;
-									if (workflow.data.identifier && !workflow.closedOn) {
+										$scope.workflow = workflow;
+										if(workflow.data.identifier && !workflow.closedOn) {
 
-										IStorage.drafts.get(workflow.data.identifier).then(function (result) {
-											$scope.document = result.data || result;
-										});
-									}
-									if (!workflow.workflowAge) {
-										workflow.workflowAge = { 'age': 12, 'type': 'weeks' };
-									}
-									var expiryDate = moment(workflow.createdOn)
-										.add(workflow.workflowAge.age, workflow.workflowAge.type);
-									workflow.daysToApproval = expiryDate.diff(moment(), 'days');
+											IStorage.drafts.get(workflow.data.identifier).then(function(result){
+												$scope.document = result.data || result;
+											});
+										}
+                                        if(!workflow.workflowAge){
+                                            workflow.workflowAge = {'age':12,'type':'weeks'};
+                                        }
+                                        var expiryDate = moment(workflow.createdOn)
+                                                                            .add(workflow.workflowAge.age,workflow.workflowAge.type);
+                                        workflow.daysToApproval = expiryDate.diff(moment(), 'days');
 
-								});
-							}
+									});
+								}
 						}
+                        
+
 
 						//==================================================
 						//
 						//
 						//==================================================
-						$scope.updateActivity = function (resultData) {
+						$scope.updateActivity = function(resultData, isDelete) {
 							$element.find('#buttonsDiv').wrap("<div class='overlayDiv'> </div>")
 
 							$element.find('#spinner').css('display', 'block');
 							$scope.isUpdating = true;
-							IWorkflows.updateActivity($scope.workflowTaskId, $scope.workflow.activities[0].name, resultData).then(function (result) {
-								
-								var msg = "";
-								if (result.result.action == 'approve') {
-									msg = "Record published";
-								}
-								else {
-									msg = "Record rejected";
-								}
+							IWorkflows.updateActivity($scope.workflowTaskId, $scope.workflow.activities[0].name, resultData)
+							.then(function(result){
+									var msg = "";
+									if(result.result.action == 'approve'){
+										if(isDelete)
+											msg = "Record deleted";
+										else
+											msg = "Record published";
+									}
+									else{
+										msg = "Record rejected";
+									}
 
-								var workflowInfo = { workflowId: $scope.workflowTaskId, activity: result, status: 'completed' }
+									var workflowInfo = {workflowId:$scope.workflowTaskId, activity:result}
 
-								if (typeof $scope.onActivityUpdate == 'function') {
-									$scope.onActivityUpdate({ document: $scope.document, workflowInfo: workflowInfo });
-									$scope.hideEverything = true;
-								}
-								else {
-									load();
-									$scope.isUpdating = false;
-								}
-								toastr.success(msg);
-								$scope.closeDialog();
-
-							}).catch(function (error) {
-								toastr.error('There was an error processing your request, please try again.');
+									if(typeof $scope.onActivityUpdate == 'function'){
+											$scope.onActivityUpdate({document:$scope.document, workflowInfo:workflowInfo});
+											$scope.hideEverything=true;
+									}
+									else{
+										load();
+										$scope.isUpdating = false;
+									}
+									toastr.success(msg);
+							}).catch(function(error) {
+								//alert(error);
+								 toastr.error('There was an error processing your request, please try again.');
+								$scope.isUpdating = false;
 								$element.find('.overlayDiv').removeClass('overlayDiv');
 								$element.find('#spiner').css('display', 'none');
 								$scope.isUpdating = false;
 							});
 						};
-						$scope.user = function () {
+						$scope.user = function() {
 							return $scope.$root.user.userID;
 						};
 						//==================================================
 						//
 						//
 						//==================================================
-						$scope.isAssignedToMe = function (activity) {
-							return activity && _.contains(activity.assignedTo || [], $scope.$root.user.userID || -1);
+						$scope.isAssignedToMe = function(activity) {
+							return activity && _.contains(activity.assignedTo||[], $scope.$root.user.userID||-1);
 						};
-
+                        
                         //==================================================
 						//
 						//
 						//==================================================
-						$scope.workFlowIsCreatedByMe = function () {
+						$scope.workFlowIsCreatedByMe = function() {
 							return $scope.workflow && ($scope.workflow.createdBy == $scope.$root.user.userID);
 						};
 
@@ -104,7 +113,7 @@ define(['app', 'ngDialog',
 						//
 						//
 						//==================================================
-						$scope.isOpen = function (element) {
+						$scope.isOpen = function(element) {
 							return element && !element.closedOn && !element.timedOut;
 						}
 
@@ -112,7 +121,7 @@ define(['app', 'ngDialog',
 						//
 						//
 						//==================================================
-						$scope.isClose = function (element) {
+						$scope.isClose = function(element) {
 							return element && element.closedOn;
 						}
 
@@ -124,56 +133,48 @@ define(['app', 'ngDialog',
 						};
 
 
-						$scope.$watch('loadTaskData', function (newValue, oldValue) {
-                            if (newValue != undefined && newValue != oldValue);
+						 $scope.$watch('loadTaskData', function (newValue, oldValue) {
+                            if(newValue != undefined && newValue != oldValue);
 							{
 								load();
 							}
-						});
+		                });
 
-						$scope.showRejectDialog = function () {
-							ngDialog.openConfirm({
-								template: 'rejectWorkflowRequestModalTemplate',
-								scope: $scope
+						$scope.showRejectDialog = function(){
+
+							rejectRecordModal.appendTo('body').modal("show");
+						}
+
+			            $scope.$on('$destroy', function(){
+			                $('#rejectModal').remove();
+			            });
+
+
+						$scope.confirmDelete = function(){
+							
+							var identifier = $scope.document.header.identifier
+							var updateActivity = $scope.updateActivity;
+							ngDialog.open({
+								closeByEscape: false, closeByDocument: false,
+								template : 'deleteConfirmation',
+								controller: ['$scope', function($scope) {
+									$scope.showDocumentHistory = identifier;
+
+									$scope.updateActivity = function(){
+										updateActivity({ action : 'approve' }, true);
+										$scope.closeConfirmation();
+									}
+									$scope.closeConfirmation = function(){
+										ngDialog.close();
+									}
+								}]
 							});
 						}
 
-						$scope.askCancelWorkflowRequest = function () {
-							$scope.loading = false;
-							ngDialog.openConfirm({
-								template: 'cancelWorkflowRequestModalTemplate',
-                            	closeByDocument: false,
-								scope: $scope
-							});
-						};
-						$scope.cancelWorkflowRequest = function (record) {
-
-							$scope.loading = true;
-							IWorkflows.cancel(record._id, { 'action': 'cancel' })
-								.then(function (result) {
-									var workflowInfo = { workflowId: record._id, activity: result, status: 'canceled' }
-
-									if (typeof $scope.onActivityUpdate == 'function') {
-										$scope.onActivityUpdate({ document: $scope.document, workflowInfo: workflowInfo });
-										$scope.hideEverything = true;
-									}
-									toastr.info('The request was successfully recalled');
-									$scope.closeDialog();
-								}).catch(function (error) {
-									toastr.error('There was an error processing your request, please try again.');
-									$scope.isUpdating = false;
-								}).finally(function () {
-									delete $scope.loading;
-								});
-						};
-
-						$scope.closeDialog = function () {
-							ngDialog.close();
-						};
 
 					}
 				]
 
-			};
-		});
-	});
+        };
+    });
+});
