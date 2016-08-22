ff29d0c6838df5035b7e15a1db6355055c61ee53
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-08-22 15:41:05 -0400

    add count of request assgined to admin users

--- app/views/register/dashboard.html.js
+++ app/views/register/dashboard.html.js
@@ -1,11 +1,13 @@
 define(['app', 'underscore',
- '/app/services/role-service.js',
- '/app/views/register/directives/register-top-menu.js', 'toastr'],
+ '/app/services/role-service.js', '/app/services/app-config-service.js',
+ '/app/views/register/directives/register-top-menu.js', 'toastr','scbd-angularjs-services'],
 function(app, _) {
     "use strict";
     app.controller("DashboardController", ["$rootScope", "$scope", "IStorage", "roleService", "$compile", "realm", "$q",
-                    "$routeParams", '$location', "$filter", "$http", "$element","$timeout", 'toastr',
-        function($rootScope, $scope, storage, roleService, $compile, realm, $q, $routeParams, $location, $filter, $http, $element, $timeout, toastr) {
+                    "$routeParams", '$location', "$filter", "$http", "$element","$timeout", 'toastr', 'appConfigService',
+                    'IWorkflows',
+        function($rootScope, $scope, storage, roleService, $compile, realm, $q, $routeParams, 
+                $location, $filter, $http, $element, $timeout, toastr, appConfigService, IWorkflows) {
 
             var schemaFacets = {};
 
@@ -68,8 +70,13 @@ function(app, _) {
                 var published = storage.documentQuery.facets("", {collection: "my"});
                 var drafts = storage.documentQuery.facets("", {collection: "mydraft"});
                 var requests = storage.documentQuery.facets("", {collection: "request"});
+                var queries = [published, drafts, requests]
 
-                $q.all([published, drafts, requests]).then(function(results) {
+                var taskQueries = loadmyTaskFacets();
+                if(taskQueries.length>0)
+                    queries.push(taskQueries);
+
+                $q.all(_.flatten(queries)).then(function(results) {
 
                     var index = 0;
                     _.each(results, function(facets) {
@@ -84,6 +91,8 @@ function(app, _) {
                                 schemaFacets[format].draftCount = count;
                             else if (index == 2)
                                 schemaFacets[format].requestCount = count;
+                            else if (index > 2)//workflow count from workflows
+                                schemaFacets[format].requestCount = (schemaFacets[format].requestCount||0) + count;
                         })
                         index++;
                     });
@@ -93,7 +102,40 @@ function(app, _) {
                     console.log(error);
                 })
             }
+             function loadmyTaskFacets(){
+                   var taskQuery = [];
+                   var referenceApproverRoles =
+                            {"resource"                 : ["AbsRequestApprovalNotification", "AbsRequestApprovalNotification-trg", "AbsRequestApprovalNotification-dev"],
+                            "capacityBuildingInitiative": ["AbsPACapacityBuildingInitiative", "AbsPACapacityBuildingInitiative-trg", "AbsPACapacityBuildingInitiative-dev"],
+                            "capacityBuildingResource"  : ["AbsPACapacityBuildingResource", "AbsPACapacityBuildingResource-trg", "AbsPACapacityBuildingResource-dev"],
+                            "modelContractualClause"    :  ["AbsPAModelContractualClause", "AbsPAModelContractualClause-dev", "AbsPAModelContractualClause-trg"],
+                            "communityProtocol"         :  ["AbsPACommunityProtocol", "AbsPACommunityProtocol-trg", "AbsPACommunityProtocol-dev"]};
+                            
+                    _.each(referenceApproverRoles, function(roles, schema){
 
+                        if(roleService.isUserInRoles(roles)){
+                        
+                            var myUserID = $scope.$root.user.userID;
+                            var query    = {
+                                $and : [
+                                    { "activities.assignedTo": myUserID } ,
+                                    { "closedOn"             : { $exists : false } },
+                                    { "data.realm"           : appConfigService.currentRealm },
+                                    { "data.metadata.schema" : schema }
+                                ]
+                            };
+                            var query = IWorkflows.query(query,true)
+                                        .then(function(data){
+                                            var facetData = {data : {}};
+                                            facetData.data[schema] = data.count;
+                                            return facetData;
+                                        });
+                            taskQuery.push(query);
+                        }
+                    })
+                    
+                    return taskQuery;
+                }
             init();
         }
     ]);
