80d111bd01696278c8d1fef967e67a621a3e0244
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-09-09 14:39:54 -0400

    move cookie language code inside lang view

--- app/views/shared/lang.html.js
+++ app/views/shared/lang.html.js
@@ -2,10 +2,14 @@ define(['app',
 		'scbd-angularjs-services/locale'
         ], function (app, moment) {
 
-    return ["$scope", "$location", "locale","$timeout", "$window",
-		function ($scope, $location, locale, $timeout, $window){
+    return ["$scope", "$location", "locale","$timeout", "$window", "$routeParams",
+		function ($scope, $location, locale, $timeout, $window, $routeParams){
+			    var lang = 'en';
+				if($routeParams.langCode)
+					lang = $routeParams.langCode;
 
-				$scope.currentLanguage = locale;
+				document.cookie="Preferences=Locale="+lang;
+                $scope.currentLanguage = lang;
 
 				$timeout(function(){
 					$window.location.href = $location.search().returnUrl;
