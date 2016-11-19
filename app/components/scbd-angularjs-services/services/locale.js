6c2599a17f4812e648c5a970982118dbbb423627
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-11-18 23:32:41 -0500

    remove dependency on cookie for lang

--- app/components/scbd-angularjs-services/services/locale.js
+++ app/components/scbd-angularjs-services/services/locale.js
@@ -2,13 +2,20 @@ define(['app'], function(app) { 'use strict';
 
     app.provider('locale', [function() {
 
-        this.$get = ['$document', function($document) {
+        this.$get = ['$document', '$window', function($document, $window) {
+        
+        var urlLangRegex       = /^\/(en|ar|fr|es|ru|zh)/;
+        var langRegex = /(ar|en|es|fr|ru|zh)/;
+        var location = window.location;
+        var lang = location.pathname.split(urlLangRegex);
+        
+        //var matches = /locale=(\w{2,3})/g.exec($document[0].cookie);
 
-            var matches = /locale=(\w{2,3})/g.exec($document[0].cookie);
-
-            if(matches)
-                return matches[1]; //
-
+            //if(matches)
+               // return matches[1]; //
+        if(lang && langRegex.test(lang[1]))
+            return lang[1];
+            
             return 'en';
 
         }];
