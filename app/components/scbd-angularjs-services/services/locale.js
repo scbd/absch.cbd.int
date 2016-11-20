9040e5d818185e747a4629d5ab37583f327938f9
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-11-20 11:14:37 -0500

    remove cookie dependency

--- app/components/scbd-angularjs-services/services/locale.js
+++ app/components/scbd-angularjs-services/services/locale.js
@@ -9,15 +9,12 @@ define(['app'], function(app) { 'use strict';
         var location = window.location;
         var lang = location.pathname.split(urlLangRegex);
         
-        //var matches = /locale=(\w{2,3})/g.exec($document[0].cookie);
-
-            //if(matches)
-               // return matches[1]; //
         if(lang && langRegex.test(lang[1]))
             return lang[1];
-            
-            return 'en';
+
+            //return 'en';
 
         }];
     }]);
+    
 });
