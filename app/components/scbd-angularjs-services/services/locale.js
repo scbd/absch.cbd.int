59dbd277c1e5e8964c6084e5354f409a1022b814
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-11-29 13:49:06 -0500

    fix map popover navigation

--- app/components/scbd-angularjs-services/services/locale.js
+++ app/components/scbd-angularjs-services/services/locale.js
@@ -17,4 +17,19 @@ define(['app'], function(app) { 'use strict';
         }];
     }]);
     
+    app.service('localeService', ['$document', '$window', function($document, $window) {
+
+        var urlLangRegex       = /^\/(en|ar|fr|es|ru|zh)/;
+        var langRegex = /(ar|en|es|fr|ru|zh)/;
+
+        this.urlHasLocale = function () {
+                var location = window.location;
+                var lang = location.pathname.split(urlLangRegex);
+                
+                if(lang && langRegex.test(lang[1]))
+                    return true;
+                
+                return false;
+        };
+    }]);
 });
