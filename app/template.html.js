62b3c97112424df70c3863c773b17860fdb75f26
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-11-29 20:37:20 -0500

    add directive for help

--- app/template.html.js
+++ app/template.html.js
@@ -4,8 +4,8 @@ define(['app', 'underscore', 'ng-breadcrumbs','angular-animate',
     '/app/views/directives/nav/portal-branding.js',
     'scbd-branding/directives/header/header',
     '/app/views/directives/nav/portal-nav.js',
-    'ngAria', 'angular-animate', 'toastr', 'ionsound', 'moment','angular-angulartics', 'angular-google-analytics'
-    //, '/app/services/app-config-service.js'
+    'ngAria', 'angular-animate', 'toastr', 'ionsound', 'moment','angular-angulartics', 'angular-google-analytics',
+    '/app/views/help/info-bar.html.js'
 ], function(app, _) {
     'use strict';
 
