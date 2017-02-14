66c589fec5967f8e10addc272e510a15eaf46b48
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-02-14 16:09:10 -0500

    handle NFP in view-contact-ref

--- app/views/forms/view/view-contact-reference.directive.js
+++ app/views/forms/view/view-contact-reference.directive.js
@@ -1,4 +1,4 @@
-define(['app','underscore', 'scbd-angularjs-services'], function (app, _) {
+define(['app','underscore', 'scbd-angularjs-services', '/app/js/common.js'], function (app, _) {
 
 	app.directive("viewContactReference", [function () {
 		return {
@@ -11,11 +11,12 @@ define(['app','underscore', 'scbd-angularjs-services'], function (app, _) {
 				locale: "=",
 				target: "@linkTarget"
 			},
-			controller: ["$scope", "IStorage", "$filter", function ($scope, storage, $filter) {
+			controller: ["$scope", "IStorage", "$filter", "commonjs", function ($scope, storage, $filter, commonjs) {
 
 				$scope.isCNA = false;
 				$scope.isPerson = false;
 				$scope.isOrganization= false;
+				$scope.isNFP = false;
 
 				//==================================================
 				function checkCNA(doc) {
@@ -54,21 +55,39 @@ define(['app','underscore', 'scbd-angularjs-services'], function (app, _) {
 							$scope.document = newVal.document;
 						}
 						else{
-							storage.documents.get(newVal.identifier, {info:true})
-								.then(function(data){
-									$scope.documentForUID = angular.copy(data.data);
-									$scope.document = angular.copy(data.data.body);
-									delete data.data.body;
-									$scope.document.info = data.data;
-									if($scope.document && $scope.document.contactOrganization){
-										storage.documents
-												.get($scope.document.contactOrganization.identifier)
-												.then(function(data){
-													_.extend($scope.document.contactOrganization, data.data);
-												});
+							 var focalPointRegex = /^52000000cbd022/;
+							if(focalPointRegex.test(newVal.identifier)){
+								commonjs.getReferenceRecordIndex('NFP', newVal.identifier)
+								.then(function(result){
+									if(result.data && result.data.schema_s == 'focalPoint'){
+										result.data.addressHTML = result.data.description_EN_t.replace(/\n/g, '<br/>');
+										result.data.phones = result.data.telephone_ss;
+										result.data.country = {identifier:result.data.government_s};
+										result.data.emails = result.data.email_ss;
+										result.data.faxes = result.data.fax_ss;
+										result.data.firstName = result.data.title_EN_t;
+										$scope.document = result.data;
 									}
+								})
+							
+							}
+							else{
+								storage.documents.get(newVal.identifier, {info:true})
+									.then(function(data){
+										$scope.documentForUID = angular.copy(data.data);
+										$scope.document = angular.copy(data.data.body);
+										delete data.data.body;
+										$scope.document.info = data.data;
+										if($scope.document && $scope.document.contactOrganization){
+											storage.documents
+													.get($scope.document.contactOrganization.identifier)
+													.then(function(data){
+														_.extend($scope.document.contactOrganization, data.data);
+													});
+										}
 
-								});
+									});
+							}
 						}
 					}
 					else if(newVal.header || newVal.source) {
@@ -80,9 +99,11 @@ define(['app','underscore', 'scbd-angularjs-services'], function (app, _) {
 				//==================================================
 				$scope.$watch("document", function(newVal, oldVal) {
 					if(newVal){
+						$scope.isNFP = $scope.document.schema_s == 'focalPoint';
 						$scope.isCNA = checkCNA(newVal);
-						$scope.isPerson = checkPerson(newVal);
-						$scope.isOrganization = !$scope.isPerson;
+						$scope.isPerson = checkPerson(newVal) && !$scope.isNFP;
+						$scope.isOrganization = !$scope.isPerson && !$scope.isNFP;
+						
 					}
 				});
 
