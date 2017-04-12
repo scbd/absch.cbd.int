f59051d95e70335ec297eef4b5ba8fd2148a7f94
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-04-12 11:06:20 -0400

    url to download guides

---
+++ app/views/about/guides.html.js
@@ -0,0 +1,11 @@
+﻿define(['app','underscore', 'json!/app/app-data/help-guides.json'], function (app, _, guidesData) {
+app.controller("guidesController",
+	["$rootScope", "$scope", "$q", '$element', '$route', function ($rootScope, $scope, $q, $element, $route) {
+
+        if($route.current.params.guideId && guidesData[$route.current.params.guideId]){
+           window.location = guidesData[$route.current.params.guideId];
+        }
+
+
+   }]);
+});
