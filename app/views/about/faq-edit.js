422a5217ad12209dfcad1afcbf24c0024e0a926d
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-06-21 12:01:17 -0400

    RTe toolbox

--- app/views/about/faq-edit.js
+++ app/views/about/faq-edit.js
@@ -20,6 +20,12 @@ function(app, template){
                         closeByDocument: false,
                         controller : ['$http', '$scope', function($http, $scope){
                             $scope.languages = ['en'];
+                            $scope.toolbar = [
+                                ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+                                ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
+                                ['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
+                                ['html', 'insertImage','insertLink', 'insertVideo', 'wordcount', 'charcount']
+                            ];
                             $scope.document = faqForEdit;
                             $scope.save = function(document){
 
