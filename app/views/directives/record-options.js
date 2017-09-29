50e39d00644d78a5ce6fb0d488ffbc072a2ac203
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-09-29 17:59:33 -0400

    enable locale from new directive

--- app/views/directives/record-options.js
+++ app/views/directives/record-options.js
@@ -1,23 +1,36 @@
-define(['app', 'text!views/directives/record-options.html', 'js/common'], function (app, template) {
-app.directive('recordOptions', function () {
+define(['app', 'text!views/directives/record-options.html', 'underscore', 'scbd-angularjs-services/locale'], function (app, template, _) {
+app.directive('recordOptions', ['locale', function (appLocale) {
         return {
             restrict: 'EAC',
             template : template,
-            scope : {
-                  date : '='
-                },
             link: function ($scope, element, attrs) {
                 
-            },
-            controller: [ "$scope", '$rootScope', '$filter', '$timeout', 'commonjs', '$q',
-					 function ($scope, $rootScope, $filter, $timeout, commonjs, $q) 
-					{
-				
-                    
 
-					}
-				]
+
+                if(!$scope.currentLocale)
+                    $scope.currentLocale = appLocale
+
+                if($scope.document){
+                    //if document does not contain application selected locale, then select one 
+                    if(!_.contains($scope.document.header.languages, appLocale)){
+
+                             if(_.contains($scope.document.header.languages, 'en')) $scope.currentLocale = 'en';                
+                        else if(_.contains($scope.document.header.languages, 'fr')) $scope.currentLocale = 'fr';
+                        else if(_.contains($scope.document.header.languages, 'es')) $scope.currentLocale = 'es';
+                        else if(_.contains($scope.document.header.languages, 'ru')) $scope.currentLocale = 'ru';
+                        else if(_.contains($scope.document.header.languages, 'ar')) $scope.currentLocale = 'ar';
+                        else if(_.contains($scope.document.header.languages, 'zh')) $scope.currentLocale = 'zh';
+                    }
+                }
+                if($scope.$parent.internalDocumentInfo)
+                    $scope.updatedOn = $scope.$parent.internalDocumentInfo.updatedOn;
+                    
+                $scope.setCurrentLocale = function(loc){
+                    $scope.currentLocale=loc
+                    $scope.locale=loc
+                }
+            }
         };
-    });
+    }]);
 });
 
