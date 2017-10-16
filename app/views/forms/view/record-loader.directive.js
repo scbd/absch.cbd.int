31c025de22974046c61ae73b7ecaedf315d3b8ca
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-10-16 15:11:34 -0400

    convert nfp id to hex

--- app/views/forms/view/record-loader.directive.js
+++ app/views/forms/view/record-loader.directive.js
@@ -153,6 +153,9 @@ define(['app', 'text!views/forms/view/record-loader.directive.html',
 							loadViewDirective(documentSchema);
 						}
 						else if (documentID) {
+							if(_.contains(['FOCALPOINT', 'NFP'], documentSchema.toUpperCase()))
+								documentID = commonjs.integerToHex(documentID);
+
 							$scope.load(documentID, documentRevision);
 						}
 					};
