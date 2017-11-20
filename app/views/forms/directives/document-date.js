7540bfb55f0b1aada69ce40c4ba578d3e88a7acc
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-11-20 14:15:45 -0500

    abs NR submitted on date

--- app/views/forms/directives/document-date.js
+++ app/views/forms/directives/document-date.js
@@ -12,7 +12,7 @@
                         $scope.updatedOn = ($scope.document||{}).createdDate_dt || $scope.$parent.internalDocumentInfo.updatedOn;
                         if($scope.$parent.internalDocumentInfo.type=='absNationalReport'){
                             var createdOn = $scope.$parent.internalDocumentInfo.createdOn;
-                            if(createdOn!=$scope.updatedOn && moment.utc(createdOn).isSame(moment.utc($scope.updatedOn), 'day')){
+                            if(createdOn!=$scope.updatedOn && !moment.utc(createdOn).isSame(moment.utc($scope.updatedOn), 'day')){
                                 $scope.createdOn = createdOn;
                             }
                         }
