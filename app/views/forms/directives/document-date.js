30102ef7513f4d017c6a0e4413b5f0e24b17ed07
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-11-20 11:58:41 -0500

    show submission date for NR

--- app/views/forms/directives/document-date.js
+++ app/views/forms/directives/document-date.js
@@ -1,4 +1,4 @@
-﻿define(['app', 'text!views/forms/directives/document-date.html'], function (app, template) {
+﻿define(['app', 'text!views/forms/directives/document-date.html', 'moment'], function (app, template, moment) {
     app.directive('documentDate', function () {
         return {
             restrict: 'EAC',
@@ -7,8 +7,16 @@
             link: function ($scope) {
                 
                  if(($scope.document||{}).createdDate_dt ||
-                    ($scope.$parent.internalDocumentInfo && $scope.$parent.internalDocumentInfo.documentID !== undefined  &&  $scope.$parent.internalDocumentInfo))
-                    $scope.updatedOn = ($scope.document||{}).createdDate_dt || $scope.$parent.internalDocumentInfo.updatedOn;
+                    ($scope.$parent.internalDocumentInfo && $scope.$parent.internalDocumentInfo.documentID !== undefined  &&  $scope.$parent.internalDocumentInfo)){
+                    
+                        $scope.updatedOn = ($scope.document||{}).createdDate_dt || $scope.$parent.internalDocumentInfo.updatedOn;
+                        if($scope.$parent.internalDocumentInfo.type=='absNationalReport'){
+                            var createdOn = $scope.$parent.internalDocumentInfo.createdOn;
+                            if(createdOn!=$scope.updatedOn && moment.utc(createdOn).isSame(moment.utc($scope.updatedOn), 'day')){
+                                $scope.createdOn = createdOn;
+                            }
+                        }
+                }
             }
         }
     })
