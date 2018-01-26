69bbc63b6d45173a7736cdf4591cd95e3fe4f79e
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-01-26 14:27:56 -0500

    remove duplicate method

--- app/views/report-analyzer/reports.js
+++ app/views/report-analyzer/reports.js
@@ -78,21 +78,6 @@
     
               return $http.get(collectionUrls[reportType], {  params: { q : query, f : fields }, cache : true });
             }
-    
-            $scope.analyze = function () {
-                
-                var data = {
-                    type: $scope.selectedReportType,
-                    regions: $scope.selectedRegions,
-                    questions: $scope.selectedQuestions,
-                    regionsPreset: $scope.selectedRegionsPreset,
-                    regionsPresetFilter: $scope.selectedRegionsPresetFilter
-                };
-                sessionStorage.setItem('nrAnalyzerData', JSON.stringify(data));
-    
-                $location.url(_.trimRight($location.path(), '/') + '/analyzer');
-            };
-            
         }
     ];
 });
