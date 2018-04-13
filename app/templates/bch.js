f42d7a30b300792b02f2e7aec7b232c9b0f72499
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-04-13 12:22:00 -0400

    add dependency lib

--- app/templates/bch.js
+++ app/templates/bch.js
@@ -1,5 +1,5 @@
 define(['app', 'angular', 'text!./bch-footer.html', './bch-header',
-        'bootstrap', 'routes/bch', 'ng-breadcrumbs',
+        'bootstrap', 'routes/bch', 'ng-breadcrumbs','toastr',
         'components/scbd-branding/directives/header/header',
         'components/scbd-branding/directives/footer'], function (app, angular, footerHtml) { 'use strict';
 
