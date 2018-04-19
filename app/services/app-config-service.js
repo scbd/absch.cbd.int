bdf2f4255fd89605286a7dc2bdb3acfb801de58f
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-04-19 12:26:44 -0400

    conver function to property

--- app/services/app-config-service.js
+++ app/services/app-config-service.js
@@ -22,7 +22,13 @@ define(['app', 'json!/api/v2018/realm-configurations/'+(window.clearingHouseHost
                 throw new Error("Unknow realm for host: "+window.location.host);
 
             return { 
-                value : realmConfig.realm,
+                value               :   realmConfig.realm,
+                uIdPrefix           :   realmConfig.uIdPrefix,
+                schemas             :   realmConfig.schemas,
+                
+                nationalSchemas     :   nationalSchemas,
+                referenceSchemas    :   referenceSchemas,
+                scbdSchemas			:   scbdSchemas,
 
                 is : function(realm, strict) {
                     
@@ -44,11 +50,7 @@ define(['app', 'json!/api/v2018/realm-configurations/'+(window.clearingHouseHost
                 
                 nationalRoles : function() {
                     return _(realmConfig.roles).values().flatten().compact().uniq().without('User', 'user').value();
-                },
-
-                schemas : function() {
-                    return realmConfig.schemas;
-                }                
+                }
             };
         }];
     });    
