a498c6813be10cba99f723a1f3977a5cae5df124
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-04-19 12:27:15 -0400

    navigate to search from top menu bar search box

--- app/templates/bch-header.js
+++ app/templates/bch-header.js
@@ -1,6 +1,6 @@
 define(['app', 'text!./bch-header.html'], function (app, html) { 'use strict';
 
-    app.directive('bchHeader', ['$http', function ($http) {
+    app.directive('bchHeader', ['$http', '$location', function ($http, $location) {
         return {
             restrict: 'E',
             template: html,
@@ -8,6 +8,14 @@ define(['app', 'text!./bch-header.html'], function (app, html) { 'use strict';
                 $http.get('/api/v2015/countries', {params: { f: { code:1, name:1 } }, cache:1 }).then(function(res){
                     $scope.countries = res.data;
                 });
+
+                $scope.isEnterPressed = function($event){
+                    if($event === true || $event.which === 13){
+
+                        $location.url('/search?q='+$scope.searchQuery)
+                        $scope.searchQuery='';
+                    }
+                }
             }
         };
     }]);
