f75bb900a27bf23b9d9dc44c72035c91ca1520e9
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-04-25 15:24:46 -0400

    add bch protocol status

--- app/views/directives/party-status.js
+++ app/views/directives/party-status.js
@@ -2,8 +2,7 @@ define(['app', 'text!views/directives/party-status.html', 'js/common'], function
 app.directive('ngPartyStatus', function () {
         return {
             restrict: 'EAC',
-            template : template,
-           
+            template : template,           
             scope : {
                     code : '=',
                     government : '='
@@ -11,43 +10,41 @@ app.directive('ngPartyStatus', function () {
             link: function ($scope, element, attrs) {
                 
             },
-            controller: [ "$scope", '$rootScope', '$filter', '$timeout', 'commonjs', '$q',
-					 function ($scope, $rootScope, $filter, $timeout, commonjs, $q) 
-					{
-					var iso_code;
+            controller: [ "$scope", '$rootScope', '$filter', '$timeout', 'commonjs', '$q', 'realm',
+            function ($scope, $rootScope, $filter, $timeout, commonjs, $q, realm) {
+                var iso_code;
+                
+                $scope.isAbs = realm.is('ABS');
+                $scope.isBch = realm.is('BCH');
+                // if($scope.code)
+                //     getStatus($scope.code);
                     
-                    // if($scope.code)
-                    //     getStatus($scope.code);
-                        
-                    // if($scope.government)
-                    //     getStatus($scope.government.identifier);
-                     
-                    function getStatus(code){ 
-                        if(!code)return;
-                        code = code.toUpperCase();
-                        $q.when(commonjs.getCountry(code))
-                            .then(function(country){
-                                $scope.partyStatus = country;
-                            });
-                    }
-                   
-                     $scope.$watch('code', function(newValue, oldValue){
-                            //if(newValue && newValue != oldValue){
-                                getStatus($scope.code); 
-                            //}
-                        });
-                        
-                        $scope.$watch('government', function(newValue, oldValue){
-                            //if(newValue && newValue != oldValue){
-                             if($scope.government)
-                                getStatus($scope.government.identifier); 
-                            //}
-                        });
-            
+                // if($scope.government)
+                //     getStatus($scope.government.identifier);
                     
+                function getStatus(code){ 
+                    if(!code)return;
+                    code = code.toUpperCase();
+                    $q.when(commonjs.getCountry(code))
+                        .then(function(country){
+                            $scope.partyStatus = country;
+                        });
+                }
+                
+                $scope.$watch('code', function(newValue, oldValue){
+                    //if(newValue && newValue != oldValue){
+                        getStatus($scope.code); 
+                    //}
+                });
+                
+                $scope.$watch('government', function(newValue, oldValue){
+                    //if(newValue && newValue != oldValue){
+                        if($scope.government)
+                        getStatus($scope.government.identifier); 
+                    //}
+                });
 
-					}
-				]
+            }]
         };
     });
 });
