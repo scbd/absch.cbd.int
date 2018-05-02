a16ae0ac41c7cd986716d69364f494cc5d194cf8
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-05-02 10:44:15 -0400

    dnaSequence view form

--- app/views/forms/view/bch/view-lmo.directive.js
+++ app/views/forms/view/bch/view-lmo.directive.js
@@ -1,6 +1,9 @@
-define(['app', "text!views/forms/view/bch/view-lmo.directive.html", 	'views/directives/record-options'], function (app, template) {
+define(['app', "text!views/forms/view/bch/view-lmo.directive.html", 
+'views/directives/record-options', './view-lmo-reference.directive', './view-lmo-gene.directive',
+'views/forms/view/view-reference-records.directive'
+], function (app, template) {
 
-app.directive("viewLmo", [function () {
+app.directive("viewModifiedOrganism", [function () {
 	return {
 		restrict   : "EAC",
 		template: template ,
