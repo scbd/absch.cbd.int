96c1c2da6d7f1fa0f729923d83e425dd9bb8537f
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-05-11 16:20:57 -0400

    move abs edit form

--- app/views/forms/edit/abs/edit-absCheckpointCommunique.js
+++ app/views/forms/edit/abs/edit-absCheckpointCommunique.js
@@ -1,5 +1,5 @@
 define(['app', 'views/forms/edit/edit',
-    '../view/abs/view-abs-checkpoint-communique.directive',
+    'views/forms/view/abs/view-abs-checkpoint-communique.directive',
     'views/forms/edit/permit-selection-directive',
     'js/common'
 ], function(app) {
