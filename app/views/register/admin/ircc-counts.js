f7633c3608b8a02c751266f8028ff391d77efc34
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-07-20 10:19:11 -0400

    use lodash

--- app/views/register/admin/ircc-counts.js
+++ app/views/register/admin/ircc-counts.js
@@ -1,4 +1,4 @@
-define(['app', 'underscore', 'js/common', 'moment', 'scbd-angularjs-controls', 'scbd-angularjs-services', 
+define(['app', 'lodash', 'js/common', 'moment', 'scbd-angularjs-controls', 'scbd-angularjs-services', 
     'views/register/directives/register-top-menu','chart-js',
     'services/search-service','services/app-config-service',
 ], function (app) {
