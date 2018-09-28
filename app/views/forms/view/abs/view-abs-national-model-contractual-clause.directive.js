d9ce2a7f1610f101b0816f0a6e099497e577be4b
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-09-28 10:20:25 -0400

    new NMCC format

---
+++ app/views/forms/view/abs/view-abs-national-model-contractual-clause.directive.js
@@ -0,0 +1,41 @@
+﻿define(["app", "text!views/forms/view/abs/view-abs-national-model-contractual-clause.directive.html", 
+'views/directives/record-options', 'views/forms/view/view-reference-records.directive',
+'views/forms/view/view-contact-reference.directive'], function(app, template){
+
+       app.directive("viewAbsNationalModelContractualClause", [function () {
+               
+           return {
+               restrict   : "EAC",
+               template: template ,
+               replace    : true,
+               transclude : false,
+               scope: {
+                   document: "=ngModel",
+                   locale  : "=",
+                   target  : "@linkTarget",
+                   allowDrafts : "@",
+                   hide:"@"
+               },
+               link : function ($scope)
+               {
+                   $scope.contacts = undefined;
+               },
+               controller : ["$scope", 'realm', "IStorage", function ($scope, realm, storage)
+               {
+                   $scope.schema = realm.schemas.absProcedure
+                   //====================
+                   //
+                   //====================
+                   $scope.display = function(field) {
+                       
+                       if(!$scope.hide) return true; //show all fields
+
+                       return( $scope.hide.indexOf(field) >= 0 ? false : true);
+                   }
+
+               }]
+           };
+       }]);
+       
+}
+);
