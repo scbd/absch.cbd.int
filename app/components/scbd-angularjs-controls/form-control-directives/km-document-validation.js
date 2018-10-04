0d77d63280119b146a0df9687c8b64f33d7f9346
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-10-04 17:00:53 -0400

    make control more dynamic

--- app/components/scbd-angularjs-controls/form-control-directives/km-document-validation.js
+++ app/components/scbd-angularjs-controls/form-control-directives/km-document-validation.js
@@ -25,14 +25,14 @@ define(['app', 'text!./km-document-validation.html','jquery'], function(app, tem
                     if (qLabel.length === 0) //handle for abs as abs has the validation directive on edit instead of view form;
                         qLabel = $element.parent().find("form[name='editForm'] label[for='" + field + "']:first");
 
-                    var sTab = 'edit';//qLabel.parents().find("[km-tab]").attr("km-tab");
+                    var sTab = qLabel.parents().find('[km-tab="edit"]').attr("km-tab")
 
                     // inner tabs
-                    var sTabName   = qLabel.parents().find(".tab-pane").attr("id");
+                    var sTabName   = qLabel.parents().filter(".tab-pane").attr("id");
                     var sPagerName = $('a[href$="'+ sTabName + '"]').parents().find("ul.pagination").attr("id");
 
                     if (sTab) {
-                         var qBody = $("html, body") //$element.parents().find("body,html");
+                         var qBody =$element.parents().filter("body,html");
 
                         $scope.$parent.tab = sTab;
 
