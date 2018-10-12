3ba0c078bb5c92eae19242f9982dca86b9335a06
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-10-12 09:56:02 -0400

    just use api for link validation

--- app/components/scbd-angularjs-services/filters/scbd-filters.js
+++ app/components/scbd-angularjs-services/filters/scbd-filters.js
@@ -15,7 +15,7 @@ function (app, _, moment, schemaName, schemaShortName) {
             var langRegex 			= new RegExp('^\/(ar|en|es|fr|ru|zh)');
             var externalUrlRegex 	= new RegExp('^(http|https|mailto)');
             var startWithRegex	 	= new RegExp('^\/');
-            var attachmentRegx    = new RegExp('api\/v2013\/documents\/(.*)\/attachments\/')
+            var attachmentRegx    = new RegExp('^\/api\/','i')
             if(lastPath != $attrs.href && !attachmentRegx.test($attrs.href) && !langRegex.test($attrs.href) && startWithRegex.test($attrs.href)){
               lastPath = $browser.baseHref() + $attrs.href.replace(/^\//, ''); 
               $attrs.$set('href', lastPath);
