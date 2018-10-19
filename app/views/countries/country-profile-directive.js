9365c64e635bab346949ae0f1a42359601ec9274
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-10-19 16:40:22 -0400

    load remaning records buttons

--- app/views/countries/country-profile-directive.js
+++ app/views/countries/country-profile-directive.js
@@ -21,8 +21,8 @@
                 $scope.api = {
                     loadCountryDetails : loadCountryRecords
                 }
-                
-                $scope.sortMeasure="[jurisdiction_sort, type_sort, status_sort, createdDate_dt, title_t]";
+                $scope.loadRecords  = loadRecords;
+                $scope.sortMeasure  = "[jurisdiction_sort, type_sort, status_sort, createdDate_dt, title_t]";
 
                 var countryRecords  = {};
                 var nationalSchemas = []
@@ -33,6 +33,24 @@
                         nationalSchemas.push(key);
                     }
                 }).value();
+
+
+                $scope.$watch('code', function(newVal){
+                    if(newVal){
+                        loadCountryRecords(newVal.toLowerCase());
+                    }
+                })
+                $scope.showHideRecords = function(key, schema){
+                    schema.display = !schema.display;
+                    if(key == 'measure'){ //for measure load all remaining records to build the measure matrix
+                        loadRecords(key, schema)
+                    }
+
+                }
+                $scope.getExportQuery = function(){
+                    return $scope.exportQuery;
+                
+                }
 
                 function loadCountryRecords(code){
 
@@ -52,46 +70,64 @@
 
                         _.each(countryResult.groups, function(group){
 
-                            var gpDetails = group.groupValue.split('_');
+                            var gpDetails = (group.groupValue||'').split('_');
+                            if(!gpDetails.length)
+                                return;
                             var schema      = gpDetails[1];
                             if(schema=='measure'){
-                                _.each(group.doclist.docs, function(document){
-                                    if(!document.retired_dt || moment.utc() <= moment.utc(document.retired_dt)){
-                                        document.measureMatrix = true;
-                                    }
-                                    else
-                                        document.measureMatrix = false;
-                                })
-                            }
-                            if($routeParams.code && $routeParams.schema && $routeParams.schema == countryRecords[schema].shortCode){                            
-                                countryRecords[schema].display = true;
-
-                                $timeout(function(){                    
-                                    var div = $element.find('#div'+$routeParams.schema.toUpperCase());
-                                    if(div.length>0){
-                                        $('html, body').animate({scrollTop: div.offset().top}, 800);
-                                    }
-                                }, 500)
+                                formatting(group.doclist.docs);
                             }
                             countryRecords[schema]     = _.extend(countryRecords[schema], group.doclist);
 
                         });
+
+                        if($routeParams.code && $routeParams.schema && $routeParams.schema == countryRecords[schema].shortCode){                            
+                            countryRecords[schema].display = true;
+
+                            $timeout(function(){                    
+                                var div = $element.find('#div'+$routeParams.schema.toUpperCase());
+                                if(div.length>0){
+                                    $('html, body').animate({scrollTop: div.offset().top}, 800);
+                                }
+                            }, 500)
+                        }
                         $scope.countryRecords = countryRecords;
                     });
                 }
 
-                $scope.$watch('code', function(newVal){
-                    if(newVal){
-                        loadCountryRecords(newVal);
-                    }
-                })
+                function loadRecords(key, schema, number){
 
-                $scope.getExportQuery = function(){
-                    return $scope.exportQuery;
-                
+                    schema.isLoading = true;
+                    var query = {
+                        fields  : 'id, rec_date:updatedDate_dt, identifier_s, uniqueIdentifier_s, url_ss, government_s, schema_s, government_EN_t, schemaSort_i, sort1_i, sort2_i, sort3_i, sort4_i, _revision_i,rec_countryName:government_EN_t, rec_title:title_EN_t, rec_summary:description_t,rec_type:type_EN_t, rec_meta1:meta1_EN_txt, rec_meta2:meta2_EN_txt, rec_meta3:meta3_EN_txt,rec_meta4:meta4_EN_txt,rec_meta5:meta5_EN_txt, entryIntoForce_dt,adoption_dt,retired_dt,limitedApplication_dt',
+                        query   : 'schema_s:(' + key +') AND government_s:' + $scope.code.toLowerCase(),
+                        rowsPerPage    : number||5000,
+                        start          : number ? undefined : (schema.start==0 ? 10 : schema.start),
+                        currentPage    : schema.start==0 ? 1 : Math.ceil((schema.start+number)/10)
+                    }
+                    return searchService.list(query)
+                    .then(function(result){
+                        schema.start = query.currentPage*10
+
+                        if(key=='measure'){
+                            formatting(result.data.response.docs);
+                        }
+                        schema.docs = schema.docs.concat(result.data.response.docs)
+                    })
+                    .finally(function(){
+                        schema.isLoading = false;
+                    })
+
                 }
 
-                loadCountryRecords($scope.code.toLowerCase());
+                function formatting(docs){
+                    _.each(docs, function(document){
+                        if(!document.retired_dt || moment.utc() <= moment.utc(document.retired_dt)){
+                            document.measureMatrix = true;
+                        }
+                    });
+                }
+                // loadCountryRecords($scope.code.toLowerCase());
 
             }]
 
