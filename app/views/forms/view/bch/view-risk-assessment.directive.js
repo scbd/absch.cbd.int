10360872aac2555c0b11299bd844366efa09db73
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-10-31 15:54:55 -0400

    use ngChange instead of ngClick

--- app/views/forms/view/bch/view-risk-assessment.directive.js
+++ app/views/forms/view/bch/view-risk-assessment.directive.js
@@ -1,10 +1,5 @@
 define(['app', "text!views/forms/view/bch/view-risk-assessment.directive.html", 	'views/directives/record-options'], function (app, template) {
-<<<<<<< HEAD
 app.directive("viewRiskAssessment", [function () {
-=======
-
-app.directive("viewRiskAssignment", [function () {
->>>>>>> add language field to edit form
 	return {
 		restrict   : "EAC",
 		template: template ,
