e62e7677588756720417c56aaa8c2ddde967087c
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-01 11:14:22 -0400

    empty structure of new BCH forms

---
+++ app/views/forms/edit/bch/edit-organism.js
@@ -0,0 +1,36 @@
+define(['app', 'lodash', 'views/forms/edit/edit', 'services/thesaurus-service',
+	'views/forms/edit/document-selector', "views/forms/view/bch/view-organism.directive"], 
+function (app, _) {
+
+	app.controller("editOrganism", ["$scope", "$routeParams", "$route", "Thesaurus", "$q", "$controller", "thesaurusService",
+	function($scope, $routeParams, $route, Thesaurus, $q, $controller, thesaurusService) {
+		
+		
+		$controller('editController', {
+			$scope: $scope
+		});
+
+		_.extend($scope.options, {		
+		});
+		
+		//==================================
+		//
+		//==================================
+		$scope.getCleanDocument = function(document) {
+
+			document = document || $scope.document;
+
+			if (!document)
+				return undefined;
+
+			if (/^\s*$/g.test(document.notes))
+				document.notes = undefined;
+
+			return document;
+		};
+		
+		$scope.setDocument({});
+
+   }]);
+
+});
