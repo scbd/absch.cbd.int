9061ba323755e946f82832e6c6d83c0a7befc156
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-01 11:22:15 -0400

    add prerender middleware

--- server.js
+++ server.js
@@ -13,21 +13,21 @@ var proxy   = require('http-proxy').createProxyServer({});
 var app     = express();
 var url     = require('url');
 var cookieParser = require('cookie-parser');
-// const rendertron = require('rendertron-middleware');
+
 // Initialize constants
 
 var appVersion = process.env.TAG;
+var oneDay   = 86400000;
+
 if(!appVersion || appVersion.trim()==''){
     appVersion =  ((process.env.BRANCH||'') + '-'+ (process.env.VERSION ||''))||process.env.COMMIT;
-    // if(appVersion == "-")
 }  
-var oneDay   = 86400000;
+
 app.set('view engine', 'ejs');
+
 app.use(cookieParser())
 
-// app.use(rendertron.makeMiddleware({
-//     proxyUrl: process.env.RENDERTRON_URL || 'http://rendertron:8080/render'
-// }));
+app.use(require('prerender-node')); // set env PRERENDER_SERVICE_URL
 
 // Set routes
 app.use('/?:lang(ar|en|es|fr|ru|zh)?/app/views/countries/worldEUHigh.js', express.static(__dirname + '/app/views/countries/worldEUHigh.js', { setHeaders: setCustomCacheControl , maxAge: 86400000*365 }) );
