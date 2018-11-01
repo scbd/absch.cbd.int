b4f0e546df946dfeb8c1152557d90aaf23267a06
matthew dias <matthew.dias@cbd.int>
2018-11-01 13:02:44 -0400

    add ddl of countries to main navigation

--- app/views/directives/nav/portal-nav.js
+++ app/views/directives/nav/portal-nav.js
@@ -1,5 +1,5 @@
 define(['app', 'text!views/directives/nav/portal-nav.html', 'underscore','ng-breadcrumbs','jquery', 'services/help-service'],
- function (app,template, _, showHelp, $) {
+ function (app, template, _, showHelp, $) {
 app.directive('portalNav', function () {
     return {
     restrict: 'EAC',
@@ -10,10 +10,14 @@ app.directive('portalNav', function () {
     //         uid: '@',
     // },
     link: ['$scope', '$q', '$element', function ($scope, $q, $element) {
-
+        
     }]
     , controller: ['$scope','$rootScope', '$q','$element','$http', '$filter','breadcrumbs', 'helpService',
             function ($scope, $rootScope, $q, $element, $http, $filter, breadcrumbs, helpService) {
+
+        $http.get('/api/v2015/countries', {params: { f: { code:1, name:1 } }, cache:1 }).then(function(res){
+            $scope.countries = res.data;
+        });
 
       $scope.breadcrumbs     = breadcrumbs;
       $scope.$root.pageTitle = { text: "" };
