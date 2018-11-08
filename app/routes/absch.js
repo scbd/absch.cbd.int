d1d309fa9088eac0352138097e9332a0e860b01b
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-08 23:12:22 +0200

    ircc-counts route

--- app/routes/absch.js
+++ app/routes/absch.js
@@ -73,7 +73,9 @@ define(['app', './common-routes'], function (app, commonRoutes) { 'use strict';
                whenAsync('/about/',           { templateUrl: 'views/about/about.html',   label:'About the ABSCH', resolveController:true, resolveUser : false}).
                whenAsync('/about/:id',        { templateUrl: 'views/about/about.html',   label:'aboutCode',       param:'true',  resolveController:true, resolveUser : false}).
                whenAsync('/about/:id/:guide', { templateUrl: 'views/about/about.html',   param:'true',   resolveController:true, resolveUser : false}).
-
+               
+               whenAsync('/register/admin/ircc-counts',                { templateUrl: 'views/register/admin/ircc-counts.html',      label:'IRCC Counts',  param:'true', resolveController: true,resolve : { securized : securize(['Administrator']) }}).
+    
 
                otherwise({templateUrl: commonRoutes.baseUrl+'views/shared/404.html', label:'404 Error'});
 
