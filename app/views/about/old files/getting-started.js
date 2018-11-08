56ad1a0d48d15fe657dea2b9f331354c9b990046
matthew dias <matthew.dias@cbd.int>
2018-11-08 09:34:56 -0500

    delete old files

--- app/views/about/old files/getting-started.js
+++
@@ -1,5 +0,0 @@
-﻿define(['app', './left-menu',], function (app) {
-app.controller("gettingStartedController",
-	["$rootScope", "$scope", "$q", '$element', '$route', function ($rootScope, $scope, $q, $element, $route) {
-   }]);
-});
