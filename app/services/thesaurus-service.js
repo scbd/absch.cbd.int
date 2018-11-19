bf5c63f68a3f13c1b03ea7df27eb67c2720a9cb0
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-19 11:51:37 +0200

    bch-law

--- app/services/thesaurus-service.js
+++ app/services/thesaurus-service.js
@@ -13,6 +13,13 @@ define(['app', 'underscore'], function (app, _) {
                 return fn_getDomainTerms(termIdentifier, includeOthers);
             };
 
+            this.getTerms = function(term){
+                return $http.get('/api/v2013/thesaurus/terms/' + terms[term] , {cache:true})
+                .then(function(termData){
+                    return termData.data;
+                });;
+            };
+
             function fn_getDomainTerms(termIdentifier, includeOthers){
                 if(!termIdentifier)
                     throw "Domain term is missing";
@@ -41,6 +48,9 @@ define(['app', 'underscore'], function (app, _) {
                                 return data.data;
                             });
             }
+            var terms = {
+                others                   : "5B6177DD-5E5E-434E-8CB7-D63D67D5EBED",
+            }
             var domainTerms = {
                 countries                : "countries",
                 regions                  : "regions",
@@ -65,7 +75,10 @@ define(['app', 'underscore'], function (app, _) {
                 decisionResults          :  'DecisionResults',
                 riskAssessmentScope      :  'RiskAssessmentScope',
                 dnaSequenceFamily        :  '82DAAF04-6698-4CA6-81D5-F200AE64C63A',
-                dnaSequenceTraits        :  'B8EB2261-7695-4DC5-9C4E-026D380DA7CB'
+                dnaSequenceTraits        :  'B8EB2261-7695-4DC5-9C4E-026D380DA7CB',
+                legislationAgreementTypes:  'Legislation And Agreement Types',
+                subjectAreas             :  'Subject Areas',
+                typeOfOrganisms          :  'TypeOfOrganisms'
             }
         }
     }]);
