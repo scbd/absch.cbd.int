98d0c5fe1991f5dfec16affd53371c5f2ce664f8
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-24 09:24:18 +0200

    add socketio notifications

--- app/templates/bch.js
+++ app/templates/bch.js
@@ -1,19 +1,15 @@
 define(['app', 'angular', 'text!./bch-footer.html', './bch-header',
         'bootstrap', 'routes/bch', 'ng-breadcrumbs','toastr','angular-animate', 
         'components/scbd-branding/directives/header/header',
-        'components/scbd-branding/directives/footer'], function (app, angular, footerHtml) { 'use strict';
+        'components/scbd-branding/directives/footer',
+        'components/scbd-angularjs-services/services/locale'], function (app, angular, footerHtml) { 'use strict';
 
     app.directive('bchFooter', [function () { return { restrict: 'E', template: footerHtml }; }]);
 
-    app.controller('BchTemplateController', ['$rootScope', '$location', '$window',
-        function ($rootScope, $location, $window) {
+    app.controller('BchTemplateController', ['$rootScope', '$location', '$window', '$scope', 'locale',
+        function ($rootScope, $location, $window, $scope, locale) {
 
             $rootScope.pageTitle = { text: "" };
-
-            $rootScope.$on('$routeChangeSuccess', function () {
-                $window.ga('set', 'page', basePath() + $location.path());
-                $window.ga('send', 'pageview');
-            });
 
             $rootScope.$on('signOut', function () {
                 $window.location.reload();
@@ -40,7 +36,71 @@ define(['app', 'angular', 'text!./bch-footer.html', './bch-header',
             updateSize();
             
             angular.element($window).on('resize', updateSize);
+
+
+            //html body attributes lang
+            $scope.lang = locale;
+            //set default moment lang
+            var lang = locale;
+            if (lang == 'zh')
+                lang = 'zh-cn'; //moment has two ZH, use ZH-CN
+            moment.lang(lang);
+            if (lang != 'en')
+                require(['css!/app/css/translation.css']);
+            if (lang == 'ar') {
+                require(['css!https://cdn.cbd.int/bootstrap-rtl@3.3.4/dist/css/bootstrap-rtl.min.css']);
+            }
+
+            var basePath = (angular.element('base').attr('href') || '').replace(/\/+$/g, '');
+            $rootScope.$on('$routeChangeSuccess', function () {
+                $window.ga('set', 'page', basePath + $location.path());
+                $window.ga('send', 'pageview');
+            });
+
+            $scope.$on('signOut', function (evt, data) {
+                $window.location.reload();
+            });
+            $rootScope.$on('event:server-pushNotification', function (evt, pushNotification) {
+                if (pushNotification.type == 'documentNotification') {
+                    // toastr.info(data.message);
+                    if (pushNotification.data && pushNotification.data.realm == appConfigService.currentRealm) {
+                        localStorageService.remove('governmentFacets');
+                        localStorageService.remove('searchFilters');
+                    }
+                }
+            });
             
+            //============================================================
+            //
+            //
+            //============================================================
+            $rootScope.$watch('user', _.debounce(function (user) {
+
+                if (!user)
+                    return;
+
+                require(["https://www.cbd.int/app/js/slaask.js"], function (_slaask) {
+
+                    if (user.isAuthenticated) {
+                        _slaask.identify(user.name, {
+                            'user-id': user.userID,
+                            'name': user.name,
+                            'email': user.email,
+                        });
+
+                        if (_slaask.initialized) {
+                            if (_slaask.slaaskSendUserInfos)
+                                _slaask.slaaskSendUserInfos();
+                        }
+                    }
+
+                    if (!_slaask.initialized) {
+                        _slaask.init('8b989bd6ee0cf49384761d4f86ddd945');
+                        _slaask.initialized = true;
+                    }
+                });
+
+            }, 1000));
         }
     ]);
     
