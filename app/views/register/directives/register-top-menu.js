8e6e53e2fb3de94bb5be8c6ff7e2f6bf9d9b447b
matthew dias <matthew.dias@cbd.int>
2019-02-12 11:33:33 -0500

    update

--- app/views/register/directives/register-top-menu.js
+++ app/views/register/directives/register-top-menu.js
@@ -1,7 +1,7 @@
 define(['app', "text!views/register/directives/register-top-menu.html",
-'underscore', 'services/role-service'], function(app, template, _) {
+'underscore', 'services/role-service', 'moment','js/common'], function(app, template, _) {
 
-    app.directive("registerTopMenu", ['roleService', '$rootScope', '$location', 'realm', function(roleService, $rootScope, $location, realm) {
+    app.directive("registerTopMenu", ['roleService',"IWorkflows", '$rootScope', '$location', 'realm', function(roleService, IWorkflows,  $rootScope, $location, realm) {
 
         return {
             restrict: "EA",
@@ -18,10 +18,26 @@ define(['app', "text!views/register/directives/register-top-menu.html",
                 $scope.user = $rootScope.user;
                 $scope.isBCH = realm.is('BCH');
                 $scope.isABS = realm.is('ABS');
-
                 $scope.path = $location.absUrl();
+                $scope.isPA = false;
+                    
+            //    if(roleService.isAbsPublishingAuthority() && !$scope.pendingCount ){
+            //         $scope.isPA = true;
+            //         var expired = moment.utc(new Date()).subtract("12", "weeks");
+            //         var query = []; 
+            //         query.push({"$and" : [
+            //             { "data.realm": realm.value }, 
+            //             { "activities.assignedTo": $rootScope.user.userID}, 
+            //             { "state": 'running'}, 
+            //             { "createdOn": {"$gte": expired }}
+            //         ]});
+                                    
+            //         IWorkflows.query(query, true).then(function (workflows) {
+            //             $scope.pendingCount = (workflows||{}).count||0;
+            //         });
+            //     }
 
-            }
-        };
+               }
+            };
     }]);
 });
