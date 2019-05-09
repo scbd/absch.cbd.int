2b10d70c27a1b882a9c4fadb06f18ece9f0143fe
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-05-09 10:15:52 -0400

    NR 2 & 3 views

---
+++ app/views/forms/view/bch/view-national-report.directive.js
@@ -0,0 +1,44 @@
+﻿define(['app', "text!views/forms/view/bch/view-national-report.directive.html", 
+'views/forms/view/view-default-reference.directive'
+], function (app, template) {
+
+app.directive("viewNationalReport", [function () {
+	return {
+		restrict   : "EA",
+		template: template ,
+		replace    : true,
+		scope: {
+			reportQuestions : "=",
+			document        : "=",
+			locale          : "=",
+			target          : "@linkTarget",
+        },
+        link : function($scope){
+            //====================
+			//
+			//====================
+			$scope.display = function(field) {
+				
+				if(!$scope.hide) return true; //show all fields
+
+				return( $scope.hide.indexOf(field) >= 0 ? false : true);
+			}
+
+			$scope.spaceSubQuestion = function(number){
+				if((number||'')=='')return '';            
+			   return number.replace(/([0-9]{1,3})([a-z])/, '$1 $2') + '. '
+				
+			}
+			
+			$scope.displayText = function(answer, question){
+				if(question.options && answer){
+					var option = _.find(question.options, {value : answer});
+					return option && option.title||answer;
+				}
+				return answer;
+			}
+        }		
+	};
+}]);
+
+});
