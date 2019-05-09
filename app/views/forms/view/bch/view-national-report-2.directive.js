5e484687a6d2d7271636ccc379a0ea5cb3727a7b
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-05-09 10:15:52 -0400

    NR 2 & 3 views

--- app/views/forms/view/bch/view-national-report-2.directive.js
+++ app/views/forms/view/bch/view-national-report-2.directive.js
@@ -1,8 +1,8 @@
-define(['app', "text!views/forms/view/bch/view-national-report-3.directive.html", 
-'lodash', 'json!app-data/bch/report-analyzer/cpbNationalReport3.json', 'views/forms/view/bch/view-national-report.directive',
+define(['app', "text!views/forms/view/bch/view-national-report-2.directive.html", 
+'lodash', 'json!app-data/bch/report-analyzer/cpbNationalReport2.json', 'views/forms/view/bch/view-national-report.directive',
  	'views/directives/record-options'], function (app, template, _, nr4Data) {
 
-	app.directive("viewCpbNationalReport3", [function () {
+	app.directive("viewCpbNationalReport2", [function () {
 		return {
 			restrict   : "EAC",
 			template: template ,
