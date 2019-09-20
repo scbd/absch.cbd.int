2eda33423218ef33a90a4b476c8b8a6e105c6c6d
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-09-20 14:53:24 -0400

    submission view

---
+++ app/views/forms/view/view-submission.directive.js
@@ -0,0 +1,19 @@
+﻿define(['app', "text!views/forms/view/view-submission.directive.html", 	'views/directives/record-options'], function (app, template) {
+
+    app.directive("viewSubmission", [function () {
+        return {
+            restrict   : "EAC",
+            template: template ,
+            replace    : true,
+            transclude : false,
+            scope: {
+                document: "=ngModel",
+                locale  : "=",
+                target  : "@linkTarget",
+                hide	: "@"
+            }
+        };
+    }]);
+    
+    });
+    
