b1d173ec58e90ac985e04c954ce4d57079b0e2aa
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-09-20 14:50:28 -0400

    add middleware for sitemap/robots endpoint

---
+++ app/app-libs/middlewares.js
@@ -0,0 +1,28 @@
+﻿
+
+function robots (req, res){
+
+    var isValidHost = ['absch.cbd.int'].includes(req.headers['hosts']);
+
+    var text = isValidHost ? 'Allow: /' : 'Disallow: /';
+
+    res.contentType('text/plain');
+    res.end('User-agent: *\n' + text);
+}
+
+function sitemap(req, res){
+    require('superagent')
+            .get(`https://attachments.cbd.int/sitemap-${process.env.CLEARINGHOUSE.toLowerCase()}.xml`)
+            .pipe(res);
+}
+    
+function errorLogs(req, res){   
+    proxy.web(req, res, { target: `${apiUrl}/api/v2016/error-logs?type=client-app-errors&serverAppVersion=${encodeURIComponent(appVersion)}`, 
+                            secure:false, ignorePath:true, xfwd:true });
+}
+
+
+module.exports = {
+    robots,
+    sitemap
+}
--- server.js
+++ server.js
@@ -34,13 +34,8 @@ app.use('(/:lang(ar|en|es|fr|ru|zh))?/app',          translation.renderLanguageF
 app.use('/cbd-forums',      express.static(__dirname + '/node_modules/@bower_components/cbd-forums', { setHeaders: cacheControl.setCustomCacheControl }));
 app.use('/favicon.ico',     express.static(__dirname + '/favicon.ico', { setHeaders: cacheControl.setCustomCacheControl , maxAge: oneDay }));
 
-app.all('/sitemap(:num([0-9]{1,3})?).xml',     (req, res) =>{
-    let sitemapName = process.env.CLEARINGHOUSE.toLowerCase();
-    if(req.params.num)
-        sitemapName += req.params.num;
-     require('superagent').get(`https://attachments.cbd.int/sitemap-${sitemapName}.xml`)
-                          .query({bust:new Date().getTime()}).pipe(res)
-});
+app.get('/robots.txt' , require('./app/app-libs/middlewares').robots);
+app.all('/sitemap(:num([0-9]{1,3})?).xml', require('./app/app-libs/middlewares').sitemap);
 
 app.all('/app/*', function(req, res) { res.status(404).send(); } );
 
