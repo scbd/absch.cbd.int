9fc3dbd79e177070f212220d3780f28deae86965
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-09-25 13:54:35 -0400

    move middlewares to root

---
+++ middlewares/sitemap.js
@@ -0,0 +1,6 @@
+﻿
+module.exports = function(req, res){
+    require('superagent')
+            .get(`https://attachments.cbd.int/sitemap-${process.env.CLEARINGHOUSE.toLowerCase()}.xml`)
+            .pipe(res);
+}
