b5dc87cb0bcc60571c278e30bc09dc4fe20fc2b4
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-09-25 13:54:35 -0400

    move middlewares to root

---
+++ middlewares/robots.js
@@ -0,0 +1,10 @@
+﻿
+module.exports = function(req, res){
+
+    var isValidHost = ['absch.cbd.int'].includes(req.headers['hosts']);
+
+    var text = isValidHost ? 'Allow: /' : 'Disallow: /';
+
+    res.contentType('text/plain');
+    res.end('User-agent: *\n' + text);
+}
