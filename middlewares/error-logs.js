1997b2151e03736b65cfbba3368ad87b2b4ab91b
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-09-25 13:54:35 -0400

    move middlewares to root

---
+++ middlewares/error-logs.js
@@ -0,0 +1,9 @@
+﻿
+module.exports = function(proxy){  
+
+    return function(req, res){ 
+        proxy.web(req, res, { target: `${apiUrl}/api/v2016/error-logs?type=client-app-errors&serverAppVersion=${encodeURIComponent(appVersion)}`, 
+                                secure:false, ignorePath:true, xfwd:true });
+    }
+}
+
