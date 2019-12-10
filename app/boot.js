89ef74e732ee2e793b38d85813eef45422b49cfe
Blaise <blaisefonseca@hotmail.com>
2019-12-10 15:59:44 -0500

    fix

--- app/boot.js
+++ app/boot.js
@@ -103,7 +103,9 @@ require.config({
             return '';
             
         if(/^\//.test(url))            
-            return (url.indexOf('?') === -1 ? '?' : '&') + 'v=' + window.appVersion
+            return (url.indexOf('?') === -1 ? '?' : '&') + 'v=' + window.appVersion;
+
+        return '';
     }
 });
 
