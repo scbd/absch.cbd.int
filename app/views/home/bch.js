89040a194608236d8df26e03fdb9d38c76a2ddeb
Blaise <blaisefonseca@hotmail.com>
2020-02-20 16:19:28 -0500

    enable map on home page

--- app/views/home/bch.js
+++ app/views/home/bch.js
@@ -1,7 +1,6 @@
 define(['app', 'services/articles-service',
+    'views/directives/map/home-map',
 'views/directives/home-articles'], function (app) {
-//     'views/directives/map/home-map',
-// 'views/directives/home-country-dashboard-directive',
     return ['$scope', 'articlesService', function ($scope, articlesService) {
         
         articlesService.getArticle('5ce467f7452a5c00015e3406')
