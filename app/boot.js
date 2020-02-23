ecca8dceac49267a0265276ebe2a54895061340e
Blaise <blaisefonseca@hotmail.com>
2020-02-23 17:35:25 -0500

    lmo decisions and risk assessments

--- app/boot.js
+++ app/boot.js
@@ -50,13 +50,14 @@ require.config({
         'ngMeta'                    : cdnHost + 'ng-meta@1.0.3/dist/ngMeta.min',
         'angular-loggly-logger'     : cdnHost + '@scbd/angular-loggly-logger@0.3.2/angular-loggly-logger.min',
         'drag-and-drop'             : cdnHost + 'angular-drag-and-drop-lists@2.1.0/angular-drag-and-drop-lists.min',
+        'angucomplete-alt'          : cdnHost + 'angucomplete-alt@3.0.0/angucomplete-alt'   ,
 
         'socket.io'                 :           'https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.5/socket.io.min',
         'cbd-forums'                :           'libs/cbd-forums/cbd-forums',
         'shim'                      :           'libs/require-shim/src/shim',
         'angular-localizer'         :           'libs/angular-localizer/localizer',
         'view-abs-checkpoint'       :           'views/forms/view/abs/view-abs-checkpoint.directive', 
-        'angular-flex'              :           'libs/angular-flex/angular-flex', 
+        'angular-flex'              :           'libs/angular-flex/angular-flex'            
     },
     'shim': {
         'angular'                       : { 'deps': ['jquery'], 'exports': 'angular' },
@@ -92,7 +93,8 @@ require.config({
         'trix'                          : { 'deps': ['angular']},
         'diacritics'                    : { 'deps': ['angular']},
         'ngMeta'                        : { 'deps': ['angular']}
-        'drag-and-drop'                 : { 'deps': ['angular']}
+        'drag-and-drop'                 : { 'deps': ['angular']},
+        'angucomplete-alt'              : { 'deps': ['angular', 'css!'+cdnHost+'angucomplete-alt@3.0.0/angucomplete-alt.css']},
     },
     urlArgs: function(id, url){
         
