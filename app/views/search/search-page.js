44fd72d854cf5d02b8ffedbcb9987fcc5555e30a
Blaise <blaisefonseca@hotmail.com>
2020-03-02 10:10:52 -0500

    add locale to canonical address

--- app/views/search/search-page.js
+++ app/views/search/search-page.js
@@ -1,14 +1,12 @@
-define(['app', 'underscore',
-    'views/search/search-directive'
-], function(app, exportExcel) {
+define(['app', 'underscore', 'components/scbd-angularjs-services/services/locale', 'views/search/search-directive'], function(app, exportExcel) {
     "use strict";
-    app.controller("SearchPageController", ["$scope", '$sce', 'ngMeta', 'realm', function($scope, $sce, ngMeta, realm) {
+    app.controller("SearchPageController", ["$scope", '$sce', 'ngMeta', 'realm', 'locale', function($scope, $sce, ngMeta, realm, locale) {
 
             
             $('[role="tooltip"]').hide();
             
             ngMeta.resetMeta();  
-            var url   = realm.originalObject.baseURL + '/search'
+            var url   = realm.originalObject.baseURL + '/' + locale + '/search'
             ngMeta.setTitle('Search')
             ngMeta.setTag('canonical', $sce.trustAsResourceUrl(url))
         }
