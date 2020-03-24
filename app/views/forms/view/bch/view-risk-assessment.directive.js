3bf7ee95e7d0dd8b32518b119f8ecf4f0cdeabd9
Blaise <blaisefonseca@hotmail.com>
2020-03-24 16:18:39 -0400

    risk assessments changes

--- app/views/forms/view/bch/view-risk-assessment.directive.js
+++ app/views/forms/view/bch/view-risk-assessment.directive.js
@@ -1,6 +1,6 @@
 define(['app', "text!views/forms/view/bch/view-risk-assessment.directive.html", 	
 'views/directives/record-options', './view-lmo-reference.directive',
-'views/forms/view/view-contact-reference.directive'], function (app, template) {
+'views/forms/view/view-contact-reference.directive', 'views/forms/directives/view-terms-hierarchy'], function (app, template) {
 
 app.directive("viewNationalRiskAssessment", viewRiskAssessment);
 app.directive("viewIndependentRiskAssessment", viewRiskAssessment);
