018051619ff6e3fa8eab6df54d4963429c05bd13
Blaise <blaisefonseca@hotmail.com>
2020-04-03 12:18:04 -0400

    redirect user to return url or home when already loggedin

--- app/views/shared/login-dialog.js
+++ app/views/shared/login-dialog.js
@@ -1,19 +1,23 @@
 define(['jquery', 'components/scbd-branding/directives/header/login'
 ], function ($) {
 
-    return ["$scope", "$location", function ($scope, $location){
+    return ["$scope", "$location", "authentication", function ($scope, $location, authentication){
 
 				$scope.$on('signIn', function(evt, data){
-
-					console.log('usersignin', data);
-
-					if ($location.search().returnUrl)
-                            $location.url($location.search().returnUrl);
-                        else
-                            $location.url('/register');
-
+					redirectUser();
 				});
+				authentication.getUser().then(function(user){
+					if(!user.isAuthenticated)
+						$('#loginDialog').modal("show");
+					else
+						redirectUser();
+				})
 
-				$('#loginDialog').modal("show");
+				function redirectUser(){
+					if ($location.search().returnUrl)
+						$location.url($location.search().returnUrl);
+					else
+						$location.url('/register');
+				}
 		}];
 });
