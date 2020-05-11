5d51bd1e9ef091bf54bb04e146df42c45021884d
Blaise Fonseca <blaisefonseca@hotmail.com>
2020-05-11 15:12:11 -0400

    highlight missing translation at component level
    * option to highlight component level missing translation
    * simplify css rules

--- app/views/register/admin.js
+++ app/views/register/admin.js
@@ -1,15 +1,26 @@
 define(['app', 
- 'services/role-service', 
+ 'services/role-service', 'services/local-storage-service',
  'views/register/directives/register-top-menu'], function(app) {
  "use strict";
-    app.controller("AdminController", ["$rootScope", "$scope", "roleService", 
-        function($rootScope, $scope, roleService) {
+    app.controller("AdminController", ["$rootScope", "$scope", "roleService", 'localStorageService',
+        function($rootScope, $scope, roleService, localStorageService) {
 
             if ($rootScope.user.isAuthenticated) {
                 $scope.roles = {
                     isAdministrator       : roleService.isAdministrator()
                 };
                
+                $scope.isValidatingTranslation = localStorageService.get('validateTranslation');
+
+                $scope.validateTranslation = function(validate){
+                    localStorageService.set('validateTranslation', validate);
+                    $scope.isValidatingTranslation = validate;
+
+                    if(validate)
+                        $('body#top').addClass('validate-translation')
+                    else
+                        $('body#top').removeClass('validate-translation')
+                }
             }
         }
     ]);
