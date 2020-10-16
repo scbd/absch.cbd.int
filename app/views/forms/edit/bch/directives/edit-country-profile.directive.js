fbf842ee8d9731665601e38ce0b5b131489c5591
awakeel <tahmeedshah@gmail.com>
2020-10-16 22:27:27 +0500

    code refacetor for country profile"

--- app/views/forms/edit/bch/directives/edit-country-profile.directive.js
+++ app/views/forms/edit/bch/directives/edit-country-profile.directive.js
@@ -1,7 +1,7 @@
 define(['app', 'lodash', 'text!./edit-country-profile.directive.html', 'views/forms/edit/edit',
 	"views/forms/view/bch/view-country-profile.directive"], 
 function (app, _, template) { 
-	app.directive("editCountryProfile", ["$controller",'IStorage', '$routeParams', 'ngDialog',"searchService", function($controller,IStorage, $routeParams, ngDialog,searchService) {
+	app.directive("editCountryProfile", ["$controller", '$routeParams', 'ngDialog',"searchService", function($controller, $routeParams, ngDialog,searchService) {
 		return {
 			restrict   : "EA",
 			template: template,
@@ -25,7 +25,8 @@ function (app, _, template) {
 					return $scope.sanitizeDocument(document);
 				}; 
 				$scope.setDocument({})  
-				$scope.checkRecordAlreadyExists = function(){
+				checkRecordAlreadyExists();
+				function checkRecordAlreadyExists(){
 					if($routeParams.identifier){ 
 						return;
 					}  
@@ -36,48 +37,47 @@ function (app, _, template) {
 					searchService.list(searchQuery) 
 					.then(function(result) { 
 						if(result.data.response.docs.length){ 
-							const identifier = result.data.response.docs[0].identifier_s;
+							var identifier = result.data.response.docs[0].identifier_s;
 							if(identifier){
-								$scope.validateCountryProfileExists(identifier)
+							  validateCountryProfileExists(identifier)
 							}
 						  }
 						
 					});
-				} 
-				$scope.checkRecordAlreadyExists();
-				$scope.validateCountryProfileExists = function(identifier){ 
-						if (identifier) {
-							$scope.blockEditForm = true; 
-							ngDialog.open({
-								template: 'recordExistsTemplate.html',													
-								closeByDocument: false,
-								closeByEscape: false,
-								showClose: false,
-								closeByNavigation: false,
-								controller: ['$scope', '$timeout', '$location', function($scope, $timeout, $location) {
-									$scope.alertSeconds = 10;
-									time(); 
-									function time(){
-										$timeout(function(){
-											if($scope.alertSeconds == 1){																	
-												$scope.openExisting();
-											}
-											else{
-												$scope.alertSeconds--;																
-												time()
-											}
-										}, 1000)
-									}
-									$scope.openExisting = function() {
-										ngDialog.close();
-										$location.path('register/BCP/' + identifier+'/edit');
-									}
-								}]
-							});
-						}
-						else{
-			
-						} 
+				}  
+				function validateCountryProfileExists(identifier){ 
+					if (identifier) {
+						$scope.blockEditForm = true; 
+						ngDialog.open({
+							template: 'recordExistsTemplate.html',													
+							closeByDocument: false,
+							closeByEscape: false,
+							showClose: false,
+							closeByNavigation: false,
+							controller: ['$scope', '$timeout', '$location', function($scope, $timeout, $location) {
+								$scope.alertSeconds = 10;
+								time(); 
+								function time(){
+									$timeout(function(){
+										if($scope.alertSeconds == 1){																	
+											$scope.openExisting();
+										}
+										else{
+											$scope.alertSeconds--;																
+											time()
+										}
+									}, 1000)
+								}
+								$scope.openExisting = function() {
+									ngDialog.close();
+									$location.path('register/BCP/' + identifier+'/edit');
+								}
+							}]
+						});
+					}
+					else{
+		
+					} 
 				}
 			}
 			
