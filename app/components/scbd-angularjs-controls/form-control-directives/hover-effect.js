67e007484a3f6d2fcac6fb34d2324084103faf78
Tahmeed Shah <tahmeedshah@gmail.com>
2020-10-20 11:37:27 +0500

    Hover-effect- fix for small devices

--- app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
+++ app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
@@ -6,10 +6,12 @@ define(['app','lodash','angular'], function(app,_,angular) {
         },
         restrict: 'EA',
         link: function (scope, element) {
-                element.on( 'mouseenter', function (e) {
-                element.children("ul").attr("style",'display:block');
+                element.on( 'mouseenter', function () {
+                    if (window.innerWidth > 800) {
+                        element.children("ul").attr("style", 'display:block');
+                    }
             });
-            element.on('mouseleave', function (e) {
+            element.on('mouseleave', function () {
                if(element.children("ul").attr("style") == 'display:block'){
                 element.children("ul").removeAttr("style");
                }
