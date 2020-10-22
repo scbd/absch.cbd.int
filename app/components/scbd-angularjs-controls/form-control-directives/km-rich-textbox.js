a7eca518665963c2ce38063c495d7de77a7cfe71
Blaise Fonseca <blaisefonseca@hotmail.com>
2020-10-22 15:44:48 -0400

    grammar
    Co-authored-by: Stephane Bilodeau <sbilodeau@users.noreply.github.com>

--- app/components/scbd-angularjs-controls/form-control-directives/km-rich-textbox.js
+++ app/components/scbd-angularjs-controls/form-control-directives/km-rich-textbox.js
@@ -103,7 +103,7 @@
 								var buttonHTML = '<button type="button" class="trix-button trix-button--icon-underline" data-trix-attribute="underline" data-trix-key="u" title="Underline" tabindex="-1">U</button>'
 								e.target.toolbarElement.querySelector(".trix-button-group.trix-button-group--text-tools")
 									.insertAdjacentHTML("beforeend", buttonHTML)
-									var errorMessage = "<p class='trix-dialog-error'>Please enter a valid URL starting with https:// | http://</p>";
+									var errorMessage = "<p class='trix-dialog-error'>Please enter a valid URL starting with https:// or http://</p>";
 								e.target.toolbarElement.querySelector(".trix-input").insertAdjacentHTML("afterend", errorMessage);
 							}
 					}
