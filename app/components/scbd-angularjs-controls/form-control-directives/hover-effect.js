f0a7c73770e8eccc5304f1eef64999a06ff2e7b4
Tahmeed Shah <tahmeedshah@gmail.com>
2020-10-23 22:03:47 +0500

    Profile menu not open when the user not signedin

--- app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
+++ app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
@@ -2,11 +2,14 @@ define(['app','lodash','angular'], function(app,_,angular) {
     app.directive('hoverEffect',['$timeout', function ($timeout  ) {
     return {
         scope: {
-           binding: '=ngModel',
+           binding: '=ngModel'
         },
-        restrict: 'EA',
-        link: function (scope, element) {
+        restrict: 'EAC',
+        link: function (scope, element, attr) {
             element.on( 'mouseenter', function () {
+                if(!element.hasClass('true') && attr.isauthenticated != undefined){
+                    return;
+                }
                 if (window.innerWidth > 800) {
                     element.children("ul").addClass("show");
                 }
@@ -14,7 +17,10 @@ define(['app','lodash','angular'], function(app,_,angular) {
 
             element.on('mouseleave', function () {
                 if(element.children("ul").hasClass("show")){
-                    element.children("ul").removeClass("show");
+                   $timeout(function(){
+                        element.children("ul").removeClass("show");
+                   }, 500);
+
                 }
             });
         }
