d43622bb667ac5efe6da6a4add45d8c70323fc5f
Tahmeed Shah <tahmeedshah@gmail.com>
2020-10-26 13:14:58 +0500

    Hover-effect directive for bootstrap dropdown

---
+++ app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
@@ -0,0 +1,26 @@
+define(['app','lodash','angular'], function(app,_,angular) {
+    app.directive('hoverEffect',['$timeout', function ($timeout  ) {
+    return {
+        scope: {
+           binding: '=ngModel',
+        },
+        restrict: 'EA',
+        link: function (scope, element) {
+            element.on( 'mouseenter', function () {
+                if (window.innerWidth > 800) {
+                    element.children("ul").addClass("show");
+                }
+            });
+
+            element.on('mouseleave', function () {
+                if(element.children("ul").hasClass("show")){
+                    $timeout(function(){
+                        element.children("ul").removeClass("show");
+                    },500);
+
+                }
+            });
+        }
+    };
+}]);
+});
