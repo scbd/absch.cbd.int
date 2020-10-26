90430c03eaa63376eefb9122f8ae09e2defc0b38
Tahmeed Shah <tahmeedshah@gmail.com>
2020-10-27 00:21:03 +0500

    let the user pass the complete selector (. or #)

--- app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
+++ app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
@@ -2,19 +2,20 @@ define(['app','lodash','angular'], function(app,_,angular) {
     app.directive('hoverEffect',['$timeout', function ($timeout  ) {
     return {
         scope: {
-            deopDownMenu: '=hoverEffect'
+            hoverEffect: '@'
         },
         restrict: 'A',
         link: function (scope, element) {
+            if(scope.hoverEffect==undefined || scope.hoverEffect.length==0) return;
             element.on( 'mouseenter', function () {
                 if (window.innerWidth > 800) {
-                    element.children("."+scope.deopDownMenu).addClass("show");
+                    element.children(scope.hoverEffect).addClass("show");
                 }
             });
             element.on('mouseleave', function () {
-                if(  element.children("."+scope.deopDownMenu).hasClass("show")){
+                if(  element.children(scope.hoverEffect).hasClass("show")){
                    $timeout(function(){
-                       element.children("."+scope.deopDownMenu).removeClass("show");
+                       element.children(scope.hoverEffect).removeClass("show");
                    }, 500);
 
                 }
