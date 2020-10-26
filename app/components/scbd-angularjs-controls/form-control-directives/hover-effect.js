d8671098fda364355a8a02f98dd9df60ba1dc47b
Tahmeed Shah <tahmeedshah@gmail.com>
2020-10-26 22:28:49 +0500

    pass the class name into the directive

--- app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
+++ app/components/scbd-angularjs-controls/form-control-directives/hover-effect.js
@@ -2,21 +2,20 @@ define(['app','lodash','angular'], function(app,_,angular) {
     app.directive('hoverEffect',['$timeout', function ($timeout  ) {
     return {
         scope: {
-           binding: '=ngModel',
+            deopDownMenu: '=hoverEffect'
         },
-        restrict: 'EA',
+        restrict: 'A',
         link: function (scope, element) {
             element.on( 'mouseenter', function () {
                 if (window.innerWidth > 800) {
-                    element.children("ul").addClass("show");
+                    element.children("."+scope.deopDownMenu).addClass("show");
                 }
             });
-
             element.on('mouseleave', function () {
-                if(element.children("ul").hasClass("show")){
-                    $timeout(function(){
-                        element.children("ul").removeClass("show");
-                    },500);
+                if(  element.children("."+scope.deopDownMenu).hasClass("show")){
+                   $timeout(function(){
+                       element.children("."+scope.deopDownMenu).removeClass("show");
+                   }, 500);
 
                 }
             });
