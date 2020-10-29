94f9d217199c400f08243dbb5f8d254423afcb0c
Tahmeed Shah <tahmeedshah@gmail.com>
2020-10-30 00:57:00 +0500

    Work in Progress-css changes-export data

--- app/views/reports/bch/registries/index.js
+++ app/views/reports/bch/registries/index.js
@@ -1,9 +1,24 @@
 define(['app', 'lodash', 'services/search-service', 'js/common',
 	'./index'], function(app, _) { 'use strict';
 
-	app.controller("RegistryReportController", ['$scope', '$routeParams',   'searchService', '$element',
-		function($scope, $routeParams,  searchService,$element ) {
-			
+	app.controller("RegistryReportController", ['$scope',    'searchService',  
+		function($scope,searchService) { 
+			var schemas = ["organism","modifiedOrganism","dnaSequence"];
+			schemas.forEach(function(schema){ 
+				loadRecords(schema);
+			}) 
+			function loadRecords(schema){ 
+				var searchQuery = {
+					fields:  'numFound',
+					query:  'schema_s:'+schema,
+					rowsPerPage:1
+				};
+				return searchService.list(searchQuery)
+					.then(function(result){  
+					  $scope[schema+"Records"] = result.data.response.numFound;    
+					}) 
+
+			}
 		}]);
 
 });
--- app/views/reports/bch/registries/view-registry.directive.js
+++ app/views/reports/bch/registries/view-registry.directive.js
@@ -1,48 +1,63 @@
-define(['app', 'text!views/reports/bch/registries/view-registry.directive.html','services/role-service'], function(app, template) {
-
-    app.directive('registry', function() {
+define(['app', 'text!views/reports/bch/registries/view-registry.directive.html', 'services/role-service'], function (app, template) {
+    app.directive('registry', function () {
         return {
             restrict: 'EA',
             template: template,
             scope: {
                 schema: '@',
-                fields: '@', 
-                caption:"@",
-                url:"@",
-                rowsPerPage:"@"
-            },
-            controller: [ '$scope','searchService', function($scope,searchService) {  
+                fields: '@',
+                caption: "@",
+                url: "@",
+               //rowsPerPage: "@"
+            }, 
+            controller: ['$scope', 'searchService', '$element', '$timeout', function ($scope, searchService, $element, $timeout) {
                 loadRecords();
-                function loadRecords( ){
+                function loadRecords() {
                     $scope.isLoading = true;
                     var searchQuery = {
-                        fields:  $scope.fields,
-                        query:  'schema_s:'+$scope.schema,
-                        rowsPerPage:$scope.rowsPerPage
+                        fields: $scope.fields,
+                        query: 'schema_s:' + $scope.schema,
+                       //rowsPerPage: $scope.rowsPerPage
                     };
                     return searchService.list(searchQuery)
-                        .then(function(result){ 
-                          $scope.data = [];
-                          result.data.response.docs.forEach(function(item) {
-                            Object.entries(item).forEach(([k,v]) => {  
-                                    if(Array.isArray(v)){
-                                        if(k != $scope.url){
-                                            item[k]= v.join(', ')
+                        .then(function (result) {
+                            $scope.data = [];
+                            $scope.numFound = result.data.response.numFound;
+                            result.data.response.docs.forEach(function (item) {
+                                for (var prop in item) {
+                                    if (Array.isArray(item[prop])) {
+                                        if (prop != $scope.url) {
+                                            item[prop] = item[prop].join(', ')
                                         }
                                     }
-                            }) 
-                            $scope.data.push(item);
-                          });
+                                }
+                                $scope.data.push(item);
+                            });
 
                         })
-                        .finally(function(){
+                        .finally(function () {
                             $scope.isLoading = false;
                         })
-    
+
                 }
+                $scope.export = function () {
+                    $scope.dataToExport = $scope.data;
+                    $scope.readyForExport = true;
+                    require(['tableexport'], function () {
+                        $element.find('#forExport').tableExport({
+                            formats: ['xlsx'],
+                            filename: $scope.schema + '-registry',
+                        });
+                        $element.find('.xlsx').click();
+                        $timeout(function () {
+                            $scope.readyForExport = false;
+                        }, 200)
+                    });
+                }
+
             }]
 
         }
     });
 
-});
+}); 
