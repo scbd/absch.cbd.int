577eaf33c38bf3fc16a07cbc593a2bab88a5cac3
Blaise Fonseca <blaise.fonseca@cbd.int>
2020-10-30 13:56:18 -0400

    thematic

--- app/views/forms/edit/edit-submission.js
+++ app/views/forms/edit/edit-submission.js
@@ -1,9 +1,11 @@
 ﻿define(['app', 'lodash', 'views/forms/edit/edit', '../view/view-submission.directive',
         'views/forms/edit/organization-selector'], function (app, _) {
 
-  app.controller("editSubmission", ["$scope", "$http", "$controller", "IStorage", function ($scope, $http, $controller, storage) {
+  app.controller("editSubmission", ["$scope", "$http", "$controller", "realm", function ($scope, $http, $controller, realm) {
 
 
+    $scope.isBch = realm.is('BCH');
+    $scope.isAbs = realm.is('ABS');
     //$scope.organizationsRef = [];
     $controller('editController', {$scope: $scope});
 
@@ -58,76 +60,76 @@
     };
 
 
-    $scope.loadNotifications = function (identifier) {
+    // $scope.loadNotifications = function (identifier) {
 
-        var params = {
-            q: "schema_s:notification AND date_s:[ " + moment().subtract(3, "years").format() + " TO " + moment().format() + " ] ",
-            fl: "identifier_s:symbol_s,title_*, reference_s, symbol_s",
-            sort: "symbol_s DESC",
-            rows: 99999999
-        };
-        if(identifier)
-            params.q = 'symbol_s:' + identifier;
-        return $http.get("/api/v2013/index", { params: params,cache: true})
-            .then(function (results) {
-                var qResult = _.map(normalizeSolrResult(results.data.response.docs), function(row){
-                    row.summary  = row.title;
-                    row.title 	 = (row.reference_s||'') + ' (' + (row.symbol_s||'') + ')';
-                    return row;
-                });
-                if(identifier)
-                    return qResult[0];
-                return qResult;
-            });
-    }
+    //     var params = {
+    //         q: "schema_s:notification AND date_s:[ " + moment().subtract(3, "years").format() + " TO " + moment().format() + " ] ",
+    //         fl: "identifier_s:symbol_s,title_*, reference_s, symbol_s",
+    //         sort: "symbol_s DESC",
+    //         rows: 99999999
+    //     };
+    //     if(identifier)
+    //         params.q = 'symbol_s:' + identifier;
+    //     return $http.get("/api/v2013/index", { params: params,cache: true})
+    //         .then(function (results) {
+    //             var qResult = _.map(normalizeSolrResult(results.data.response.docs), function(row){
+    //                 row.summary  = row.title;
+    //                 row.title 	 = (row.reference_s||'') + ' (' + (row.symbol_s||'') + ')';
+    //                 return row;
+    //             });
+    //             if(identifier)
+    //                 return qResult[0];
+    //             return qResult;
+    //         });
+    // }
 
-    $scope.loadSchemaRecords = function (identifier, schema) {
+    // $scope.loadSchemaRecords = function (identifier, schema) {
 
-        var sQuery;
-        if (schema != '*')
-            sQuery = "type eq '" + schema + "'";
+    //     var sQuery;
+    //     if (schema != '*')
+    //         sQuery = "type eq '" + schema + "'";
 
-        var params = {
-            q: "schema_s:" + schema,
-            fl: "identifier_s,title_*,summary:acronym_t",
-            sort: "title_s ASC",
-            rows: 99999999
-        };
+    //     var params = {
+    //         q: "schema_s:" + schema,
+    //         fl: "identifier_s,title_*,summary:acronym_t",
+    //         sort: "title_s ASC",
+    //         rows: 99999999
+    //     };
 
-        if(identifier)//assume identifier, temp TODO: change logic
-            params.q = 'identifier_s:' + identifier;
-        return $http.get("/api/v2013/index", { params: params, cache: true })
-        .then(function (results) {
-            var qResult = normalizeSolrResult(results.data.response.docs);
-            if(identifier)
-                return qResult[0];
-            return qResult;
-        });
-    };
+    //     if(identifier)//assume identifier, temp TODO: change logic
+    //         params.q = 'identifier_s:' + identifier;
+    //     return $http.get("/api/v2013/index", { params: params, cache: true })
+    //     .then(function (results) {
+    //         var qResult = normalizeSolrResult(results.data.response.docs);
+    //         if(identifier)
+    //             return qResult[0];
+    //         return qResult;
+    //     });
+    // };
 
-    function normalizeSolrResult(data) {
-        var normalData = []
-        for (var i = 0; i < data.length; i++) {
-            normalData[i] = data[i]
-            normalData[i].identifier = data[i].identifier_s
-            normalData[i].title = solrPropTolString('title', data[i]);								
-        }
-        return normalData
-    }
+    // function normalizeSolrResult(data) {
+    //     var normalData = []
+    //     for (var i = 0; i < data.length; i++) {
+    //         normalData[i] = data[i]
+    //         normalData[i].identifier = data[i].identifier_s
+    //         normalData[i].title = solrPropTolString('title', data[i]);								
+    //     }
+    //     return normalData
+    // }
 
-    function solrPropTolString(propertyName, solrDoc) {
-        if (!solrDoc[propertyName + '_t']) return {}
+    // function solrPropTolString(propertyName, solrDoc) {
+    //     if (!solrDoc[propertyName + '_t']) return {}
 
-        var langs = ['EN', 'AR', 'ES', 'FR', 'RU', 'ZH']
-        var lString = {}
+    //     var langs = ['EN', 'AR', 'ES', 'FR', 'RU', 'ZH']
+    //     var lString = {}
 
-        for (var i = 0; i < langs.length; i++) {
-            lString[langs[i].toLowerCase()] = solrDoc[propertyName + '_' + langs[i] + '_t']
-        }
-        return lString
-    }
+    //     for (var i = 0; i < langs.length; i++) {
+    //         lString[langs[i].toLowerCase()] = solrDoc[propertyName + '_' + langs[i] + '_t']
+    //     }
+    //     return lString
+    // }
 
-    $scope.setDocument({});
+    $scope.setDocument({}, true);
 
   }]);
 });
