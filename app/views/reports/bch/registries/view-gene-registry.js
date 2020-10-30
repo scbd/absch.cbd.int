35936c49fe05c542c349e8099adbf9b8a4e08565
Tahmeed Shah <tahmeedshah@gmail.com>
2020-10-31 01:08:50 +0500

    BCH Registry views

--- app/views/reports/bch/registries/view-gene-registry.js
+++ app/views/reports/bch/registries/view-gene-registry.js
@@ -1,10 +1,41 @@
-define(['app', 'lodash', 'services/search-service', 'js/common',
-	'./view-gene-registry','views/reports/bch/registries/view-registry.directive'], function(app, _) { 'use strict';
+define(['app', 'services/search-service'], function(app) { 'use strict';
 
-	app.controller("geneRegistryController", ['$scope', '$routeParams',   'searchService', '$element',
-		function($scope, $routeParams,  searchService,$element ) { 
-			$scope.isArray = angular.isArray;
- 
+	app.controller("geneRegistryController", ['$scope','searchService','$element', '$timeout',
+		function($scope,searchService,$element,$timeout) { 
+				$scope.isLoading = false;
+				loadRecords();
+				function loadRecords(){ 
+					$scope.isLoading = true;
+				var searchQuery = {
+					fields:  'Record_ID:uniqueIdentifier_s,Name:name_s,Trait:summary_t,Donor_organism:geneDonorOrganism,geneFunction:biologicalFunction_EN_t,url_ss',
+					query:  'schema_s:dnaSequence',
+					rowsPerPage: 10000					
+				};
+				return searchService.list(searchQuery)
+					.then(function(result){ 
+						$scope.geneRecords = [];
+						$scope.numFound = 0;
+						if(result.data){
+							$scope.geneRecords = result.data.response.docs;
+							$scope.numFound = result.data.response.numFound;
+						}
+						$scope.isLoading = false;
+					  }) 
+			}
+			$scope.export = function () {
+				$scope.dataToExport = $scope.data;
+				$scope.readyForExport = true;
+				require(['tableexport'], function () {
+					$element.find('#forExport').tableExport({
+						formats: ['xlsx'],
+						filename: 'GENE-registry',
+					});
+					$element.find('.xlsx').click();
+					$timeout(function () {
+						$scope.readyForExport = false;
+					}, 200)
+				});
+			}
 		}]);
 
 });
--- app/views/reports/bch/registries/view-lmo-registry.js
+++ app/views/reports/bch/registries/view-lmo-registry.js
@@ -1,9 +1,43 @@
-define(['app', 'lodash', 'services/search-service', 'js/common',
-	'./view-lmo-registry','views/reports/bch/registries/view-registry.directive'], function(app, _) { 'use strict';
+define(['app', 'services/search-service'], function(app) { 'use strict';
 
-	app.controller("lmoRegistryController", ['$scope', '$routeParams',   'searchService', '$element',
-		function($scope, $routeParams,  searchService,$element ) { 
-			$scope.isArray = angular.isArray;  
+	app.controller("lmoRegistryController", ['$scope','searchService','$element', '$timeout',
+		function($scope,searchService,$element,$timeout) { 
+				$scope.isLoading = false;
+				loadRecords();
+				function loadRecords(){ 
+					$scope.isLoading = true;
+				var searchQuery = {
+					fields:  'Record_ID:uniqueIdentifier_s,Identity:identity_s,Transformation_event:transformationEvent_s,Organism:lmoCommonNames_EN_txt,Description:summary_t,url_ss',
+					query:  'schema_s:modifiedOrganism',
+					rowsPerPage: 10000
+					
+				};
+				return searchService.list(searchQuery)
+					.then(function(result){ 
+						$scope.lmoRecords = [];
+						$scope.numFound = 0;
+						if(result.data){
+							$scope.lmoRecords = result.data.response.docs;
+							$scope.numFound = result.data.response.numFound;
+						}
+						$scope.isLoading = false;
+					  }) 
+					  
+			}
+			$scope.export = function () {
+				$scope.dataToExport = $scope.data;
+				$scope.readyForExport = true;
+				require(['tableexport'], function () {
+					$element.find('#forExport').tableExport({
+						formats: ['xlsx'],
+						filename: 'LMO-registry',
+					});
+					$element.find('.xlsx').click();
+					$timeout(function () {
+						$scope.readyForExport = false;
+					}, 200)
+				});
+			}
 		}]);
 
 });
