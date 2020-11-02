1551a6ab3e5e1d637b9808b7281675bfa8efcdec
Tahmeed Shah <tahmeedshah@gmail.com>
2020-11-03 02:31:47 +0500

    show Error message in HTML-CSS changes

--- app/views/reports/bch/registries/view-gene-registry.js
+++ app/views/reports/bch/registries/view-gene-registry.js
@@ -3,11 +3,12 @@ define(['app','css!/app/css/registry.css','services/search-service'], function(a
 app.controller("geneRegistryController", ['$scope','searchService','$element', '$timeout', 'toastr', '$log',
 function($scope,searchService,$element,$timeout, toastr, $log) { 
 		$scope.isLoading = false;
+		$scope.isError = false;
 		loadRecords();
 		function loadRecords(){ 
 			$scope.isLoading = true;
 		var searchQuery = {
-			fields:  'recordId:uniqueIdentifier_s,name:name_s,trait:summary_t,donorOrganism:geneDonorOrganism,geneFunction:biologicalFunction_EN_t,url_ss',
+			fields:  'recordId:uniqueIdentifier_s,name:name_s,trait:summary_t,donorOrganism:geneDonorOrganismCommonNames_EN_txt,geneFunction:biologicalFunction_EN_t,url_ss',
 			query:  'schema_s:dnaSequence',
 			rowsPerPage: 10000
 			
@@ -21,21 +22,15 @@ function($scope,searchService,$element,$timeout, toastr, $log) {
 					$scope.numFound = result.data.response.numFound;
 				}
 			  }) 
-			.catch(function(e){
-				toastr.error('There was an error running search query.')
-				var exception = {
-					data    :  e.data||e.message, status:e.status,
-					url     : (e.config||{}).url, params: (e.config||{}).params,
-					stack   : e.stack
-				}                                
-				$log.error(JSON.stringify(exception))
+			  .catch(function(e){
+				$scope.isError = true;
+				throw e;
 			})
 			.finally(function(){
 				$scope.isLoading = false;
 			})					  
 	}
 	$scope.export = function () {
-		$scope.dataToExport = $scope.data;
 		$scope.readyForExport = true;
 		require(['tableexport'], function () {
 			$element.find('#forExport').tableExport({
