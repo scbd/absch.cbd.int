e2e99884416d7822ab62ea48cc90c4b7b700b7af
Tahmeed Shah <tahmeedshah@gmail.com>
2020-11-03 02:31:47 +0500

    show Error message in HTML-CSS changes

--- app/views/reports/bch/registries/index.js
+++ app/views/reports/bch/registries/index.js
@@ -1,28 +1,24 @@
-define(['app', 'lodash', 'css!/app/css/registry.css', 'services/search-service', 'js/common','views/register/directives/register-top-menu',
+define(['app', 'lodash', 'css!/app/css/registry.css', 'services/search-service', 'js/common', 'views/register/directives/register-top-menu', 'services/solr',
 		'./index'], function(app, _) { 'use strict';
 
-		app.controller("registryReportController", ['$scope', 'searchService', 'toastr', '$log',
-			function ($scope, searchService, toastr, $log) {
+		app.controller("registryReportController", ['$scope', 'searchService', 'toastr', '$log', 'solr',
+			function ($scope, searchService, toastr, $log, solr) {
 				var schemas = ["organism", "modifiedOrganism", "dnaSequence"];
+				
 				loadRecords();
 				function loadRecords(schema) {
 					var recordQuery = { 
                         fields: 'schema_s',
-                        query: 'schema_s:(' + schemas.join(' ') + ')'
+						query: 'schema_s:(' + _.map(schemas, solr.escape).join(' ') + ')'
+						
                     };
-				 
 					return searchService.facets(recordQuery)
 						.then(function (result) { 
 							$scope.count = result.schema_s;
 						})
 					.catch(function(e){
-						toastr.error('There was an error running search query.')
-						var exception = {
-							data    :  e.data||e.message, status:e.status,
-							url     : (e.config||{}).url, params: (e.config||{}).params,
-							stack   : e.stack
-						}                                
-						$log.error(JSON.stringify(exception))
+
+       					 throw e;
 					})
 				}
 			}]);
--- app/views/reports/bch/registries/view-lmo-registry.js
+++ app/views/reports/bch/registries/view-lmo-registry.js
@@ -3,11 +3,12 @@ define(['app','css!/app/css/registry.css', 'services/search-service'], function(
 	app.controller("lmoRegistryController", ['$scope','searchService','$element', '$timeout', 'toastr', '$log',
 		function($scope, searchService, $element, $timeout, toastr, $log) { 
 				$scope.isLoading = false;
+				$scope.isError = false;
 				loadRecords();
 				function loadRecords(){ 
 					$scope.isLoading = true;
 				var searchQuery = {
-					fields:  'recordId:uniqueIdentifier_s,identity:identity_s,transformationEvent:transformationEvent_s,organism:lmoCommonNames_EN_txt,description:summary_t,url_ss',
+					fields:  'recordId:uniqueIdentifier_s,identity:identity_s,transformationEvent:transformationEvent_s,organism:lmoCommonNames_EN_txt,description:summary_EN_t,url_ss',
 					query:  'schema_s:modifiedOrganism',
 					rowsPerPage: 10000
 					
@@ -22,20 +23,14 @@ define(['app','css!/app/css/registry.css', 'services/search-service'], function(
 						}
 					  }) 
 					.catch(function(e){
-						toastr.error('There was an error running search query.')
-						var exception = {
-							data    :  e.data||e.message, status:e.status,
-							url     : (e.config||{}).url, params: (e.config||{}).params,
-							stack   : e.stack
-						}                                
-						$log.error(JSON.stringify(exception))
+						$scope.isError = true;
+						throw e;
 					})
 					.finally(function(){
                         $scope.isLoading = false;
                     })					  
 			}
 			$scope.export = function () {
-				$scope.dataToExport = $scope.data;
 				$scope.readyForExport = true;
 				require(['tableexport'], function () {
 					$element.find('#forExport').tableExport({
