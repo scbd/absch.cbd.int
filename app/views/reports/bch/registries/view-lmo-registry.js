f187f6c168465ea0059c1274e57116a1307197bb
Tahmeed Shah <tahmeedshah@gmail.com>
2020-11-02 20:00:51 +0500

    CSS changes and error handling

--- app/views/reports/bch/registries/view-lmo-registry.js
+++ app/views/reports/bch/registries/view-lmo-registry.js
@@ -1,18 +1,18 @@
-define(['app', 'services/search-service'], function(app) { 'use strict';
+define(['app','css!/app/css/registry.css', 'services/search-service'], function(app) { 'use strict';
 
-	app.controller("lmoRegistryController", ['$scope','searchService','$element', '$timeout',
-		function($scope,searchService,$element,$timeout) { 
+	app.controller("lmoRegistryController", ['$scope','searchService','$element', '$timeout', 'toastr', '$log',
+		function($scope, searchService, $element, $timeout, toastr, $log) { 
 				$scope.isLoading = false;
 				loadRecords();
 				function loadRecords(){ 
 					$scope.isLoading = true;
 				var searchQuery = {
-					fields:  'Record_ID:uniqueIdentifier_s,Identity:identity_s,Transformation_event:transformationEvent_s,Organism:lmoCommonNames_EN_txt,Description:summary_t,url_ss',
+					fields:  'recordId:uniqueIdentifier_s,identity:identity_s,transformationEvent:transformationEvent_s,organism:lmoCommonNames_EN_txt,description:summary_t,url_ss',
 					query:  'schema_s:modifiedOrganism',
-					rowsPerPage: 10000
+					rowsPerPage: 1000
 					
 				};
-				return searchService.list(searchQuery)
+					return searchService.list(searchQuery)
 					.then(function(result){ 
 						$scope.lmoRecords = [];
 						$scope.numFound = 0;
@@ -20,9 +20,19 @@ define(['app', 'services/search-service'], function(app) { 'use strict';
 							$scope.lmoRecords = result.data.response.docs;
 							$scope.numFound = result.data.response.numFound;
 						}
-						$scope.isLoading = false;
 					  }) 
-					  
+					.catch(function(e){
+						toastr.error('There was an error running search query.')
+						var exception = {
+							data    :  e.data||e.message, status:e.status,
+							url     : (e.config||{}).url, params: (e.config||{}).params,
+							stack   : e.stack
+						}                                
+						$log.error(JSON.stringify(exception))
+					})
+					.finally(function(){
+                        $scope.isLoading = false;
+                    })					  
 			}
 			$scope.export = function () {
 				$scope.dataToExport = $scope.data;
