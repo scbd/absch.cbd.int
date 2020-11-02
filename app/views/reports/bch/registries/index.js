9e7d549bed209c710268d4b57545c8372435ed5e
Tahmeed Shah <tahmeedshah@gmail.com>
2020-11-02 20:00:51 +0500

    CSS changes and error handling

--- app/views/reports/bch/registries/index.js
+++ app/views/reports/bch/registries/index.js
@@ -1,24 +1,30 @@
-define(['app', 'lodash', 'services/search-service', 'js/common',
-	'./index'], function(app, _) { 'use strict';
+define(['app', 'lodash', 'css!/app/css/registry.css', 'services/search-service', 'js/common','views/register/directives/register-top-menu',
+		'./index'], function(app, _) { 'use strict';
 
-	app.controller("registryReportController", ['$scope',    'searchService',  
-		function($scope,searchService) { 
-			var schemas = ["organism","modifiedOrganism","dnaSequence"];
-			schemas.forEach(function(schema){ 
-				loadRecords(schema);
-			}) 
-			function loadRecords(schema){ 
-				var searchQuery = {
-					fields:  'numFound',
-					query:  'schema_s:'+schema,
-					rowsPerPage:1
-				};
-				return searchService.list(searchQuery)
-					.then(function(result){  
-					  $scope[schema+"Records"] = result.data.response.numFound;    
-					}) 
+		app.controller("registryReportController", ['$scope', 'searchService', 'toastr', '$log',
+			function ($scope, searchService, toastr, $log) {
+				var schemas = ["organism", "modifiedOrganism", "dnaSequence"];
+				loadRecords();
+				function loadRecords(schema) {
+					var recordQuery = { 
+                        fields: 'schema_s',
+                        query: 'schema_s:(' + schemas.join(' ') + ')'
+                    };
+				 
+					return searchService.facets(recordQuery)
+						.then(function (result) { 
+							$scope.count = result.schema_s;
+						})
+					.catch(function(e){
+						toastr.error('There was an error running search query.')
+						var exception = {
+							data    :  e.data||e.message, status:e.status,
+							url     : (e.config||{}).url, params: (e.config||{}).params,
+							stack   : e.stack
+						}                                
+						$log.error(JSON.stringify(exception))
+					})
+				}
+			}]);
 
-			}
-		}]);
-
-});
+	});
