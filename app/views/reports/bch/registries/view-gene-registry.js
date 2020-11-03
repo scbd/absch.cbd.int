bd342828514150957e24afb8eaccebec77aa057c
Blaise Fonseca <blaise.fonseca@cbd.int>
2020-11-03 10:01:33 -0500

    make gene registry responsive

--- app/views/reports/bch/registries/view-gene-registry.js
+++ app/views/reports/bch/registries/view-gene-registry.js
@@ -1,18 +1,20 @@
 define(['app','css!/app/css/registry.css','services/search-service'], function(app) { 'use strict';
 
-app.controller("geneRegistryController", ['$scope','searchService','$element', '$timeout', 'toastr', '$log',
-function($scope,searchService,$element,$timeout, toastr, $log) { 
+app.controller("geneRegistryController", ['$scope','searchService','$element', '$timeout', '$rootScope', '$log',
+function($scope,searchService,$element,$timeout, $rootScope, $log) { 
 		$scope.isLoading = false;
 		$scope.isError = false;
-		loadRecords();
+		$scope.deviceSize = $rootScope.deviceSize;
+		
 		function loadRecords(){ 
+
 			$scope.isLoading = true;
-		var searchQuery = {
-			fields:  'recordId:uniqueIdentifier_s,name:name_s,trait:summary_t,donorOrganism:geneDonorOrganismCommonNames_EN_txt,geneFunction:biologicalFunction_EN_t,url_ss',
-			query:  'schema_s:dnaSequence',
-			rowsPerPage: 10000
-			
-		};
+			var searchQuery = {
+				fields:  'recordId:uniqueIdentifier_s,name:name_s,trait:summary_t,donorOrganism:geneDonorOrganismCommonNames_EN_txt,geneFunction:biologicalFunction_EN_t,url_ss',
+				query:  'schema_s:dnaSequence',
+				rowsPerPage: 10000
+				
+			};
 			return searchService.list(searchQuery)
 			.then(function(result){ 
 				$scope.geneRecords = [];
@@ -29,20 +31,23 @@ function($scope,searchService,$element,$timeout, toastr, $log) {
 			.finally(function(){
 				$scope.isLoading = false;
 			})					  
-	}
-	$scope.export = function () {
-		$scope.readyForExport = true;
-		require(['tableexport'], function () {
-			$element.find('#forExport').tableExport({
-				formats: ['xlsx'],
-				filename: 'GENE-registry',
+		}
+
+		$scope.export = function () {
+			$scope.readyForExport = true;
+			require(['tableexport'], function () {
+				$element.find('#forExport').tableExport({
+					formats: ['xlsx'],
+					filename: 'GENE-registry',
+				});
+				$element.find('.xlsx').click();
+				$timeout(function () {
+					$scope.readyForExport = false;
+				}, 200)
 			});
-			$element.find('.xlsx').click();
-			$timeout(function () {
-				$scope.readyForExport = false;
-			}, 200)
-		});
-	}
+		}
+
+		loadRecords();
 }]);
 
 });
