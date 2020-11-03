da78218e7da95808e5683a1aa5e5e91e4e223d9d
Blaise Fonseca <blaise.fonseca@cbd.int>
2020-11-03 14:14:30 -0500

    revised changes

--- app/views/forms/view/view-submission.directive.js
+++ app/views/forms/view/view-submission.directive.js
@@ -1,16 +1,35 @@
-﻿define(['app', "text!views/forms/view/view-submission.directive.html", 	'views/directives/record-options'], function (app, template) {
+﻿define(['app', "text!views/forms/view/view-submission.directive.html", 	'views/directives/record-options', 'services/solr', 'services/search-service'
+], function (app, template) {
 
-    app.directive("viewSubmission", [function () {
+    app.directive("viewSubmission", ['searchService', 'solr', function (searchService, solr) {
         return {
-            restrict   : "EAC",
+            restrict   : "EA",
             template: template ,
             replace    : true,
-            transclude : false,
             scope: {
                 document: "=ngModel",
                 locale  : "=",
                 target  : "@linkTarget",
                 hide	: "@"
+            },
+            link:function($scope){
+
+                $scope.$watch("document.notifications", function(newVal, oldVal)
+				{
+					if(newVal){
+						if(~($scope.notifications||[]).length || newVal!=oldVal){
+							var query = {
+								query: "symbol_s:(" + _.map(newVal, 'identifier').map(solr.escape).join(' ') + ')',
+								fields: "identifier_s,title_s,acronym_s,reference_s, symbol_s"
+							};
+							searchService.list(query).then(function(data){
+								$scope.notifications  = data;
+							});
+						}
+					}
+					else
+						$scope.notifications  = [];
+				});
             }
         };
     }]);
