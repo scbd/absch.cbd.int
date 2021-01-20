389f970ee6f94a6ae524e2c4dc08293653a9893c
matthew dias <matthew.dias@cbd.int>
2021-01-20 11:21:20 -0500

    redesign search

---
+++ app/views/search/directives/search-input.js
@@ -0,0 +1,14 @@
+﻿define(['text!./search-input.html', 'app', 'lodash', 'ngDialog'], function (template, app, _) {
+    'use strict';
+
+    app.directive('searchInput', ['$location', 'ngDialog', 'locale', function ($location, ngDialog, locale) {
+        return {
+            restrict: 'EA',
+            template: template,
+            scope: false,
+            link: function ($scope) {
+            }
+        }
+    }])
+
+});
---
+++ app/views/search/directives/search-sort.js
@@ -0,0 +1,14 @@
+﻿define(['text!./search-sort.html', 'app', 'lodash', 'ngDialog'], function (template, app, _) {
+    'use strict';
+
+    app.directive('searchSort', ['$location', 'ngDialog', 'locale', function ($location, ngDialog, locale) {
+        return {
+            restrict: 'EA',
+            template: template,
+            scope: false,
+            link: function ($scope) {
+            }
+        }
+    }])
+
+});
