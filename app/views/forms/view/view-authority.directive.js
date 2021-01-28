dd458cac5db17593e3c747d036f459eee80157d6
Tahmeed Shah <tahmeedshah@gmail.com>
2021-01-28 19:09:07 +0500

    display the Law record(s) linked to the CNA/SPCA

--- app/views/forms/view/view-authority.directive.js
+++ app/views/forms/view/view-authority.directive.js
@@ -1,8 +1,8 @@
-define(['app', "text!views/forms/view/view-authority.directive.html",
+define(['app','lodash', "text!views/forms/view/view-authority.directive.html",
 'views/directives/record-options','views/directives/party-status', 'services/app-config-service',
-'views/forms/view/directives/view-reference-records.directive', 'views/forms/directives/view-terms-hierarchy'], function (app, template) {
+'views/forms/view/directives/view-reference-records.directive', 'views/forms/directives/view-terms-hierarchy'], function (app, _, template) {
 
-	app.directive("viewAuthority", ["IStorage", "realm", function (storage, realm) {
+	app.directive("viewAuthority", ["IStorage", "realm","solr","searchService",'$routeParams', function (storage, realm,solr,searchService,$routeParams) {
 		return {
 			restrict   : "EAC",
 			template: template ,
@@ -17,12 +17,26 @@ define(['app', "text!views/forms/view/view-authority.directive.html",
 			},
 			link : function ($scope)
 			{
+				$scope.policyBasisForCompetencyRef =[];
+				function getCnaLawRecords() {
+					var query = {
+						query : 'schema_s:biosafetyLaw AND referenceRecord_ss:' + solr.escape( $routeParams.documentID ),
+					}
+					searchService.list( query ).then( function ( r ) {
+						if ( r.data.response.docs.length > 0 ) {
+							_.forEach( r.data.response.docs, function ( record ) {
+								$scope.policyBasisForCompetencyRef.push({ identifier:record.identifier_s });
+							});
+						}
+					}).catch( function ( error ) {
+						console.log( 'ERROR:', error );
+					});
+				}
 				if(realm.is('ABS'))		$scope.isABS=true;
 				else if(realm.is('BCH'))$scope.isBCH=true;
-
+				getCnaLawRecords();
 				$scope.contacts = undefined;
 				$scope.display = function(field) {
-
 					if(!$scope.hide) return true; //show all fields
 
 					return( $scope.hide.indexOf(field) >= 0 ? false : true);
