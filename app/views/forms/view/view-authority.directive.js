6de09eb30ab654070db7712c28dbbd2c3b544994
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2021-02-02 22:15:30 +0500

    display the Law record(s) linked to the CNA/SPCA (#468)
    * Display the BCH Law record(s) linked to the CNA/SPCA instead of selection

--- app/views/forms/view/view-authority.directive.js
+++ app/views/forms/view/view-authority.directive.js
@@ -1,6 +1,6 @@
-define(['app', "text!views/forms/view/view-authority.directive.html",
+define(['app','lodash', "text!views/forms/view/view-authority.directive.html",
 'views/directives/record-options','views/directives/party-status', 'services/app-config-service',
-'views/forms/view/directives/view-reference-records.directive', 'views/forms/directives/view-terms-hierarchy'], function (app, template) {
+'views/forms/view/directives/view-reference-records.directive', 'views/forms/directives/view-terms-hierarchy'], function (app, _, template) {
 
 	app.directive("viewAuthority", ["IStorage", "realm", function (storage, realm) {
 		return {
@@ -20,7 +20,12 @@ define(['app', "text!views/forms/view/view-authority.directive.html",
 				if(realm.is('ABS'))		$scope.isABS=true;
 				else if(realm.is('BCH'))$scope.isBCH=true;
 
-				$scope.contacts = undefined;
+				$scope.onReferencedRecordsDataFetch = function(data){
+					if(data && (data.authorities||{}).docs){
+						if(data.authorities.docs.length)
+							$scope.policyBasisForCompetencyRef = _.map(data.authorities.docs, 'identifier')
+					}
+				}
 				$scope.display = function(field) {
 
 					if(!$scope.hide) return true; //show all fields
