ce5a74a7b4c817a462e5a62382a5cd2ea9307c0b
Tahmeed Shah <tahmeedshah@gmail.com>
2021-02-14 00:39:09 +0500

    separate countries and regions at view

--- app/views/forms/view/bch/view-biosafety-expert.directive.js
+++ app/views/forms/view/bch/view-biosafety-expert.directive.js
@@ -1,6 +1,5 @@
-define(['app', "text!views/forms/view/bch/view-biosafety-expert.directive.html", 	'views/directives/record-options'], function (app, template) {
-
-app.directive("viewBiosafetyExpert", [function () {
+define(['app', 'lodash', 'text!views/forms/view/bch/view-biosafety-expert.directive.html', 	'views/directives/record-options','services/thesaurus-service'], function (app, _, template) {
+	app.directive("viewBiosafetyExpert", ["thesaurusService", function (thesaurusService) {
 	return {
 		restrict   : "EAC",
 		template: template ,
@@ -12,11 +11,18 @@ app.directive("viewBiosafetyExpert", [function () {
 			target  : "@linkTarget",
 			hide	: "@"
 		},
-		controller : ["$scope", function ($scope)
-		{
-			
-			
-			
+		controller : ["$scope", function ($scope){
+					$scope.$watch( "document.countryRegionsWorkedIn", function ( value ) {
+						if(value == undefined) return;
+						return thesaurusService.getDomainTerms('countries').then(function(countries) {
+							$scope.document.countriesWorkedIn = _.filter(value, function(country){
+								return _.find(countries, {identifier:country.identifier});
+							});
+							$scope.document.regionsWorkedIn = _.filter(value, function(region){
+								return !_.find(countries, {identifier:region.identifier});
+							});
+						});
+					});		
 			//====================
 			//
 			//====================
