6dbb543d4a56b55ae1de79de405b1a6da02b84cf
Tahmeed Shah <tahmeedshah@gmail.com>
2021-02-15 23:43:18 +0500

    make single list for both countries and regions

--- app/views/forms/view/bch/view-biosafety-expert.directive.js
+++ app/views/forms/view/bch/view-biosafety-expert.directive.js
@@ -1,5 +1,6 @@
-define(['app', 'lodash', 'text!views/forms/view/bch/view-biosafety-expert.directive.html', 	'views/directives/record-options','services/thesaurus-service'], function (app, _, template) {
-	app.directive("viewBiosafetyExpert", ["thesaurusService", function (thesaurusService) {
+define(['app', "text!views/forms/view/bch/view-biosafety-expert.directive.html", 	'views/directives/record-options'], function (app, template) {
+
+	app.directive("viewBiosafetyExpert", [function () {
 	return {
 		restrict   : "EAC",
 		template: template ,
@@ -11,18 +12,8 @@ define(['app', 'lodash', 'text!views/forms/view/bch/view-biosafety-expert.direct
 			target  : "@linkTarget",
 			hide	: "@"
 		},
-		controller : ["$scope", function ($scope){
-					$scope.$watch( "document.countryRegionsWorkedIn", function ( value ) {
-						if(value == undefined) return;
-						return thesaurusService.getDomainTerms('countries').then(function(countries) {
-							$scope.document.countriesWorkedIn = _.filter(value, function(country){
-								return _.find(countries, {identifier:country.identifier});
-							});
-							$scope.document.regionsWorkedIn = _.filter(value, function(region){
-								return !_.find(countries, {identifier:region.identifier});
-							});
-						});
-					});		
+		controller : ["$scope", function ($scope)
+			{		
 			//====================
 			//
 			//====================
