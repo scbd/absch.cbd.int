6f98cc444cecd3540ad13cae8a7eedb83ca95cff
Tahmeed Shah <tahmeedshah@gmail.com>
2021-02-25 01:00:29 +0500

    update document-selector at RA

--- app/views/forms/edit/bch/directives/edit-risk-assessment.directive.js
+++ app/views/forms/edit/bch/directives/edit-risk-assessment.directive.js
@@ -35,7 +35,23 @@ import "views/forms/view/bch/view-risk-assessment.directive";
 					}
 					
 				});
-				
+
+				$scope.onBuildQuery = function(searchText, schema){
+					var queryOptions = {
+						schemas	  : [schema],
+						searchText: searchText
+					}
+					if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+						queryOptions.government = $scope.document.government.identifier;
+					}
+
+					if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+						queryOptions.identifier = $scope.document.header.identifier;
+					}
+
+					return $scope.onBuildDocumentSelectorQuery(queryOptions);
+				}
+
 				//==================================
 				//
 				//==================================
