5339e0b5d31ca214911519c3309e5c110387049a
Tahmeed Shah <tahmeedshah@gmail.com>
2021-02-25 11:17:45 +0500

    update document-selector at GENE

--- app/views/forms/edit/bch/directives/edit-dna-sequence.directive.js
+++ app/views/forms/edit/bch/directives/edit-dna-sequence.directive.js
@@ -58,7 +58,22 @@ import 'views/forms/directives/traits-selector.directive';
 						
 					return $scope.sanitizeDocument(document);
 				};
+				$scope.onBuildOrganismQuery = function(searchText, tab){
+					
+                    var queryOptions = {
+						schemas	  : ['organism'],
+                        searchText: searchText
+                    }
+					if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+                        queryOptions.government = $scope.document.government.identifier;
+                    }
 
+                    if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+                        queryOptions.identifier = $scope.document.header.identifier;
+                    }
+					
+					return $scope.onBuildDocumentSelectorQuery(queryOptions);
+                }
 				$scope.setDocument({}, true)
 				.then(function(doc){
 					
