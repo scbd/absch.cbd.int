199813cb97fa5f52a9eb89e88b2da9de28f65c75
Tahmeed Shah <tahmeedshah@gmail.com>
2021-02-25 11:06:29 +0500

    update document-selector at EXP

--- app/views/forms/edit/bch/directives/edit-biosafety-expert.directive.js
+++ app/views/forms/edit/bch/directives/edit-biosafety-expert.directive.js
@@ -70,7 +70,24 @@ import 'services/main';
 								});
 					}),
 				});
-                
+                $scope.onContactQuery = function(searchText, tab){
+					var queryOptions = {
+						schemas	  : ['contact'],
+						contactType: 'person',
+						searchText: searchText
+					}
+		
+					if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+						queryOptions.government = $scope.document.government.identifier;
+					}
+		
+					if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+						queryOptions.identifier = $scope.document.header.identifier;
+					}
+		
+					return $scope.onBuildDocumentSelectorQuery(queryOptions);
+		
+				}
                 $scope.setTab = function () {
                     $scope.tab = 'edit';
                 };
