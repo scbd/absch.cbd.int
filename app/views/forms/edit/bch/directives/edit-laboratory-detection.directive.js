ad9e31f000f9d027047cff25baec0fb5fb1ca430
Tahmeed Shah <tahmeedshah@gmail.com>
2021-02-25 22:34:12 +0500

    update document-selector at LAB

--- app/views/forms/edit/bch/directives/edit-laboratory-detection.directive.js
+++ app/views/forms/edit/bch/directives/edit-laboratory-detection.directive.js
@@ -60,6 +60,26 @@ import 'views/forms/directives/view-terms-hierarchy';
 				//==================================
 				//
 				//==================================
+
+
+				$scope.onBuildQuery = function(searchText, tab, schemaVal){
+					
+                    var queryOptions = {
+						schemas	  : [schemaVal],
+                        searchText: searchText
+                    }
+					if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+                        queryOptions.government = $scope.document.government.identifier;
+                    }
+
+                    if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+                        queryOptions.identifier = $scope.document.header.identifier;
+                    }
+					
+					return $scope.onBuildDocumentSelectorQuery(queryOptions);
+                }
+				
+
 				$scope.getCleanDocument = function(document) {
 
 					document = document || $scope.document;
