36827bb82ade0894f5c694c24f270fd8f2128efe
Tahmeed Shah <tahmeedshah@gmail.com>
2021-02-25 23:34:34 +0500

    update document-selector at VLR

--- app/views/forms/edit/edit-resource-schema-base-directive.js
+++ app/views/forms/edit/edit-resource-schema-base-directive.js
@@ -181,6 +181,41 @@ import 'views/forms/edit/organization-selector';
 
 			    };
 
+				$scope.onBuildQuery = function(searchText, schema){
+					var queryOptions = {
+						schemas	  : [schema],
+						searchText: searchText
+					}
+					if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+						queryOptions.government = $scope.document.government.identifier;
+					}
+
+					if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+						queryOptions.identifier = $scope.document.header.identifier;
+					}
+
+					return $scope.onBuildDocumentSelectorQuery(queryOptions);
+				}
+
+				$scope.onContactQuery = function(searchText, tab){
+					var queryOptions = {
+						schemas	  : ['contact'],
+						contactType: 'person',
+						searchText: searchText
+					}
+		
+					if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+						queryOptions.government = $scope.document.government.identifier;
+					}
+		
+					if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+						queryOptions.identifier = $scope.document.header.identifier;
+					}
+		
+					return $scope.onBuildDocumentSelectorQuery(queryOptions);
+		
+				}
+
 				//==================================
 				//
 				//==================================
