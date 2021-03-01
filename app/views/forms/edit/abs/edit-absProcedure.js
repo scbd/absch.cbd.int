8e91b7e5e25ce300dd0008dbc4ac1fda08ccc150
Tahmeed Shah <tahmeedshah@gmail.com>
2021-03-01 17:08:01 +0500

    update document-selector at PRO

--- app/views/forms/edit/abs/edit-absProcedure.js
+++ app/views/forms/edit/abs/edit-absProcedure.js
@@ -44,6 +44,37 @@ define(['app','lodash', 'views/forms/edit/edit',
             });
         };
 
+        $scope.onContactQuery = function(searchText, tab){
+            var queryOptions = {
+                schemas	  : ['authority', 'contact'],
+                searchText: searchText
+            }
+            if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+                queryOptions.government = $scope.document.government.identifier;
+            }
+
+            if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+                queryOptions.identifier = $scope.document.header.identifier;
+            }
+
+            return $scope.onBuildDocumentSelectorQuery(queryOptions);
+        }
+
+        $scope.onMeaureQuery = function(searchText, tab){
+            var queryOptions = {
+                schemas	  : ['measure'],
+                searchText: searchText
+            }
+            if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+                queryOptions.government = $scope.document.government.identifier;
+            }
+
+            if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+                queryOptions.identifier = $scope.document.header.identifier;
+            }
+
+            return $scope.onBuildDocumentSelectorQuery(queryOptions);
+        }
         //==================================
         //
         //==================================
