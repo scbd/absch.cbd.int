1e86584980b8b156ed55b239f17f3201a1bed32d
Tahmeed Shah <tahmeedshah@gmail.com>
2021-03-01 16:50:37 +0500

    update document-selector at CP

--- app/views/forms/edit/abs/edit-absCheckpoint.js
+++ app/views/forms/edit/abs/edit-absCheckpoint.js
@@ -41,6 +41,22 @@ define(['app', 'lodash', 'views/forms/edit/edit',
             });
         };
 
+        $scope.onContactQuery = function(searchText, tab){
+            var queryOptions = {
+                schemas	  : ['contact', 'authority'],
+                searchText: searchText
+            }
+            if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+                queryOptions.government = $scope.document.government.identifier;
+            }
+
+            if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+                queryOptions.identifier = $scope.document.header.identifier;
+            }
+
+            return $scope.onBuildDocumentSelectorQuery(queryOptions);
+        }
+
         //==================================
         //
         //==================================
