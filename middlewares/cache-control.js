1cd19ebe8b3489d62ce37565b060f956b5d0d7da
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-03-01 11:15:39 -0500

    hash files req cache for 1 yr

--- middlewares/cache-control.js
+++ middlewares/cache-control.js
@@ -8,6 +8,10 @@ function setCustomCacheControl(res, path) {
 	let versionWrong = false;
 	let versionMatch = false;
     let localhostRegex = /^http:\/\/localhost:([0-9]{4})\//;
+    let hashFileRegex  = /\.[a-z0-9]{8}\./i
+    
+    const isHashFile = hashFileRegex.test(res.req.originalUrl);
+
     if(res.req.headers && !localhostRegex.test(res.req.headers.referer)){
         versionWrong |= res.req.query && res.req.query.v && res.req.query.v!=global.app.version;
         versionWrong |= res.req.cookies && res.req.cookies.VERSION && res.req.cookies.VERSION!=global.app.version;
@@ -15,7 +19,7 @@ function setCustomCacheControl(res, path) {
         versionMatch |= res.req.cookies && res.req.cookies.VERSION && res.req.cookies.VERSION==global.app.version;
     }
 
-	if(versionWrong || !versionMatch)
+	if(!isHashFile && (versionWrong || !versionMatch))
 		return res.setHeader('Cache-Control', 'public, max-age=0');
 
 	res.setHeader('Cache-Control', `public, max-age=${oneYear}`);
