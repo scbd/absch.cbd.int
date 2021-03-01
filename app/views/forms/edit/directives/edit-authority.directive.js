cbbbc19c9e07bbc6af40801e0656a7878a6c0248
Tahmeed Shah <tahmeedshah@gmail.com>
2021-03-01 20:46:48 +0500

    update document-selector at CNA

--- app/views/forms/edit/directives/edit-authority.directive.js
+++ app/views/forms/edit/directives/edit-authority.directive.js
@@ -145,16 +145,40 @@ import 'views/forms/view/view-authority.directive';
                 $scope.onContactQuery = function(searchText, tab){
                     var queryOptions = {
                         realm     : realm.value,
-                        identifier: $scope.document.header.identifier,
-                        government: $scope.document.government.identifier,
                         searchText: searchText
                     }
+                    if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+						queryOptions.government = $scope.document.government.identifier;
+					}
+		
+					if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+						queryOptions.identifier = $scope.document.header.identifier;
+					}
 
                     if($scope.isAbs)
                         queryOptions.schemas = ['contact', 'authority']
                     else
                         queryOptions.schemas = ['contact']
 
+                    return $scope.onBuildDocumentSelectorQuery(queryOptions);
+
+                }
+
+
+                $scope.onMeasureQuery = function(searchText, tab){
+                    var queryOptions = {
+                        schemas     : ['measure'],
+                        searchText  : searchText
+                    }
+                    if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+						queryOptions.government = $scope.document.government.identifier;
+					}
+		
+					if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+						queryOptions.identifier = $scope.document.header.identifier;
+					}
+
+                   
                     return $scope.onBuildDocumentSelectorQuery(queryOptions);
 
                 }
