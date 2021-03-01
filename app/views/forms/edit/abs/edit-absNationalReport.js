81401c67ec5f4bb54fd0f20a9a4f902ab77da2e2
Tahmeed Shah <tahmeedshah@gmail.com>
2021-03-01 23:18:43 +0500

    update document-selector at absNationalRecord

--- app/views/forms/edit/abs/edit-absNationalReport.js
+++ app/views/forms/edit/abs/edit-absNationalReport.js
@@ -25,7 +25,38 @@ define(['app', 'lodash', 'views/forms/edit/edit' , 'views/forms/edit/document-se
     });
 
 
+    $scope.onRecordQuery = function(searchText, tab){
 
+      var queryOptions = {
+        schemas	  : ['absPermit', 'absCheckpoint', 'absCheckpointCommunique', 'authority', 'measure', 'database', 'focalPoint'],
+        searchText: searchText
+      }
+      if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+        queryOptions.government = $scope.document.government.identifier;
+      }
+
+      if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+        queryOptions.identifier = $scope.document.header.identifier;
+      }
+
+      return $scope.onBuildDocumentSelectorQuery(queryOptions);
+    }
+    $scope.oncontactQuery = function(searchText, tab){
+
+      var queryOptions = {
+        schemas	  : ['contact', 'authority', 'focalPoint'],
+        searchText: searchText
+      }
+      if($scope.document != undefined && $scope.document.government != undefined && $scope.document.government.identifier != undefined){
+        queryOptions.government = $scope.document.government.identifier;
+      }
+
+      if( $scope.document != undefined && $scope.document.header != undefined && $scope.document.header.identifier != undefined){
+        queryOptions.identifier = $scope.document.header.identifier;
+      }
+
+      return $scope.onBuildDocumentSelectorQuery(queryOptions);
+    }
     //==================================
     //
     //==================================
