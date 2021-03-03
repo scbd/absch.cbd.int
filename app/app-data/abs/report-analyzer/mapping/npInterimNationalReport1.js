5965a36153d953432948542da7ec2c1065176d7a
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-03-03 11:48:30 -0500

    bring back .json files required for translation

--- app/app-data/abs/report-analyzer/mapping/npInterimNationalReport1.js
+++
@@ -1,9 +0,0 @@
-define(function () { 'use strict';
-
-  var npInterimNationalReport1 = {
-  	schema: "npInterimNationalReport1"
-  };
-
-  return npInterimNationalReport1;
-
-});
--- app/app-data/bch/report-analyzer/mapping/cpbNationalReport2.js
+++
@@ -1,9 +0,0 @@
-define(function () { 'use strict';
-
-  var cpbNationalReport2 = {
-  	schema: "cpbNationalReport2"
-  };
-
-  return cpbNationalReport2;
-
-});
