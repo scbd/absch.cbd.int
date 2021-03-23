f7a9fa22203e6efd219139e7ee44306718396529
Blaise Fonseca <blaisefonseca@hotmail.com>
2021-03-23 09:28:43 -0400

    Small fix (#487)
    * add missing var for variables
    
    * fix securize for national-roles paths
    
    * fix  '!important'
     at template.css
    
    * change lodash fn
    
    * fix paths
    
    * fix
    
    * regex for windows path
    
    * fix path
    
    * name contains with includes
    
    * fix condition
    
    * declare variable type
    
    Co-authored-by: Tahmeed Shah <tahmeedshah@gmail.com>

--- app/routes/mixin.js
+++ app/routes/mixin.js
@@ -51,49 +51,73 @@ export function currentUser() {
   }];
 }
 
-export function securize(roleList, useSchemaRoles, checkEmailVerified) {
-  return ["$location", "authentication", "appConfigService", "$filter", "$route", "realm", function ($location, authentication, appConfigService, $filter, $route, realm) {
-    return authentication.getUser().then(function (user) {
-      if (checkEmailVerified && user.isAuthenticated && !user.isEmailVerified) {
-        $location.path(appConfigService.getSiteMapUrls().user.verifyEmail);
-        return;
-      }
-      var roles = _.clone(roleList || []);
-      if (roles && !_.isEmpty(roles)) {
-        roles = _.flatten(_.map(roles, function (role) {
-          return realm.getRole(role);
-        }));
-      }
-      if (useSchemaRoles) {
-        var path = $location.path().replace("/register/", "");
-        var schema;
-        if (path.indexOf("/") > 0) schema = path.substr(0, path.indexOf("/")); else schema = path;
-        var schemaName = $filter("mapSchema")(schema);
-        var appSchemas = [].concat(realm.nationalSchemas, realm.referenceSchemas).concat(realm.scbdSchemas);
-        if (_.intersection(realm.referenceSchemas, appSchemas).length) {
-          roles = (roles || []).concat(realm.schemaRoles(schemaName));
-        } else {
-          roles = (roles || []).concat(realm.nationalRoles());
-        }
-        roles = (roles || []).concat(realm.getRole("Administrator") || []);
-      }
-      if (!user.isAuthenticated) {
-        console.log("securize: force sign in");
-        if (!$location.search().returnUrl) $location.search({
-          returnUrl: $location.url()
+export function securize(roleList, useSchemaRoles, checkEmailVerified, userNationalRoles)
+{
+    return ["$location", "authentication", "appConfigService", "$filter", "$route", "realm",
+      function ($location, authentication, appConfigService, $filter, $route, realm) {
+
+        return authentication.getUser().then(function (user) {
+
+            if(checkEmailVerified && user.isAuthenticated && !user.isEmailVerified){
+                $location.path(appConfigService.getSiteMapUrls().user.verifyEmail);
+                return;
+            }
+
+            var roles = _.clone(roleList||[]);
+
+            if (roles && !_.isEmpty(roles)) {
+                roles = _.flatten(_.map(roles, function(role){ return realm.getRole(role)}));
+            }
+            if(useSchemaRoles || userNationalRoles){
+                var path = $location.path().replace('/register/','');
+                var schema;
+
+                if(path.indexOf('/')>0)
+                    schema = path.substr(0, path.indexOf('/'));
+                else
+                    schema = path;
+
+                if(userNationalRoles){
+                    roles = (roles || []).concat(realm.nationalRoles());
+                }
+                else{
+                    var schemaName = $filter('mapSchema')(schema);
+                    var appSchemas = ([]).concat(realm.nationalSchemas, realm.referenceSchemas).concat(realm.scbdSchemas);
+
+                    if(_.intersection(realm.referenceSchemas, appSchemas).length){
+                        roles = (roles || []).concat(realm.schemaRoles(schemaName));
+                    }
+                    else{
+                        roles = (roles || []).concat(realm.nationalRoles());
+                    }
+                }
+
+                roles = (roles || []).concat(realm.getRole("Administrator")||[]);
+            }
+            if (!user.isAuthenticated) {
+
+                console.log("securize: force sign in");
+
+                if (!$location.search().returnUrl)
+                    $location.search({ returnUrl: $location.url() });
+
+                $location.path(appConfigService.getSiteMapUrls().user.signIn);
+                // throw "Force sign-in";
+
+            }
+            else if ((userNationalRoles && !user.government) || (roles && !_.isEmpty(roles) && _.isEmpty(_.intersection(roles, user.roles)))){
+
+                console.log("securize: not authorized");
+
+                $location.search({ path: $location.url() });
+                $location.path(appConfigService.getSiteMapUrls().errors.notAuthorized);
+            }
+
+            return user;
+        })
+        .catch(function(e){
+            console.log(e)
+            $location.path(appConfigService.getSiteMapUrls().errors.notAuthorized);
         });
-        $location.path(appConfigService.getSiteMapUrls().user.signIn);
-      } else if (roles && !_.isEmpty(roles) && _.isEmpty(_.intersection(roles, user.roles))) {
-        console.log("securize: not authorized");
-        $location.search({
-          path: $location.url()
-        });
-        $location.path(appConfigService.getSiteMapUrls().errors.notAuthorized);
-      }
-      return user;
-    }).catch(function (e) {
-      console.log(e);
-      $location.path(appConfigService.getSiteMapUrls().errors.notAuthorized);
-    });
-  }];
+    }];
 }
--- app/views/forms/edit/edit-capacityBuildingInitiative.js
+++ app/views/forms/edit/edit-capacityBuildingInitiative.js
@@ -1,7 +1,7 @@
 import app from 'app';
 import _ from 'lodash';
 import 'views/forms/edit/edit';
-import '.views/forms/edit/view/view-capacity-building-initiative.directive';
+import 'views/forms/view/view-capacity-building-initiative.directive';
 import 'views/forms/edit/organization-selector';
 import 'services/main';
 
