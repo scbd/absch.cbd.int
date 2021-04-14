6526981db5f3c11aa43146e62465a61173c4d52b
Tahmeed Shah <tahmeedshah@gmail.com>
2021-04-14 23:01:42 +0500

    update terms-hierarchy at SUB

--- app/views/forms/view/view-submission.directive.js
+++ app/views/forms/view/view-submission.directive.js
@@ -18,28 +18,19 @@ import 'views/forms/directives/view-terms-hierarchy';
             },
             link:function($scope){
                 $scope.onThematicAreasTerms = function(terms){
-                    if(($scope.document||{}).cpbThematicAreas){
-                      var parentIds = ['B18CE475-8D23-4DEC-A9F1-13F0243C9233',
-                            '5A1A07F7-FF4B-4302-AC9F-711DA47B215B',
-                            '837523BF-6ED8-42FC-A39D-197442BD736B',
-                            'F4B40E3C-6804-43E1-AF68-BCE9C2ACF7ED',
-                            '0CBE5BD3-84C8-4197-A39A-360EC8841631',
-                            '2A6F3EC5-775B-477C-A88E-80CE7DF522E0',
-                            'CC16EA96-352B-4A31-ADBE-49C2DD912569'] ;
-                             _.forEach(parentIds, function(item) {
-                            var term = _.find(terms, {identifier: item})
-                            if (term) {
-
-                            var cpbThematicAreas = $scope.document.cpbThematicAreas;
-                             if (_(cpbThematicAreas).map('identifier').intersection(term.narrowerTerms).value().length == term.narrowerTerms.length) {
-                                terms = _.filter(terms, function (t) {
-                                    return !_.includes(term.narrowerTerms, t.identifier)
-                                })
-                            }
-                            }
-                        })
-                        return terms;
-                    }
+									if(($scope.document||{}).cpbThematicAreas){
+										_.forEach(terms, function(item){
+											if(item.broaderTerms.length == 0 || item.broaderTerms == []){
+												var parent =_.find($scope.document.cpbThematicAreas, {identifier: item.identifier});
+												if(parent){
+													terms = _.filter(terms, function(t){
+														return !_.includes(item.narrowerTerms, t.identifier)
+													})
+												}
+											}
+										});
+										return terms;
+									}
                 }
 
                 $scope.$watch("document.notifications", function(newVal, oldVal)
