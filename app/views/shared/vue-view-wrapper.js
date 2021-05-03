408fd8e9a030260e4c2e343b712576e7e892b773
Blaise Fonseca <blaisefonseca@hotmail.com>
2021-05-03 15:32:32 -0400

    enable vue integration (#503)
    * enable vue integration
    
    * Update app/views/shared/vue-view-wrapper.js
    
    Co-authored-by: Stephane Bilodeau <sbilodeau@users.noreply.github.com>
    
    Co-authored-by: Stephane Bilodeau <sbilodeau@users.noreply.github.com>

--- app/views/shared/vue-view-wrapper.js
+++ app/views/shared/vue-view-wrapper.js
@@ -1,11 +1,12 @@
 import 'angular-vue'
-import require from 'require'
 import VueI18n from 'vue-i18n'
+import 'components/scbd-branding/main';
 
 export { default as template } from './vue-view-wrapper.html';
 
 export default ['$scope', 'apiToken', '$route', 'component', function ($scope, apiToken, $route, component) {
-console.log(component);
+
+  component = component.default || component;
 
   $scope.tokenReader = function(){ return apiToken.get()}
   $scope.route       = { params : $route.current.params }
