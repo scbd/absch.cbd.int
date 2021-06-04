1b836d70d54ac61ce20498b1a889f5dde8abed1d
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-06-04 13:20:10 -0400

    remove aichiTargets from hierarchy

--- app/views/forms/view/view-capacity-building-initiative.directive.js
+++ app/views/forms/view/view-capacity-building-initiative.directive.js
@@ -71,21 +71,6 @@ app.directive("viewCapacityBuildingInitiative", [function () {
 				}
 			}
 
-			$scope.onAichiTargetsTerms = function(terms){
-				if(($scope.document||{}).aichiTargets){
-					_.forEach(terms, function(item){
-						if(item.broaderTerms.length == 0 || item.broaderTerms == []){
-							var parent =_.find($scope.document.aichiTargets, {identifier: item.identifier});
-							if(parent){
-								terms = _.filter(terms, function(t){
-									return !_.includes(item.narrowerTerms, t.identifier)
-								})
-							}
-						}
-					});
-					return terms;
-				}
-			}
 			//====================
 			//
 			//====================
