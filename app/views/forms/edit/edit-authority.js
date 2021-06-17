58c6ea4e7bdc01b629d76b170d526d3e89c3f509
Tahmeed <tahmeedshah@gmail.com>
2021-06-17 23:10:01 +0500

    wip-update path

--- app/views/forms/edit/edit-authority.js
+++ app/views/forms/edit/edit-authority.js
@@ -2,7 +2,10 @@ import app from 'app';
 import 'views/forms/edit/directives/edit-authority.directive';
 import 'views/forms/edit/edit';
 import  'angular-vue';
-import exportDetails from "components/embed/export-details.vue";
+// import exportDetails from "components/export-details.vue";
+// import exportDetails from "app/components/export-details.vue";
+import exportDetails from "~/components/export-details.vue";
+
 
     export { default as template } from './edit-authority.html';
 
