29a3b6ab8d3e54cd09102775a69b83e4264a35e2
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2021-06-23 23:04:38 +0500

    Update reference records (#525)
    * create json file for reference-records
    
    * update reference-record for json file
    
    * update view-reference-records
    
    * update table width
    
    * update view-reference-records & BCH json file
    
    * reduce the title lenght
    
    * update json file paths
    
    * update view-reference-record table
    
    * ABS- create a json file for linked-records
    
    * update table class
    
    * add sorting and other feature
    
    * fix sort
    
    Co-authored-by: Blaise Fonseca <blaise.fonseca@cbd.int>

--- app/views/forms/view/directives/view-reference-records.directive.js
+++ app/views/forms/view/directives/view-reference-records.directive.js
@@ -16,18 +16,21 @@ app.directive("viewReferencedRecords", [function () {
 			target: "@linkTarget",
 			onDataFetch: "&?"
 		},
-		controller: ["$scope", "solr", '$q', 'searchService', 'realm', function ($scope, solr, $q, searchService, realm) {
+		controller: ["$scope", "solr", '$q', 'searchService', 'realm', 'commonjs', function ($scope, solr, $q, searchService, realm, commonjs) {
 
+			$scope.sortField='updatedDate_dt';
+			$scope.sortSequence='desc'
 			// //==================================
-		    // //
-		    // //==================================
-		    $scope.$watch('model', function(newValue, oldValue){
-		        if(newValue){
+			// //
+			// //==================================
+			$scope.$watch('model', function(newValue, oldValue){
+				if(newValue){
 
 					var searchQuery = {
 						rowsPerPage:5000,
 						query 	: "referenceRecord_ss:" + solr.escape($scope.model),
-						fields	: 'title:title_EN_t, referenceRecord_ss, referenceRecord_info_ss,schemaCode:schema_s,schema:schema_EN_s, identifier:identifier_s, uniqueId:uniqueIdentifier_s, government_s,government:government_EN_s,'
+						fields	: 'updatedOn:updatedDate_dt,title:title_EN_t, referenceRecord_ss, referenceRecord_info_ss,schemaCode:schema_s,schema:schema_EN_s, identifier:identifier_s, uniqueId:uniqueIdentifier_s, government_s,government:government_EN_s,schemaType:schemaType_s,',
+						sort	: 'updatedDate_dt desc'
 					}
 					if(realm.is('BCH')){
 						searchQuery.fields += 'scopeRelease_b,scopeConfined_b,scopeFood_b,scopeFeed_b,scopeProcessing _b,scopeOther_b,scopePharmaceutical_b,scopeTransit_b,traitsDiseasesResistance_b,traitsHerbicidesResistance_b,traitsPhysiologyChanges_b,traitsQualityChanges_b,traitsMedicalProduction_b,traitsOther_b'
@@ -35,49 +38,67 @@ app.directive("viewReferencedRecords", [function () {
 					$q.when(searchService.list(searchQuery))
 					.then(function(data) {
 
-						if(data.data.response.docs.length > 0){							
-							_.forEach(data.data.response.docs, function(record){
-								_.forEach(record.referenceRecord_info_ss, function(info){
-									info = JSON.parse(info);
-									_.forEach(info.identifiers, function(identifier){
-										if(removeRevisonNumber(identifier) == $scope.model){
-											if(record.schemaCode=='modifiedOrganism' || record.schemaCode=='nationalRiskAssessment' || record.schemaCode=='independentRiskAssessment')
-												record.showIcons = true;
-											if(!$scope.referenceRecords)
-												$scope.referenceRecords = {};
+						if(data.data.response.docs.length > 0){
+							return getFieldTitles().then(fieldTitles=>{
+								_.forEach(data.data.response.docs, function(record){
+									_.forEach(record.referenceRecord_info_ss, function(info){
+										info = JSON.parse(info);
+										_.forEach(info.identifiers, function(identifier){
+											if(removeRevisonNumber(identifier) == $scope.model){
+												if(record.schemaCode=='modifiedOrganism' || record.schemaCode=='nationalRiskAssessment' || record.schemaCode=='independentRiskAssessment')
+													record.showIcons = true;
+												if(!$scope.referenceRecords)
+													$scope.referenceRecords = {};
 
-											if(!$scope.referenceRecords[record.schemaCode])
-												$scope.referenceRecords[record.schemaCode] = {
-													schema:record.schema,
-													fields : {}
-												};
+												if(!$scope.referenceRecords[record.schemaCode]){
+													$scope.referenceRecords[record.schemaCode] = {
+														schema	  : record.schema,
+														fields 	  : {},
+														schemaType: record.schemaType
+													};
+												}
 
-											$scope.referenceRecords[record.schemaCode].fields[info.field] = $scope.referenceRecords[record.schemaCode].fields[info.field] || {count:0, docs:[], schema:record.schema}
-											
-											$scope.referenceRecords[record.schemaCode].fields[info.field].count += 1;
-											$scope.referenceRecords[record.schemaCode].fields[info.field].docs.push(record)
-										}
-									})
-								});								
+												const fieldTitle = fieldTitles[record.schemaCode+"."+info.field] || info.field;														
+												$scope.referenceRecords[record.schemaCode].fields[fieldTitle] = $scope.referenceRecords[record.schemaCode].fields[fieldTitle] || {count : 0, docs : [], schema : record.schema};
+												$scope.referenceRecords[record.schemaCode].fields[fieldTitle].count += 1;
+												$scope.referenceRecords[record.schemaCode].fields[fieldTitle].docs.push( record );
+											}
+										});
+									});
+								})
+								if(typeof $scope.onDataFetch == 'function'){
+									$scope.onDataFetch({data:$scope.referenceRecords})
+								}
+								
 							})
-							if(typeof $scope.onDataFetch == 'function'){
-								$scope.onDataFetch({data:$scope.referenceRecords})
-							}
 						}
 					});
 
-		        }
+				}
 			});
-			
+
 			$scope.encode = function(query){
 				return encodeURIComponent(query);
+			}
+
+			function getFieldTitles(){
+				let isABS = realm.is('ABS');
+				let file = 'app-data/bch/linked-records-field-titles.json';
+				if(isABS)
+					file = 'app-data/abs/linked-records-field-titles.json';
+				return commonjs.loadJsonFile(file)
 			}
 
 			function removeRevisonNumber(identifier){
 				return identifier.replace(/@[0-9]+$/, '');
 			}
 
-		 }] //controller
+			$scope.toggleTitle = function(event){
+				event.target.classList.toggle("truncate-record");
+			}
+
+		}] //controller
 	};
 }]);
+
 
