e870b2b455bebbfb7a8b616841d1e6baf2c31a3b
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-07-15 06:11:03 -0400

    KB categories

---
+++ app/app-data/kb-categories.json.js
@@ -0,0 +1,47 @@
+
+export default [
+    { 
+        "title"    : "About",
+        "adminTags": ["about"]
+    },
+    { 
+        "title"    : "Getting started",
+        "adminTags": ["getting-started"]
+    },
+    { 
+        "title"    : "Submitting information",
+        "adminTags": ["submitting-information"]
+    },
+    { 
+        "title"    : "Finding information",
+        "adminTags": ["finding-information"]
+    },
+    { 
+        "title"    : "LMOs/Genetic elements/Organisms",
+        "adminTags": ["lmo-gene-orga"]
+    },
+    { 
+        "title"    : "Monitoring",
+        "adminTags": ["monitoring"]
+    },
+    { 
+        "title"    : "Interoperability",
+        "adminTags": ["interoperability"]
+    },
+    { 
+        "title"    : "Informal Advisory Committee (IAC)",
+        "adminTags": ["iac", "meetings"]
+    },
+    { 
+        "title"    : "Getting help",
+        "adminTags": ["getting-help"]
+    },
+    { 
+        "title"    : "Frequently Asked Questions (FAQs)",
+        "adminTags": ["faq"]
+    }
+]
+
+
+
+
