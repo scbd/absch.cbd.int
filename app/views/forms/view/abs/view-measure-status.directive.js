a968bc1052db7ac5fd5c6d4ff443a2a4afaf2c8b
Tahmeed <tahmeedshah@gmail.com>
2021-08-10 09:00:02 +0500

    create SMSR view page

---
+++ app/views/forms/view/abs/view-measure-status.directive.js
@@ -0,0 +1,36 @@
+import app from "app";
+import template from "text!./view-measure-status.directive.html";
+import 'views/directives/record-options';
+import 'views/forms/view/directives/view-record-reference.directive';
+
+	app.directive("viewMeasureStatus", [function () {
+
+		return {
+			restrict   : "EAC",
+			template: template ,
+			replace    : true,
+			transclude : false,
+			scope: {
+				document: "=ngModel",
+				locale  : "=",
+				target  : "@linkTarget",
+				allowDrafts : "@",
+				hide:"@"
+			},
+			controller : ["$scope", 'realm', function ($scope, realm)
+			{
+				//====================
+				//
+				//====================
+				$scope.display = function(field) {
+
+					if(!$scope.hide) return true; //show all fields
+
+					return( $scope.hide.indexOf(field) >= 0 ? false : true);
+				}
+
+			}]
+		};
+	}]);
+
+
