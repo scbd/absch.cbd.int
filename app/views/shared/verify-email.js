3d08e3c8b0a1735bb1570e4e93f43c700185e7b1
Blaise Fonseca <blaisefonseca@hotmail.com>
2021-08-20 10:23:08 -0400

    fix recursive reference

--- app/views/shared/verify-email.js
+++ app/views/shared/verify-email.js
@@ -1,8 +1,5 @@
 import app from "app";
 
-import { default as template } from './verify-email';
-
-
 export { default as template } from './verify-email.html';
 
 export default ["$scope",function ($scope) {
