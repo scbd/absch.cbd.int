f834b51906a878ab775172ea9b8d1d6fed8c4619
Blaise Fonseca <blaisefonseca@hotmail.com>
2021-08-24 12:38:36 -0400

    convert identifier to string

--- app/views/search/search-directive.js
+++ app/views/search/search-directive.js
@@ -993,7 +993,7 @@ import 'views/reports/matrix/data-matrix.directive';
                                             subQuery = q;
                                     }
                                     else if(!_.isEmpty(filter.selectedItems)){
-                                        var ids = _.map(filter.selectedItems, s=>s.identifier.replace(/\@[0-9]{1,3}$/, ''));
+                                        var ids = _.map(filter.selectedItems, s=>s.identifier.toString().replace(/\@[0-9]{1,3}$/, ''));
                                         if(filter.type == 'freeText'){                                          
                                             ids = _.map(filter.selectedItems,  function(filter){
                                                 return {name:filter.title};
