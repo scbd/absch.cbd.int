a300e205cc2bb68029a0acb808db1219ac54f28f
Tahmeed <tahmeedshah@gmail.com>
2021-09-28 01:03:04 +0500

    update share component

--- app/components/common/share-record.vue
+++ app/components/common/share-record.vue
@@ -18,11 +18,13 @@
                     <div class="col-md-3">{{$t('embed')}}</div>
                     <div class="col-md-9">{{$t('send')}}</div>
                 </div>
-                <div class="row">
-                    <div class="col-md-3"><a @click="shareType='embed'" :disabled="loading"><i class="fa fa-code fa-lg" aria-hidden="true"></i></a></div>
+                <div class="row icon-list">
+                    <div class="col-md-3" v-bind:class="{ selected: shareType=='embed'}">
+                        <a @click="shareType='embed'" :disabled="loading"><i class="fa fa-code fa-lg" aria-hidden="true"></i></a>
+                    </div>
                     <div class="col-md-9">
                         <div class="row">
-                            <div class="col-md-3">
+                            <div class="col-md-3" v-bind:class="{ selected: shareType=='email'}">
                                 <a @click="shareType='email'" :disabled="loading"><i class="fa fa-envelope-o fa-lg" aria-hidden="true"></i></a>
                                 <a><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a>
                                 <a><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a>
@@ -32,23 +34,35 @@
                     </div>
                     </div>
               <div class="input-option" v-if="shareType =='email'">
+                  <label class="heading">Email</label>
                  <input
                         type="email"
                         v-model="email"
-                        name="name"
                         placeholder="Email(s)"
                  >
                  <label class="color-red" v-if="emailRequired">{{$t('domainError')}}</label>
               </div>
-              <div class="input-option" v-if="shareType =='embed' && domain">
-                  <textarea
-                    type="text"
-                    v-model="domain"
-                    name="name"
-                    disabled="disabled"
-                  ></textarea>
+                <div class="input-option" v-if="shareType =='embed'">
+                  <label class="heading">Domain</label>
+                  <input
+                      type="text"
+                      v-model="domain"
+                      placeholder="website url"
+                  >
+                 <label class="color-red" v-if="domainRequired">{{$t('domainError')}}</label>
               </div>
 
+              <div class="input-option" v-if="shareType =='embed' && frame">
+                <textarea
+                  ref="text"
+                  v-model="frame"
+                  rows="4"
+                ></textarea>
+                  <label><b>Note</b>: Copy this code and paste it in your <span v-bind="domain"><span> website</label>
+                  <button type="button" class="btn btn-primary pull-right" @click="copyCode()">
+                    Copy code
+                </button>
+              </div>
             </div>
             <div slot="footer">
                 <button type="button" class="btn btn-default" aria-label="Close" @click="closeDialog()" :disabled="loading">{{$t('cancel')}}</button>
@@ -73,7 +87,9 @@
                 loading        : false,
                 open           : false,
                 emailRequired  : false,
+                domainRequired : false,
 				shareType      :'email',
+                frame          :'',
 				domain         :'',
                 query          :'',
                 type           :''
@@ -85,44 +101,79 @@
         async mounted() {
         },
         methods: {
-                async onShowDialog(){
+            async onShowDialog() {
                 this.emailRequired = false;
+                this.domainRequired = false;
                 this.shareType = 'email';
-                const {type, query } = await this.getQuery();
-                this.query = query
-                this.type     = type;
+                const {type, query} = await this.getQuery();
+                this.query = query;
+                this.type = type;
             },
-            async send(){ // for testing check network call
+            async send() { // for testing check network call
                 this.emailRequired = false;
-                if(!this.email) {
+                if (!this.email) {
                     this.emailRequired = true;
                     return;
                 }
-                this.loading      = true;
-                const param = {"$shareType": "email", "$email": this.email, "$type": this.type, "&query": this.query};
+                this.loading = true;
+                const param = {"shareType": "email", "email": this.email, "type": this.type, "query": this.query};
+                try{
                 const response = await this.CommonApi.queryShare(param);
-                if((response || []).length) {
-                //
+                if ((response || []).length) {
+                    //
                 }
+            } catch(err) {
+            } finally {
                 this.loading = false;
+            }
             },
             async embed(){
+                this.domainRequired = false;
+                this.frame='';
+                if(!this.domain) {
+                    this.domainRequired = true;
+                    return;
+                }
                 let pageUrl = location.href;
+                this.loading      = true;
+                 const param = {"shareType": "embed", "domain": this.domain, "type": this.type, "query": this.query};
+                try {
+                // TODO: const response = await this.CommonApi.queryShare(param);
+                // if((response || []).length) {
+                // in success case create emebede string
+                if(this.type == 'document'){
+                    const origin = document.location.origin;
+                    pageUrl = origin+"/"+this.$locale+"/database/"+this.query[0]+"/"+this.query[1];
+                }
                 setTimeout(()=> {
                     pageUrl += ( pageUrl.match( /[\?]/g ) ? '&' : '?' )+'embed=true';
-                    this.domain = '<iframe src="' + pageUrl + '" width="100%" height="100%"></iframe>';
+                    this.frame = '<iframe src="' + pageUrl + '" width="100%" height="100%"></iframe>';
                 },100);
+                // }
+                } catch (err) {
+                } finally {
+                    this.loading = false;
+                }
             },
 
             sendRecord(){
                 this.emailRequired = false;
+                this.domainRequired = false;
                 if(this.shareType == 'email')
                     this.send();
                     else
                     this.embed();
 
             },
-
+            copyCode(){
+                try {
+                    this.$refs.text.select();
+                    let successful = document.execCommand('copy');
+                    if (!successful) throw successful;
+                } catch (err) {
+                } finally {
+                }
+            },
             closeDialog(){
                 this.open = false;
             }
@@ -141,9 +192,23 @@
         border: 1px solid #ccc;
         border-radius: 4px;
         box-sizing: border-box;
-        margin-top: 15px;
         margin-bottom: 16px;
         resize: vertical;
         display: block;
     }
+    .input-option label.heading {
+        font-weight: normal;
+        margin-top: 15px;
+    }
+    .share-body .icon-list a{
+        color: #000;
+    }
+    .share-body .icon-list a:hover{
+        cursor: pointer;
+        color: #428bca;
+    }
+    .share-body .icon-list .selected a:nth-child(1) {
+        color: #428bca;
+    }
+
 </style>
