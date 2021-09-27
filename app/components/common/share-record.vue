025fbb9dcaacbc0907d4300c0c08a8d1e86c9ac1
Tahmeed <tahmeedshah@gmail.com>
2021-09-28 01:41:42 +0500

    small fix

--- app/components/common/share-record.vue
+++ app/components/common/share-record.vue
@@ -45,9 +45,9 @@
                 <div class="input-option" v-if="shareType =='embed'">
                   <label class="heading">Domain</label>
                   <input
-                      type="text"
                       v-model="domain"
-                      placeholder="website url"
+                      type="url"
+                      placeholder="https://example.com"
                   >
                  <label class="color-red" v-if="domainRequired">{{$t('domainError')}}</label>
               </div>
@@ -58,7 +58,7 @@
                   v-model="frame"
                   rows="4"
                 ></textarea>
-                  <label><b>Note</b>: Copy this code and paste it in your <span v-bind="domain"><span> website</label>
+                  <label><b>Note</b>: Copy this code and paste in <b><a v-bind:href="domain" target="_blank" >{{ domain }}</a></b></label>
                   <button type="button" class="btn btn-primary pull-right" @click="copyCode()">
                     Copy code
                 </button>
@@ -131,6 +131,11 @@
                 this.domainRequired = false;
                 this.frame='';
                 if(!this.domain) {
+                    this.domainRequired = true;
+                    return;
+                }
+                const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+                if (!regexp.test(this.domain)) {
                     this.domainRequired = true;
                     return;
                 }
