a5d90bb12725e8320f35ce68ffbbae89749a557e
Tahmeed <tahmeedshah@gmail.com>
2021-10-20 16:56:16 +0500

    Fix spacing issue

--- app/views/kb/home.vue
+++ app/views/kb/home.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-     <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+     <link href='http://fonts.googleapis.com/css?family=Nunito' rel='stylesheet' type='text/css'>
    
     <div class="knowledge-base">
         <auto-complete-search></auto-complete-search>
