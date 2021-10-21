3405d6d3f1154fa2ee30fda7cd311de3593f4175
Tahmeed <tahmeedshah@gmail.com>
2021-10-21 22:32:57 +0500

    WIP

--- app/components/common/national-records.vue
+++ app/components/common/national-records.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <ul class="bs5 nav nav-pills">
+  <ul class="bs5 mx-lg-3 nav nav-pills">
     <li class="bs5 nav-item">
       <a class="bs5 nav-link" v-bind:class="{ active: sort == 'updatedDate_dt desc' }" aria-current="page" href="" @click="nationalSort()">Recently added</a>
     </li>
@@ -15,12 +15,12 @@
       <div class="bs5 position-relative new-css-callout new-css-callout-national shadow">
         <span class="bs5 badge position-absolute top-0 end-0">{{record.rec_date|dateFormat}}</span>
         <h4><a :href="record.url_ss" class="bs5 text-dark text-decoration-none stretched-link">{{record.rec_title}}</a></h4>
-        <p v-if="record.rec_summary">{{record.rec_summary}}</p>
+        <p class="new-css-summary-text" v-if="record.rec_summary">{{record.rec_summary}}</p>
         
         <div style="position:absolute;bottom:5px;">
           <span class="bs5 badge bg-light text-uppercase new-css-text-national new-css-rectype-national">{{record.schema_s}}</span>
           <span class="bs5 badge bg-light text-dark new-css-rectype-national"><i class="bs5 pe-1 flag-icon" v-bind:class="'flag-icon-'+record.government_s"></i> {{record.rec_countryName}}</span>
-          <span class="bs5 bg-light text-uppercase badge text-dark new-css-rectype-national">{{record.uniqueIdentifier_s}}</span>
+          <!-- <span class="bs5 bg-light text-uppercase badge text-dark new-css-rectype-national">{{record.uniqueIdentifier_s}}</span> -->
         </div>
       </div>
     </div>
@@ -81,7 +81,7 @@
             "fl": "id, rec_date:updatedDate_dt, rec_creationDate:createdDate_dt, identifier_s, uniqueIdentifier_s, url_ss, government_s, schema_s, government_EN_t, schemaSort_i, sort1_i, sort2_i, sort3_i, sort4_i, _revision_i,rec_countryName:government_EN_t, rec_title:title_EN_t, rec_summary:summary_t, rec_type:type_EN_t, rec_meta1:meta1_EN_txt, rec_meta2:meta2_EN_txt, rec_meta3:meta3_EN_txt,rec_meta4:meta4_EN_txt,rec_meta5:meta5_EN_txt",
               "wt": "json",
               "start": 0,
-              "rows": 10,
+              "rows": 8,
               "facet": true,
               "facet.field": ["{!ex=schemaType}schemaType_s", "{!ex=schema,schemaType,schemaSub}schema_s", "{!ex=government}countryRegions_ss", "{!ex=keywords}all_terms_ss", "{!ex=region}countryRegions_REL_ss"]
           };
