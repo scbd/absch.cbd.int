be66c69d8c1bd142961bf848484d751dcb4943e0
Tahmeed <tahmeedshah@gmail.com>
2021-10-28 23:17:23 +0500

    update code for homepage-records

--- app/views/home/bch.js
+++ app/views/home/bch.js
@@ -5,7 +5,9 @@ import 'views/directives/home-articles';
 import homepageRecords from '~/components/common/homepage-records.vue';
 export { default as template } from './bch.html';
 export default ['$scope', 'articlesService', function ($scope, articlesService) {
-        
+        $scope.exportVueComponent = {
+          components: { homepageRecords }
+        }
         articlesService.getArticle('5ce467f7452a5c00015e3406')
         .then(function(article){
             $scope.betaArticle = article;
@@ -28,19 +30,4 @@ app.directive('exploreMoreOnClick', ['$anchorScroll', function ($anchorScroll) {
       });
     }
   };
-}]);
-
-app.directive('homepageRecord', [function () {
-  return {
-    restrict : 'E',
-    template: `<div ng-if="exportVueComponent && recordType">
-                  <homepage-records ng-vue="exportVueComponent" :type="recordType"></homepage-records></div>`,
-    scope : {},
-    link : function ( scope, $element, attr ) {
-      scope.recordType = attr.type;
-      scope.exportVueComponent = {
-        components : { homepageRecords }
-      }
-    }
-  }
 }]);
