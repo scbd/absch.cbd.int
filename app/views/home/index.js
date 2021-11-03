9815f738cbf3d7d6bd8da593221409e250c04321
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2021-11-03 19:28:20 +0500

    ABS new homepage (#626)
    * ABS new homepage
    
    * updates to ABSCH homepage
    
    * absch homepage map updates
    
    * update map colors
    
    Co-authored-by: matt <diasm@un.oirg>

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -3,7 +3,9 @@ import moment from 'moment';
 import 'services/main';
 import 'views/directives/home-country-dashboard-directive';
 import 'views/directives/map/home-map';
-import 'views/directives/home-articles-small';
+import 'views/directives/home-articles';
+import homepageRecords from '~/components/common/homepage-records.vue';
+
 import 'angular-cookies';
     export { default as template } from './index.html';
 export default ['$scope',
@@ -35,5 +37,26 @@ export default ['$scope',
         $scope.monthsSinceEntry = months;
         $scope.daysSinceEntry = days;
 
+        $scope.exportVueComponent = {
+          components: { homepageRecords }
+        }
+
     }];
 
+    app.directive('exploreMoreOnClick', ['$anchorScroll', function ($anchorScroll) {
+      return {
+        restrict: 'E',
+        replace: true,
+        transclude: true,
+        template: '<div class="cursor-pointer bs5 my-2 my-lg-5 mt-lg-5 p-2 pt-lg-5 text-center   hidden-sm hidden-xs"><a class= "text-decoration-none"><span class="bs5 display-6 new-css-abs-card-footer fs-1 rounded  py-3 px-5 color-white new-explore-btn">Explore the map <i class="fa  fa-arrow-down"></i></span></a></div >',
+        scope: {
+        },
+        link: function (scope, element) {
+          element.on('click', function () {
+            $('html, body').animate({
+              scrollTop: $("#homePageMapSection").offset().top
+            }, 1000);
+          });
+        }
+      };
+    }]);
