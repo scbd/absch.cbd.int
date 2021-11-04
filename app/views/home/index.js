123f77379e93fe9cc057065663c57f55c47aae03
Tahmeed <tahmeedshah@gmail.com>
2021-11-04 15:49:38 +0500

    Fixed the "Explore Map button"

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -41,22 +41,10 @@ export default ['$scope',
           components: { homepageRecords }
         }
 
-    }];
+      $scope.exploreMore = function (  ){
+        $('html, body').animate({
+          scrollTop: $("#homePageMapSection").offset().top
+        }, 1000);
+      }
 
-    app.directive('exploreMoreOnClick', ['$anchorScroll', function ($anchorScroll) {
-      return {
-        restrict: 'E',
-        replace: true,
-        transclude: true,
-        template: '<div class="cursor-pointer bs5 my-2 my-lg-5 mt-lg-5 p-2 pt-lg-5 text-center   hidden-sm hidden-xs"><a class= "text-decoration-none"><span class="bs5 display-6 new-css-abs-card-footer fs-1 rounded  py-3 px-5 color-white new-explore-btn">Explore the map <i class="fa  fa-arrow-down"></i></span></a></div >',
-        scope: {
-        },
-        link: function (scope, element) {
-          element.on('click', function () {
-            $('html, body').animate({
-              scrollTop: $("#homePageMapSection").offset().top
-            }, 1000);
-          });
-        }
-      };
-    }]);
+    }];
