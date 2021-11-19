79f401fe4905a8d524606ba00d9997d2e79aff30
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-11-19 14:51:41 -0500

    fix context-help control for abs

--- app/templates/bch/bch.js
+++ app/templates/bch/bch.js
@@ -1,22 +1,9 @@
 import app from 'app';
 import angular from 'angular';
 
-import 'bootstrap';
 import 'routes/bch';
-import 'ng-breadcrumbs';
-import 'toastr';
-import 'angular-animate';
-import 'angular-loggly-logger';
-
-import 'services/main'; 
-import 'components/scbd-branding/main';
-import 'components/scbd-angularjs-services/main';
-import 'views/directives/route-loading-directive';
-import '~/views/directives/docked-side-bar';
 import './header';
 import './footer';
-import '../directives/cbd-footer';
-import '../directives/app-loading';
 import { templateController, bootstrapApp} from '../template-controller';
 
 // console.log(templateController);
