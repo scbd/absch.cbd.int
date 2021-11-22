5a593b1551f6e8f647f567ae797f283777415f10
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2021-11-22 19:58:43 +0500

    ABS/BCH Devdesk changes (#658)
    * ABS: add "Explore the map" button
    
    * fix map height
    
    * Home page - changes to Mobile version-DEVDESK-916
    
    * ABS: fix the contactQuery
    
    * Fix the modal header- DEVDESK-925
    
    * fix the link in right-side-help
    
    * fix "ABS articles appear in KB in BCH"-DEVDESK-930

--- app/views/home/bch.js
+++ app/views/home/bch.js
@@ -6,8 +6,10 @@ import homepageRecords from '~/components/common/homepage-records.vue';
 export { default as template } from './bch.html';
 export default ['$scope', 'articlesService', '$rootScope', function ($scope, articlesService, $rootScope) {
         $scope.recordCount = 4;
+        $scope.announcementCounts = 3;
         if($rootScope.deviceSize == 'lg'){
           $scope.recordCount = 8;
+          $scope.announcementCounts = 6;
         }
         $scope.exportVueComponent = {
           components: { homepageRecords }
