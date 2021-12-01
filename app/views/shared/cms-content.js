8fe8b270a67f9b14e076533beb05d380cb64e856
Stephane <bilodeaustephane@gmail.com>
2021-12-01 17:45:00 -0500

    Add menu and rebase

--- app/views/shared/cms-content.js
+++ app/views/shared/cms-content.js
@@ -1,46 +1,76 @@
 import $ from 'jquery';
+import _ from 'lodash';
 import html404 from 'text!./403.html';
 import html403 from 'app'; 
 
-    function escapeRegExp(str) {
-      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+function escapeRegExp(str) {
+    return _.escapeRegExp(str);
+}
+
+function rebaseUrl(pathOrUrl, base) {
+
+    var newPathOrUrl = pathOrUrl;
+
+    if(newPathOrUrl) {
+        const url = new URL(newPathOrUrl, "https://bch.cbd.int")
+
+        if(url.host=='bch.cbd.int') {
+            newPathOrUrl = `${url.pathname}${url.search}${url.hash}`;
+
+            if(newPathOrUrl.indexOf(base)!=0 && !/\.(png|gif|pdf|jpg|css|js)$/i.test(url.pathname))
+                newPathOrUrl = [base.replace(/\/$/g, ''), newPathOrUrl.replace(/^\//g, '')].join('/');
+        }
+    }
+    console.log(pathOrUrl, '=>', newPathOrUrl);
+
+    return newPathOrUrl;
+}
+export { default as template } from './cms-content.html';
+
+export default ["$scope", "$rootScope", "$route", '$http', function($scope, $rootScope, $route, $http) {
+    debugger
+    var url =  $route.current.$$route.target;
+    var params = {};
+    
+    for(var key in $route.current.params) {
+        
+        var re  = new RegExp(escapeRegExp(':'+key)+'\\b', 'g');
+        var val = $route.current.params[key];
+        
+        if(re.test(url)) url = url.replace(re, val);
+        else             params[key] = val;
     }
     
-    export { default as template } from './cms-content.html';
-export default ["$rootScope", "$route", '$http', function($rootScope, $route, $http) {
+    url = url.replace(/:\w+\b/g, '');
+    
+    $http.get(url, {cache:true, params: params }).then(function(res) {
         
-        var url =  $route.current.$$route.target;
-        var params = {};
-        
-        for(var key in $route.current.params) {
-            
-            var re  = new RegExp(escapeRegExp(':'+key)+'\\b', 'g');
-            var val = $route.current.params[key];
-            
-            if(re.test(url)) url = url.replace(re, val);
-            else             params[key] = val;
-        }
-        
-        url = url.replace(/:\w+\b/g, '');
-        
-        $http.get(url, {cache:true, params: params }).then(function(res) {
-            
-            var page = $('<div></div>').html(res.data)
-            
-            $('head>title').text(page.find('head>title').text());
-            var content     = page.find('#CPageContent');
-            var breadcrumbs = page.find('#CPositionHolder');
-            var menu        = page.find('#CPageLeft');
-            
-            breadcrumbs.remove();
-            
-            $('#cms-content').html(content.html());
-            
-        }).catch(function(e) {
-            
-            if(e.status==404) $('#cms-content').html(html404);
-            if(e.status==403) $('#cms-content').html(html403);
-        });
+        const base  = $('base').attr('href') || '/';
+        const page  = $('<div></div>').html(res.data)
+        const title = page.find('title').text();
 
-    }];
+        page.find('[href]').each(function() { $(this).attr('href', rebaseUrl($(this).attr('href'), base));  });
+        page.find('[src]' ).each(function() { $(this).attr('src',  rebaseUrl($(this).attr('src' ), base));  });
+
+        var CContent     = page    .find('#CPageContent');
+        var CTitle       = CContent.find('#CTitleHolder');
+        var CBreadcrumbs = page    .find('#CPositionHolder');
+        var CMenu        = page    .find('#CPageLeft');
+
+        CTitle      .remove();
+        CBreadcrumbs.remove();
+
+        $scope.title = title || CTitle.text();
+        
+        $('#cms-left-menu').html(CMenu  .html());
+        $('#cms-content'  ).html(CContent.html());
+
+        
+    }).catch(function(e) {
+        
+        if(e.status==404) $('#cms-content').html(html404);
+        if(e.status==403) $('#cms-content').html(html403);
+    });
+
+}];
 
