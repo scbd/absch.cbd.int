3adae3fa0c7534cb845560eb6eb416d4837f3224
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-12-06 17:21:49 -0500

    changeorigin for error-logs route

--- middlewares/error-logs.js
+++ middlewares/error-logs.js
@@ -4,7 +4,7 @@ module.exports = function(proxy, options){
     return function(req, res){ 
         proxy.web(req, res, { 
             target: `${options.apiUrl}/api/v2016/error-logs?type=client-app-errors&serverAppVersion=${encodeURIComponent(options.appVersion)}`, 
-            secure:false, ignorePath:true, xfwd:true });
+            secure:false, ignorePath:true, xfwd:true, changeOrigin: true });
     }
 }
 
