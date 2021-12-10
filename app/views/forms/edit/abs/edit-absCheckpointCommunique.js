282ab8704961a8e8fd0ff611efd3a0384622b8cb
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2021-12-10 12:53:52 +0500

    ABS-CPC: update onContactQuery

--- app/views/forms/edit/abs/edit-absCheckpointCommunique.js
+++ app/views/forms/edit/abs/edit-absCheckpointCommunique.js
@@ -67,10 +67,10 @@ import 'services/main';
 
             $scope.onContactQuery = function(searchText){
                 var queryOptions = {
-                schemas : ['contact', 'authority'],
+                schemas : ['focalPoint', 'authority', 'contact'],
                 realm : realm.value,
                 searchText: searchText,
-                query : `(schema_s:authority AND government_s:${$scope.document.government.identifier}) OR (schema_s:contact)`
+                query : `(((schema_s:authority OR schema_s:focalPoint) AND government_s:${$scope.document.government.identifier}) OR (schema_s:contact))`
                 }
                 return $scope.onBuildDocumentSelectorQuery(queryOptions);
             }
