77a1d5d84f90ad1eedfdf3f3d6471a4c705a5df4
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2022-02-15 00:08:28 +0500

    update links in Vue components (#703)
    Co-authored-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/components/kb/article-categories.vue
+++ app/components/kb/article-categories.vue
@@ -3,19 +3,14 @@
         <div class="row match-height">
             <div class="col-md-12 col-sm-12 " v-for="category in KbCategories">
                 <div class="categories-list widget_categories tag-count">
-                    <h3>
-                        <a href="#" @click="goToTag(category)">{{category.title}}</a>
-                        <a style="display:none" :href="`${tagUrl(category)}`">{{category.title}}</a>
-                    </h3>
+                    <h3><a :href="`${tagUrl(category)}`">{{category.title}}</a></h3>
                    <hr>
                     <ul class="cate-list-ul" style="list-style: none;">
                         <li class="cate-list-li" v-for="article in category.articles">
-                            <a v-show="article.url" style="display:none" :href="`${articleUrl(article, category.adminTags[0])}`">{{article.title}}</a>
-                            <a v-if="article.identifier" href="#" @click="goToArticle(article, category.adminTags[0])">{{article.title}}</a>
+                            <a v-if="article.identifier" :href="`${articleUrl(article, category.adminTags[0])}`">{{article.title}}</a>
                         </li>
                     </ul>
-                    <a style="display:none" :href="`${tagUrl(category)}`">{{ $t("viewMore") }}</a>
-                    <a class="view-more" href="#" @click="goToTag(category)">+  {{ $t("viewMore") }}</a>
+                    <a  :href="`${tagUrl(category)}`">{{ $t("viewMore") }}</a>
                 </div>
             </div>
         </div>
@@ -49,14 +44,6 @@ export default {
            } else {
             return this.getUrl(article.title, article.identifier, tag);
           }
-        },
-        goToArticle(article, tag){
-          this.$router.push({
-            path:this.articleUrl(article, tag)
-          });
-        },
-        goToTag(category){
-          this.$router.push({path: this.tagUrl(category)});
         }
     },
     i18n: {
