0ecba3917b56ea43949b7d4d28b6326ef542c30e
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2022-06-15 12:22:10 +0500

    homepinPopupAbs Translation

--- app/views/countries/directives/homepin-popup-abs.js
+++ app/views/countries/directives/homepin-popup-abs.js
@@ -1,10 +1,14 @@
 import app from '~/app';
 import template from './homepin-popup-abs.html';
+import homepinPopupAbsTranslation from '~/app-text/views/countries/directives/homepin-popup-abs.json';
 
-app.directive('homepinPopupAbs', function(){
+app.directive('homepinPopupAbs', ['translationService', function(translationService){
     return {
         restrict:'E',
         replace: true,
-        template:template
+        template:template,
+        link: function() {
+         translationService.set('homepinPopupAbsTranslation', homepinPopupAbsTranslation);
+        }
     }
-})
+}])
