4edfc16e14e6c5a7afa3ca693f069b80563469eb
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2022-06-16 09:16:48 +0500

    translation: pin-popup-abs

--- app/views/countries/directives/pin-popup-abs.js
+++ app/views/countries/directives/pin-popup-abs.js
@@ -1,10 +1,14 @@
 import app from '~/app';
 import template from './pin-popup-abs.html';
+import pinPopupAbsT from '~/app-text/views/countries/directives/pin-popup-abs.json';
 
-app.directive('pinPopupAbs', function(){
+app.directive('pinPopupAbs', ['translationService', function(translationService){
     return {
         restrict:'E',
         replace: true,
-        template:template
+        template:template,
+        link: function() {
+            translationService.set('pinPopupAbsT', pinPopupAbsT);
+        }
     }
-})
+}])
