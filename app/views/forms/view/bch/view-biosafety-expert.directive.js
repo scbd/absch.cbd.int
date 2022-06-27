91e0fdabefa14e3a151896f95cb71236f8bae503
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2022-06-27 23:28:47 +0500

    Translation: View EXP

--- app/views/forms/view/bch/view-biosafety-expert.directive.js
+++ app/views/forms/view/bch/view-biosafety-expert.directive.js
@@ -1,8 +1,9 @@
 import app from 'app';
 import template from "text!./view-biosafety-expert.directive.html";
 import 'views/directives/record-options';
+import viewExpT from '~/app-text/views/forms/view/bch/view-biosafety-expert.json';
 
-	app.directive("viewBiosafetyExpert", [function () {
+	app.directive("viewBiosafetyExpert", ['translationService', function (translationService) {
 	return {
 		restrict   : "EAC",
 		template: template ,
@@ -16,6 +17,7 @@ import 'views/directives/record-options';
 		},
 		controller : ["$scope", function ($scope)
 			{		
+			translationService.set('viewExpT', viewExpT);
 			//====================
 			//
 			//====================
