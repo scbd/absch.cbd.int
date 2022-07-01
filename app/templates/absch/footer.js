090c90c4eb86fe51e5f4cdb33b67d190f112a945
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2022-07-01 16:47:41 +0500

    translation: ABS footer

--- app/templates/absch/footer.js
+++ app/templates/absch/footer.js
@@ -1,9 +1,13 @@
 import template from 'text!./footer.html';
 import app from 'app';
+import abschFooterT from '~/app-text/templates/absch/footer.json';
 
-app.directive('abschFooter', [function () {
+app.directive('abschFooter', ['translationService', function (translationService) {
     return {
         restrict: 'E',
-        template: template
+        template: template,
+        link: function() {
+            translationService.set('abschFooterT', abschFooterT);
+        }
     };
 }]);
