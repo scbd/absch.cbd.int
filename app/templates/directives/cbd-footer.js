89b51b53f1184cfb99ab324b2ba8ec4bef81fa7a
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2022-07-01 14:23:43 +0500

    translation: scbd footer

--- app/templates/directives/cbd-footer.js
+++ app/templates/directives/cbd-footer.js
@@ -1,4 +1,14 @@
 import app from 'app'
 import cbdFooterHtml from 'text!./cbd-footer.html';
+import cbdFooterT from '~/app-text/templates/directives/cbd-footer.json';
 
-app.directive('cbdFooter', [function () { return { restrict: 'E', template: cbdFooterHtml }; }]);   
+app.directive('cbdFooter', ['translationService', function (translationService) {
+     return { 
+        restrict: 'E', 
+        template: cbdFooterHtml,
+        link: function(){
+            translationService.set('cbdFooterT', cbdFooterT);
+        }
+    }; 
+
+    }]);   
