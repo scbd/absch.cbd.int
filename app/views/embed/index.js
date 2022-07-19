d83ac1fbf2a9daf591a7485ed9e27dc3165ae337
Blaise Fonseca <blaise.fonseca@cbd.int>
2022-07-19 16:20:57 -0400

    handle legacy bch ajax plugin data

--- app/views/embed/index.js
+++ app/views/embed/index.js
@@ -2,8 +2,8 @@ import app from 'app';
 
 export { default as template } from './index.html';
 
-export default ['$scope', '$routeParams', '$http', '$location', 'locale', 'localStorageService', 'realm',
- function ($scope, $routeParams, $http, $location, locale, localStorageService, realm){
+export default ['$scope', '$routeParams', '$http', '$location', 'locale', 'localStorageService', 'realm', '$filter',
+ function ($scope, $routeParams, $http, $location, locale, localStorageService, realm, $filter){
 
     const elementMapping = {
         'chm-document' : '#recordContent',
@@ -12,11 +12,51 @@ export default ['$scope', '$routeParams', '$http', '$location', 'locale', 'local
 
     async function init(){
 
+        const searchQuery  = $location.search();
         $scope.loading = true;
-
+        
         try{
             if($routeParams.accessKey){
-                const data = (await $http.get(`/api/v2018/document-sharing/${$routeParams.accessKey}`)).data;
+                let data;
+                
+                if($routeParams.accessKey == 'legacy-widget'){
+                    const realmSchemas = [...realm.nationalSchemas, ...realm.referenceSchemas, ...realm.scbdSchemas];
+                    //old bch schema names were not case sensitive, find from realm
+                    const realmSchema  = realmSchemas.find(e=>e.toLowerCase() == searchQuery.schema.toLowerCase())                    
+                    const schema       = realm.schemas[realmSchema]
+
+                    data = {
+                        storageType : 'chm-search-result',
+                        sharedData  : { 
+                            realm : realm.value,
+                            searchQuery : { 
+                                _id : `${$routeParams.accessKey}_${realmSchema}`,
+                                filters : [{
+                                    id: realmSchema,
+                                    name: $filter('lstring')(schema.title, locale),
+                                    otherType: schema.type,
+                                    type: "schema"
+                                }] 
+                            } 
+                        } 
+                    };
+                    if(searchQuery.countries && searchQuery.countries != '*'){
+                        const countries = searchQuery.countries.split(/\s/g);
+                        for (let i = 0; i < countries.length; i++) {
+                           
+                            data.sharedData.searchQuery.filters.push({
+                                id: countries[i],
+                                name: countries[i],
+                                otherType: undefined,
+                                type: "country"
+                            });                            
+                        }
+                    }
+                    
+                }
+                else{
+                    data = (await $http.get(`/api/v2018/document-sharing/${$routeParams.accessKey}`)).data;
+                }
                 
                 if(data.sharedData.realm != realm.value){
                     console.error('wrong realm for share id loaded')
@@ -27,13 +67,19 @@ export default ['$scope', '$routeParams', '$http', '$location', 'locale', 'local
                 }
 
                 if(data.storageType == 'chm-document'){
-                    $scope.$apply(()=>{
+                    safeapply(()=>{
                         $location.path(`/database/record/${data.sharedData.recordKey}`);
                     })
                 }
                 else if(data.storageType == 'chm-search-result'){
                     localStorageService.set(data.sharedData.searchQuery._id, data.sharedData.searchQuery);
-                    $scope.$apply(()=>{
+                    safeapply(()=>{
+
+                        if($routeParams.accessKey == 'legacy-widget'){
+                            $location.search('schema', undefined);
+                            $location.search('countries', undefined);
+                        }
+
                         $location.path(`/search`);
                         $location.search('searchShareQueryId', data.sharedData.searchQuery._id);
                     })
@@ -48,7 +94,9 @@ export default ['$scope', '$routeParams', '$http', '$location', 'locale', 'local
             $scope.loading = false;
         }
     }
-
+    function safeapply(fn) {
+        ($scope.$$phase || $scope.$root.$$phase) ? fn() : $scope.$apply(fn);
+    }  
     // function registerIframeCommunication(selector){
 
     //     window.addEventListener('message', (evt)=>{
