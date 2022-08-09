4fc05ef575c33d3e33ae31f580ad3523440a48c9
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2022-08-09 19:17:03 +0500

    update UI, add embed button

--- app/components/common/shared-links.vue
+++ app/components/common/shared-links.vue
@@ -2,86 +2,90 @@
 	<div class="p-4 bg-white shared-links">
 		<div class="register-content-header bg-secondary text-white">
 			<div class="p-2">
-				<h3>{{ $t("sharedUrls") }}</h3> </div>
+				<h3>{{ $t("mySharing") }}</h3> </div>
 		</div>
 		<table class="table mt-4">
 			<thead>
 				<tr>
 					<th scope="col">#</th>
                     <th scope="col">{{ $t("createdOn") }}</th>
-					<th scope="col" class="d-none d-sm-table-cell">{{ $t("expiryStatus") }}</th>
+					<th scope="col" class="d-none d-sm-table-cell">{{ $t("expiry") }}</th>
+                    <th scope="col">{{ $t("status") }}</th>
                     <th scope="col" class="d-none d-sm-table-cell">{{ $t("sharedDetail") }}</th>
-					<th scope="col" colspan="4">{{ $t("linkEmail") }}</th>	
+					<th scope="col" colspan="4">{{ $t("actions") }}</th>	
 				</tr>
 			</thead>
 			<tbody>
 				<tr v-for="(link, index) in sharedLinks.slice().reverse()">
 					<th scope="row">{{index+1}}</th>
-                    <td>{{link.meta.createdOn|formatDate}}</td>
-					<td class="d-none d-sm-table-cell"> <span>{{link.expiry|formatDate}}</span>
-                        <span v-if="hasStatus('active', link)" class="action badge bg-primary">{{ $t("active") }}</span>
+                    <td colspan="1">{{link.meta.createdOn|formatDate}}</td>
+                    <td colspan="1" class="d-none d-sm-table-cell">{{link.expiry|formatDate}}</td>
+					<td colspan="1"> 
+                        <span v-if="hasStatus('active', link)" class="action badge bg-success">{{ $t("active") }}</span>
                         <span v-if="hasStatus('expired', link)" class="action badge bg-warning text-dark">{{ $t("expired") }}</span>
                         <span v-if="hasStatus('revoked', link)" class="action badge bg-danger">{{ $t("revoked") }}</span>
                     </td>
                     <td colspan="1" class="d-none d-sm-table-cell">
-                        <span v-if="hasStorageType('document', link)" class="action badge bg-primary">{{ $t("document") }}</span>
-                        <span v-if="hasStorageType('search', link)" class="action badge bg-warning text-dark">{{ $t("search") }}</span>
-                        <span v-if="hasStorageType('country-profile', link)" class="action badge bg-danger">{{ $t("countryProfile") }}</span>
-                        <div class="mt-1 email-links d-none d-sm-inline-block">
-                            <span v-if="link.shareType =='email' && link.sharedWith.emails">
-                                <span class="fw-bold">{{ $t("email") }}</span>
-                                <ul v-if="link.sharedWith.emails.length>0">
-                                    <li v-for="email in link.sharedWith.emails.split(',')">{{email}}</li>
-                                </ul>
+                        <span v-if="hasStorageType('document', link)" style="display:initial" class="action badge bg-info">{{ $t("document") }}</span>
+                        <span v-if="hasStorageType('search', link)" style="display:initial" class="action badge bg-primary">{{ $t("search") }}</span>
+                        <span v-if="hasStorageType('country-profile', link)" style="display:initial" class="action badge bg-secondary">{{ $t("countryProfile") }}</span>
+                        <span v-if="link.shareType =='embed' && link.sharedData.domain">
+                            {{ $t("embeddedIn") }} {{link.sharedData.domain}}
+                        </span>
+                        <span v-if="link.shareType =='email' && link.sharedWith.emails" style="display:initial">
+                            <span>{{ $t("emailedTo") }} </span> 
+                            <span> {{link.sharedWith.emails.split(',')[0]}}</span>
+                            <span v-if="link.sharedWith.emails.split(',').length>1">
+                                <a data-bs-toggle="collapse" class="show-emails" :href="'#toggle'+index" role="button" aria-expanded="false">
+                                    <i class="bi bi-chevron-down"></i>
+                                    <i class="bi bi-chevron-up"></i>
+                                </a>                        
+                                <div class="mt-1 email-links collapse" :id="'toggle'+index"> 
+                                        <ul class="ms-3" v-if="link.sharedWith.emails.length > 0">
+                                            <li v-for="(email, e) in link.sharedWith.emails.split(',')" v-if="e!=0">
+                                                <span>{{email}}</span>
+                                            </li>
+                                        </ul>
+                                </div>
                             </span>
-                            <span v-if="link.shareType =='embed' && link.sharedData.domain"><span class="fw-bold">{{ $t("domain") }}</span> {{link.sharedData.domain}}</span>
-                        </div>
+                        </span>
                     </td>
 
         			<td colspan="4">
                         <div class="input-group" >
                             <input ref="textToCopy" type="text" width="100%" class="highlight form-control" :value="getUrl(link.urlHash)" disabled readonly/>
-                            <span class="input-group-text cursor-pointer" id="basic-addon1">
-                                <a :href="getUrl(link.urlHash)" target="_blank"><i class="bi bi-link"> </i> 
-                                    <span class="d-none d-sm-inline-block">{{ $t("Open") }}</span></a>
+                            <div class="btn-group" role="group" aria-label="Basic outlined example">
+                                <a type="button" class="btn btn-outline-primary rounded-0" :href="getUrl(link.urlHash)" target="_blank"><i class="bi bi-link"> </i> 
+                                    <span class="d-none d-sm-inline-block">{{ $t("Open") }}</span>
+                                </a>
+                                    <span v-if="!link.revoked" type="button" class="btn btn-outline-primary" @click="copyUrl(link.urlHash)">
+                                        <i class="bi bi-clipboard pe-1"> </i> <span class="d-none d-sm-inline-block">{{ $t("copy") }}</span>
+                                    </span>
+                                    <span  v-if="!link.revoked && hasStorageType('search', link)" type="button" class="btn btn-outline-primary">
+                                        <a :href="`/register/user-preferences/?id=${link.sharedData.recordKey}`" target="_blank"><i class="bi bi-pencil-square"> </i> 
+                                            <span class="d-none d-sm-inline-block">{{ $t("edit") }}</span></a>
+                                    </span>
+                                    <span  v-if="!link.revoked && link.sharedData.domain" data-bs-toggle="collapse" :href="'#embed'+index" role="button" aria-expanded="false" class="btn btn-outline-primary"
+                                        @click="embed(link.storageType, link.urlHash)">
+                                        <i class="bi bi-code-slash pe-1"> </i> <span class="d-none d-sm-inline-block">{{ $t("embed") }}</span>
+                                    </span>
+                                    <span v-if="!link.revoked && hasStatus('active', link)" disabled="link.status=='revokingLink'" type="button" class="btn btn-outline-secondary" @click="revokeLink(link)">
+                                        <i class="spinner-border pe-2" v-if="link.status=='revokingLink'"></i>
+                                        <i class="bi bi-hand-thumbs-down-fill pe-1"> </i> <span class="d-none d-sm-inline-block">{{ $t("revoke") }}</span>
+                                    </span>
+                            </div>  
+                        </div>
+                        <div v-if="!link.revoked && link.sharedData.domain" class="pt-2 collapse input-group" :id="'embed'+index">
+                            <input :id="'embedText'+link.urlHash" type="text" width="100%" class="highlight form-control" :value="`<script src='https://bch.cbddev.xyz/widgets.js'></script><div class='scbd-chm-embed' data-type='chm-search-result' data-access-key='${link.urlHash}' width='100%'></div>`" />
+                            <span type="button" class="btn btn-primary" @click="copyUrl(link.urlHash, true)">
+                                        <i class="bi bi-clipboard pe-1"> </i> <span class="d-none d-sm-inline-block">{{ $t("copyEmbedCode") }}</span>
                             </span>
-                            <span class="display-content" v-if="!link.revoked">
-                                <span class="input-group-text cursor-pointer" id="basic-addon2" @click="copyUrl(link.urlHash)">
-                                    <i class="bi bi-clipboard pe-1"> </i> <span class="d-none d-sm-inline-block">{{ $t("copy") }}</span>
-                                </span>
-                                <span  v-if="hasStorageType('search', link)" class="input-group-text cursor-pointer" id="basic-addon1">
-                                    <a :href="`/register/user-preferences/?id=${link.sharedData.recordKey}`" target="_blank"><i class="bi bi-pencil-square"> </i> 
-                                        <span class="d-none d-sm-inline-block">{{ $t("edit") }}</span></a>
-                                </span>
-                                <span  v-if="link.sharedWith.domain" class="input-group-text cursor-pointer" id="basic-addon2" 
-                                    data-bs-toggle="modal" data-bs-target="#exampleModal" @click="embed(link.storageType, link.urlHash)">
-                                    <i class="bi bi-code-slash pe-1"> </i> <span class="d-none d-sm-inline-block">{{ $t("embed") }}</span>
-                                </span>
-                                <span v-if="hasStatus('active', link)" disabled="link.status=='revokingLink'" class="input-group-text cursor-pointer" id="basic-addon3" @click="revokeLink(link)">
-                                    <i class="spinner-border pe-2" v-if="link.status=='revokingLink'"></i>
-                                    <i class="bi bi-hand-thumbs-down-fill pe-1"> </i> <span class="d-none d-sm-inline-block">{{ $t("revoke") }}</span>
-                                </span>
-                            </span>    
                         </div>
 					</td>
                     
 				</tr>
 			</tbody>
 		</table>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">{{ $t("embedLink") }}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <a :href="embededLink" target="_blank">
-                        {{embededLink}}</a>
-                </div>
-                </div>
-            </div>
-        </div>
 	</div>
 </template>
 
@@ -94,11 +98,10 @@
     import i18n from "../../app-text/components/common/shared-links.json"; 
     import '../kb/filters';
     export default {
-        name:'sharedUrls',
+        name:'mySharing',
         props: ["tokenReader"],
         data:  () => {
             return {
-                embededLink: null,
                 sharedLinks: [],
             }
         },
@@ -117,10 +120,14 @@
             getUrl(hash){
                 return location.origin + '/database/share/' + hash;
             },
-            copyUrl(hash){
+            copyUrl(hash, isembed){
                 const el = document.createElement('textarea');  
-                const url = this.getUrl(hash);
-                el.value = url;                    
+                if(isembed){
+                    const embededValue = document.getElementById("embedText"+hash);
+                    el.value = embededValue.value;
+                } else{
+                    el.value = this.getUrl(hash);;    
+                }                                   
                 el.setAttribute('readonly', '');                
                 el.style.position = 'absolute';                     
                 el.style.left = '-9999px';                      
