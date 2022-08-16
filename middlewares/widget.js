6bad5984009baf1379b0ba49b72d326bb3d8b780
Blaise Fonseca <blaisefonseca@hotmail.com>
2022-08-16 16:14:26 -0400

    use express static middleware for widgets resource (#821)

--- middlewares/widget.js
+++
@@ -1,8 +0,0 @@
-const { readFile, writeFile, stat } = require('fs').promises;
-const url = require('url');
-
-module.exports = async function(req, res){
-
-    res.sendFile(`${global.app.rootPath}/dist/en/app/assets${req.originalUrl}`);
-
-}
