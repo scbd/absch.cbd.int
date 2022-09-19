d6571e7189967d4981f8041a7cc20d97ed5dcd4f
Blaise Fonseca <blaise.fonseca@cbd.int>
2022-09-19 14:20:13 -0400

    user-setting directive

--- app/views/register/user-preferences/preferences.js
+++ app/views/register/user-preferences/preferences.js
@@ -2,12 +2,14 @@ import app from '~/app';
 import '~/views/register/directives/register-top-menu';
 import '~/views/register/user-preferences/user-alerts';
 import '~/views/search/search-directive';
+import './user-setting';
 import preferencesT from '~/app-text/views/register/user-preferences/preferences.json';
 
-    export { default as template } from './preferences.html';
+export { default as template } from './preferences.html';
 export default ['$scope','translationService',
     function ($scope, translationService) {
         translationService.set('preferencesT', preferencesT);  
+        $scope.tab = 'setting'
     }];
 
 
