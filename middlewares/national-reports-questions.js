86203922ab2783bb44fbb0a8bcae0646cc1f3d31
Blaise Fonseca <blaise.fonseca@cbd.int>
2023-05-19 11:43:04 -0400

    read report paths from existing mapping file

--- middlewares/national-reports-questions.js
+++ middlewares/national-reports-questions.js
@@ -1,18 +1,25 @@
-
-
+import path from 'path';
+import {analyzerMapping} from '../app/app-data/report-analyzer-mapping.js'
 
 module.exports = async function(req, res){
 
     let clearingHouse = process.env.CLEARINGHOUSE.toLowerCase();
     let report        = req.params.report;
     
-    const reportPaths = {
-        'absch' : 'app/app-data/absch/report-analyzer',
-        'bch'   : 'app/app-data/bch/report-analyzer'
-    }
+    let reports = [];
+    if(clearingHouse == 'absch')
+        reports = analyzerMapping.abs;
+    else if(clearingHouse == 'bch')
+        reports = analyzerMapping.bch;
+
+    const reportDetails = reports.find(e=>e.type == report);
+
+    if(!reportDetails)
+        return res.status(404).send();
+
     try{
-        
-        const reportData = await import(`../${reportPaths[clearingHouse]}/${report}.js`)
+        const reportPath = path.join('..', 'app', `${reportDetails.questionsUrl}.js`)
+        const reportData = await import(reportPath)
         res.status(200).json(reportData);
     }
     catch(e){
