af9dec4757952a244c76ea4132e3b7627a6f73da
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2023-05-29 10:42:52 +0500

    local testing for NR4

---
+++ app/views/forms/edit/bch/edit-national-report-4-test.js
@@ -0,0 +1,372 @@
+﻿import app from '~/app';
+import _ from 'lodash';
+import '~/views/directives/workflow-arrow-buttons';
+import '~/components/scbd-angularjs-services/main';
+import '~/views/forms/edit/editFormUtility';
+import '~/views/forms/edit/edit';
+import '~/services/main';
+import '~/views/forms/directives/nr-yes-no';
+import '~/views/forms/edit/document-selector';
+import '~/views/directives/block-region-directive';
+import '~/views/forms/view/bch/view-national-report-4.directive';
+export { default as template } from './edit-national-report-4.html';
+import 'ngDialog';
+import '~/views/forms/edit/directives/edit-national-report.directive';
+import {cpbNationalReport4} from '~/app-data/bch/report-analyzer/cpbNationalReport4';
+import {cpbNationalReport3} from '~/app-data/bch/report-analyzer/cpbNationalReport3';
+import editNRT from '~/app-text/views/forms/edit/directives/edit-national-report.json'; 
+import numbers from '~/app-text/numbers.json';
+export default ["$scope", "$rootScope", "locale", "$q", "$controller", "$timeout", 
+'commonjs', 'IStorage', '$routeParams', 'ngDialog', 'realm', 'translationService',
+function ($scope,  $rootScope, locale, $q, $controller, $timeout, commonjs, storage, $routeParams, ngDialog, realm, translationService) {
+
+        // since it was decided to use string type for terms fields in schema
+        // map string to ETerm ({identifier:'xxxx-xxx'}) type which is the type expected by term-checkbox.
+        $scope.multiTermModel = {};
+        translationService.set('editNRT', editNRT);
+        translationService.set('numbers', numbers);
+        $scope.tab = 'intro';
+        var user = $rootScope.user;
+        $scope.activeTab = 1
+        $scope.nr4Tabs = [{
+                "tab":1,
+                "title":"1 - 10",
+                render:true
+            },
+            {
+                "tab":2,
+                "title":"11 - 26",
+                "sections" : [{key:"General"}, {key:"Article2"}, {key:"Article5"}, {key:"Article6"}]
+            },
+            {
+                "tab":3,
+                "title":"27 - 38",
+                "sections" : [{key:"Articles7-10"}]
+            },
+            {
+                "tab":4,
+                "title":"39 - 52",
+                "sections" : [{key:"Article11"}, {key:"Article12"}]
+            },
+            {
+                "tab":5,
+                "title":"53 - 60",
+                "sections" : [{key:"Article13"}, {key:"Article14"}]
+            },
+            {
+                "tab":6,
+                "title":"61 - 90",
+                "sections" : [{key:"Article15_16"}, {key:"Article17"}]
+            },
+            {
+                "tab":7,
+                "title":"91 - 111",
+                "sections" : [{key:"Article18"}, {key:"Article19"}]
+            },
+            {
+                "tab":8,
+                "title":"112 - 138",
+                "sections" : [{key:"Article20"},{key:"Article21"}, {key:"Article22"}]
+            },
+            {
+                "tab":9,
+                "title":"139 - 161",
+                "sections" : [{key:"Article23"}, {key:"Article24"}, {key:"Article25"}]
+            },
+            {
+                "tab":10,
+                "title":"162 - 185",
+                "sections" : [{key:"Article26"}, {key:"Article28"}, {key:"Article33"}, {key:"LiabilityRedress"} ]
+            },
+            {
+                "tab":11,
+                "title":"186 - 187",
+                "sections" : [{key:"AdditionalInformation"}, {key:"Comments"}]
+            }
+        ];
+
+        $scope.customValidations = {
+            is71aOr71b   : is71aOr71b,
+            is173aOr173b : is173aOr173b,
+            is154Or155   : is154Or155,
+            is91Or92Or93 : is91Or92Or93
+        } 
+        // TODO: read from mapping file
+        var previousAnswerMapping = $scope.previousAnswerMapping = {
+            "Q012_party"        : { prevQuestion : "Q013",   showMessage: false },
+            "Q012_partyInProgress"  : { prevQuestion : "Q012",   showMessage: false },
+            "Q014"        : { prevQuestion : "Q014",   showMessage: false },
+            "Q015"        : { prevQuestion : "Q016",   showMessage: false },
+            "Q017"        : { prevQuestion : "Q017",   showMessage: false },
+            "Q018"        : { prevQuestion : "Q018",   showMessage: false },
+            "Q019"        : { prevQuestion : "Q019",   showMessage: false },
+            "Q020"        : { prevQuestion : "Q021",   showMessage: false },
+            "Q021"        : { prevQuestion : "Q022",   showMessage: false },
+            "Q022"        : { prevQuestion : "Q024",   showMessage: false },
+            "Q023"        : { prevQuestion : "Q025",   showMessage: false },
+            "Q024"        : { prevQuestion : "Q026",   showMessage: false },
+            "Q026"        : { prevQuestion : "Q028",   showMessage: false },
+            "Q027"        : { prevQuestion : "Q032",   showMessage: false },
+            "Q028"        : { prevQuestion : "Q033",   showMessage: false },
+            "Q038"        : { prevQuestion : "Q046",   showMessage: false },
+            "Q039"        : { prevQuestion : "Q047",   showMessage: false },
+            "Q040"        : { prevQuestion : "Q048",   showMessage: false },
+            "Q042"        : { prevQuestion : "Q050",   showMessage: false },
+            "Q044"        : { prevQuestion : "Q058",   showMessage: false },
+            "Q045"        : { prevQuestion : "Q059",   showMessage: false },
+            "Q052"        : { prevQuestion : "Q066",   showMessage: false },
+            "Q053"        : { prevQuestion : "Q067",   showMessage: false },
+            "Q057"        : { prevQuestion : "Q071",   showMessage: false },
+            "Q058"        : { prevQuestion : "Q073",   showMessage: false },
+            "Q059"        : { prevQuestion : "Q075",   showMessage: false },
+            "Q060"        : { prevQuestion : "Q076",   showMessage: false },
+            "Q063"        : { prevQuestion : "Q077",   showMessage: false },
+            "Q064"        : { prevQuestion : "Q078",   showMessage: false },
+            "Q065_a"        : { prevQuestion : "Q079_a",   showMessage: false },
+            "Q065_b"        : { prevQuestion : "Q079_b",   showMessage: false },
+            "Q065_c"        : { prevQuestion : "Q079_c",   showMessage: false },
+            "Q066"        : { prevQuestion : "Q080",   showMessage: false },
+            "Q067"        : { prevQuestion : "Q081",   showMessage: false },
+            "Q068"        : { prevQuestion : "Q082",   showMessage: false },
+            "Q070_a"        : { prevQuestion : "Q084_a",   showMessage: false },
+            "Q070_b"        : { prevQuestion : "Q084_b",   showMessage: false },
+            "Q070_c"        : { prevQuestion : "Q084_c",   showMessage: false },
+            "Q070_d"        : { prevQuestion : "Q084_d",   showMessage: false },
+            "Q071_a"        : { prevQuestion : "Q085_a",   showMessage: false },
+            "Q071_b"        : { prevQuestion : "Q085_b",   showMessage: false },
+            "Q072"        : { prevQuestion : "Q086",   showMessage: false },
+            "Q073"        : { prevQuestion : "Q087",   showMessage: false },
+            "Q074"        : { prevQuestion : "Q088",   showMessage: false },
+            "Q081"        : { prevQuestion : "Q094",   showMessage: false },
+            "Q082"        : { prevQuestion : "Q095",   showMessage: false },
+            "Q083"        : { prevQuestion : "Q096",   showMessage: false },
+            "Q084"        : { prevQuestion : "Q097",   showMessage: false },
+            "Q088"        : { prevQuestion : "Q100",   showMessage: false },
+            "Q090"        : { prevQuestion : "Q105",   showMessage: false },
+            "Q091"        : { prevQuestion : "Q106",   showMessage: false },
+            "Q092"        : { prevQuestion : "Q107",   showMessage: false },
+            "Q093"        : { prevQuestion : "Q108",   showMessage: false },
+            "Q095"        : { prevQuestion : "Q110",   showMessage: false },
+            "Q097"        : { prevQuestion : "Q112",   showMessage: false },
+            "Q099"        : { prevQuestion : "Q114",   showMessage: false },
+            "Q100"        : { prevQuestion : "Q115",   showMessage: false },
+            "Q101"        : { prevQuestion : "Q116",   showMessage: false },
+            "Q102"        : { prevQuestion : "Q117",   showMessage: false },
+            "Q103"        : { prevQuestion : "Q118",   showMessage: false },
+            "Q104"        : { prevQuestion : "Q119",   showMessage: false },
+            "Q105"        : { prevQuestion : "Q120",   showMessage: false },
+            "Q106"        : { prevQuestion : "Q121",   showMessage: false },
+            "Q107"        : { prevQuestion : "Q122",   showMessage: false },
+            "Q108"        : { prevQuestion : "Q123",   showMessage: false },
+            "Q109"        : { prevQuestion : "Q124",   showMessage: false },
+            "Q111"        : { prevQuestion : "Q125",   showMessage: false },
+            "Q112_a"      : { prevQuestion : "Q126_a", showMessage: false },
+            "Q112_b"      : { prevQuestion : "Q126_b", showMessage: false },
+            "Q112_c"      : { prevQuestion : "Q126_c", showMessage: false },
+            "Q112_d"      : { prevQuestion : "Q126_d", showMessage: false },
+            "Q112_e"      : { prevQuestion : "Q126_f", showMessage: false },
+            "Q112_g"      : { prevQuestion : "Q126_g", showMessage: false },
+            "Q112_j"      : { prevQuestion : "Q126_j", showMessage: false },
+            "Q112_k"      : { prevQuestion : "Q126_k", showMessage: false },
+            "Q112_l"      : { prevQuestion : "Q126_l", showMessage: false },
+            "Q112_m"      : { prevQuestion : "Q126_m", showMessage: false },
+            "Q112_n"      : { prevQuestion : "Q126_n", showMessage: false },
+            "Q112_o"      : { prevQuestion : "Q126_p", showMessage: false },
+            "Q112_p"      : { prevQuestion : "Q126_o", showMessage: false },
+            "Q112_q"      : { prevQuestion : "Q126_q", showMessage: false },
+            "Q114"        : { prevQuestion : "Q127",   showMessage: false },
+            "Q115"        : { prevQuestion : "Q128",   showMessage: false },
+            "Q116"        : { prevQuestion : "Q129",   showMessage: false },
+            "Q117"        : { prevQuestion : "Q130",   showMessage: false },
+            "Q120"        : { prevQuestion : "Q135",   showMessage: false },
+            "Q121"        : { prevQuestion : "Q136",   showMessage: false },
+            "Q122"        : { prevQuestion : "Q137",   showMessage: false },
+            "Q123"        : { prevQuestion : "Q138",   showMessage: false },
+            "Q124"        : { prevQuestion : "Q139",   showMessage: false },
+            "Q125"        : { prevQuestion : "Q140",   showMessage: false },
+            "Q126"        : { prevQuestion : "Q141",   showMessage: false },
+            "Q127"        : { prevQuestion : "Q142",   showMessage: false },
+            "Q128"        : { prevQuestion : "Q143",   showMessage: false },
+            "Q134"        : { prevQuestion : "Q151",   showMessage: false },
+            "Q135"        : { prevQuestion : "Q152",   showMessage: false },
+            "Q136"        : { prevQuestion : "Q153",   showMessage: false },
+            "Q137"        : { prevQuestion : "Q154",   showMessage: false },
+            "Q138"        : { prevQuestion : "Q157",   showMessage: false },
+            "Q139"        : { prevQuestion : "Q158",   showMessage: false },
+            "Q141"        : { prevQuestion : "Q164",   showMessage: false },
+            "Q142"        : { prevQuestion : "Q159",   showMessage: false },
+            "Q143"        : { prevQuestion : "Q160",   showMessage: false },
+            "Q144"        : { prevQuestion : "Q162",   showMessage: false },
+            "Q145"        : { prevQuestion : "Q171",   showMessage: false },
+            "Q146"        : { prevQuestion : "Q172",   showMessage: false },
+            "Q147"        : { prevQuestion : "Q165",   showMessage: false },
+            "Q148"        : { prevQuestion : "Q167",   showMessage: false },
+            "Q149"        : { prevQuestion : "Q168",   showMessage: false },
+            "Q151"        : { prevQuestion : "Q170",   showMessage: false },
+            "Q152"        : { prevQuestion : "Q176",   showMessage: false },
+            "Q153"        : { prevQuestion : "Q177",   showMessage: false },
+            "Q157"        : { prevQuestion : "Q183",   showMessage: false },
+            "Q158"        : { prevQuestion : "Q184",   showMessage: false },
+            "Q161"        : { prevQuestion : "Q191",   showMessage: false },
+            "Q162"        : { prevQuestion : "Q192",   showMessage: false },
+            "Q164"        : { prevQuestion : "Q194",   showMessage: false },
+            "Q165"        : { prevQuestion : "Q196",   showMessage: false },
+            "Q166"        : { prevQuestion : "Q197",   showMessage: false },
+            "Q185"        : { prevQuestion : "Q202",   showMessage: false },
+            "Q186"        : { prevQuestion : "Q207",   showMessage: false },
+            "Q187"        : { prevQuestion : "Q208",   showMessage: false }
+        }
+
+		$controller('editController', {
+			$scope: $scope
+		});
+
+        $scope.onContactQuery = function(searchText){
+            var queryOptions = {
+            schemas : ['contact', 'focalPoint'],
+            realm : realm.value,
+            searchText: searchText,
+            query : `((schema_s:focalPoint AND government_s:${$scope.document.government.identifier}) OR (schema_s:contact))`
+            }
+            return $scope.onBuildDocumentSelectorQuery(queryOptions);
+        }
+
+		//==================================
+		//
+		//==================================
+		$scope.getCleanDocument = function(document) {
+
+			document = document || $scope.document;
+
+			if (!document)
+				return undefined;
+
+			if (/^\s*$/g.test(document.notes))
+				document.notes = undefined;
+
+			return $scope.sanitizeDocument(document);
+		};
+        
+
+        $scope.userHasGovernment = function(){
+            return user.government;
+        }
+        $scope.onGovernmentChange = function(government){
+            if(government && $scope.document){
+
+                verifyCountryHasReport();
+                commonjs.getCountry(government.identifier).then(function(country){
+                    $scope.document['Q012_party'] = { value : country.isParty.toString() };
+                    if(country.isParty){
+                        $scope.document['Q012_progressForParty'] = undefined;
+                        $scope.document['Q013'] = undefined;
+                    }
+                    var question = $scope.nr4Tabs[1].sections[0].questions[0];
+                    question.hasValidation = true;
+                    question.enable = false;
+                    $scope.updateAnswer(question);
+                    loadPreviousReport();
+                })
+            }
+        }
+
+        function is71aOr71b(){
+            return ($scope.document['Q071_a']||{}).value == 'true' || ($scope.document['Q071_b']||{}).value == 'true';
+        }
+
+        function is173aOr173b(){
+            return ($scope.document['Q173_a']||{}).value == 'true' || ($scope.document['Q173_b']||{}).value == 'true';
+        }
+        function is154Or155(){
+            return ($scope.document['Q154']||{}).value == 'true' || ($scope.document['Q155']||{}).value == 'true';
+        }
+        function is91Or92Or93(){
+            var selectedValues = [
+                                  ($scope.document['Q091']||{}).value,
+                                  ($scope.document['Q092']||{}).value,
+                                  ($scope.document['Q093']||{}).value
+                                ]
+            return _.intersection(_.compact(selectedValues), ['true', 'true.some']).length > 0;
+        }
+        
+        
+
+
+        function init(){
+
+
+            $scope.setDocument({}).then(function(document){
+                if(document && document.header.identifier){
+                    _.forEach(document, function(element, key){
+                        if(/^Q/.test(key) && _.isArray(element)){//only fields starting with Q
+                            $scope.multiTermModel[key] = _.map(element, function(e){ return { identifier : e.value, customValue: e.additionalInformation }});
+                        }
+                    })
+                    transformNr4Data();//workaround as in the first call not all questions are built so the disable/visible clause does not work.
+
+                }
+                //render remaining tabs
+                // var timeout = 2000;
+                // _.each($scope.nr4Tabs, function(t){                 
+                //     $timeout(function(){ t.render=true}, timeout );
+                //     timeout += 1000;
+                // })
+            });
+
+        }
+
+        function verifyCountryHasReport(){
+            $q.all([
+                storage.documents.query("(type eq 'cpbNationalReport4')", "my", {$top:10}),
+                storage.drafts.query("(type eq 'cpbNationalReport4')", {$top:10})
+            ])
+            .then(function(nationalRecords) {
+                var filterByGovernment = function(item){
+                    return item && (item.metadata||{}).government == $scope.document.government.identifier
+                }              
+                var published   = _.find((nationalRecords[0].data||{}).Items,  filterByGovernment);
+                var draft       = _.find((nationalRecords[1].data||{}).Items,  filterByGovernment);
+
+                if (((published || draft) && (!$routeParams.identifier || $routeParams.identifier != (draft||published).identifier))) {
+                    $scope.blockEditForm = true;
+                    ngDialog.open({
+                        template: 'recordExistsTemplate.html',													
+                        closeByDocument: false,
+                        closeByEscape: false,
+                        showClose: false,
+                        closeByNavigation: false,
+                        controller: ['$scope', '$timeout', '$location', function($scope, $timeout, $location) {
+                            $scope.alertSeconds = 10;
+                            time();
+
+                            function time(){
+                                $timeout(function(){
+                                    if($scope.alertSeconds == 1){																	
+                                        $scope.openExisting();
+                                    }
+                                    else{
+                                        $scope.alertSeconds--;																
+                                        time()
+                                    }
+                                }, 1000)
+                            }
+                            $scope.openExisting = function() {
+                                ngDialog.close();
+                                $location.path('register/NR4/' + (draft||published).identifier+'/edit');
+                            }
+                        }]
+                    });
+                }
+                else{
+
+                }
+            });
+        }
+
+        init();
+        $scope.questions = [cpbNationalReport4, cpbNationalReport3];
+   }];
+
+
+   
+
