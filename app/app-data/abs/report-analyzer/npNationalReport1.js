604a4e8596df49364955a492797cae3fa436d43f
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2023-05-30 20:30:58 +0500

    CHM-71-ABS-NR1-Part-II (#919)
    * ABS-NR1-Part-II copy text to translation file
    
    * ABS-NR1-Part-II questions structure
    
    * fix validation
    
    * rename to "caption"
    
    * put the caption with each lstring
    
    * update common text
    
    * remove hasValidation

---
+++ app/app-data/abs/report-analyzer/npNationalReport1.js
@@ -0,0 +1,228 @@
+﻿import nr1T from '../../../app-text/views/forms/edit/abs/edit-national-report-1.json';
+
+export const npNationalReport1 = [
+   {
+      "key": "Part-II",
+      "title": nr1T.part_II_title,
+      "questions": [
+         {
+            "key": "Article13.1",
+            "section": "Part-II",
+            "number": "",
+            "type": "legend",
+            "title": nr1T.article13_1
+         },
+         {
+            "key": "Q003",
+            "section": "Part-II",
+            "number": "3",
+            "type": "option",
+            "title": nr1T.part_II_question3_title,
+            "multiple": false,
+            "mandatory": true,
+            "options": [
+               {
+                  "value": "true",
+                  "title": nr1T.yes,
+                  "type": "lstring",
+                  "caption": nr1T.provideMoreInfo
+               },
+               {
+                  "value": "false",
+                  "title": nr1T.no,
+                  "type": "lstring",
+                  "caption": nr1T.provideMoreInfo
+               }
+            ]
+         },
+         {
+            "key": "Article13.2",
+            "section": "Part-II",
+            "number": "",
+            "type": "legend",
+            "title": nr1T.article13_2
+         },
+         {
+            "key": "Q004",
+            "section": "Part-II",
+            "number": "4",
+            "type": "option",
+            "title": nr1T.part_II_question4_title,
+            "multiple": false,
+            "mandatory": true,
+            "options": [
+               {
+                  "value": "true",
+                  "title": nr1T.yes,
+                  "type": "lstring",
+                  "caption": nr1T.provideMoreInfo
+               },
+               {
+                  "value": "false",
+                  "title": nr1T.no,
+                  "type": "lstring",
+                  "caption": nr1T.provideMoreInfo
+               }
+            ],
+            "validations": [
+               {
+                  "question": "Q004_a",
+                  "values": [
+                     "true"
+                  ],
+                  "type": "@hasValues",
+                  "trigger": "enable"
+               },
+               {
+                  "question": "Q004_b",
+                  "values": [
+                     "true"
+                  ],
+                  "type": "@hasValues",
+                  "trigger": "enable"
+               }
+            ]
+         },
+         {
+            "key": "Q004_a",
+            "section": "Part-II",
+            "number": "4.1",
+            "type": "option",
+            "title": nr1T.part_II_question4_1_title,
+            "multiple": false,
+            "mandatory": true,
+            "options": [
+               {
+                  "value": "true",
+                  "title": nr1T.yes
+               },
+               {
+                  "value": "true.some",
+                  "title": nr1T.yesToSomeExtent
+               },
+               {
+                  "value": "false",
+                  "title": nr1T.no
+               }
+            ]
+         },
+         {
+            "key": "Q004_b",
+            "section": "Part-II",
+            "number": "4.2",
+            "type": "option",
+            "title": nr1T.part_II_question4_2_title,
+            "multiple": false,
+            "mandatory": true,
+            "options": [
+               {
+                  "value": "true",
+                  "title": nr1T.yes
+               },
+               {
+                  "value": "false",
+                  "title": nr1T.no
+               }
+            ]
+         },
+
+         {
+            "key": "Article17",
+            "section": "Part-II",
+            "number": "",
+            "type": "legend",
+            "title": nr1T.article17
+         },
+         {
+            "key": "Q005",
+            "section": "Part-II",
+            "number": "5",
+            "type": "option",
+            "title": nr1T.part_II_question5_title,
+            "multiple": false,
+            "mandatory": true,
+            "options": [
+               {
+                  "value": "true",
+                  "title": nr1T.yes,
+                  "type": "lstring",
+                  "caption": nr1T.provideMoreInfo
+               },
+               {
+                  "value": "false",
+                  "title": nr1T.no,
+                  "type": "lstring",
+                  "caption": nr1T.provideMoreInfo
+               }
+            ],
+            "validations": [
+               {
+                  "question": "Q005_a",
+                  "values": [
+                     "true"
+                  ],
+                  "type": "@hasValues",
+                  "trigger": "enable"
+               },
+               {
+                  "question": "Q005_b",
+                  "values": [
+                     "true"
+                  ],
+                  "type": "@hasValues",
+                  "trigger": "enable"
+               }
+            ]
+         },
+         {
+            "key": "Q005_a",
+            "section": "Part-II",
+            "number": "5.1",
+            "type": "option",
+            "title": nr1T.part_II_question5_1_title,
+            "multiple": false,
+            "mandatory": true,
+            "options": [
+               {
+                  "value": "true",
+                  "title": nr1T.yes
+               },
+               {
+                  "value": "true.some",
+                  "title": nr1T.yesToSomeExtent
+               },
+               {
+                  "value": "false",
+                  "title": nr1T.no
+               }
+            ]
+         },
+         {
+            "key": "Q005_b",
+            "section": "Part-II",
+            "number": "5.2",
+            "type": "option",
+            "title": nr1T.part_II_question5_2_title,
+            "multiple": false,
+            "options": [
+               {
+                  "value": "true",
+                  "title": nr1T.yes
+               },
+               {
+                  "value": "false",
+                  "title": nr1T.no
+               }
+            ]
+         },
+         {
+            "key": "Q006",
+            "section": "Part-II",
+            "number": "6",
+            "type": "link",
+            "title": nr1T.part_II_question6_title,
+            "multiple": false
+         }
+      ]
+   }
+]
