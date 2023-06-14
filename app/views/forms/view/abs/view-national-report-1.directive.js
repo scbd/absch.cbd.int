07c8c0bc1f33170babbbe9b19eb6e7e1771cf3c4
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2023-06-14 15:32:21 +0500

    ABS-NR1 View form

---
+++ app/views/forms/view/abs/view-national-report-1.directive.js
@@ -0,0 +1,31 @@
+import app from '~/app';
+import template from "text!./view-national-report-1.directive.html";
+import _ from 'lodash';
+import { npNationalReport1 } from '~/app-data/abs/report-analyzer/npNationalReport1';
+import '~/views/forms/view/view-national-report.directive';
+import '~/views/directives/record-options';
+import '~/views/forms/view/directives/view-record-reference.directive';
+import viewNr1T from '~/app-text/views/forms/view/abs/view-national-report.json';
+import numbers from '~/app-text/numbers.json';
+
+	app.directive("viewNpbNationalReport1", ['translationService', function (translationService) {
+		return {
+			restrict   : "EAC",
+			template: template ,
+			replace    : true,
+			transclude : false,
+			scope: {
+				document: "=ngModel",
+				locale  : "=",
+				target  : "@linkTarget",
+				hide	: "@"
+			},
+			link : function ($scope){
+				translationService.set('viewNr1T', viewNr1T);
+				translationService.set('numbers', numbers);
+				$scope.nr1Data = npNationalReport1;			
+			}
+		};
+	}]);
+
+
