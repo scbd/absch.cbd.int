74fa8687d389ea9721c84699b6b4f01957f3a08d
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2023-06-14 18:41:44 +0500

    CHM-42 : Build BCH-NR5 View Form (#939)
    * Build BCH-NR5 View Form
    
    * small fix

---
+++ app/views/forms/view/bch/view-national-report-5.directive.js
@@ -0,0 +1,31 @@
+import app from '~/app';
+import template from "text!./view-national-report-5.directive.html";
+import _ from 'lodash';
+import {cpbNationalReport5} from '~/app-data/bch/report-analyzer/cpbNationalReport5';
+import '~/views/forms/view/view-national-report.directive';
+import '~/views/directives/record-options';
+import '~/views/forms/view/directives/view-record-reference.directive';
+import viewNr5T from '~/app-text/views/forms/view/bch/view-national-report.json';
+import numbers from '~/app-text/numbers.json';
+
+	app.directive("viewCpbNationalReport5", ['translationService', function (translationService) {
+		return {
+			restrict   : "EAC",
+			template: template ,
+			replace    : true,
+			transclude : false,
+			scope: {
+				document: "=ngModel",
+				locale  : "=",
+				target  : "@linkTarget",
+				hide	: "@"
+			},
+			link : function ($scope){
+				translationService.set('viewNr5T', viewNr5T);
+				translationService.set('numbers', numbers);
+				$scope.nr5Data = cpbNationalReport5;			
+			}
+		};
+	}]);
+
+
