f823c7a67da4ac592493caf7be10dd1147f20c16
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2023-06-16 11:35:00 +0500

    Build Km-Document API

---
+++ app/api/km-document.js
@@ -0,0 +1,13 @@
+import ApiBase, { tryCastToApiError } from './api-base';
+
+export default class KmDocumentApi extends ApiBase
+{
+    constructor(options) {
+        super(options);
+    }
+
+    async queryDocuments(params)  {
+        return this.http.get(`api/v2013/documents/`, { params }).then(res => res.data).catch(tryCastToApiError);
+    }
+
+}
