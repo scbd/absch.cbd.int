8bdcaa9fee0739e438cad6cb67c2e1310706dbbb
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2023-06-22 20:30:09 +0500

    NR-Directive fix validation issue (#952)
    * NR-Directive fix validation issue
    
    * add timeout to fix questions like  Q027
    
    * add timeout on transformQuestion in sub-section

--- app/views/forms/edit/directives/edit-national-report.directive.js
+++ app/views/forms/edit/directives/edit-national-report.directive.js
@@ -21,6 +21,7 @@ app.directive("editNationalReport", ["$controller", "$http", 'IStorage', '$route
                 reportTabs: "=",
                 questions: "=",
                 customValidations: "=",
+                validationReport: "=",
                 identifier: '=',
                 locales: '='
             },
@@ -254,12 +255,18 @@ app.directive("editNationalReport", ["$controller", "$http", 'IStorage', '$route
 
                             _.forEach(section.questions, function (question) {
 
-                                if (question.type != 'sub-section')
-                                    transformQuestion(question)
+                                if (question.type != 'sub-section') {
+                                    $timeout(function () {
+                                        transformQuestion(question);
+                                    },1);
+                                }
                                 else {
                                     question.visible = true;
                                     _.forEach(question.questions, function (subQuestion) {
-                                        transformQuestion(subQuestion);//, question.key1
+                                        $timeout(function () {
+                                            transformQuestion(subQuestion);//, question.key1
+                                        },1);
+
                                     })
                                 }
 
