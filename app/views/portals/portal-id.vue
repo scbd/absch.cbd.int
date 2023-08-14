3fe5b9947101d1b44ed79110807fe94f4c890465
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2023-08-14 14:44:30 -0400

    Fix portal change was not reloading menu

--- app/views/portals/portal-id.vue
+++ app/views/portals/portal-id.vue
@@ -57,15 +57,25 @@ export default {
   },
   methods:{
     buildMenu,
-    onRouteChange
+    onRouteChange,
+    initializePortal,
   },
   watch: {
-    path: onRouteChange
+    portalId: initializePortal
   },
   async created() {
+    await this.initializePortal();
+  }
+}
 
-    const { portalId } = this;
+async function initializePortal() {
+
+  console.log('initializePortal', this.portalId)
+
+    const { portalId, unwatchPath } = this;
     const { realm } = this.$realm;
+
+    if(unwatchPath) unwatchPath();
 
     this.menusApi    = new MenusApi();
     this.articlesApi = new ArticlesApi();
@@ -75,10 +85,10 @@ export default {
 
     subRouter = buildRoutes(portalMenu);
 
-    this.portalMenu = portalMenu;
-    
+    this.portalMenu  = portalMenu;
+    this.unwatchPath = this.$watch('path', onRouteChange);
+
     this.onRouteChange();
-  }
 }
 
 function onRouteChange() {
