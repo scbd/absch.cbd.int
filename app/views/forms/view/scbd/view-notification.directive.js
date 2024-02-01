4a3b38fa9fa5b75f379d18e58337a023af613c99
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-02-02 00:11:23 +0500

    Fix links on notifications

--- app/views/forms/view/scbd/view-notification.directive.js
+++ app/views/forms/view/scbd/view-notification.directive.js
@@ -2,7 +2,7 @@ import app from '~/app';
 import template from 'text!./view-notification.directive.html';
 import '~/views/directives/record-options';
 
-app.directive('viewNotification', [function() {
+app.directive('viewNotification', ['realm',function(realm) {
 	return {
 		restrict: 'EAC',
 		template: template, 
@@ -14,7 +14,7 @@ app.directive('viewNotification', [function() {
 			target: "@linkTarget"
 		},
 		controller: ['$scope', function ($scope) {
-			
+			$scope.isBCH = realm.is('BCH');
 			const documentWatch = $scope.$watch("document", (newVal)=>{
 				if(newVal){
 					$scope.notificationSymbol = `NT-${$scope.document?.symbol_s}`;
