e5fb8e305aa9be1bc838dabf11c9fa3aa937ecec
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2024-02-20 15:26:04 -0500

    Convert project to type=module

--- rollup/strip-bom.js
+++ rollup/strip-bom.js
@@ -1,12 +1,16 @@
-export default function stripBom(options = {}) {
+import stripBom from 'strip-bom';
+
+
+export default function (options = {}) {
 
   return {
     name: 'stripBom',
 
     transform(code) {
-
-      if (typeof (code) == "string")
-        code = code.replace(/^\uFEFF/gm, "").replace(/^\u00BB\u00BF/gm,"");
+      
+      if (typeof (code) == "string") {
+        code = stripBom(code).replace(/^\u00BB\u00BF/gm,"");
+      }
 
       return { code, map: this.getCombinedSourcemap() };
     }
