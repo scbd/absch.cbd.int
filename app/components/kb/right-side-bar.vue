54086ffe88912a9c678f42d6bd472921bd818e49
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-03-05 14:04:23 +0500

    migrate kb components to vu3

--- app/components/kb/right-side-bar.vue
+++ app/components/kb/right-side-bar.vue
@@ -2,32 +2,30 @@
         <div>
             <div class="mb-3">
                <div v-if="tag"><relevant-articles></relevant-articles></div>
-			   <div v-if="!tag && $realm.is('BCH')"><faq-articles></faq-articles></div>
+			   <div v-if="!tag && realm.is('BCH')"><faq-articles></faq-articles></div>
             </div>
             <popular-tags is-categories="true"></popular-tags>
         </div>
 </template>
-
+  
+<script setup>
+  import { ref, onMounted } from "vue";
+  import relevantArticles from './relevant-articles.vue';
+  import faqArticles from './faq-articles.vue';
+  import popularTags from './popular-tags.vue';
+  import { useRealm } from '../../services/composables/realm.js';
+  import { useRoute } from "@scbd/angular-vue/src/index.js";   
+  const realm = useRealm();
+  const route = useRoute();
+  const tag = ref('');
+  
+  onMounted(() => {
+	  tag.value = route.params?.tag;
+  });
+  
+</script>
 <script>
-	import i18n from '../../app-text/components/kb.json';
-	import relevantArticles from './relevant-articles.vue';
-	import faqArticles from './faq-articles.vue';
-	import popularTags from './popular-tags.vue';
-	export default {
-      name:'kbRightSideBar',
-		components:{
-			popularTags,
-			relevantArticles,
-			faqArticles
-		},
-		data:  () => {
-        return {
-            tag:''
-        }
-		},
-		created(){
-			this.tag = (this.$route.params.tag)
-		},
-		i18n: { messages:{ en: i18n }}
-	}
+  export default {
+    name:'kbRightSideBar',
+  }
 </script>
