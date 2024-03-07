306905922ddcd2951f60d8b7e73732a7b2a1779b
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-03-08 00:54:15 +0500

    create articles composables (#1055)

---
+++ app/services/composables/articles.js
@@ -0,0 +1,62 @@
+import ArticlesApi from '../../components/kb/article-api';
+const articlesApi = new ArticlesApi();
+
+export async function loadKbCategories(isBch, locale) {
+
+            let categories
+            if(!isBch) {
+                categories = (await import('~/app-data/abs/kb-categories.js')).categories;
+            }
+            else {
+                categories = (await import('~/app-data/bch/kb-categories.js')).categories;
+            }
+            if(categories){
+
+                const articleIds = categories.map(e=>e.articles).map(e=>e.map(i=>i.identifier)).flat()
+                                    .filter(e=>e).map(e=>{return {$oid:e}})
+                
+               const query = [
+                    {
+                        "$match": {
+                            _id :{
+                                $in: articleIds
+                            }
+                        }
+                    },
+                    {
+                        "$project": {
+                            [`title`]: 1,
+                            [`summary`]: 1
+                        }
+                    },
+                    { "$limit": articleIds.length }
+                ];
+                const articleTitles = await articlesApi.queryArticles({ag : JSON.stringify(query)}, {cache:true})
+                
+                categories.forEach(e=>{
+
+                    if(e.articles){
+                        e.articles.forEach(a=>{
+                            const articleTitle = articleTitles.find(at=>at._id == a.identifier)
+                            if(articleTitle)
+                                a.title = articleTitle.title[locale] || articleTitle.title['en'];
+                        })
+                    }
+
+                })
+                
+                return categories;
+
+            }
+        };
+
+        export function getUrl(title, id, tag){
+            const urlTitle = title ? title.trim().replace(/[^a-z0-9]/gi, '-').replace(/-+/g, '-') : undefined;
+            if(title && id){
+                return `kb/tags/${encodeURIComponent(tag)}/${encodeURIComponent(urlTitle)}/${encodeURIComponent(id)}`;
+            } else if (title && !id) {
+                return `kb/tags/${encodeURIComponent(tag)}/${encodeURIComponent(urlTitle)}`;
+            } else if (!title && !id) {
+                return `kb/tags/${encodeURIComponent( tag )}`;
+            }
+        };
