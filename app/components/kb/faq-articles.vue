cc9b10bed1e6bb111b0931dd6f6f057b3e706079
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-03-07 13:26:28 +0500

    Upgrade FAQ-Articles component to Vue3

--- app/components/kb/faq-articles.vue
+++ app/components/kb/faq-articles.vue
@@ -1,44 +1,36 @@
 <template>
     <div>
-        <h4>
-           <span>{{ $t("faqs") }}</span>
-        </h4>
-        <hr>
-        <div class="loading" v-if="loading"><i class="fa fa-cog fa-spin fa-lg" ></i> {{ $t("loading") }}...</div>
-         <ul>
-            <li v-for="article in articles" class="mb-1">
-                <a class="link-dark fs-6" :href="`${articleUrl(article)}`">{{article.title}}</a>
-            </li>
-        </ul>
+      <h4>
+        <span>{{ t("faqs") }}</span>
+      </h4>
+      <hr>
+      <div class="loading" v-if="loading"><i class="fa fa-cog fa-spin fa-lg" ></i> {{ t("loading") }}...</div>
+      <ul>
+        <li v-for="article in articles" class="mb-1">
+          <a class="link-dark fs-6" :href="`${articleUrl(article)}`">{{article.title}}</a>
+        </li>
+      </ul>
     </div>
-</template>
-
-<script>
-import i18n from '../../app-text/components/kb.json';
-import articlesMaxin from '../maxin/article';
-export default {
-    name: 'KbRightSideFaqs',
-    props: {},
-    data: () => {
-        return {
-            articles: [], 
-        }
-    },
-    mixins: [articlesMaxin],
-    async mounted() {
-        const categories = await this.loadKbCategories(this.$realm.is('BCH'));
+  </template>
+  
+<script setup>
+    import { ref, onMounted } from 'vue';
+    import { loadKbCategories , getUrl } from '../../services/composables/articles.js';
+    import { useI18n } from 'vue-i18n';
+    import messages from '../../app-text/components/kb.json';
+    const { t } = useI18n({ messages });
+    const articles = ref([]);
+    const loading = ref(true);
+    
+    onMounted(async () => {
+        const categories = await loadKbCategories(true);
         const faqArticles = categories.filter(tag => tag.adminTags[0] === "faq");
-        this.articles = faqArticles[0].articles;
-    },
-    methods: {
-        articleUrl(article, tag){       
-            return this.getUrl(article.title, article.identifier, 'faq');
-        },
-    },
-    i18n: {
-        messages: {
-            en: i18n
-        }
-    }
-}
+        articles.value = faqArticles[0].articles;
+        loading.value = false;
+    });
+
+    const articleUrl = (article) => {
+        return getUrl(article.title, article.identifier, 'faq');
+    };
 </script>
+  
