ac582d3dfd6a8492e0851b3e7da499a3dc8a2fbe
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-03-19 15:20:28 +0500

    WIP: DEC

--- app/app-data/bch/download-schemas.js
+++ app/app-data/bch/download-schemas.js
@@ -2,6 +2,24 @@ import fieldsT from '../../app-text/views/search/search-results/bch-download-sch
 import { downloadSchemas as commonDownloadSchemas } from '../common-download-schemas.js';
 
 export const downloadSchemas = {	
-	...commonDownloadSchemas
+	...commonDownloadSchemas,
+
+	"biosafetyDecision" : {
+        "uniqueId"      					: fieldsT["biosafetyDecision.uniqueId"],
+        "government"        				: fieldsT["biosafetyDecision.government"],
+		"authorities"						: fieldsT["biosafetyDecision.authorities"],
+		"title"								: fieldsT["biosafetyDecision.title"],
+		"date"								: fieldsT["biosafetyDecision.date"],
+		"commonDecisions"				    : fieldsT["biosafetyDecision.commonDecisions"],
+		"involvesFieldTrial"			    : fieldsT["biosafetyDecision.involvesFieldTrial"],
+		"involvesCommercialRelease"		    : fieldsT["biosafetyDecision.involvesCommercialRelease"],
+		"appliesToSubsequentIntroductions"  : fieldsT["biosafetyDecision.appliesToSubsequentIntroductions"],
+		"uses"								: fieldsT["biosafetyDecision.uses"],
+		"directUseDecisions"				: fieldsT["biosafetyDecision.directUseDecisions"],
+		"decisionResult"					: fieldsT["biosafetyDecision.decisionResult"],
+		"modifiedOrganisms"					: fieldsT["biosafetyDecision.modifiedOrganisms"],
+		"riskAssessments"					: fieldsT["biosafetyDecision.riskAssessments"],
+		"publishedOn"						: fieldsT["biosafetyDecision.publishedOn"]
+	}
 }
 
