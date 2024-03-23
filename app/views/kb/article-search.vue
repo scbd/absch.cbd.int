0c98e5c870f5e3018fb184b0466a8ab3728e11b5
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-03-23 23:02:05 +0500

    Migrate KB search view component

--- app/views/kb/article-search.vue
+++ app/views/kb/article-search.vue
@@ -4,45 +4,24 @@
       <search @changeSearch="onChangeSearch"></search>
       <section class="categories">
         <div class="container p-0">
-          <article-search ref="articlesearch"></article-search>
+          <article-search ref="articleSearchRef"></article-search>
         </div>
       </section>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
+  import articleSearch from '../../components/kb/article-search.vue';
+  import search from '../../components/kb/search.vue';
+  import { ref } from 'vue';
+  
+  const articleSearchRef = ref(null);
 
-import articleSearch from '../../components/kb/article-search.vue';
-import search from '../../components/kb/search.vue';
-import i18n from '../../app-text/components/kb.json';
-
-export default {
-  name:'kbHome',
-  components:{
-    articleSearch,
-    search
-  },
-
-  props:{
-  },
-  data:  () => {
-    return {
-
+  const onChangeSearch = (text) => {
+    if (articleSearchRef.value) {
+      articleSearchRef.value.loadArticles(1, text);
     }
-  },
+  };
 
-  watch: {
-    tokenReader: function(tokenReader) {
-      // if(tokenReader) addApiOptions({ tokenReader })
-    }
-  },
-   methods: {
-      onChangeSearch(text) {
-        this.$refs.articlesearch.loadArticles(1,text);
-      },
-    },
-  i18n: { messages:{ en: i18n }}
-
-}
 </script>
