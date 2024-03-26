6b0f6e3e19847b62ba1b695b9e068708a601a714
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-03-26 15:30:19 +0500

    Migrate Model component to Vue3

--- app/components/common/modal.vue
+++ app/components/common/modal.vue
@@ -1,12 +1,12 @@
 
 <template>
-    <div ref="modal" class="modal fade"  aria-hidden="true">
+    <div ref="modalRef" class="modal fade"  aria-hidden="true">
     <div class="modal-dialog modal-xl">
         <div class="modal-content">
         <div class="modal-header">
             <slot name="header">
                 <h5 class="modal-title" id="staticBackdropLabel">
-                    {{ title | lstring }}
+                    {{ lstring(title, locale) }}
                 </h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </slot>
@@ -21,7 +21,7 @@
                     <div class="col">
                     </div>
                     <div class="col-auto">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ $t("closeButton") }}</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ t("closeButton") }}</button>
                     </div>
                 </div>
             </slot>
@@ -31,40 +31,55 @@
     </div>
 
 </template>
+<script setup>
+    import { ref, onMounted } from "vue";
+    import { Modal } from "bootstrap";
+    import { useI18n } from 'vue-i18n';
+    import messages from '../../app-text/components/common/modal.json';
+    import { lstring } from '../../components/kb/filters';
+    const { t, locale } = useI18n({ messages });
+    const modalRef = ref(null);
+    const titleRef = ref('')
+    let modal = null;
     
-<script>
-import bootstrap from 'bootstrap'
-import i18n from "../../app-text/components/common/modal.json";
+    const { title } = defineProps({
+        title: [String, Object], 
+        required: true,
+    });
 
-export default {
-    name: 'SimpleModal',
-    i18n: { messages: { en: i18n } },
-    emits: ['show', 'close'],
-    props: {
-        title:    { type:  [String, Object] },
-    },
-    computed: {
-    },
-    methods: {
-        close,
-    },
-    async mounted() {
+    onMounted(() => {
+        titleRef.value = title;
+        modal = new Modal(modalRef.value);
+    });
 
-        const el = this.$refs.modal;
-        const modal = bootstrap.Modal.getOrCreateInstance(el);
+    const show = () => {
+        modal = Modal.getInstance(modalRef.value);
+        modal.show('static');
+    };
 
-        el.addEventListener('shown.bs.modal',  ()=>this.$emit('show'));
-        el.addEventListener('hidden.bs.modal', ()=>this.$emit('close'));
+    const close = () => {
+        modal = Modal.getInstance(modalRef.value);
+        modal.hide();
+    };
 
-        this.modal = modal;
-
-        modal.show();
-    }
-}
-
-function close() {
-    const { modal } = this;
-    modal.hide();
-}
-
+    defineExpose({show, close})
 </script>
+
+<!-- How to use:  <template>
+    <div>
+      <button @click="showModal">Open Modal</button>
+      <Modal ref="modelNew" title="this is a test title">
+        <template v-slot:default>test  content</template>
+      </Modal>
+    </div>
+  </template> 
+  
+  <script setup>
+  import { ref } from 'vue';
+  import Modal from '~/components/common/modal.vue';
+  const modelNew = ref(null)
+  
+  const showModal = () => {
+    modelNew.value.show();
+  };
+  </script> -->
