4ed31b7bbdfe1856bd1dffbab32670e5d30be7fd
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-03-28 23:55:59 +0500

    formate code

--- app/components/kb/right-side-bar.vue
+++ app/components/kb/right-side-bar.vue
@@ -1,15 +1,13 @@
 <template>
-	<div>
-		<div class="mb-3">
-		   <div v-if="tag">
-				<relevant-articles></relevant-articles>
-			</div>
-		   <div v-if="!tag && realm.is('BCH')">
-				<faq-articles></faq-articles>
-			</div>
+	<div class="mb-3">
+		<div v-if="tag">
+			<relevant-articles></relevant-articles>
 		</div>
-		<popular-tags is-categories="true"></popular-tags>
+		<div v-if="!tag && realm.is('BCH')">
+			<faq-articles></faq-articles>
+		</div>
 	</div>
+	<popular-tags is-categories="true"></popular-tags>
 </template>
 
 <script setup>
@@ -19,7 +17,8 @@
 	import faqArticles from './faq-articles.vue';
 	import popularTags from './popular-tags.vue';
 	import { useRealm } from '../../services/composables/realm.js';
-	import { useRoute } from "@scbd/angular-vue/src/index.js";   
+	import { useRoute } from "@scbd/angular-vue/src/index.js"; 
+
 	const realm = useRealm();
 	const route = useRoute();
 	const tag = ref('');
