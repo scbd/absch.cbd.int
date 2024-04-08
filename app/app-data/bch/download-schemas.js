c7bd83001103bcabaea51d58cdce25407e3e80a5
Pramod Jacob Sam <pramod.sam.consultant@cbd.int>
2024-04-08 19:48:14 -0400

    added schema for lab

--- app/app-data/bch/download-schemas.js
+++ app/app-data/bch/download-schemas.js
@@ -11,6 +11,17 @@ export const downloadSchemas = {
 		"government":fieldsT["supplementaryAuthority.government"],
 		"name":fieldsT["supplementaryAuthority.name"],
 		"publishedOn": fieldsT["supplementaryAuthority.publishedOn"]
+	},
+	"laboratoryDetection":{
+		"uniqueIdentifier": fieldsT["laboratoryDetection.uniqueIdentifier"],
+		"organization": fieldsT["laboratoryDetection.organization"],
+		"services": fieldsT["laboratoryDetection.services"],
+		"agreements": fieldsT["laboratoryDetection.agreements"],
+		"certifications": fieldsT["laboratoryDetection.certifications"],
+		"detectionMethods": fieldsT["laboratoryDetection.detectionMethods"],
+		"lmoTypes": fieldsT["laboratoryDetection.lmoTypes"],
+		"detectableLmos": fieldsT["laboratoryDetection.detectableLmos"],
+		"detectableGenes": fieldsT["laboratoryDetection.detectableGenes"]
 	}
 }
 
