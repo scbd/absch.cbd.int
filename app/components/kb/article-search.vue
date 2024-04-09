dedafb62bc6fa5516549bab213732da4fd032470
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-04-09 21:06:53 +0500

    Migrate kb search components to Vue3 (#1077)

--- app/components/kb/article-search.vue
+++ app/components/kb/article-search.vue
@@ -1,239 +1,200 @@
 <template>
     <div>
-        <div class="loading" v-if="loading"><i class="fa fa-cog fa-spin fa-lg"></i> {{ $t("loading") }}...</div>
-        <div v-if="!loading">
-            <div class="article-by-tags" v-if="articles">
-                <h4>
-                    {{$t("searchResults") }} <span><small>({{articlesCount}})</small></span>
-                </h4>
-                <hr>
-
-                <div v-for="article in articles">
-                    <div class="card mb-3">
-
-                        <div class="d-flex flex-row bd-highlight ">
-                            <div class="p-2 bd-highlight" v-if="article.coverImage">
-                                <img class="img-fluid img-thumbnail" style="max-height:140px;"
-                                    v-bind:src="getSizedImage(article.coverImage.url, '300x300')">
-                            </div>
-                            <div class="p-2 bd-highlight w-100">
-                                <div class="card-body">
-                                    <span class="badge bg-secondary position-absolute top-0 end-0">
-                                        {{formatDate(article.meta.createdOn, 'DD MMM YYYY')}}</span>
-
-                                    <h5 class="card-title"><a class="link-dark"
-                                            :href="`${articleUrl(article, getTag(article.adminTags) )}`">{{lstring(article.title, $locale)}}</a>
-                                    </h5>
-                                        <p v-if="article.summary" class="card-text h-100">
-                                        <a class="link-dark" :href="`${articleUrl(article, getTag(article.adminTags) )}`">
-                                            {{lstring(article.summary, $locale)}}
-                                        </a>
-                                    </p>
-
-                                    <div class="inner-area">
-                                        <i class="fa fa-tag" aria-hidden="true"></i>
-                                        <a class="btn btn-mini" :href="`${tagUrl(tag)}`"  v-for="tag in article.adminTags">{{tag}}</a>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
+      <div class="loading" v-if="loading"><i class="fa fa-cog fa-spin fa-lg"></i> {{ t("loading") }}...</div>
+      <div v-if="!loading">
+        <div class="article-by-tags" v-if="articles">
+          <h4>
+            {{t("searchResults") }} <span><small>({{articlesCount}})</small></span>
+          </h4>
+          <hr>
+          <div v-for="article in articles">
+            <div class="card mb-3">
+              <div class="d-flex flex-row bd-highlight ">
+                <div class="p-2 bd-highlight" v-if="article.coverImage">
+                  <img class="img-fluid img-thumbnail" style="max-height:140px;"
+                    v-bind:src="getSizedImage(article.coverImage.url, '300x300')">
+                </div>
+                <div class="p-2 bd-highlight w-100">
+                  <div class="card-body">
+                    <span class="badge bg-secondary position-absolute top-0 end-0">
+                      {{formatDate(article.meta.createdOn, 'DD MMM YYYY')}}</span>
+  
+                    <h5 class="card-title"><a class="link-dark"
+                        :href="`${articleUrl(article, getTag(article.adminTags) )}`">{{lstring(article.title,
+                        $locale)}}</a>
+                    </h5>
+                    <p v-if="article.summary" class="card-text h-100">
+                      <a class="link-dark" :href="`${articleUrl(article, getTag(article.adminTags) )}`">
+                        {{lstring(article.summary, $locale)}}
+                      </a>
+                    </p>
+  
+                    <div class="inner-area">
+                      <i class="fa fa-tag" aria-hidden="true"></i>
+                      <a class="btn btn-mini" :href="`${tagUrl(tag)}`" v-for="tag in article.adminTags">{{tag}}</a>
                     </div>
-
+                  </div>
                 </div>
-                
-                <div v-if="articlesCount<1" class="alert alert-light">
-                    <strong>{{ $t("noResultFound") }}</strong>
-                </div>
+              </div>
             </div>
+          </div>
+  
+          <div v-if="articlesCount<1" class="alert alert-light">
+            <strong>{{ t("noResultFound") }}</strong>
+          </div>
         </div>
-        <div class="d-inline-block" v-if="articlesCount>10">
-            <paginate :records-per-page="recordsPerPage" :record-count="articlesCount" @changePage="onChangePage"
-                :current-page="pageNumber"></paginate>
-        </div>
+      </div>
+      <div class="d-inline-block" v-if="articlesCount>10">
+        <paginate :records-per-page="recordsPerPage" :record-count="articlesCount" @changePage="onChangePage"
+          :current-page="pageNumber"></paginate>
+      </div>
     </div>
-</template>
-<script>
+  </template>
+  
+  <script setup>
+      import { ref, onMounted, defineExpose } from "vue";
+      import { useI18n } from 'vue-i18n';
+      import messages from '../../app-text/components/kb.json';
+      import { useRealm } from '../../services/composables/realm.js';
+      import {  useRoute } from "@scbd/angular-vue/src/index.js";
+      import { loadKbCategories, getUrl } from '../../services/composables/articles.js';
+      import paginate from '../common/pagination.vue';
+      import ArticlesApi from './article-api';
+      import "../kb/filters";
+      import { formatDate, lstring } from './filters';
+      const route = useRoute();
+      const { t, locale } = useI18n({ messages });
+      const realm = useRealm();
+      const articlesApi = new ArticlesApi();
+      const articles = ref([]);
+      const loading = ref(true);
+      const pageNumber = ref(1);
+      const recordsPerPage = ref(10);
+      const realmTag = ref('');
+      const categories = ref([]);
+      const search = ref('');
+      const articlesCount = ref(0);
+  
+      onMounted( async() => {
+          categories.value = await loadKbCategories(realm.is('BCH'));
+          if (route.value?.params?.search) {
+              search.value = route.value.params.search.replace(/"/g, "");
+          }
+          realmTag.value = realm.is('BCH') ? 'bch' : 'abs';
+          await loadArticles(1);
+      });
 
-import i18n from '../../app-text/components/kb.json';
-import paginate from '../common/pagination.vue';
-import ArticlesApi from './article-api';
-import './filters';
-import articlesMaxin from '../maxin/article';
-import { formatDate, lstring } from './filters';
-
-export default {
-    name: 'KbArticlesByTag',
-    components: {
-        paginate,
-    },
-    props: {},
-    created() {
-        this.articlesApi = new ArticlesApi();
-    },
-    data: () => {
-        return {
-            articles: [],
-            loading: true,
-            pageNumber: 1,
-            recordsPerPage: 10,
-            realmTag: '',
-            categories: [],
-            search: ''
+      const getTag = (adminTags) => {
+        const realm = realmTag.value === 'BCH' ? 'bch' : 'absch';
+        if (adminTags && adminTags.length >= 2) {
+          if (adminTags[0] === realm) return adminTags[1];
+          else return adminTags[0];
+        } else return realm;
+      };
+  
+      const tagUrl = (tag) => {
+        if(categories){
+          const tagDetails = categories.value.find((e) => e.adminTags.includes(tag));
+          const tagTitle = tagDetails?.title || '';
+          return getUrl(tagTitle, undefined, tag);
         }
-    },
-    mixins: [articlesMaxin],
-    async mounted() {
-        this.categories = await this.loadKbCategories(this.$realm.is('BCH'));
-        if (this.$route.params.search) {
-            this.search = decodeURIComponent(this.$route.params.search).replace(/"/g, "");
-        }
-        console.log(this.search)
-        this.realmTag = this.$realm.is('BCH') ? 'bch' : 'abs';
-        this.loadArticles(1);
-    },
-    methods: {
-        formatDate,
-        getTag(adminTags) {
-            var realm = this.$realm.is('BCH') ? 'bch' : 'absch';
-            console.log(realm);
-            if(adminTags && adminTags.length >= 2){
-                if(adminTags[0] == realm)
-                    return  adminTags[1];
-                else
-                    return  adminTags[0];
-            }
-            else return realm;
-        },
-        tagUrl(tag) {
-            const tagDetails = this.categories.find(e => e.adminTags.includes(tag))
-            const tagTitle = (tagDetails?.title || '');
-            return this.getUrl(tagTitle, undefined, tag);
-        },
-        articleUrl(article, tag) {
-            return this.getUrl(lstring(article.title), article._id, tag);
-        },
-        onChangePage(pageNumber) {
-            this.article = [];
-            this.loading = true;
-            window.scrollTo(0,0);
-            this.loadArticles(pageNumber);
-        },
-        async loadArticles(pageNumber, text) {
-            this.pageNumber = pageNumber;
-            if (text) {
-                this.search = text;
-            }
-            this.articlesCount = 0;
-            this.articles = [];
-            this.loading = true;
-            let countAg = [];
-            let searchAg = [];
-            if (this.search) {
-                const match = {
+      };
+  
+      const articleUrl = (article, tag) => {
+        return getUrl(lstring(article.title), article._id, tag);
+      };
+  
+      const loadArticles = async (page) => {
+                pageNumber.value = page;
+                articlesCount.value = 0;
+                articles.value = [];
+                loading.value = true;
+                const countAg = [];
+                const searchAg = [];
+                if (search.value !== undefined) {
+                    const match = {
                     "$match": {
-                        "$and": [{
-                            "$or": [{
-                                [`title.${this.$locale}`]: {
-                                    "$$contains": (this.search)
-                                }
-                            },
-                            {
-                                [`summary.${this.$locale}`]: {
-                                    "$$contains": (this.search)
-                                }
-                            },
-                            {
-                                [`content.${this.$locale}`]: {
-                                    "$$contains": (this.search)
-                                }
-                            }
+                        "$and": [
+                        {
+                            "$or": [
+                            { [`title.${locale.value}`]: { "$$contains": (search.value) } },
+                            { [`summary.${locale.value}`]: { "$$contains": (search.value) } },
+                            { [`content.${locale.value}`]: { "$$contains": (search.value) } }
                             ]
                         },
-                        {
-                            "adminTags": {
-                                $all: [this.realmTag]
-                            }
-                        }
+                        { "adminTags": { $all: [realmTag.value] } }
                         ]
                     }
-                };
-                searchAg.push(match);
-                countAg.push(match);
-            } else {
-                const matchTag = {
+                    };
+                    searchAg.push(match);
+                    countAg.push(match);
+                } else {
+                    const matchTag = {
                     "$match": {
-                        "$and": [{
-                            "adminTags": {
-                                $all: [this.realmTag]
-                            }
-                        }]
+                        "$and": [
+                        { "adminTags": { $all: [realmTag.value] } }
+                        ]
                     }
-                };
-                searchAg.push(matchTag);
-                countAg.push(matchTag);
-            }
-            countAg.push({
-                "$match": {
-                    "$and": [{
-                        "adminTags": {
-                            $all: [this.realmTag]
-                        }
-                    }]
+                    };
+                    searchAg.push(matchTag);
+                    countAg.push(matchTag);
                 }
-            });
-            searchAg.push({
-                "$project":
-
-                {
+                countAg.push({
+                    "$match": {
+                    "$and": [
+                        { "adminTags": { $all: [realmTag.value] } }
+                    ]
+                    }
+                });
+                searchAg.push({
+                    "$project":
+                    {
                     [`title`]: 1,
                     [`summary`]: 1,
                     [`coverImage`]: 1,
                     adminTags: 1,
                     "meta": 1,
                     _id: 1
+                    }
+                });
+                searchAg.push({
+                    "$limit": pageNumber.value * recordsPerPage.value
+                });
+                searchAg.push({
+                    "$skip": (pageNumber.value - 1) * recordsPerPage.value
+                });
+                countAg.push({
+                    "$count": "count"
+                });
+                const countQuery = { "ag": JSON.stringify(countAg) };
+                const searchQuery = { "ag": JSON.stringify(searchAg) };
+                try {
+                    const [count, articlesList] = await Promise.all([articlesApi.queryArticles(countQuery), articlesApi.queryArticles(searchQuery)]);
+                    if ((articlesList || []).length) {
+                    articles.value = articlesList;
+                    articlesCount.value = count[0].count;
+                    }
+                } catch (e) {
+                    console.error(e);
+                } finally {
+                    loading.value = false;
                 }
-            });
-            searchAg.push({
-                "$limit": pageNumber * this.recordsPerPage
-            });
-            searchAg.push({
-                "$skip": (pageNumber - 1) * this.recordsPerPage
-            });
-            countAg.push({
-                "$count": "count"
-            })
-            const countQuery = {
-                "ag": JSON.stringify(countAg)
             };
-            const searchQuery = {
-                "ag": JSON.stringify(searchAg)
-            };
-
-            try {
-                const [count, articlesList] = await Promise.all([this.articlesApi.queryArticles(countQuery), this.articlesApi.queryArticles(searchQuery)]);
-                if ((articlesList || []).length) {
-                    this.articles = articlesList;
-                    this.articlesCount = count[0].count;
-                }
-            }
-            catch (e) {
-                console.error(e);
-            }
-            finally {
-                this.loading = false;
-            }
-        },
-        getSizedImage(url, size) {
-            return url && url
-                .replace(/attachments.cbd.int\//, '$&' + size + '/')
-                .replace(/\.s3-website-us-east-1\.amazonaws\.com\//, '$&' + size + '/')
-        }
-    },
-    i18n: {
-        messages: {
-            en: i18n
-        }
-    }
-}
+      const onChangePage = async (newPage) => {
+        articles.value = [];
+        loading.value = true;
+        window.scrollTo(0, 0);
+        await loadArticles(newPage);
+      };
+  
+  
+      const getSizedImage = (url, size) => {
+          return url && url
+          .replace(/attachments.cbd.int\//, '$&' + size + '/')
+          .replace(/\.s3-website-us-east-1\.amazonaws\.com\//, '$&' + size + '/');
+      };
+      defineExpose({
+        loadArticles
+     });
 </script>
+  
