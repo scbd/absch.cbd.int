e6fe8560149239fb14853b861b486062604f55da
Yuan Li <liyuan.montreal@gmail.com>
2024-04-12 15:51:22 -0400

    check if route and currentcountry available

--- app/views/countries/directives/home-pin-popup-chm.vue
+++ app/views/countries/directives/home-pin-popup-chm.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>  Hello
-        <div v-if="currentCountry.code" style="padding:5px;margin-bottom:10px;" :class="{ 
+    <div>  
+        route: {{route}}
+        <!-- currentCountry:{{ currentCountry }} -->
+        
+        <!-- <div v-if="currentCountry.code" style="padding:5px;margin-bottom:10px;" :class="{ 
         'bg-chm': isCHM && currentCountry.isParty && !currentCountry.isInbetweenParty, 
         'bg-darkgrey':!currentCountry.isParty && !currentCountry.isInbetweenParty, 
         'bg-inbetween': currentCountry.isInbetweenParty}">
@@ -16,10 +19,10 @@
                         {{t('homepinPopupChmTranslation.notPartyToConvention')}}</span>
     
             </div>
-        </div>
-        <div>
-     
-            <table class="table table-map table-condensed" style="display: block;overflow-x: auto;white-space: nowrap;" >
+        </div> -->
+
+        <div>     
+            <!-- <table class="table table-map table-condensed" style="display: block;overflow-x: auto;white-space: nowrap;" >
                 <tr v-for="schema in countryMapScope.currentCountry.schemas" style="border:none;">
                     <td>
                         <span class="rounded badge ms-2"  :class="{'bg-gray-300' : schema.count==0, 'bg-chm':schema.count>0 }" v-bind="schema.count"></span>
@@ -30,39 +33,36 @@
                         </a>                
                         <a rel="noopener" v-if="!currentCountry.code" translation-url href="{{translationUrl}}/search?schema={{schema.shortCode|mapSchema}}" class="  text-black ">
                             <span v-bind="lstring(schema.title| locale) "></span>   
-                           
-                           
+                            
+                            
                         </a>                   
                     </td>
                 </tr>
-            </table> 
+            </table>  -->
         </div>
     </div>
 </template>
 
 <script setup>
-    import homepinPopupChmTranslation from '~/app-text/views/countries/directives/homepin-popup-chm.json';
-    import {  useRoute } from "@scbd/angular-vue/src/index.js";
-// app.directive('homepinPopupChm', ['translationService', '$routeParams', '$location', function(translationService, $routeParams, $location){
-     
+    //import homepinPopupChmTranslation from '~/app-text/views/countries/directives/homepin-popup-chm.json';
+    import { useRoute } from "@scbd/angular-vue/src/index.js";    
     //translationService.set('homepinPopupChmTranslation', homepinPopupChmTranslation);
-    const route = useRoute();
+    const route = useRoute().value;
     
 
-    const ToggleCountyList = (code, currentCountryCode) => {
-        if ((route.value?.params?.code) && $("#div" + code).length>0) {
-            $('html, body').animate({
-                scrollTop: $("#div"+code).offset().top
-            }, 500);
-            if ($("#div" + code).find('i').hasClass("bi-chevron-down")){
-                $("#div" + code).trigger('click');
-            }
-        }
-        else{
-            //$router.path(`countries/${currentCountryCode}/${code}`);
-            route.value?.params?.path(`countries/${currentCountryCode}/${code}`);
-        }
-    }
+    // const ToggleCountyList = (code, currentCountryCode) => {
+    //     if ((route?.params?.code) && $("#div" + code).length>0) {
+    //         $('html, body').animate({
+    //             scrollTop: $("#div"+code).offset().top
+    //         }, 500);
+    //         if ($("#div" + code).find('i').hasClass("bi-chevron-down")){
+    //             $("#div" + code).trigger('click');
+    //         }
+    //     }
+    //     else{
+    //         route?.params?.path(`countries/${currentCountryCode}/${code}`);
+    //     }
+    // }
  
 
 </script>
