f8566ffb78df844cad02a1a6d3c95bd2910ad41d
Yuan Li <liyuan.montreal@gmail.com>
2024-04-12 22:15:08 -0400

    remove pin-popup files; pass country and countryMap props to component;

--- app/views/countries/directives/home-pin-popup-chm.vue
+++ app/views/countries/directives/home-pin-popup-chm.vue
@@ -1,68 +1,83 @@
 <template>
-    <div>  
-        route: {{route}}
-        <!-- currentCountry:{{ currentCountry }} -->
-        
-        <!-- <div v-if="currentCountry.code" style="padding:5px;margin-bottom:10px;" :class="{ 
-        'bg-chm': isCHM && currentCountry.isParty && !currentCountry.isInbetweenParty, 
-        'bg-darkgrey':!currentCountry.isParty && !currentCountry.isInbetweenParty, 
-        'bg-inbetween': currentCountry.isInbetweenParty}">
+    <div> 
+        route: {{route}}        
+        country: {{ country }}
+        countryMap: {{ countryMap }}
+   
+               
+        <!-- <div v-if="country.code" style="padding:5px;margin-bottom:10px;" :class="{ 
+        'bg-chm': isCHM && country.isParty && !country.isInbetweenParty, 
+        'bg-darkgrey':!country.isParty && !country.isInbetweenParty, 
+        'bg-inbetween': country.isInbetweenParty}">
             <div>
                
-                <span class="color-white fw-bold" v-if="((isCHM && currentCountry.isParty) || (isCHM && currentCountry.isParty)) && !currentCountry.isInbetweenParty">
+                <span class="color-white fw-bold" v-if="((isCHM && country.isParty) || (isCHM && country.isParty)) && !country.isInbetweenParty">
                     {{t('homepinPopupChmTranslation.partyToConvention')}}</span>
                 
-                    <span  class="color-white fw-bold" v-if="(isCHM && currentCountry.isInbetweenParty)">
+                    <span  class="color-white fw-bold" v-if="(isCHM && country.isInbetweenParty)">
                         {{t('homepinPopupChmTranslation.ratifiedNotYetParty')}}</span>
     
-                    <span  class="color-white fw-bold" v-if="(isCHM && !currentCountry.isParty && !currentCountry.isInbetweenParty) || (isCHM && !currentCountry.isParty)">
+                    <span  class="color-white fw-bold" v-if="(isCHM && !country.isParty && !country.isInbetweenParty) || (isCHM && !country.isParty)">
                         {{t('homepinPopupChmTranslation.notPartyToConvention')}}</span>
     
             </div>
         </div> -->
 
         <div>     
-            <!-- <table class="table table-map table-condensed" style="display: block;overflow-x: auto;white-space: nowrap;" >
-                <tr v-for="schema in countryMapScope.currentCountry.schemas" style="border:none;">
+            <table class="table table-map table-condensed" style="display: block;overflow-x: auto;white-space: nowrap;" >
+                <tr v-for="schema in countryMap.country.schemas" style="border:none;">
                     <td>
                         <span class="rounded badge ms-2"  :class="{'bg-gray-300' : schema.count==0, 'bg-chm':schema.count>0 }" v-bind="schema.count"></span>
                     </td>
                     <td>                  
-                        <a rel="noopener" v-if="currentCountry.code" translation-url @click="ToggleCountyList(schema.shortCode,currentCountry.code)" class="  text-black cursor-pointer" >
+                        <a rel="noopener" v-if="country.code" translation-url @click="ToggleCountyList(schema.shortCode,country.code)" class="  text-black cursor-pointer" >
                             <span v-bind="lstring(schema.title| locale) "></span>
                         </a>                
-                        <a rel="noopener" v-if="!currentCountry.code" translation-url href="{{translationUrl}}/search?schema={{schema.shortCode|mapSchema}}" class="  text-black ">
+                        <a rel="noopener" v-if="!country.code" translation-url href="{{translationUrl}}/search?schema={{schema.shortCode|mapSchema}}" class="  text-black ">
                             <span v-bind="lstring(schema.title| locale) "></span>   
                             
                             
                         </a>                   
                     </td>
                 </tr>
-            </table>  -->
+            </table> 
         </div>
     </div>
 </template>
 
 <script setup>
-    //import homepinPopupChmTranslation from '~/app-text/views/countries/directives/homepin-popup-chm.json';
-    import { useRoute } from "@scbd/angular-vue/src/index.js";    
-    //translationService.set('homepinPopupChmTranslation', homepinPopupChmTranslation);
-    const route = useRoute().value;
+    import messages from '~/app-text/views/countries/directives/home-pin-popup-chm.json';
+    import { useRoute } from "@scbd/angular-vue/src/index.js";  
+    import { useI18n } from 'vue-i18n';   
+ 
+  
+    //const route = useRoute().value; 
+    const {value:route} = useRoute();    
+    const { t } = useI18n({ messages }); 
+
+    const props = defineProps({
+        country: {type: Object, require:true, default:{}},
+        countryMap: {type: Object, require:true, default:{}},
+    })
+
+    console.log(props.country)
+    console.log(props.countryMap)
+
     
 
-    // const ToggleCountyList = (code, currentCountryCode) => {
-    //     if ((route?.params?.code) && $("#div" + code).length>0) {
-    //         $('html, body').animate({
-    //             scrollTop: $("#div"+code).offset().top
-    //         }, 500);
-    //         if ($("#div" + code).find('i').hasClass("bi-chevron-down")){
-    //             $("#div" + code).trigger('click');
-    //         }
-    //     }
-    //     else{
-    //         route?.params?.path(`countries/${currentCountryCode}/${code}`);
-    //     }
-    // }
+    const ToggleCountyList = (code, currentCountryCode) => {
+        if ((route?.params?.code) && $("#div" + code).length>0) {
+            $('html, body').animate({
+                scrollTop: $("#div"+code).offset().top
+            }, 500);
+            if ($("#div" + code).find('i').hasClass("bi-chevron-down")){
+                $("#div" + code).trigger('click');
+            }
+        }
+        else{
+            route?.params?.path(`countries/${currentCountryCode}/${code}`);
+        }
+    }
  
 
 </script>
