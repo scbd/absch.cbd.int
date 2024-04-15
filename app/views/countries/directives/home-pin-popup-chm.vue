47f9eefe163dac93bf4a81224b348314d52fe404
Yuan Li <liyuan.montreal@gmail.com>
2024-04-15 15:56:37 -0400

    fix click report can't jump to specific place issue, fix code style issue

--- app/views/countries/directives/home-pin-popup-chm.vue
+++ app/views/countries/directives/home-pin-popup-chm.vue
@@ -26,7 +26,7 @@
                         <span class="rounded badge ms-2 "  :class="{'bg-gray-300' : schema.count==0, 'bg-chm':schema.count>0 }" >{{schema.count}}</span>
                     </td>
                     <td >                  
-                        <a rel="noopener" v-if="country.code"  @click="ToggleCountyList(schema.shortCode,country.code)" class="text-black cursor-pointer" style="color:#fff" >
+                        <a rel="noopener" v-if="country.code"  @click="toggleCountyList(schema.shortCode,country.code)" class="text-black cursor-pointer" style="color:#fff" >
                             <span >{{lstring(schema.title,locale)}}</span>                        
                         </a>                
                         <a rel="noopener" v-if="!country.code" :href="`search?schema=${schema.shortCode}`" class="text-black " style="color:#fff">
@@ -41,20 +41,19 @@
 
 <script setup>
     import messages from '~/app-text/views/countries/directives/home-pin-popup-chm.json';
-    import { useRoute } from "@scbd/angular-vue/src/index.js";  
+    import { useRoute, useRouter} from "@scbd/angular-vue/src/index.js";      
     import { useI18n } from 'vue-i18n';    
     import { lstring } from '../../../components/kb/filters';
  
-  
-    //const route = useRoute().value; 
     const {value:route} = useRoute();    
+    const router = useRouter();    
     const { t,locale } = useI18n({ messages }); 
 
     const props = defineProps({
-        country: {type: Object, require:true, default:{}}     
+        country: {type: Object, require:true}     
     })
 
-    const ToggleCountyList = (code, currentCountryCode) => {
+    const toggleCountyList = (code, currentCountryCode) => {       
         if ((route?.params?.code) && $("#div" + code).length>0) {
             $('html, body').animate({
                 scrollTop: $("#div"+code).offset().top
@@ -63,8 +62,8 @@
                 $("#div" + code).trigger('click');
             }
         }
-        else{
-            route?.params?.path(`countries/${currentCountryCode}/${code}`);
+        else{            
+            router.push({path:`countries/${currentCountryCode}/${code}`});
         }
     }
  
