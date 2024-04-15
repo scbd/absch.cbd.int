5051c3009dde772313368e86fb06d1164457e0f8
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2024-04-15 15:20:31 -0400

    CHM347-add getApplicationArticleRealm function (#1097)
    * add getApplicationArticleRealm function
    
    * change getApplicationArticleRealm function , and put it to composable file to can be used as a common function
    
    * change realm in article-related file ,faq file, focalpoint file
    
    * Revert "change realm in article-related file ,faq file, focalpoint file"
    
    This reverts commit 03c5819d92c835c09d8be96bb59903006d2dc228.
    
    * put getApplicationArticleRealm file to composables; use it in article-related files (under kb folder)
    
    * Call composables in setup
    
    * change use function from composable to  under <script setup> not in other functions
    
    * Name variable with context
    
    ---------
    
    Co-authored-by: Blaise Fonseca <blaise.fonseca@un.org>

--- app/components/kb/faqs.vue
+++ app/components/kb/faqs.vue
@@ -39,7 +39,7 @@
 	import { ref, onMounted } from 'vue';
 	import Paginate from '../common/pagination.vue';
 	import ArticlesApi from './article-api';
-	import { loadKbCategories , getUrl } from '../../services/composables/articles.js'
+	import { loadKbCategories , getUrl , getRealmArticleTag  } from '../../services/composables/articles.js'
 	import { lstring } from './filters';
 	import { useI18n } from 'vue-i18n';
 	import messages from '../../app-text/components/kb.json';
@@ -58,6 +58,7 @@
 	// let pageNumber = 1;
 	let pageNumber = ref(1);
 	let recordsPerPage = 10;
+	const realmArticleTag = getRealmArticleTag();
 	
 	onMounted(async ()=>{
 		faqFilterTag.value = route.value?.params?.tag?.replace(/"/g, ""); 
@@ -73,7 +74,7 @@
 	};
 
 	const getAdminTag = function() {
-		return realm.is('BCH') ? 'bch' : 'absch';
+		return  realmArticleTag;	
 		
 	};
 
@@ -94,7 +95,7 @@
 
 				faqCount.value = 0;
 				faqs.value = [];
-				const realmTag = realm.is('BCH') ? 'bch' : 'abs';
+				const realmTag = realmArticleTag;    
 				const q = { 
 					$and : [
 						{ adminTags : { $all : [realmTag, 'faq']}},
