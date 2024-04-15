240dc6affc89e23d0c5a3c9dd1a770971747721d
Blaise Fonseca <blaise.fonseca@un.org>
2024-04-15 15:20:09 -0400

    Name variable with context

--- app/components/kb/relevant-articles.vue
+++ app/components/kb/relevant-articles.vue
@@ -30,7 +30,7 @@
     
     const { t, locale } = useI18n({ messages });
     const realm = useRealm();
-    const articleRealm = getRealmArticleTag();
+    const articleRealmTag = getRealmArticleTag();
     const props = defineProps({
         tag: { type: String, required: false },
         type:{ type: String, required: false },
@@ -48,7 +48,7 @@
 
     onMounted(async () => {
     let ag = [];
-    ag.push({"$match":{"$and":[{"adminTags": { $all : [articleRealm]}}]}});
+    ag.push({"$match":{"$and":[{"adminTags": { $all : [articleRealmTag]}}]}});
         ag.push({"$match":{"$and":[{"adminTags":props.tag}]}});
         ag.push({"$project" : {[`title`]:1}});
         ag.push({"$limit" : 6});
