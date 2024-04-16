c6aecaa82193fd0c66f33776105e8042b5c61385
Yuan Li <liyuan.montreal@gmail.com>
2024-04-16 17:04:32 -0400

    fix alphabet issue in country tab in search page

--- app/views/search/search-filters/country-filter.js
+++ app/views/search/search-filters/country-filter.js
@@ -18,7 +18,7 @@ app.directive('countryFilter', ['locale', 'translationService', '$filter',  func
                $scope.cf_countries = searchDirectiveCtrl.getSearchFilters("country");
                $scope.cf_countryFilter = null;
                $scope.cf_partyFilter = null;
-               $scope.alphabet = alphabets;
+               $scope.alphabet = alphabets[locale];
                
               //*************************************************************************************************************************************
                $scope.cf_setCountryFilter = function(letter) {
