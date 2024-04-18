d249908e47aad4fb29ac1ddb9eac331669cc4abc
pjacobsam <162193392+pjacobsam@users.noreply.github.com>
2024-04-18 11:12:44 -0400

    added schema for spca (#1087)
    * added schema for spca
    
    * added fieldT
    
    * replaced uniqueIdentifier with uniqueId
    
    * Added export field for publication date
    
    * Replaced updatedDate with publishedOn

--- app/app-data/bch/download-schemas.js
+++ app/app-data/bch/download-schemas.js
@@ -2,6 +2,13 @@ import fieldsT from '../../app-text/views/search/search-results/bch-download-sch
 import { downloadSchemas as commonDownloadSchemas } from '../common-download-schemas.js';
 
 export const downloadSchemas = {	
-	...commonDownloadSchemas
+	...commonDownloadSchemas,
+
+	"supplementaryAuthority":{
+		"uniqueId": fieldsT["supplementaryAuthority.uniqueId"],
+		"government":fieldsT["supplementaryAuthority.government"],
+		"name":fieldsT["supplementaryAuthority.name"],
+		"publishedOn": fieldsT["supplementaryAuthority.publishedOn"]
+	}
 }
 
