5a2d704a964d4332912e9f9a0c614f518bbfeb88
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-04-18 22:02:54 +0500

    add mergeTranslationKeys in download-schemas file

--- app/app-data/bch/download-schemas.js
+++ app/app-data/bch/download-schemas.js
@@ -1,4 +1,6 @@
-import fieldsT from '../../app-text/views/search/search-results/bch-download-schemas.json';
+import fieldsTranslations from '../../app-text/views/search/search-results/bch-download-schemas.json';
+import { mergeTranslationKeys } from '../../services/translation-service';
+const fieldsT = mergeTranslationKeys(fieldsTranslations);
 import { downloadSchemas as commonDownloadSchemas } from '../common-download-schemas.js';
 
 export const downloadSchemas = {	
