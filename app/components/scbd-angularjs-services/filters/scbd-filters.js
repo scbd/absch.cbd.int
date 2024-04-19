67faa89e40abf9035a93370b64ab274d7660fde8
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2024-04-19 10:26:00 -0400

    fix import warning

--- app/components/scbd-angularjs-services/filters/scbd-filters.js
+++ app/components/scbd-angularjs-services/filters/scbd-filters.js
@@ -1,7 +1,7 @@
 import app from '~/app';
 import _ from 'lodash';
 import moment from 'moment';
-import scbdSchemaDetails from './schema-name.json';
+import scbdSchemaDetails from './schema-name.json'  assert { type:'json'};
 import '../services/locale';
 import { setAnchorTarget, escapeHtmlAttributeId } from '~/services/html.js'
     
