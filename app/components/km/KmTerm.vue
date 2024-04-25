8a6c2b6c62da5dfd7117994593508eb5f586dd9d
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2024-04-25 15:09:42 -0400

    chm350-aichi-target view (#1144)
    * Vue record loader directive
    
    * add initial aichi-target.vue file
    
    * updated computed property
    
    * sample use of record-loader
    
    * change km-value css to directive; comment indicator info
    
    * Add km-term control to show thesaurus terms
    
    * add multilanguage support for label
    
    * fix token issue when use doesn't log in
    
    * change angular-vue component grammar
    
    * fix code review issues
    
    ---------
    
    Co-authored-by: Blaise Fonseca <blaise.fonseca@un.org>

---
+++ app/components/km/KmTerm.vue
@@ -0,0 +1,48 @@
+<template>
+    <span>
+        <div class="alert alert-danger" v-if="error">Error loading term {{ value.identifier }}</div>
+        <span v-if="term">
+            {{ lstring(term.title, locale) }}
+        </span>
+        
+        <slot name="help" :term="term">
+
+        </slot>
+    </span>
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue';
+    import thesaurus from '~/api/thesaurus.js'
+    import { lstring } from '~/services/filters/lstring.js';
+
+    const thesaurusApi = new thesaurus();
+    const term = ref();
+    const error= ref()
+
+    const emit = defineEmits(['onTermLoad'])
+    const props = defineProps({
+        value   : {type:Object, required:true },
+        locale  : {type:String, required:true },
+    })
+
+    async function loadTerm(identifier){
+        if(identifier && !term.value){
+            try{
+                term.value = await thesaurusApi.getTerm(identifier);
+                if(term.value){
+                    emit('onTermLoad', term.value);
+                }
+            }
+            catch(error){
+                error.value = error;
+                // useLogger().error(`Error loading term ${value.value}`, error);
+            }
+        }
+    }
+
+    onMounted(()=>{
+        loadTerm(props.value?.identifier)
+    })
+
+</script>
---
+++ app/views/forms/view/record-loader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Suspense>
+            <aichi-target v-if="documentInfo?.type == 'aichiTarget'"  
+                 :document-info="documentInfo" :locale="locale"></aichi-target>
+            <!-- loading state via #fallback slot -->
+            <template #fallback>
+                Loading...
+            </template>
+        </Suspense>
+        <!-- TODO: put schema view definition element and add v-if for schema name -->
+    </div>
+</template>
+
+<script setup>
+
+    import { defineAsyncComponent, Suspense } from 'vue';
+    import aichiTarget from '~/views/forms/view/chm/aichi-target.vue'
+
+    const props = defineProps({
+        linkTarget          : { type : String, default:'_blank'},
+        locale              : { type : String, required:true },
+        documentInfo        : { type : Object, required:true },
+    })
+
+    //TODO : use defineAsyncComponent to load components based on the schema 
+    // const aichiTarget = defineAsyncComponent(()=>import('~/views/forms/view/chm/aichi-target.vue'));
+</script>
+
+<style lang="scss" scoped>
+
+</style>
