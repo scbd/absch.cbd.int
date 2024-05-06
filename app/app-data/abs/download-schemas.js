38c9cc5d3f9201d233185a435ebf7334438c104f
pjacobsam <162193392+pjacobsam@users.noreply.github.com>
2024-05-06 12:52:55 -0400

    added export field for NMCC (#1136)
    * added export field for NMCC
    
    * rebased

--- app/app-data/abs/download-schemas.js
+++ app/app-data/abs/download-schemas.js
@@ -73,5 +73,17 @@ export const downloadSchemas = {
         "duration": fieldsT["absProcedure.duration"],
         "relevantMeasures": fieldsT["absProcedure.relevantMeasures"],
         "keywords": fieldsT["absProcedure.keywords"]
-    }
+    },
+
+    "absNationalModelContractualClause":{
+        "uniqueId": fieldsT["absNationalModelContractualClause.uniqueId"],
+        "government": fieldsT["absNationalModelContractualClause.government"],
+        "title": fieldsT["absNationalModelContractualClause.title"],
+        "description": fieldsT["absNationalModelContractualClause.description"],
+        "jurisdiction": fieldsT["absNationalModelContractualClause.jurisdiction"],
+        "relevantMeasures": fieldsT["absNationalModelContractualClause.relevantMeasures"],
+        "keywords": fieldsT["absNationalModelContractualClause.keywords"],
+        "thematicAreas": fieldsT["absNationalModelContractualClause.thematicAreas"],
+        "relevantInformation": fieldsT["absNationalModelContractualClause.relevantInformation"]
+    },
 }
