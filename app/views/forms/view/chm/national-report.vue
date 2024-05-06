4135842a79b4a091d90498e147a49576698c61e9
Yuan Li <liyuan.montreal@gmail.com>
2024-05-06 11:21:24 -0400

    remove panel in template

--- app/views/forms/view/chm/national-report.vue
+++ app/views/forms/view/chm/national-report.vue
@@ -1,20 +1,15 @@
 <template> 
-    <div id="Record" class="record panel panel-default">
-        <div class="record-body panel-body bg-white" v-if="document">             
-            <!-- <document-data  :document="document"></document-data>  -->
-
-            <div class="panel panel-info">
-                <div class="panel-heading">                 
-                    <legend>{{ t("generalInformation") }} </legend> 
-                </div>
-                <div class="panel-body">
-
+    <div id="Record" class="record ">
+        <div class="record-body bg-white" v-if="document"> 
+       
+           <section>
+                <legend>{{ t("generalInformation") }} </legend> 
                 <div v-if="document.government">
                     <label>{{ t("country") }} </label> 
                     <div class="km-value">
                         <km-term :value="document.government" :locale="locale"></km-term>   
                     </div>
-                </div>
+                </div>                   
 
                 <div v-if="document.reportType">
                     <label>{{ t("typeOfDocument") }} </label>   
@@ -34,102 +29,87 @@
                         <km-term :value="document.jurisdiction" :locale="locale" ></km-term>
                     </div>
                 </div>
-            </div>
-		    </div>
+            </section> 
+           
 
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                 <legend>{{ t("status") }} </legend>
-                </div>
-                <div class="panel-body">            
-                    <div v-if="document.status">
-                        <label>{{ t("status") }} </label> 
-                        <div class="km-value">
-                            <km-term :value="document.status" :locale="locale" ></km-term>
-                        </div>
+            <section> 
+                <legend>{{ t("status") }} </legend>       
+                        
+                <div v-if="document.status">
+                    <label>{{ t("status") }} </label> 
+                    <div class="km-value">
+                        <km-term :value="document.status" :locale="locale" ></km-term>
                     </div>
+                </div>
+
+                <div v-if="document.adoptionDate">
+                    <label>{{ t("dateOfCompletionAndAdoption") }} </label>   
+                    <ng v-vue-ng:km-value-ml  :value="document.adoptionDate" :locales="locale" html></ng>  
+                </div>
+
+                <div v-if="document.approvedStatus">
+                    <label>{{ t("statusOfApprovedDocument") }} </label> 
+                    <div class="km-value">
+                        <km-term :value="document.approvedStatus" :locale="locale"></km-term>
+                    </div>
+                </div>
+
+                <div v-if="document.approvingBody">
+                    <label>{{ t("approvingBody") }} </label>
+                    <div class="km-value">
+                        <km-term :value="document.approvingBody" :locale="locale"></km-term>
+                    </div>
+                </div>            
+            </section> 
+
+            <section>
+                <legend>{{ t("timePeriod") }} </legend>
+                <div v-if="document.startDate">
+                    <label>{{ t("from") }} </label>    
+                    <ng v-vue-ng:km-value-ml  :value="document.startDate" :locales="locale" html></ng>  
+                </div>
+
+                <div v-if="document.endDate">
+                    <label>{{ t("to") }} </label>    
+                    <ng v-vue-ng:km-value-ml  :value="document.endDate" :locales="locale" html></ng>  
+                </div>
+            </section>
+         
+           <section>
+                <legend>{{ t("relevantDocumentsAndInformation") }}</legend>
         
-                    <div v-if="document.adoptionDate">
-                        <label>{{ t("dateOfCompletionAndAdoption") }} </label>   
-                        <ng v-vue-ng:km-value-ml  :value="document.adoptionDate" :locales="locale" html></ng>  
-                    </div>
-
-                    <div v-if="document.approvedStatus">
-                        <label>{{ t("statusOfApprovedDocument") }} </label> 
-                        <div class="km-value">
-                            <km-term :value="document.approvedStatus" :locale="locale"></km-term>
-                        </div>
-                    </div>
-
-                    <div v-if="document.approvingBody">
-                        <label>{{ t("approvingBody") }} </label>
-                        <div class="km-value">
-                            <km-term :value="document.approvingBody" :locale="locale"></km-term>
-                        </div>
+                <div v-if="document.documentText">
+                    <label>{{ t("relevantInformation") }} </label>   
+                    <ng v-vue-ng:km-value-ml  :value="document.documentText" :locales="locale" html ></ng>  
+                </div>
+        
+                <div v-if="document.documentLinks">
+                    <label>{{ t("relevantWebsitesLinksAndFiles") }} </label> 
+                    <div class="km-value" compare-val>                 
+                        <ng v-vue-ng:km-link-list v-model:ng-model="document.documentLinks"  ></ng>                    
                     </div>
                 </div>
-            </div>
 
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <legend>{{ t("timePeriod") }} </legend>   
+                <div v-if="document.relevantInformation">
+                    <label>{{ t("additionalInformation") }} </label>   
+                    <ng v-vue-ng:km-value-ml  :value="document.relevantInformation" :locales="locale" html></ng>
                 </div>
-                <div class="panel-body">
-              
-                    <div v-if="document.startDate">
-                        <label>{{ t("from") }} </label>    
-                        <ng v-vue-ng:km-value-ml  :value="document.startDate" :locales="locale" html></ng>  
-                    </div>
 
-                    <div v-if="document.endDate">
-                        <label>{{ t("to") }} </label>    
-                        <ng v-vue-ng:km-value-ml  :value="document.endDate" :locales="locale" html></ng>  
+                <div v-if="document.relevantDocuments">
+                    <label>{{ t("otherRelevantWebsiteAddressOrAttachedDocuments") }} </label> 
+                    <div class="km-value" compare-val>                   
+                        <ng v-vue-ng:km-link-list v-model:ng-model="document.relevantDocuments" ></ng>                  
                     </div>
-                </div>
-            </div>
-
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <legend>{{ t("relevantDocumentsAndInformation") }}</legend>
-                </div>
-                <div class="panel-body">   
-                    <div v-if="document.documentText">
-                        <label>{{ t("relevantInformation") }} </label>   
-                        <ng v-vue-ng:km-value-ml  :value="document.documentText" :locales="locale" html ></ng>  
-                    </div>
-            
-                    <div v-if="document.documentLinks">
-                        <label>{{ t("relevantWebsitesLinksAndFiles") }} </label> 
-                        <div class="km-value" compare-val>                 
-                            <ng v-vue-ng:km-link-list v-model:ng-model="document.documentLinks"  ></ng>                    
-                        </div>
-                    </div>
-
-                    <div v-if="document.relevantInformation">
-                        <label>{{ t("additionalInformation") }} </label>   
-                        <ng v-vue-ng:km-value-ml  :value="document.relevantInformation" :locales="locale" html></ng>
-                    </div>
-
-                    <div v-if="document.relevantDocuments">
-                        <label>{{ t("otherRelevantWebsiteAddressOrAttachedDocuments") }} </label> 
-                        <div class="km-value" compare-val>                   
-                            <ng v-vue-ng:km-link-list v-model:ng-model="document.relevantDocuments" ></ng>                  
-                        </div>
-                    </div>  
-                </div>
-            </div>         
-        </div>       
-
-        <!-- <document-metadata :document="document"></document-metadata> -->
-       
+                </div>  
+            </section> 
+        </div>                      
     </div>
 </template>
+
 <script setup>
     import { computed } from 'vue';
     import '~/components/scbd-angularjs-controls/form-control-directives/km-value-ml.js'  
     import '~/components/scbd-angularjs-controls/form-control-directives/km-link-list.js'
-    // import documentMetadata from '~/views/directives/document-metadata.vue'
-    // import documentData from '~/views/forms/directives/document-date.vue'
     import kmTerm from '~/components/km/KmTerm.vue';
     import messages from '~/app-text/views/reports/chm/national-report.json';
     import { useI18n } from 'vue-i18n';
