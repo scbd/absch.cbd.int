f85db27063f495792c9e8e11dec01af19d967b06
Yuan Li <liyuan.montreal@gmail.com>
2024-05-06 14:34:20 -0400

    remove panel

--- app/views/forms/view/chm/strategic-plan-indicator.vue
+++ app/views/forms/view/chm/strategic-plan-indicator.vue
@@ -1,52 +1,46 @@
 <template> 
-    <div id="Record" class="record panel panel-default">
-       <div class="record-body panel-body bg-white" v-if="document"> 
-           <div class="panel panel-info" >
-               <div class="panel-heading"> 
-               </div>
-               <div class="panel-body">
-                    <div v-if="document.aichiTarget">
-                       <label>{{ t("aichiBiodiversityTarget") }} </label> 
-                       <div class="km-value">
-                           <km-term :value="document.aichiTarget" :locale="locale"></km-term>   
-                       </div>
-                   </div>
-                 
-                   <div v-if="document.availabilityStatus">
-                        <label>{{ t("availabilityOfIndicator") }} </label> 
-                        <div class="km-value">
-                            <km-term :value="document.availabilityStatus" :locale="locale"></km-term>   
-                        </div> 
-                    </div> 
-                
-                    <div v-if="document.isEasyToCommunicate">
-                        <label>{{ t("isEasyToCommunicate") }} </label>            
-                        <ng v-vue-ng:km-value-bool  :value="document.isEasyToCommunicate" :locales="locale" html></ng>
-                    </div>                
+    <div id="Record" class="record ">
+       <div class="record-body  bg-white" v-if="document">  
+           <div v-if="document.aichiTarget">
+                <label>{{ t("aichiBiodiversityTarget") }} </label> 
+                <div class="km-value">
+                    <km-term :value="document.aichiTarget" :locale="locale"></km-term>   
+                </div>
+            </div>
+            
+            <div v-if="document.availabilityStatus">
+                <label>{{ t("availabilityOfIndicator") }} </label> 
+                <div class="km-value">
+                    <km-term :value="document.availabilityStatus" :locale="locale"></km-term>   
+                </div> 
+            </div> 
+        
+            <div v-if="document.isEasyToCommunicate">
+                <label>{{ t("isEasyToCommunicate") }} </label>            
+                <ng v-vue-ng:km-value-bool  :value="document.isEasyToCommunicate" :locales="locale" html></ng>
+            </div>                
 
-                    <div v-if="document.isFromGlobalToNationalIndicator">
-                        <label>{{ t("isFromGlobalToNationalIndicator") }} </label>                     
-                        <ng v-vue-ng:km-value-bool  :value="document.isFromGlobalToNationalIndicator" :locales="locale" html></ng>                     
-                    </div>
-                
-                    <div v-if="document.isFromNationalToGlobalIndicator">
-                        <label>{{ t("isFromNationalToGlobalIndicator") }} </label>                        
-                        <ng v-vue-ng:km-value-bool  :value="document.isFromNationalToGlobalIndicator" :locales="locale" html></ng> 
-                    </div>
-                
-                    <div v-if="document.usedInGbo3OrGbo4">
-                        <label>{{ t("usedInGBbo3Gbo4") }} </label>                                         
-                        <ng v-vue-ng:km-value-bool  :value="document.usedInGbo3OrGbo4" :locales="locale" html></ng> 
-                    </div>
+            <div v-if="document.isFromGlobalToNationalIndicator">
+                <label>{{ t("isFromGlobalToNationalIndicator") }} </label>                     
+                <ng v-vue-ng:km-value-bool  :value="document.isFromGlobalToNationalIndicator" :locales="locale" html></ng>                     
+            </div>
+        
+            <div v-if="document.isFromNationalToGlobalIndicator">
+                <label>{{ t("isFromNationalToGlobalIndicator") }} </label>                        
+                <ng v-vue-ng:km-value-bool  :value="document.isFromNationalToGlobalIndicator" :locales="locale" html></ng> 
+            </div>
+        
+             <div v-if="document.usedInGbo3OrGbo4">
+                <label>{{ t("usedInGBbo3Gbo4") }} </label>                                         
+                <ng v-vue-ng:km-value-bool  :value="document.usedInGbo3OrGbo4" :locales="locale" html></ng> 
+            </div>
 
-                    <div v-if="document.source">
-                        <label>{{ t("Source") }} </label> 
-                        <div class="km-value"> 
-                            <ng v-vue-ng:km-link-list v-model:ng-model="document.source" ></ng> 
-                        </div>
-                    </div>
-               </div>
-           </div>                         
+            <div v-if="document.source">
+                <label>{{ t("Source") }} </label> 
+                <div class="km-value"> 
+                    <ng v-vue-ng:km-link-list v-model:ng-model="document.source" ></ng> 
+                </div>
+            </div>            
        </div>  
    </div>
 </template>
