7970df28295e0720785c9bce1c560444cffe59aa
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-05-06 14:24:41 +0500

    wip

--- app/views/portals/article-id.vue
+++ app/views/portals/article-id.vue
@@ -1,34 +1,35 @@
 <template>
   <div>
     <cbd-article :query="articleQuery" v-if="articleQuery">
-      <!-- @load="onArticleLoad($event)" :admin-tags="adminTags" -->
       <template #article-empty>&nbsp;</template>
     </cbd-article>
   </div>
 </template>
 
 <script setup>
-import { mapObjectId, isObjectId } from '~/api/api-base.js';
-import cbdArticle from './cbd-article.vue';
-import { ref, computed, onBeforeMount } from 'vue';
+  import { mapObjectId, isObjectId } from '~/api/api-base.js';
+  import cbdArticle from './cbd-article.vue';
+  import { ref, computed, onBeforeMount } from 'vue';
 
-const props = defineProps({
-      articleId: [String, Object], 
-      required: true,
-});
-const articleQuery = ref(null);
-const portalId = computed(() => $route.params.portalId);
-// const { portalId, articleId } = this; // Todo ???
+  const props = defineProps({
+        articleId: {
+          type: String,
+          required: true
+        }
+  });
 
-onBeforeMount(async () => {
+  const articleQuery = ref(null);
 
-  let ag = [];
-console.log('articleId:', props.articleId);
-  if (isObjectId(props.articleId)) { 
-    ag.push({ $match: { _id: mapObjectId(props.articleId) } });
+  const setQuery = function () {
+    let ag = [];
+    if (isObjectId(props.articleId)) { 
+      ag.push({ $match: { _id: mapObjectId(props.articleId) } });
+    }
+    articleQuery.value = { ag: JSON.stringify(ag) };
   }
 
-  articleQuery.value = { ag: JSON.stringify(ag) };
-});
+  onBeforeMount(async () => {
+    setQuery();
+  });
 
 </script>
