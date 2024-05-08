77f94dea7bfeadf78f555db513a30d69a44713f5
Yuan Li <liyuan.montreal@gmail.com>
2024-05-08 12:34:28 -0400

    add image , fields, use component to show relevant info; change json file

--- app/views/forms/view/chm/national-assessment.vue
+++ app/views/forms/view/chm/national-assessment.vue
@@ -1,122 +1,143 @@
 <template>
-    <div id="Record" class="record panel panel-default">
-        <div class="record-body panel-body bg-white" v-if="document">              
-            <div class="panel panel-info">
-                <div class="panel-heading">                 
-                    <legend>{{ t("generalInformation") }} </legend> 
-                </div>
-                <div class="panel-body">
-                    <div v-if="document.government">
-                        <label>{{ t("country") }} </label> 
-                        <div class="km-value">
-                            <km-term :value="document.government" :locale="locale"></km-term>   
-                        </div>
+    <div id="Record" class="record ">
+        <div class="record-body bg-white" v-if="document">   
+            <section>  
+                <div class="row">
+                    <div class="col-md-1">
+                        <img v-if="document.progress.identifier!='69C77387-2D3C-474C-BEA8-30F55D47AFBD'" :src="`app/img/chm/ratings/${document.progress.identifier}.png`" class="float-left">
                     </div>
+                
+                    <div class="col-md-11">
+                        <legend v-if="document.progress">{{document.date.substr(0,4)}} -    <km-term :value="document.progress" :locale="locale" ></km-term></legend>                
+                    </div>
+                </div>
+            </section>   
 
-                 
+            <section>            
+                <legend>{{ t("generalInformation") }} </legend> 
+                
+                <div v-if="document.government">
+                    <label>{{ t("country") }} </label> 
+                    <div class="km-value">
+                        <km-term :value="document.government" :locale="locale"></km-term>   
+                    </div>
+                </div>
+
+                <div v-if="document.description" >                   
+                    <label>{{ t("description") }} </label> 
+                    <ng v-vue-ng:km-value-ml  :value="document.description" :locales="locale" html></ng>                
+                </div>
+            </section>
+
+            <section>
+                <div v-if="document.aichiTarget || document.nationalTarget" > 
+
                     <div v-if="document.nationalTarget">
                         <label>{{ t("targets") }} </label>
                         <div class="km-value" compare-val>
                             <ng v-vue-ng:view-record-reference  v-model:ng-model="document.nationalTarget" :locales="locale" html></ng>                         
                         </div>        
-                    </div>                
-                </div>
-		    </div>
+                    </div> 
 
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                 <legend>{{ t("categoryOfProgressTowardsTheImplementationOfTheSelectedTarget") }} </legend>
-                </div>
-                <div class="panel-body">            
+                    <div v-if="document.aichiTarget">
+                        <km-term :value="document.aichiTarget" :locale="locale"></km-term>   
+                    </div>
+                </div>              
+            </section>
+
+           <section>
+                <div v-if="document.assessmentText || document.progress || document.date">
+                    <legend>{{ t("category") }} </legend>
+                
                     <div v-if="document.progress">
-                        <label>{{ t("rateOfProgressesTowardTheImplementationOfTheSelectedTarget") }} </label> 
+                        <label>{{ t("rate") }} </label> 
                         <div class="km-value">
                             <km-term :value="document.progress" :locale="locale" ></km-term>
                         </div>
                     </div>
         
                     <div v-if="document.date">
-                        <label>{{ t("dateTheAssessmentWasDone") }} </label>   
+                        <label>{{ t("date") }} </label>   
                         <ng v-vue-ng:km-value-ml  :value="formatDate(document.date, 'DD MMM YYYY')" :locales="locale" html></ng>  
                     </div>
-                   
+                    
                     <div v-if="document.assessmentText">
-                        <label>{{ t("summaryOfTheAssessmentOfProgressesTowardTheImplementationOfTheSelectedTarget") }} </label>                        
-                        <ng v-vue-ng:km-value-ml  :value="document.assessmentText" :locales="locale" html></ng>  
-                    </div>              
+                        <label>{{ t("summary") }} </label>                        
+                        <ng v-vue-ng:km-value-ml  :value="document.assessmentText" :locales="locale" html></ng> 
+                    </div>                
                 </div>
-            </div>
+            </section>
 
-            <div class="panel panel-info" v-if="document.nationalIndicators">
-                <div class="panel-heading">
-                    <legend>{{ t("indicatorsAndActivities") }} </legend>   
-                </div>
-                <div class="panel-body">              
-                    <div v-if="document.nationalIndicators">
-                        <label>{{ t("indicatorsUsedInThisAssessment") }} </label>    
-                        <div v-if="Array.isArray(document.nationalIndicators)">
-                            <div  v-for="indicator in document.nationalIndicators" >                     
-                                <div v-if="indicator.description">
-                                    <ng v-vue-ng:km-value-ml  :value="indicator.description" :locales="locale" html></ng>  
-                                </div>                 
-                            </div>  
-                        </div>
-                        <div v-else>
-                            <ng v-vue-ng:km-value-ml  :value="document.nationalIndicators" :locales="locale" html></ng>  
+            <section>
+                <div v-if="document.nationalIndicatorsUsed || document.nationalIndicators || document.strategicPlanIndicators || document.implementationActivities">
+                    <legend>{{ t("indicatorsAndActivities") }} </legend>    
+    
+                    <div v-if="document.nationalIndicatorsUsed===false">                        
+                        <div class="km-value" >
+                            {{ t("noIndicators") }} 
                         </div>
                     </div>
 
-                    <div v-if="document.documentText">                        
-                        <label>{{ t("anyOtherToolsOrMeansUsedForAssessingProgress") }} </label>                        
-                        <ng v-vue-ng:km-value-ml  :value="document.documentText" :locales="locale" html></ng> 
+                    <div v-if="document.nationalIndicatorsUsed && document.nationalIndicators"> 
+                        <label>{{ t("indicators") }} </label>                        
+                        <ng v-vue-ng:km-value-ml  :value="document.nationalIndicators" :locales="locale" html></ng>                                        
                     </div>
-
-                    <div v-if="document.documentLinks">                        
-                        <label>{{ t("relevantWebsitesLinksAndFiles") }} </label>
-                       <div class="km-value" compare-val>                 
-                            <ng v-vue-ng:km-link-list v-model:ng-model="document.documentLinks"  ></ng>                    
+    
+                    <div v-if="document.strategicPlanIndicators">
+                        <label>{{ t("linkToIndicator") }}</label>
+                        <div class="km-value">
+                            <div v-for="item in strategicPlanIndicators" >
+                                {{lstring(item.title ,locale) }}
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div v-if="document.implementationActivities">
+                        <label>{{ t("linkToActivity") }}</label>
+                        <div class="km-value">
+                            <div v-for="item in document.implementationActivities" >
+                                {{lstring(item.title ,locale) }}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
 
-            <div class="panel panel-info">
-                <div class="panel-heading">
+            <section>
+                <div v-if="document.confidence || document.confidenceInfo || document.adequacy||document.adequacyDescription" >
                     <legend>{{t("levelOfConfidence") }}</legend>
-                </div>
-                <div class="panel-body">   
+    
                     <div v-if="document.confidence">
-                        <label>{{ t("levelOfConfidenceOfTheAboveAssessment") }} </label>   
+                        <label>{{ t("levelOfConfidence") }} </label>   
                         <div class="km-value">
                             <km-term :value="document.confidence" :locale="locale"></km-term>
                         </div> 
                     </div>
-
+    
                     <div v-if="document.confidenceInfo">
-                        <label>{{ t("levelOfConfidenceOfTheAboveAssessment") }} </label>   
+                        <label>{{ t("levelOfAboveAssessment") }} </label>   
                         <ng v-vue-ng:km-value-ml  :value="document.confidenceInfo" :locales="locale" html ></ng>  
                     </div>
-            
+
                     <div v-if="document.adequacy">
-                        <label>{{ t("adequacyOfMonitoringInformationToSupportAssessment") }} </label>                         
+                        <label>{{ t("adequacy") }} </label>                         
                         <div class="km-value">
                             <km-term :value="document.adequacy" :locale="locale"></km-term>
                         </div> 
                     </div>
 
                     <div v-if="document.adequacyDescription">
-                        <label>{{ t("monitoringSystemForTheTarget") }} </label>   
+                        <label>{{ t("monitoringSystem") }} </label>   
                         <ng v-vue-ng:km-value-ml  :value="document.adequacyDescription" :locales="locale" html></ng>
                     </div> 
 
-                    <div v-if="document.otherRelevantDocuments">
-                        <label>{{ t("otherRelevantWebsiteAddressOrAttachedDocuments") }} </label>   
-                        <div class="km-value" compare-val>                 
-                            <ng v-vue-ng:km-link-list v-model:ng-model="document.otherRelevantDocuments"  ></ng>                    
-                        </div>
-                    </div> 
                 </div>
-            </div>         
+            </section> 
+
+            <view-relevant-information :relevantDocs="document.otherRelevantDocuments"  :documentText="document.documentText" 
+                                       :documentLinks="document.documentLinks"          :locale="locale">
+            </view-relevant-information> 
+                                   
         </div>   
     </div>
 </template>
@@ -127,8 +148,10 @@
     import '~/views/forms/view/directives/view-record-reference.directive.js'
     import kmTerm from '~/components/km/KmTerm.vue';
     import messages from '~/app-text/views/reports/chm/national-assessment.json';
-    import { useI18n } from 'vue-i18n';
+    import viewRelevantInformation from '~/views/forms/view/directives/view-relevant-information.vue';
+    import { lstring    } from '~/services/filters/lstring.js'; 
     import { formatDate } from '~/components/kb/filters';
+    import { useI18n    } from 'vue-i18n';
 
     const { t } = useI18n({ messages });
 
@@ -136,6 +159,6 @@
         documentInfo: { type: Object, required: true },
         locale      : { type:String}
     })
-    const document = computed(()=>props.documentInfo?.body);
+    const document = computed(()=>props.documentInfo?.body);  
     
 </script>
