831d64802c1b32065c04f0d25bfbcb26aa1e811f
Blaise Fonseca <blaise.fonseca@un.org>
2024-05-09 16:07:42 -0400

    Robot redirect (#1169)
    * Disallow robots to index /pdf location
    
    * If a bot request pdf redirect them to html view page
    
    * Add protocol and host to redirect for prerender
    
    * Upgrade node docker file version

---
+++ middlewares/reject-bots-pdf.js
@@ -0,0 +1,15 @@
+import { isbot } from "isbot";
+
+export default function(req, res, next){
+    
+    if(isbot(req.get("user-agent")) || req.header['x-prerender']!= undefined){
+        let url = `${req.protocol}://${req.get('host')}/${req.params.lang||'en'}/database/${req.params.documentId}`;
+
+        if(req.params.revision)
+            url += `-${req.params.revision}`;
+
+        return res.redirect(url);
+    }
+
+    next()
+}
