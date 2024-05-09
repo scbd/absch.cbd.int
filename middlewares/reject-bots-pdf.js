4a28f0380286a8b6558113202dc9b16cef2a9b0b
Blaise Fonseca <blaise.fonseca@un.org>
2024-05-09 14:28:22 -0400

    Add protocol and host to redirect for prerender

--- middlewares/reject-bots-pdf.js
+++ middlewares/reject-bots-pdf.js
@@ -3,10 +3,10 @@ import { isbot } from "isbot";
 export default function(req, res, next){
     
     if(isbot(req.get("user-agent")) || req.header['x-prerender']!= undefined){
-        let url = `/${req.params.lang||'en'}/database/${req.params.documentId}`;
+        let url = `${req.protocol}://${req.get('host')}/${req.params.lang||'en'}/database/${req.params.documentId}`;
 
         if(req.params.revision)
-            url += `/${req.params.revision}`;
+            url += `-${req.params.revision}`;
 
         return res.redirect(url);
     }
