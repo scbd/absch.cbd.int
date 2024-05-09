93fd2872fdb558fa6afc0fa1b8e96c02847105f8
Blaise Fonseca <blaise.fonseca@un.org>
2024-05-09 16:35:19 -0400

    remove redirect

--- middlewares/reject-bots-pdf.js
+++ middlewares/reject-bots-pdf.js
@@ -1,14 +1,15 @@
 import { isbot } from "isbot";
 
 export default function(req, res, next){
-    
+    console.log(req.get("user-agent"));
+
     if(isbot(req.get("user-agent")) || req.header['x-prerender']!= undefined){
         let url = `${req.protocol}://${req.get('host')}/${req.params.lang||'en'}/database/${req.params.documentId}`;
 
         if(req.params.revision)
             url += `-${req.params.revision}`;
 
-        return res.redirect(url);
+        // return res.redirect(url);
     }
 
     next()
