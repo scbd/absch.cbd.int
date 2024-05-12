9dc4f3aa62316be9dec29ec9a9df7d64d99c7d74
Yuan Li <liyuan.montreal@gmail.com>
2024-05-12 15:22:43 -0400

    remove panel in style

--- app/views/forms/view/chm/marine-ebsa.vue
+++ app/views/forms/view/chm/marine-ebsa.vue
@@ -1,130 +1,105 @@
 <template>
-    <div id="Record" class="record panel panel-default">   
-        <div class="record-body panel-body bg-white" v-if="document">             
-            <!-- <document-data  :document="document"></document-data>  -->
-
-            <div class="panel panel-info">
-                <div class="panel-heading">                 
-                    <legend>{{ t("generalInformation") }} </legend> 
+    <div id="Record" class="record ">   
+        <div class="record-body bg-white" v-if="document"> 
+            <section>       
+                <legend>{{ t("generalInformation") }} </legend> 
+            
+                <div v-if="document.summary">
+                    <label>{{ t("summary") }} </label> 
+                    <ng  v-vue-ng:km-value-ml  :value="document.summary" :locales="locale" html></ng> 
                 </div>
-                <div class="panel-body">
-                    <div v-if="document.summary">
-                        <label>{{ t("summary") }} </label> 
-                        <ng  v-vue-ng:km-value-ml  :value="document.summary" :locales="locale" html></ng> 
-                    </div>
-                
-                    <div v-if="document.areaIntroducion">
-                        <label>{{ t("introductionOfTheArea") }} </label>
-                        <ng  v-vue-ng:km-value-ml  :value="document.areaIntroducion" :locales="locale" html></ng> 
-                    </div>
-                </div>
-		    </div>
+            
+                <div v-if="document.areaIntroducion">
+                    <label>{{ t("introductionOfTheArea") }} </label>
+                    <ng  v-vue-ng:km-value-ml  :value="document.areaIntroducion" :locales="locale" html></ng> 
+                </div>               
+		    </section> 
 
-            <div class="panel panel-info">
-                <div class="panel-heading">
+            <section>
                  <legend>{{ t("descriptionOfTheLocation") }} </legend>
-                </div>                
-                <div class="panel-body">            
-                    <div v-if="document.region">
-                        <label>{{ t("ebsaRegion") }} </label> 
-                        <div class="km-value">
-                            <km-term :value="document.region" :locale="locale" ></km-term>
-                        </div>
-                    </div>
-
-                    <div v-if="document.location">
-                        <label>{{ t("descriptionOfLocation") }} </label>   
-                        <ng v-vue-ng:km-value-ml  :value="document.location" :locales="locale" html></ng>  
-                    </div>
-
-                    <div v-if="document.gisFiles">
-                        <label>{{ t("geoLocation") }} </label> 
-                        <div class="km-value">
-                            <div v-if="document.gisMapCenter">                                              
-                                <ng v-vue-ng:km-link-list v-model:ng-model="document.gisFiles"  ></ng> 
-                            </div>     
-                            <div v-if="mapConfig">  
-                                <ng v-vue-ng:leaflet :map-config="mapConfig"  :center="document.gisMapCenter" :layers="gisLayer" :scroll-wheel-zoom="false"></ng> 
-                            </div> 
-                        </div>
+                         
+                <div v-if="document.region">
+                    <label>{{ t("ebsaRegion") }} </label> 
+                    <div class="km-value">
+                        <km-term :value="document.region" :locale="locale" ></km-term>
                     </div>
                 </div>
-            </div>
 
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <legend>{{ t("areaDetails") }} </legend>   
+                <div v-if="document.location">
+                    <label>{{ t("descriptionOfLocation") }} </label>   
+                    <ng v-vue-ng:km-value-ml  :value="document.location" :locales="locale" html></ng>  
                 </div>
 
-                <div class="panel-body">
-                    <div v-if="document.areaDescription"> 
-                        <label>{{ t("featureDescriptionOfTheArea") }} </label> 
-                        <ng v-vue-ng:km-value-ml  :value="document.areaDescription" :locales="locale" html></ng>                        
+                <div v-if="document.gisFiles">
+                    <label>{{ t("geoLocation") }} </label> 
+                    <div class="km-value">
+                        <div v-if="document.gisMapCenter">                                              
+                            <ng v-vue-ng:km-link-list v-model:ng-model="document.gisFiles"  ></ng> 
+                        </div>     
+                        <div v-if="mapConfig">  
+                            <ng v-vue-ng:leaflet :map-config="mapConfig"  :center="document.gisMapCenter" :layers="gisLayer" :scroll-wheel-zoom="false"></ng> 
+                        </div> 
                     </div>
-                    <div v-if="document.areaConditions">
-                        <label>{{ t("featureConditionsAndFutureOutlookOfThArea") }} </label>  
-                        <ng v-vue-ng:km-value-ml  :value="document.areaConditions" :locales="locale" html></ng>  
+                </div>              
+            </section>
+
+            <section>
+                <legend>{{ t("areaDetails") }} </legend> 
+                <div v-if="document.areaDescription"> 
+                    <label>{{ t("featureDescriptionOfTheArea") }} </label> 
+                    <ng v-vue-ng:km-value-ml  :value="document.areaDescription" :locales="locale" html></ng>                        
+                </div>
+
+                <div v-if="document.areaConditions">
+                    <label>{{ t("featureConditionsAndFutureOutlookOfThArea") }} </label>  
+                    <ng v-vue-ng:km-value-ml  :value="document.areaConditions" :locales="locale" html></ng>  
+                </div>              
+            </section>
+
+            <section>
+                <legend>{{ t("references") }}</legend>                 
+                <div v-if="document.referenceText">
+                    <label>{{ t("references") }} </label>   
+                    <ng v-vue-ng:km-value-ml  :value="document.referenceText" :locales="locale" html ></ng>  
+                </div>
+
+                <div v-if="document.relation">
+                    <label>{{ t("otherRelevantWebsiteAddressOrAttachedDocuments") }} </label> 
+                    <div class="km-value" compare-val>                 
+                        <ng v-vue-ng:km-link-list v-model:ng-model="document.relation"  ></ng>                    
                     </div>
                 </div>
-            </div>
-
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <legend>{{ t("references") }}</legend>
-                </div>
+            </section>
             
-                <div class="panel-body">   
-                    <div v-if="document.referenceText">
-                        <label>{{ t("references") }} </label>   
-                        <ng v-vue-ng:km-value-ml  :value="document.referenceText" :locales="locale" html ></ng>  
-                    </div>
-
-                    <div v-if="document.relation">
-                        <label>{{ t("otherRelevantWebsiteAddressOrAttachedDocuments") }} </label> 
-                        <div class="km-value" compare-val>                 
-                            <ng v-vue-ng:km-link-list v-model:ng-model="document.relation"  ></ng>                    
-                        </div>
-                    </div>
-                </div>
-            </div> 
+            <section>
+                <legend>{{ t("statusOfSubmission") }}</legend>
             
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <legend>{{ t("statusOfSubmission") }}</legend>
-                </div>                
-                
-                <div class="panel-body">   
-                    <div v-if="document.status">
-                         <label>{{ t( "copDecision") }} </label> 
-                        <div class="km-value" > 
-                            <label>{{ lstring( document?.approvedByCopDecision?.identifier) }} </label>                           
-                        </div>
-                    </div>                    
-                </div>
-            </div>
+                <div v-if="document.status">
+                        <label>{{ t( "copDecision") }} </label> 
+                    <div class="km-value" > 
+                        <label>{{ lstring( document?.approvedByCopDecision?.identifier) }} </label>                           
+                    </div>
+                </div>  
+            </section>
             
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <legend>{{ t("assessmentOfTheAreaAgainstCbdEbsaCriteria") }}</legend>
-                </div>     
-                 <div class="panel-body">   
-                    <div v-if="document.assessments">                    
-                        <div class="km-value">                    
-                            <div  v-for="assessment in document.assessments" >  
-                                <div>                           
-                                    <div v-if="assessment.identifier">  
-                                        <label> {{t(assessment.identifier) }}   <span v-if="assessment.level"   :class="`${getBgColor(assessment.level)} text-white p-1`" > {{t(assessment.level)}}</span> </label>
-                                        <br/>
-                                    </div>                                
+            <section>
+                <legend>{{ t("assessmentOfTheAreaAgainstCbdEbsaCriteria") }}</legend>
+              
+                <div v-if="document.assessments">                    
+                    <div class="km-value">                    
+                        <div  v-for="assessment in document.assessments" >  
+                            <div>                           
+                                <div v-if="assessment.identifier">  
+                                    <label> {{t(assessment.identifier) }}   <span v-if="assessment.level"   :class="`${getBgColor(assessment.level)} text-white p-1`" > {{t(assessment.level)}}</span> </label>
+                                    <br/>
                                 </div>                                
-                                <label>{{ t("justification") }} </label>   
-                                <ng v-vue-ng:km-value-ml  :value="assessment.justification" :locales="locale" html ></ng>
-                            </div>
+                            </div>                                
+                            <label>{{ t("justification") }} </label>   
+                            <ng v-vue-ng:km-value-ml  :value="assessment.justification" :locales="locale" html ></ng>
                         </div>
-                    </div>                    
+                    </div>
                 </div>
-            </div> 
-
+            </section>
         </div>
     </div>
 </template>
@@ -172,12 +147,9 @@
             return  await L.geoJson(data);
         });
 
-        const s = await Promise.all(qLayers); 
-       
+        const s = await Promise.all(qLayers);        
         //console.log("s", s)
-
         gisLayer.value = s; 
-
         //console.log("gisLayer.value", gisLayer.value)    
     };
    
