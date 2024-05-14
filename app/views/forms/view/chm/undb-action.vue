94fc102d9d728ba23f3eec2a39b9911a43b5755d
Yuan Li <liyuan.montreal@gmail.com>
2024-05-14 15:41:40 -0400

    fix code review issues

--- app/views/forms/view/chm/undb-action.vue
+++ app/views/forms/view/chm/undb-action.vue
@@ -3,28 +3,28 @@
         <div class="record-body bg-white" v-if="document"> 
             <section>
                 <div v-if="document.title">
-                    <label for="subTopic">{{ t("title") }}</label>
+                    <label>{{ t("title") }}</label>
                     <ng v-vue-ng:km-value-ml  :value="document.title" :locales="locale" html></ng>  
                 </div>
             </section>
 
             <section>                
                 <div v-if="document.startDate">
-                    <label for="subTopic">{{ t("startDate") }}</label>
+                    <label>{{ t("startDate") }}</label>
                     <ng v-vue-ng:km-value-ml  :value="formatDate(document.startDate, 'YYYY-MM-DD')" :locales="locale" html></ng>  
                 </div>
                 
                 <div v-if="document.endDate">
-                    <label for="subTopic">{{ t("endDate") }}</label>	
+                    <label>{{ t("endDate") }}</label>	
                     <ng v-vue-ng:km-value-ml  :value="formatDate(document.endDate, 'YYYY-MM-DD')" :locales="locale" html></ng>  
                 </div>
                 <div v-if="document.startTime">
-                    <label for="subTopic">{{ t("fromTime") }}</label>
+                    <label>{{ t("fromTime") }}</label>
                     <div class="km-value"> {{document.startTime}}</div>
                 </div>
 
                 <div v-if="document.endTime">
-                    <label for="subTopic">{{ t("toTime") }}</label>	
+                    <label>{{ t("toTime") }}</label>	
                     <div class="km-value">{{document.endTime}} </div>
                 </div> 
             </section>
@@ -38,65 +38,76 @@
                 </div>
 
                 <div v-if="document.address">
-                    <label for="subTopic">{{ t("address") }}</label>   
+                    <label>{{ t("address") }}</label>   
                     <ng v-vue-ng:km-value-ml  :value="document.address" :locales="locale" html></ng>  
                 </div>
 
                 <div v-if="document.onlineEvent">
-                    <label for="subTopic">{{ t("eventIsOnline") }}</label>
+                    <label>{{ t("eventIsOnline") }}</label>
                 </div>
 
                 <div v-if="document.googleMaps">
-                    <label for="subTopic">{{ t("googleMapsLink") }}</label>    
-                    <ng v-vue-ng:km-value-ml  :value="document.googleMaps" :locales="locale" html></ng>  
+                    <label>{{ t("googleMapsLink") }}</label>
+                    <div class="km-value"> 
+                        <a :href="document.googleMaps" target="_blank">{{document.googleMaps}}</a>  
+                    </div> 
                 </div>
             </section>
 
             <section>
                 <div v-if="document.email">
-                    <label for="subTopic">{{ t("email") }}</label>    
-                    <div class="km-value">{{document.email}} </div>
+                    <label>{{ t("email") }}</label> 
+                    <div class="km-value">
+                        <a :href="`mailto:${document.email}`">{{document.email}} </a>
+                    </div> 
                 </div>
                 <div v-if="document.phone">
-                    <label for="subTopic">{{ t("telephone") }}</label>
+                    <label>{{ t("telephone") }}</label>
                     <div class="km-value">{{document.phone}} </div>
                 </div>
                 <div v-if="document.facebook">
-                    <label for="subTopic">{{ t("facebook") }}</label>
-                <div class="km-value">{{document.facebook}}</div>
+                    <label>{{ t("facebook") }}</label>
+                    <div class="km-value"> 
+                        <a :href="document.facebook" target="_blank">{{document.facebook}}</a>  
+                    </div> 
                 </div>
                 <div v-if="document.twitter">
-                    <label for="subTopic">{{ t("twitter") }}</label>
-                    <div class="km-value">{{document.twitter}}</div>
+                    <label>{{ t("twitter") }}</label>                   
+                    <div class="km-value"> 
+                        <a :href="document.twitter" target="_blank">{{document.twitter}}</a>  
+                    </div>
                 </div>
+
                 <div v-if="document.youtube">
-                    <label for="subTopic">{{ t("youtube") }}</label>  
-                    <div class="km-value">{{document.youtube}}</div>
+                    <label>{{ t("youtube") }}</label> 
+                    <div class="km-value"> 
+                        <a :href="document.youtube" target="_blank">{{document.youtube}}</a>  
+                    </div>
                 </div>
             </section>
 
             <section>
                 <div v-if="document.description">
-                    <label for="subTopic">{{ t("shortDescription") }}</label>
+                    <label >{{ t("shortDescription") }}</label>
                     <ng v-vue-ng:km-value-ml  :value="document.description" :locales="locale" html></ng>  
                 </div>
 
                 <div v-if="document.descriptionNative">
-                    <label for="subTopic">{{ t("shortDescriptionInOfficialLanguage") }}</label> 
-                    <ng v-vue-ng:km-value-ml  :value="document.descriptionNative" :locales="locale" html></ng>  
+                    <label >{{ t("shortDescriptionInOfficialLanguage") }}</label> 
+                    <div class="km-value">
+                        {{ document.descriptionNative }}
+                    </div>
                 </div>
-
+               
                 <div v-if="document.notes">
-                    <label for="subTopic">{{ t("commentsOrAdditionalInfo") }}</label>  
-                    <div class="km-value">
-                        {{document.descriptionNative}}
-                    </div> 
+                    <label >{{ t("commentsOrAdditionalInfo") }}</label>                      
+                    <ng v-vue-ng:km-value-ml  :value="document.notes" :locales="locale" html></ng>  
                 </div>
-
+              
                 <div v-if="document.logo">
-                    <label for="subTopic">{{ t("logo") }}</label>
-                    <div class="km-value">
-                        <img :src="`${document.logo}/thumbnail`"/>
+                    <label >{{ t("logo") }}</label>                  
+                    <div class="km-value">                    
+                        <img :src="thumbnailLogoUrl" height=100  />
                     </div>
                 </div>
             </section>
@@ -120,12 +131,22 @@
 
     const { t } = useI18n({ messages });    
    
-    const country = computed(()=>{
-        const term = ref({});
+    const term = ref({});
+    const country = computed(()=>{   
         term.value.identifier=document.value.country;        
         return term.value;    
     });
     
     const document = computed(()=>props.documentInfo?.body);   
+
+    const thumbnailLogoUrl = computed(()=>{
+        if (!document.value.logo) return "";
+        const url = document.value.logo
+        const index = url.lastIndexOf("/");
+        const imgPath = url.substring(0, index);
+        const imgName = url.substring(index+1);
+        const fullPath =  imgPath+"/thumbnail/"+imgName;
+        return  fullPath;
+    });  
    
 </script>
