bfd2c36e96120efd2a4962d4c76a3899336ec33a
Yuan Li <liyuan.montreal@gmail.com>
2024-05-16 18:10:29 -0400

    use view-terms-hierarchy; add todos for compare-val and footer; add view-reference-record; change locale as required true; change <section> condition;

--- app/views/forms/view/chm/capacity-building-resource.vue
+++ app/views/forms/view/chm/capacity-building-resource.vue
@@ -1,195 +1,196 @@
 <template>
     <div id="Record" class="record" >
         <div class="record-body bg-white" v-if="document"> 
-           <legend>{{t("generalInformation")}}</legend>
-           
-           <div v-if="document.title">
-                <label>{{t("title")}}</label>
-                <ng v-vue-ng:km-value-ml  :value="document.title" :locales="locale" ></ng>
-            </div>
-           
+            <!--TODO: add compare-val for fields  -->
 
-           <div v-if="document.resourceTypes">
-                <label>{{t("typeOfResource")}}</label>
-                <ul class="km-value">
-                    <li v-for="resourceType in document.resourceTypes">                   
-                        <km-term :value="resourceType" :locale="locale"></km-term>
-                    </li>
-                </ul>
-            </div>
+            <!-- TODO: add publish date -->            
+            <!-- <ng v-vue-ng:document-date></ng> -->
+            <section>
+            <legend>{{t("generalInformation")}}</legend>           
+            <div v-if="document.title">
+                    <label>{{t("title")}}</label>
+                    <ng v-vue-ng:km-value-ml  :value="document.title" :locales="locale" ></ng>
+                </div>           
 
-            <div v-if="document.fileFormat">
-                <label>{{t("format")}}</label>
-                <div class="km-value">                
-                    <km-term :value="document.fileFormat" :locale="locale"></km-term>
+            <div v-if="document.resourceTypes">
+                    <label>{{t("typeOfResource")}}</label>
+                    <ul class="km-value">
+                        <li v-for="resourceType in document.resourceTypes">                   
+                            <km-term :value="resourceType" :locale="locale"></km-term>
+                        </li>
+                    </ul>
                 </div>
-            </div>
 
-            <div v-if="document.formatDetails">
-                <label>{{t("formatDetails")}}</label>              
-                <ng v-vue-ng:km-value-ml  :value="document.formatDetails" :locales="locale" html></ng>
-            </div>
+                <div v-if="document.fileFormat">
+                    <label>{{t("format")}}</label>
+                    <div class="km-value">                
+                        <km-term :value="document.fileFormat" :locale="locale"></km-term>
+                    </div>
+                </div>
 
-            <div v-if="document.purpose">
-                <label>{{t("pleaseIdentify")}}</label>
-                <ul class="km-value">
-                    <li v-for="term in document.purpose">                       
-                        <km-term :value="term" :locale="locale"></km-term>
-                    </li>
-                </ul>
-            </div>
+                <div v-if="document.formatDetails">
+                    <label>{{t("formatDetails")}}</label>              
+                    <ng v-vue-ng:km-value-ml  :value="document.formatDetails" :locales="locale" html></ng>
+                </div>
 
-            <div v-if="document.targetGroups">
-                <label>{{t("mainTarget")}}</label>
-                <div class="km-value">
-                    <ul>
-                        <li v-for="term in document.targetGroups">                      
+                <div v-if="document.purpose">
+                    <label>{{t("pleaseIdentify")}}</label>
+                    <ul class="km-value">
+                        <li v-for="term in document.purpose">                       
                             <km-term :value="term" :locale="locale"></km-term>
                         </li>
                     </ul>
                 </div>
-            </div>
 
-            <div v-if="document.expertiseLevel">
-                <label>{{t("level")}}</label>
-                <div class="km-value">
-                    <km-term :value="document.expertiseLevel" :locale="locale"></km-term>
+                <div v-if="document.targetGroups">
+                    <label>{{t("mainTarget")}}</label>
+                    <div class="km-value">                       
+                        <li v-for="term in document.targetGroups">                      
+                            <km-term :value="term" :locale="locale"></km-term>
+                        </li>                        
+                    </div>                   
                 </div>
-            </div>
 
-            <div v-if="document.authors">
-                <label>{{t("authors")}}</label>            
-                <ng v-vue-ng:km-value-ml  :value="document.authors" :locales="locale" html></ng>           
-            </div>
-
-            <div v-if="document.organizations">
-                <label>{{t("organizations")}}</label>
-                <div class="km-value" v-for="org in authoringOrganizations">
-                    <strong>{{lstring(org.name,locale)}}
-                         <i v-if="org.acronym">
-                            ({{string(org.acronym,locale)}})
-                        </i>
-                    </strong>
-                <div>  
-                <km-term :value="org.organizationType" :locale="locale"></km-term>                     
+                <div v-if="document.expertiseLevel">
+                    <label>{{t("level")}}</label>
+                    <div class="km-value">
+                        <km-term :value="document.expertiseLevel" :locale="locale"></km-term>
+                    </div>
                 </div>
-            </div>
 
-            <div v-if="document.publicationYear">
-                <label>{{t("publicationYear")}}</label>
-                <div class="km-value">{{document.publicationYear}}</div>
-            </div>
-
-            <div v-if="document.rights">
-                <label>{{t("rights")}}</label>	
-                <ng v-vue-ng:km-value-ml  :value="document.rights" :locales="locale" html></ng> 
-            </div>
-        
-            <div v-if="document.source">
-                <label>{{t("source")}}</label>
-                <ng v-vue-ng:km-value-ml  :value="document.source" :locales="locale" html></ng> 
-            </div>
-
-            <legend v-if="document.resourceLinks || document.covers">{{t("access")}}</legend>
-
-            <div v-if="document.resourceLinks">
-                <label>{{t("link")}}</label>
-                <div class="km-value" v-for="item in document.resourceLinks">
-                    <a translation-url target="target" :href="item.url">{{item.name||item.url}}</a>
-                    <i v-if="item.name && item.url.indexOf('/api/v2013/documents/')<0">({{item.url}})</i>
+                <div v-if="document.authors">
+                    <label>{{t("authors")}}</label>            
+                    <ng v-vue-ng:km-value-ml  :value="document.authors" :locales="locale" html></ng>           
                 </div>
-            </div>
-        
-            <div v-if="document.covers">
-                <label>{{t("coverImages")}}</label>
-                <div class="km-value">
-                    <a translation-url v-for="link in document.covers" :href="link.url" target="target">
-                        <img class="img-polaroid" :src="link.url" alt="link.name" style="max-height: 150px; max-width: 150px" />
-                    </a>
+
+                <div v-if="document.organizations">
+                    <label>{{t("organizations")}}</label>
+                    <div class="km-value" v-for="org in authoringOrganizations">
+                        <strong>{{lstring(org.name,locale)}}
+                            <i v-if="org.acronym">
+                                ({{string(org.acronym,locale)}})
+                            </i>
+                        </strong>
+                    </div>  
+                    <km-term :value="org.organizationType" :locale="locale"></km-term> 
                 </div>
-            </div>
 
-            <legend v-if="document.summary || document.regions">{{t("information")}}</legend>
+                <div v-if="document.publicationYear">
+                    <label>{{t("publicationYear")}}</label>
+                    <div class="km-value">{{document.publicationYear}}</div>
+                </div>
 
-            <div v-if="document.summary">
-                <label>{{t("summary")}}</label>		
-                <ng v-vue-ng:km-value-ml  :value="document.summary" :locales="locale" html></ng>           
-            </div>
+                <div v-if="document.rights">
+                    <label>{{t("rights")}}</label>	
+                    <ng v-vue-ng:km-value-ml  :value="document.rights" :locales="locale" html></ng> 
+                </div>
+            
+                <div v-if="document.source">
+                    <label>{{t("source")}}</label>
+                    <ng v-vue-ng:km-value-ml  :value="document.source" :locales="locale" html></ng> 
+                </div>
+            </section>
 
-            <div v-if="orderedRegions.length>0">
-                <label>{{t("countriesAndGroups")}}</label>
-                <ul class="km-value">               
-                    <li v-for="term in orderedRegions">          
-                        <km-term :value="term" :locale="locale"></km-term> 
-                    </li>
-                </ul>
-            </div>
+            <section v-if="document.resourceLinks || document.covers">
+                <legend>{{t("access")}}</legend>
 
-            <div v-if="document.cbdSubjects || document.gbfTargets || document.aichiTargets">
+                <div v-if="document.resourceLinks">
+                    <label>{{t("link")}}</label>
+                    <div class="km-value" v-for="item in document.resourceLinks">
+                        <a translation-url target="target" :href="item.url">{{item.name||item.url}}</a>
+                        <i v-if="item.name && item.url.indexOf('/api/v2013/documents/')<0">({{item.url}})</i>
+                    </div>
+                </div>
+            
+                <div v-if="document.covers">
+                    <label>{{t("coverImages")}}</label>
+                    <div class="km-value">
+                        <a translation-url v-for="link in document.covers" :href="link.url" target="target">
+                            <img class="img-polaroid" :src="link.url" alt="link.name" style="max-height: 150px; max-width: 150px" />
+                        </a>
+                    </div>
+                </div>
+            </section>
 
+            <section v-if="document.summary || document.regions">
+                <legend>{{t("information")}}</legend>
+
+                <div v-if="document.summary">
+                    <label>{{t("summary")}}</label>		
+                    <ng v-vue-ng:km-value-ml  :value="document.summary" :locales="locale" html></ng>           
+                </div>
+
+                <div v-if="orderedRegions.length>0">
+                    <label>{{t("countriesAndGroups")}}</label>
+                    <div class="km-value">               
+                        <li v-for="term in orderedRegions">          
+                            <km-term :value="term" :locale="locale"></km-term> 
+                        </li>
+                    </div>
+                </div>
+            </section>
+
+            <section v-if="document.cbdSubjects || document.gbfTargets || document.aichiTargets">
                 <legend>{{t("generalInformation")}}{{t("subjectAreas")}}</legend>
         
                 <div v-if="document.cbdSubjects">
-                    <label>{{t("subject")}}</label>
-                    <ul class="km-value">
-                        <li v-for="term in document.cbdSubjects">
-                            <km-term :value="term" :locale="locale"></km-term> 
-                        </li>
-                    </ul>
+                    <label>{{t("subject")}}</label>                   
+                    <div class="km-value">                           
+                        <ng v-vue-ng:view-terms-hierarchy  v-model:ng-model="document.cbdSubjects" locale="locale"   term-domain="cbdSubjects"></ng>
+                    </div>
                 </div>
         
                 <div v-if="document.gbfTargets">
                     <div v-if="document.gbfTargets">
-                        <label>{{t("gbfTarget")}}</label>
-                        <ul class="km-value">
-                            <li v-for="term in document.gbfTargets">
-                                <km-term :value="term" :locale="locale"></km-term> 
-                            </li>
-                        </ul>
+                        <label>{{t("gbfTarget")}}</label>                        
+                        <div class="km-value">                           
+                            <ng v-vue-ng:view-terms-hierarchy  v-model:ng-model="document.gbfTargets" locale="locale"  term-domain="gbfTargets"></ng>                                               
+                        </div>
                     </div>
                 </div>
         
                 <div v-if="document.aichiTargets">
-                    <div v-if="document.aichiTargets">
-                        <label>{{t("aichiTarget")}}</label>
-                        <ul class="km-value">
-                            <li v-for="term in document.aichiTargets">                       
-                                <km-term :value="term" :locale="locale"></km-term> 
-                            </li>
-                        </ul>
+                    <div v-if="document.aichiTargets" >
+                        <label>{{t("aichiTarget")}}</label>                        
+                        <div class="km-value">
+                            <ng v-vue-ng:view-terms-hierarchy  v-model:ng-model="document.aichiTargets" locale="locale"  term-domain="aichiTargets"></ng>
+                         </div>                        
                     </div>
                 </div>
-            </div>
+            </section>
 
-            <div v-if="document.absKeyAreas">
+            <section v-if="document.absKeyAreas">
                 <legend>{{t("absch")}}</legend>        
-                <label>{{t("keyAreas")}}</label>
-                <ul class="km-value">
-                    <li v-for="term in document.absKeyAreas">
-                        <km-term :value="term" :locale="locale"></km-term> 
-                    </li>
-                </ul>
-            </div>
+                <label>{{t("keyAreas")}}</label>                
+                <div class="km-value">                           
+                    <ng v-vue-ng:view-terms-hierarchy  v-model:ng-model="document.absKeyAreas" locale="locale"  term-domain="absKeyAreas"></ng>                      
+                </div>
+            </section>
 
-            <div v-if="document.bchSubjects">
+            <section v-if="document.bchSubjects">
                 <legend>{{t("cartagenaProtocol")}}</legend>        
                 <div v-if="document.bchSubjects">
-                    <label>{{t("thematicAreass")}}</label>
-                    <ul class="km-value">
-                        <li v-for="term in document.bchSubjects">
-                            <km-term :value="term" :locale="locale"></km-term> 
-                        </li>
-                    </ul>
+                    <label>{{t("thematicAreass")}}</label>                    
+                    <div class="km-value">                           
+                        <ng v-vue-ng:view-terms-hierarchy  v-model:ng-model="document.bchSubjects" locale="locale"  term-domain="bchSubjects"></ng>                                 
+                    </div>
                 </div>
-            </div>
-
+            </section>
             
-            <view-relevant-information :relevantInfos="document.relevantInformation" 
-                                       :relevantDocs="document.relevantDocuments"  :locale="locale">
-            </view-relevant-information> 
+            <section v-if="document.relevantInformation || document.relevantDocuments">
+                <legend>{{ t("additionalInformation") }}</legend>
+                <view-relevant-information :relevant-information="document.relevantInformation" :relevant-documents="document.relevantDocuments" :locale="locale"> 
+                </view-relevant-information> 
+            </section>           
 
+            <div> 
+                <!-- TODO: test -->
+                <ng v-vue-ng:view-referenced-records  v-model:ng-model="document.header.identifier" ></ng>  
+            </div>         
         </div>  
-        </div>
+
+        <!-- TODO: add footer  -->
+        <!-- <ng v-vue-ng:document-metadata  :document="document"></ng>  -->
     </div>
 </template>
 <script setup>
@@ -197,18 +198,20 @@
     import '~/components/scbd-angularjs-controls/form-control-directives/km-value-ml.js'  
     import '~/components/scbd-angularjs-controls/form-control-directives/km-link-list.js'
     import '~/views/forms/view/directives/view-record-reference.directive.js'
+    import '~/views/forms/view/directives/view-reference-records.directive.js'        
     import viewRelevantInformation from '~/views/forms/view/directives/view-relevant-information.vue';
     import kmTerm from '~/components/km/KmTerm.vue';
     import messages from '~/app-text/views/reports/chm/capacity-building-resource.json';
     import { useI18n } from 'vue-i18n';
     import { lstring } from '~/components/kb/filters';
+    import '~/views/forms/directives/view-terms-hierarchy.js'    
    
 
     const { t } = useI18n({ messages });
 
     const props = defineProps({
-        documentInfo: { type: Object, required: true },
-        locale      : { type:String}
+        documentInfo: { type:Object, required: true},
+        locale      : { type:String, required: true}
     })
     const document = computed(()=>props.documentInfo?.body);
     const orderedRegions = computed(()=>{
