39805a0ca8881d2038fde41f60dc677c5538c5e3
Yuan Li <liyuan.montreal@gmail.com>
2024-05-16 11:03:15 -0400

    change relevant information section; add section condition; add Todos and referenced-record

--- app/views/forms/view/chm/national-target.vue
+++ app/views/forms/view/chm/national-target.vue
@@ -1,87 +1,103 @@
 <template> 
      <div id="Record" class="record ">
         <div class="record-body  bg-white" v-if="document">
+            <!--TODO: add compare-val for fields  -->
 
-            <section>
-                <div  v-if="document.government || document.title || document.description">                          
-                    <legend>{{ t("generalInformation") }} </legend>               
-                    <div v-if="document.government">
-                        <label>{{ t("country") }} </label> 
-                        <div class="km-value">
-                            <km-term :value="document.government" :locale="locale"></km-term>   
-                        </div>
-                    </div>  
+            <!-- TODO: add publish date -->            
+            <!-- <ng v-vue-ng:document-date></ng> -->
 
-                    <div v-if="document.title">
-                        <label>{{ t("title") }} </label> 
-                        <div class="km-value">
-                             <span>{{lstring(document.title,locale)}}</span>
-					    <i v-if="document.acronym">( {{lstring(document.acronym,locale)}} )</i>
-                        </div>                             
-                    </div>                
-
-                    <div v-if="document.description">
-                        <label>{{ t("rationale") }} </label> 
-                        <ng v-vue-ng:km-value-ml  :value="document.description" :locales="locale" html></ng>  
+            <section v-if="document.government || document.title || document.description">                                      
+                <legend>{{ t("generalInformation") }} </legend>               
+                <div v-if="document.government">
+                    <label>{{ t("country") }} </label> 
+                    <div class="km-value">
+                        <km-term :value="document.government" :locale="locale"></km-term>   
                     </div>
-                </div>
+                </div>  
+
+                <div v-if="document.title">
+                    <label>{{ t("title") }} </label> 
+                    <div class="km-value">
+                            <span>{{lstring(document.title,locale)}}</span>
+                    <i v-if="document.acronym">( {{lstring(document.acronym,locale)}} )</i>
+                    </div>                             
+                </div>                
+
+                <div v-if="document.description">
+                    <label>{{ t("rationale") }} </label> 
+                    <ng v-vue-ng:km-value-ml  :value="document.description" :locales="locale" html></ng>  
+                </div>              
             </section>
             
-            <section>
-                <div v-if="document.jurisdiction || document.jurisdictionInfo"> 
-                    <legend>{{ t("level") }} </legend> 
-                
-                    <div v-if="document.jurisdiction">
-                        <label>{{ t("jurisdiction") }} </label> 
-                        <div class="km-value">
-                            <km-term :value="document.jurisdiction" :locale="locale" ></km-term>
-                        </div>
-                    </div>  
+            <section v-if="document.jurisdiction || document.jurisdictionInfo">               
+                <legend>{{ t("level") }} </legend> 
+            
+                <div v-if="document.jurisdiction">
+                    <label>{{ t("jurisdiction") }} </label> 
+                    <div class="km-value">
+                        <km-term :value="document.jurisdiction" :locale="locale" ></km-term>
+                    </div>
+                </div>  
 
-                    <div v-if="document.jurisdictionInfo">
-                        <label>{{ t("details") }} </label> 
-                        <ng v-vue-ng:km-value-ml  :value="document.jurisdictionInfo" :locales="locale" html></ng> 
-                    </div> 
+                <div v-if="document.jurisdictionInfo">
+                    <label>{{ t("details") }} </label> 
+                    <ng v-vue-ng:km-value-ml  :value="document.jurisdictionInfo" :locales="locale" html></ng> 
                 </div>
             </section>  
 
-            <section>
-                <div  v-if="document.aichiTargets || document.otherAichiTargets || document.noOtherAichiTargetsDescription" >              
-                    <legend>{{ t("relevance") }} </legend>
-                
-                    <div v-if="document.aichiTargets" >  
-                        <label v-if="document.isAichiTarget" >{{ t("aichiTarget") }}</label>
-                        <label v-if="!document.isAichiTarget" >{{ t("aichiTargetComponents") }} </label>
+            <section v-if="document.aichiTargets || document.otherAichiTargets || document.noOtherAichiTargetsDescription" >             
+                <legend>{{ t("relevance") }} </legend>
+            
+                <div v-if="document.aichiTargets" >  
+                    <label v-if="document.isAichiTarget" >{{ t("aichiTarget") }}</label>
+                    <label v-if="!document.isAichiTarget" >{{ t("aichiTargetComponents") }} </label>
 
-                        <div class="km-value">                    
-                            <div  v-for="target in document.aichiTargets" >                    
-                                <km-term :value="target" :locale="locale" ></km-term>                
-                            </div>
+                    <div class="km-value">                    
+                        <div  v-for="target in document.aichiTargets" >                    
+                            <km-term :value="target" :locale="locale" ></km-term>                
                         </div>
-                    </div> 
-        
-                    <div v-if="document.otherAichiTargets">     
-                        <label>{{ t("SubAichiTargets") }} </label>      
-                        <label v-if="!document.isAichiTarget">{{ t("OrTargetComponents") }} </label>  
-                        <div class="km-value">
-                            <div v-for="target in document.otherAichiTargets" >                    
-                                <km-term :value="target" :locale="locale" ></km-term>                
-                            </div>
-                        </div>
-                    </div>  
-                    
-                    
-                    <div v-if="document.noOtherAichiTargetsDescription">                    
-                        <label>{{ t("explanation") }} </label>    
-                        <ng v-vue-ng:km-value-ml  :value="document.noOtherAichiTargetsDescription" :locales="locale" html></ng>  
                     </div>
-                </div>
-            </section> 
-              
-            <view-relevant-information :relevantInfos="document.relevantInformation" :relevantDocs="document.relevantDocuments"
-                                       :documentText="document.documentText" :documentLinks="document.documentLinks"  :locale="locale">
+                </div> 
+    
+                <div v-if="document.otherAichiTargets">     
+                    <label>{{ t("subAichiTargets") }} </label>      
+                    <label v-if="!document.isAichiTarget">{{ t("orTargetComponents") }} </label>  
+                    <div class="km-value">
+                        <div v-for="target in document.otherAichiTargets" >                    
+                            <km-term :value="target" :locale="locale" ></km-term>                
+                        </div>
+                    </div>
+                </div>  
+                
+                <div v-if="document.noOtherAichiTargetsDescription">                    
+                    <label>{{ t("explanation") }} </label>    
+                    <ng v-vue-ng:km-value-ml  :value="document.noOtherAichiTargetsDescription" :locales="locale" html></ng>  
+                </div>            
+            </section>               
+            
+            <view-relevant-information :information="document.documentText" :documents="document.documentLinks":locale="locale">
+                <template v-slot:legend>
+                    <legend>{{ t("relevantDocumentAndInformation") }}</legend>
+                </template>  
+                <template v-slot:information>
+                    <label>{{ t("relevantInfo") }} </label>   
+                </template>
+                <template v-slot:document>
+                    <label>{{ t("relevantWebsites") }} </label> 
+                </template>
             </view-relevant-information> 
+
+            <view-relevant-information :information="document.relevantInformation" :documents="document.relevantDocuments":locale="locale"> 
+           </view-relevant-information>            
+
+            <div> 
+                <!-- TODO: test -->
+                <ng v-vue-ng:view-referenced-records  v-model:ng-model="document.header.identifier" ></ng>  
+            </div>         
         </div>  
+        <!-- TODO: add footer  -->
+        <!-- <ng v-vue-ng:document-metadata  :document="document"></ng>  -->   
+           
     </div>
 </template>
 
@@ -93,6 +109,7 @@
     import kmTerm from '~/components/km/KmTerm.vue';
     import messages from '~/app-text/views/reports/chm/national-target.json'; 
     import { useI18n } from 'vue-i18n';
+    import { lstring } from '~/services/filters/lstring.js'; 
 
     const { t } = useI18n({ messages });
 
