f8c23767fb96d5a93b962bd2fe819c8916e7abac
Yuan Li <liyuan.montreal@gmail.com>
2024-05-16 16:23:37 -0400

    import view-reference-records

--- app/views/forms/view/chm/undb-actor.vue
+++ app/views/forms/view/chm/undb-actor.vue
@@ -84,6 +84,7 @@
     import '~/components/scbd-angularjs-controls/form-control-directives/km-value-ml.js'  
     import '~/components/scbd-angularjs-controls/form-control-directives/km-link-list.js'
     import '~/views/forms/view/directives/view-record-reference.directive.js'
+    import '~/views/forms/view/directives/view-reference-records.directive.js'   
     import kmTerm from '~/components/km/KmTerm.vue';
     import messages from '~/app-text/views/reports/chm/undb-actor.json';
     import { useI18n } from 'vue-i18n';  
