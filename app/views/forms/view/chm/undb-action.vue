267e75147663549d535c1702912a107b784c9be7
Yuan Li <liyuan.montreal@gmail.com>
2024-05-26 22:49:55 -0400

    fix footer link issue

--- app/views/forms/view/chm/undb-action.vue
+++ app/views/forms/view/chm/undb-action.vue
@@ -120,8 +120,7 @@
             </section>
 
             <div> 
-                <!-- TODO: test -->
-                <ng v-vue-ng:view-referenced-records  v-model:ng-model="document.header.identifier" ></ng>  
+                <ng v-vue-ng:view-record-reference  v-model:ng-model="document.header.identifier" ></ng>  
             </div>         
         </div> 
 
@@ -133,7 +132,7 @@
 <script setup>
     import { computed } from 'vue'; 
     import '~/components/scbd-angularjs-controls/form-control-directives/km-value-ml.js'
-    import '~/views/forms/view/directives/view-reference-records.directive.js'   
+    import '~/views/forms/view/directives/view-record-reference.directive.js'   
     import kmTerm from '~/components/km/KmTerm.vue';
     import messages from '~/app-text/views/reports/chm/undb-action.json';
     import { useI18n } from 'vue-i18n';
