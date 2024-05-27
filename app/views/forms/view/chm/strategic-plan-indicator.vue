a79d41cfe331c76d450cc6628ce87ee3844bd700
Yuan Li <liyuan.montreal@gmail.com>
2024-05-26 22:33:22 -0400

    fix footer link display issue

--- app/views/forms/view/chm/strategic-plan-indicator.vue
+++ app/views/forms/view/chm/strategic-plan-indicator.vue
@@ -53,8 +53,7 @@
             </div>     
 
             <div> 
-                <!-- TODO: test -->
-                <ng v-vue-ng:view-referenced-records  v-model:ng-model="document.header.identifier" ></ng>  
+                <ng v-vue-ng:view-record-reference  v-model:ng-model="document.header.identifier" ></ng>  
             </div>         
         </div>  
 
@@ -68,7 +67,7 @@
    import '~/components/scbd-angularjs-controls/form-control-directives/km-value-ml.js'  
    import '~/components/scbd-angularjs-controls/form-control-directives/km-value-bool.js'  
    import '~/components/scbd-angularjs-controls/form-control-directives/km-link-list.js' 
-   import '~/views/forms/view/directives/view-reference-records.directive.js'     
+   import '~/views/forms/view/directives/view-record-reference.directive.js'     
    import kmTerm from '~/components/km/KmTerm.vue';
    import messages from '~/app-text/views/reports/chm/strategic-plan-indicator.json';
    import { useI18n } from 'vue-i18n'; 
