c807abe3681636945f7d3103b13623945ae18c48
Yuan Li <liyuan.montreal@gmail.com>
2024-06-10 14:39:33 -0400

    fix code review issues( use question mark ; add translation; move filter, currencyString function inside component)

--- app/views/forms/view/chm/financial-report-2020.vue
+++ app/views/forms/view/chm/financial-report-2020.vue
@@ -11,12 +11,12 @@
            </view-financial-report>            
 
            <!-- section 4 begin -->
-           <section v-if="document.domesticExpendituresData">
+           <section v-if="document?.domesticExpendituresData">
                 <legend>{{t("role")}}</legend>
                 <div><label>{{t("hasYourCountry")}}</label></div>
-                <div v-if="document.domesticExpendituresData.domesticCollectiveAction">
+                <div v-if="document?.domesticExpendituresData?.domesticCollectiveAction">
                     <div  class="km-value km-pre"  style="list-style-type: none;" >                  
-                        <li v-for="term in filter(options.assessments,document.domesticExpendituresData.domesticCollectiveAction)">                     
+                        <li v-for="term in filter(options.assessments,document?.domesticExpendituresData?.domesticCollectiveAction)">                     
                             {{lstring(term.title,locale)}}
                         </li>
                     </div>
@@ -24,27 +24,27 @@
 
                 <div><label>{{t("additionalInformationOnAssessment")}}</label></div>
         
-                <div v-if="document.domesticExpendituresData.currency" >
+                <div v-if="document?.domesticExpendituresData?.currency" >
                     <label>{{t("currency")}}</label>
                     <span class="km-value">
-                       <km-term :value="document.domesticExpendituresData.currency" :locale="locale"></km-term>   
+                       <km-term :value="document?.domesticExpendituresData?.currency" :locale="locale"></km-term>   
                     </span>
                 </div>
-                <div v-if="document.domesticExpendituresData.multiplier" >
+                <div v-if="document?.domesticExpendituresData?.multiplier" >
                     <label>{{t("allValues")}}</label>
                     <div class="km-value">
-                        <span v-for="term in filter(options.multipliers,document.domesticExpendituresData.multiplier)">
+                        <span v-for="term in filter(options.multipliers,document?.domesticExpendituresData?.multiplier)">
                             {{lstring(term.title,locale)}} 
                         </span>
                     </div>                    
                 </div>       
                
-                <div v-if="document.domesticExpendituresData.measurementUnit" >
+                <div v-if="document?.domesticExpendituresData?.measurementUnit" >
                     <label>{{t("measurementUnit")}}</label>                                   
-                    <ng v-vue-ng:km-value-ml  :value="document.domesticExpendituresData.measurementUnit" :locales="locale" html km-pre></ng>
+                    <ng v-vue-ng:km-value-ml  :value="document?.domesticExpendituresData?.measurementUnit" :locales="locale" html km-pre></ng>
                 </div>
                 
-                <table v-if="document.domesticExpendituresData.contributions" class="table table-hover table-condensed">
+                <table v-if="document?.domesticExpendituresData?.contributions" class="table table-hover table-condensed">
                     <thead>
                         <tr>
                             <th>{{t("year")}}</th>
@@ -60,55 +60,55 @@
                         </tr>
                         <tr class="active">
                             <td><strong>{{t("average")}}</strong></td>
-                            <td>{{currencyString(typeAverageAmount(document.domesticExpendituresData.contributions,'amount'))}}</td>
-                            <td>{{confidenceAverage(document.domesticExpendituresData.contributions)}}</td>
+                            <td>{{currencyString(typeAverageAmount(document?.domesticExpendituresData?.contributions,'amount'))}}</td>
+                            <td>{{confidenceAverage(document?.domesticExpendituresData?.contributions)}}</td>
                         </tr>
                     </tbody>
                 </table>
         
-                <div v-if="document.domesticExpendituresData.domesticCollectiveActionMethodology || document.domesticExpendituresData.domesticCollectiveActionMethodologyComments">
+                <div v-if="document?.domesticExpendituresData?.domesticCollectiveActionMethodology || document?.domesticExpendituresData?.domesticCollectiveActionMethodologyComments">
                     <label>{{t("methodologicalInformation")}}</label>
                 </div>        
                
-                <div v-if="document.domesticExpendituresData.domesticCollectiveActionMethodology" >
+                <div v-if="document?.domesticExpendituresData?.domesticCollectiveActionMethodology" >
                     <label>{{t("methodology")}}</label>
-                    <div  v-if="!document.domesticExpendituresData.domesticCollectiveActionMethodologyOther" >                    
-                        <ng v-vue-ng:km-value-ml  :value="document.domesticExpendituresData.domesticCollectiveActionMethodology" :locales="locale" html km-pre></ng>              
+                    <div  v-if="!document?.domesticExpendituresData?.domesticCollectiveActionMethodologyOther" >                    
+                        <ng v-vue-ng:km-value-ml  :value="document?.domesticExpendituresData?.domesticCollectiveActionMethodology" :locales="locale" html km-pre></ng>              
                     </div>
-                    <div  v-if="document.domesticExpendituresData.domesticCollectiveActionMethodologyOther" >                       
-                        <ng v-vue-ng:km-value-ml  :value="document.domesticExpendituresData.domesticCollectiveActionMethodologyOther" :locales="locale" html km-pre></ng>            
+                    <div  v-if="document?.domesticExpendituresData?.domesticCollectiveActionMethodologyOther" >                       
+                        <ng v-vue-ng:km-value-ml  :value="document?.domesticExpendituresData?.domesticCollectiveActionMethodologyOther" :locales="locale" html km-pre></ng>            
                     </div>
                 </div>        
               
-                <div v-if="document.domesticExpendituresData.domesticCollectiveActionMethodologyComments" >
+                <div v-if="document?.domesticExpendituresData?.domesticCollectiveActionMethodologyComments" >
                     <label>{{t("otherMethodological")}}</label> 
-                    <ng v-vue-ng:km-value-ml  :value="document.domesticExpendituresData.domesticCollectiveActionMethodologyComments" :locales="locale" html km-pre></ng>  
+                    <ng v-vue-ng:km-value-ml  :value="document?.domesticExpendituresData?.domesticCollectiveActionMethodologyComments" :locales="locale" html km-pre></ng>  
                 </div>
-            </section> 
+            </section>  
             <!-- section 4 end -->
             
             <!-- section 5 begin -->
-            <section v-if="document.nationalPlansData">
+            <section v-if="document?.nationalPlansData">
                 <legend>{{t("reportingProgress")}}</legend>
                 <label>{{t("achievedResourceMobilization")}}</label>
-                <div v-if="document.nationalPlansData.currency" >
+                <div v-if="document?.nationalPlansData?.currency" >
                     <label>{{t("currency")}}</label>
                     <span class="km-value"> 
-                        <km-term :value="document.nationalPlansData.currency" :locale="locale"></km-term>   
+                        <km-term :value="document?.nationalPlansData?.currency" :locale="locale"></km-term>   
                     </span>
                 </div>
         
-                <div v-if="document.nationalPlansData.multiplier" >
+                <div v-if="document?.nationalPlansData?.multiplier" >
                     <label>{{t("allValues")}}</label>
                     <div class="km-value">
-                        <span v-for="term in filter(options.multipliers,document.nationalPlansData.multiplier)">
+                        <span v-for="term in filter(options.multipliers,document?.nationalPlansData?.multiplier)">
                             {{lstring(term.title,locale)}} 
                         </span>
                     </div>                  
                 </div>
         
-                <!-- TODO:test -->
-                <table v-if="baselineDocument.fundingNeedsData && baselineDocument.fundingNeedsData.annualEstimates" class="table table-hover table-condensed">
+               
+                <table v-if="baselineDocument?.fundingNeedsData?.annualEstimates" class="table table-hover table-condensed">
                     <thead>
                         <tr>
                             <th></th>
@@ -166,8 +166,8 @@
                         <tr class="active">
                             <td><strong>{{t("gapReduced")}}</strong></td>
                             <td v-for="(estimate,index) in orderedAnnualEstimates" class="col-sm-1 text-center">
-                                <span class="km-value km-pre" v-if="document.nationalPlansData.gapReductions[index].hasReduced">
-                                    {{document.nationalPlansData.gapReductions[index].hasReduced}}
+                                <span class="km-value km-pre" v-if="document?.nationalPlansData?.gapReductions[index].hasReduced">
+                                    {{document?.nationalPlansData?.gapReductions[index].hasReduced}}
                                 </span>
                             </td>
                         </tr>
@@ -175,35 +175,35 @@
                             <td><strong>{{t("gapReducedOverall")}}</strong></td>
                             <td  colspan="{{baselineDocument.fundingNeedsData.annualEstimates.length || 1}}" class="text-center">
                                 <span class="km-pre">
-                                    {{lstring(document.nationalPlansData.hasReduceGapOverall|term,locales)}}  
+                                    {{lstring(document?.nationalPlansData?.hasReduceGapOverall|term,locales)}}  
                                 </span>
                             </td>
                         </tr>								
                     </tbody>
                 </table>
 
-                <div v-if="document.nationalPlansData.additionalComments">
+                <div v-if="document?.nationalPlansData?.additionalComments">
                     <label>{{t("additionalMethodologicalObservations")}}</label> 
-                    <ng v-vue-ng:km-value-ml  :value="document.nationalPlansData.additionalComments" :locales="locale" html km-pre></ng> 
+                    <ng v-vue-ng:km-value-ml  :value="document?.nationalPlansData?.additionalComments" :locales="locale" html km-pre></ng> 
                 </div>            
         
-                <div v-if="document.nationalPlansData.hasDomesticPrivateSectorMeasures">
+                <div v-if="document?.nationalPlansData?.hasDomesticPrivateSectorMeasures">
                     <div><label>{{t("measuresPrivateSectorDomesticSupport")}}</label></div>
                     <div class="km-value" style="list-style-type: none;">
-                        <li v-for="term in filter(options.measures,document.nationalPlansData.hasDomesticPrivateSectorMeasures)">
+                        <li v-for="term in filter(options.measures,document?.nationalPlansData?.hasDomesticPrivateSectorMeasures)">
                             {{lstring(term.title,locale)}}
                         </li>
                     </div>
-                    <div v-if="document.nationalPlansData.hasDomesticPrivateSectorMeasuresComments">
+                    <div v-if="document?.nationalPlansData?.hasDomesticPrivateSectorMeasuresComments">
                         <label>{{t("provideAdditionalInformation")}}</label>  
-                        <ng v-vue-ng:km-value-ml  :value="document.nationalPlansData.hasDomesticPrivateSectorMeasuresComments" :locales="locale" html km-pre></ng>                       
+                        <ng v-vue-ng:km-value-ml  :value="document?.nationalPlansData?.hasDomesticPrivateSectorMeasuresComments" :locales="locale" html km-pre></ng>                       
                     </div>
                 </div>   
             </section>
             <!-- section 5 end -->
    
             <!-- section relevant information begin -->  
-            <section v-if="document.relevantInformation || document.relevantDocuments">
+            <section v-if="document?.relevantInformation || document?.relevantDocuments">
                 <legend>{{ t("additionalInformation") }}</legend>
                 <view-relevant-information :relevant-information="document.relevantInformation" :relevant-documents="document.relevantDocuments" :locale="locale"> 
                 </view-relevant-information> 
@@ -230,7 +230,7 @@
    import messages from '~/app-text/views/reports/chm/financial-report-2020.json'; 
    import { useI18n } from 'vue-i18n';
    import { lstring } from '~/services/filters/lstring.js'; 
-   import { currencyString, filter }  from '~/components/kb/filters.js';
+//    import { currencyString, filter }  from '~/components/kb/filters.js';
    import KmDocumentApi from "~/api/km-document";
    import { useAuth } from "@scbd/angular-vue/src/index.js";
    const auth = useAuth();
@@ -245,25 +245,25 @@
    const document = computed(()=>props.documentInfo?.body);
 
     const orderedContributions = computed(()=>{       
-        if (!(document.value.domesticExpendituresData && document.value.domesticExpendituresData.contributions)) return [];   
+        if (!(document?.value?.domesticExpendituresData?.contributions)) return [];   
         //remove {} from array
-        var newArray = document.value.domesticExpendituresData.contributions.filter(value => Object.keys(value).length !== 0);    
+        var newArray = document?.value?.domesticExpendituresData?.contributions.filter(value => Object.keys(value).length !== 0);    
         return  _.orderBy(newArray , 'year');
     });
 
     const orderedAnnualEstimates  = computed(()=>{
-        if (!(baselineDocument.value.fundingNeedsData && baselineDocument.value.fundingNeedsData.annualEstimates)) return [];     
-        return  _.orderBy(baselineDocument.value.fundingNeedsData.annualEstimates, 'year');
+        if (!(baselineDocument?.value?.fundingNeedsData?.annualEstimates)) return [];     
+        return  _.orderBy(baselineDocument?.value?.fundingNeedsData?.annualEstimates, 'year');
     });
 
     const orderedDomesticSources = computed(()=>{
-        if (!(document.value.nationalPlansData && document.value.nationalPlansData.domesticSources)) return [];     
-        return  _.orderBy(document.value.nationalPlansData.domesticSources, 'name');
+        if (!(document?.value?.nationalPlansData?.domesticSources)) return [];     
+        return  _.orderBy(document?.value?.nationalPlansData?.domesticSources, 'name');
     });
 
     const orderedInternationalSources = computed(()=>{
-        if (! (document.value.nationalPlansData && document.value.nationalPlansData.internationalSources)) return [];     
-        return  _.orderBy( document.value.nationalPlansData.internationalSources, 'name');
+        if (! (document?.value?.nationalPlansData?.internationalSources)) return [];     
+        return  _.orderBy( document?.value?.nationalPlansData?.internationalSources, 'name');
     });
 
 
@@ -314,9 +314,9 @@
 
     const confidenceAverage = function (resources) {
         var values = _.compact(_.map(resources, function (resource) {
-            if (resource && resource.confidenceLevel && resource.confidenceLevel.identifier == "D8BC6348-D1F9-4DA4-A8C0-7AE149939ABE") return 3; //high
-            if (resource && resource.confidenceLevel && resource.confidenceLevel.identifier == "42526EE6-68F3-4E8A-BC2B-3BE60DA2EB32") return 2; //medium
-            if (resource && resource.confidenceLevel && resource.confidenceLevel.identifier == "6FBEDE59-88DB-45FB-AACB-13C68406BD67") return 1; //low
+            if (resource?.confidenceLevel?.identifier == "D8BC6348-D1F9-4DA4-A8C0-7AE149939ABE") return 3; //high
+            if (resource?.confidenceLevel?.identifier == "42526EE6-68F3-4E8A-BC2B-3BE60DA2EB32") return 2; //medium
+            if (resource?.confidenceLevel?.identifier == "6FBEDE59-88DB-45FB-AACB-13C68406BD67") return 1; //low
             return 0;
         }));
 
@@ -325,21 +325,20 @@
             value = Math.round(_.reduce(values, function(memo, value) { return memo + value; }) / values.length);
         }
 
-        if ( value == 3) return "High";
-        if ( value == 2) return "Medium";
-        if ( value == 1) return "Low";
-
-        return "No value selected";
+        if ( value == 3) return t("high");
+        if ( value == 2) return t("medium");
+        if ( value == 1) return t("low");
+        return t("noValueSelected"); 
     };
 
     const getFundingGapYear = function(year){   
         if(!year) return 0;
 
-        if(document.value && document.value.fundingNeedsData && document.value.fundingNeedsData.annualEstimates){
+        if(document?.value?.fundingNeedsData?.annualEstimates){
             const estimate = document.value.fundingNeedsData.annualEstimates.find((item) => {
                 item.year == year
             });  
-            if(estimate && estimate.fundingGapAmount)
+            if(estimate?.fundingGapAmount)
                 return estimate.fundingGapAmount;  
         }
         return 0;
@@ -352,7 +351,7 @@
     const getNationalPlansSourcesTotal = function(member, year){
         if(!year || !member) return 0;
 
-        if(document.value && document.value.nationalPlansData && document.value.nationalPlansData[member]){
+        if(document?.value?.nationalPlansData[member]){
 
             var prop = "amount"+year;
             var items;      
@@ -381,12 +380,12 @@
      
     const getBaselineFundingGapYear = function(year){
         if(!year) return 0;
-        if(baselineDocument.value && baselineDocument.value.fundingNeedsData && baselineDocument.value.fundingNeedsData.annualEstimates){
+        if(baselineDocument?.value?.fundingNeedsData?.annualEstimates){
 
             var estimates = baselineDocument.value.fundingNeedsData.annualEstimates;
             var estimate = _.findWhere(estimates, {year:year});
 
-            if(estimate && estimate.fundingGapAmount)
+            if(estimate?.fundingGapAmount)
                 return estimate.fundingGapAmount;
         }
         return 0;
@@ -394,7 +393,7 @@
     
 	const annualEstimatesHasYear = function (year) {
         if(!year) return false;
-        if(baselineDocument.value && baselineDocument.value.fundingNeedsData && baselineDocument.value.fundingNeedsData.annualEstimates){
+        if(baselineDocument?.value?.fundingNeedsData?.annualEstimates){
             var estimates = baselineDocument.value.fundingNeedsData.annualEstimates;
             var estimate = _.findWhere(estimates, {year:year});
             if(estimate)
@@ -458,5 +457,19 @@
     onMounted(() => {
        loadBaselineDocuments();
    })
+
+const currencyString=function (number) {
+  if (number) {
+    var formatter = new Intl.NumberFormat( { style: 'currency' });
+    return  formatter.format(number); 
+  }
+  else {
+    return "0";
+  }
+}
+
+const  filter=function(array, id) {     
+    return array.filter((option) => option.identifier===id );
+};  
 
 </script>
