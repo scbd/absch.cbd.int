e024ea6bf6808d8c300f6d115a415f782460429e
Yuan Li <liyuan.montreal@gmail.com>
2024-06-14 15:13:55 -0400

    fix capital letter in Json file;  remove or add km-value, set background color

--- app/views/forms/view/chm/national-report-6.vue
+++ app/views/forms/view/chm/national-report-6.vue
@@ -7,35 +7,37 @@
             <!-- <ng v-vue-ng:document-date></ng> --> 
             
             <!-- section I begin -->
-            <section v-if="document.government || document.notReportingOnNationalTargets || (document.targetPursued && document.nationalTargets.length >0)"   >
+            <section v-if="hasSectionIData">
                 <legend> {{ t("sectionI") }} </legend>
                
-                <div v-if="document.government">                       
+                <div v-if="document?.government">                       
                     <label> {{ t("country") }}</label>
                     <div class="km-value">                   
                         <km-term :value="document.government" :locale="locale"></km-term>                       
                     </div>                        
                 </div>
            
-                <div v-if="document.notReportingOnNationalTargets">
-                    <label><km-term :value="document.government" :locale="locale"></km-term> {{ t("hasAdopted") }} </label><br/>
-                    <label>{{ t("informationOnWhy") }} <km-term :value="document.government " :locale="locale"></km-term>{{ t("isChoosingTo") }}  </label>
-                    <ng v-vue-ng:km-value-ml  :value="document.notReportingOnNationalTargetsReason" :locales="locale" html></ng>   
+                <div v-if="document?.notReportingOnNationalTargets">
+                    <label v-if="document?.government"><km-term :value="document?.government" :locale="locale"></km-term> {{ t("hasAdopted") }} </label><br/>
+                    <label v-if="document?.government">{{ t("informationOnWhy") }} <km-term :value="document?.government " :locale="locale"></km-term>{{ t("isChoosingTo") }}  </label>
+                    <ng v-vue-ng:km-value-ml  :value="document?.notReportingOnNationalTargetsReason" :locales="locale" html></ng>   
                 </div>
-                <div v-if="document.targetPursued && document.nationalTargets?.length>0">
+                
+                <div v-if="document?.targetPursued && document?.nationalTargets?.length>0">
                     <label>{{ t("nationalTargets") }}</label>  
-                    <div v-for="item in document.nationalTargets ">  
-                        <div class="km-value" v-if="nationalTargets[item.identifier]"> 
+                    <div v-for="item in document?.nationalTargets ">  
+                        <div v-if="nationalTargets[item.identifier]"> 
                             <national-target :document-info="nationalTargets[item.identifier]" :locale="locale"></national-target>
                         </div>
+                        <br/>
                     </div>
                 </div>
             </section> 
             <!-- section I end -->
             <!-- section II begin -->
-            <section v-if="document.implementationMeasures">
+            <section v-if="document?.implementationMeasures">
                 <legend> {{ t("sectionII") }} </legend> 
-                <div v-for="implementationMeasure in document.implementationMeasures" >   
+                <div v-for="implementationMeasure in document?.implementationMeasures" >   
                     <div class="km-value bg-white">    
                         <div v-if="implementationMeasure.title">
                             <legend> {{lstring(implementationMeasure.title,locale)}}</legend>
@@ -112,24 +114,24 @@
             </section> 
             <!-- section II end -->
             <!-- section III begin -->
-            <section v-if="document.progressAssessments"> 
+            <section v-if="document?.progressAssessments"> 
                 <legend> {{ t("sectionIII") }} </legend>               
                 <div v-for="progressAssessment in document.progressAssessments"   > 
-                    <div class="km-value bg-white">
+                    <div >
                             <!-- Progress Assessment for -->                        
-                            <span v-if="progressAssessment?.aichiTarget?.length>0"> 
+                            <span v-if="progressAssessment.aichiTarget?.length>0"> 
                                 <!-- AICHI target  --> 
                                 <km-term :value="progressAssessment.aichiTarget" :locale="locale"></km-term>                        
                             </span>
                         
-                        <div v-if="progressAssessment?.nationalTarget?.length>0">
+                        <div v-if="progressAssessment.nationalTarget?.length>0">
                             <div v-if="nationalTargets[progressAssessment.nationalTarget.identifier]">
                                 <!-- national target  -->                                                      
                                 <ng v-vue-ng:km-value-ml  :value="nationalTargets[progressAssessment.nationalTarget.identifier]?.body?.title" :locales="locale" html></ng>
                             </div>
                         </div>        
                       
-                        <div v-if="progressAssessment?.assessment?.identifier" class="km-value">
+                        <div v-if="progressAssessment.assessment?.identifier" >
                             <national-assessment :document-info="progressAssessments[progressAssessment.assessment.identifier] " :locale="locale"></national-assessment>
                         </div>                        
                     </div>
@@ -140,9 +142,9 @@
             </section>  
             <!-- section III end -->
             <!-- section IV begin -->
-            <section  v-if="document.nationalContribution">            
+            <section  v-if="document?.nationalContribution">            
                 <legend> {{ t("sectionIV") }} </legend>                    
-                <div v-for="aichiTarget in document.nationalContribution" > 
+                <div v-for="aichiTarget in document?.nationalContribution" > 
                     <div v-if="aichiTarget.identifier" class="km-value bg-white" >                                                
                         <km-term :value="aichiTarget" :locale="locale"></km-term> 
                                             
@@ -193,7 +195,7 @@
             <!-- section V begin -->
             <section v-if="document.gspcNationalContribution">
                 <legend>{{t("sectionV")}}</legend>
-                <div>                               
+                <div class="km-value bg-white">                               
                     <div v-if="document?.gspcNationalContribution?.hasGSPCTargets">                                          
                         <div class="km-value">
                             <strong><km-term :value="document.government" :locale="locale"></km-term>{{t("hasNationalTargets")}}</strong>
@@ -222,7 +224,7 @@
                     </div>                              
                 </div>
                 
-                <div v-for="gspcTarget in document.gspcNationalContribution" >       
+                <div v-for="gspcTarget in document?.gspcNationalContribution" >       
                     <div v-if="gspcTarget.identifier">                                           
                         <div class="km-value">
                             <km-term :value="gspcTarget" :locale="locale"></km-term> 
@@ -251,12 +253,12 @@
             <!-- section V end -->
             
             <!-- section VI begin -->
-            <section>
-                <legend>{{t("sectionVI")}}</legend>              
-                <div v-if="document.iplcContribution">                  
+            <section >
+                <legend>{{t("sectionVI")}}</legend> 
+                <div v-if="document?.iplcContribution" class="km-value bg-white">                  
                     <ng v-vue-ng:km-value-ml  :value="document.iplcContribution" :locales="locale" html></ng>
                 </div>              
-                <div v-else class="km-value">
+                <div v-else class="km-value bg-white">
                     {{t("noInformationAvailable")}}                  
                 </div>
             </section>
@@ -264,7 +266,7 @@
             
             
             <!-- section VII begin -->
-            <section v-if="document.biodiversityCountryProfile">  
+            <section v-if="document?.biodiversityCountryProfile">  
                 <legend>{{t("sectionVII")}}</legend>   
                 <div class="km-value bg-white">             
                     <div v-if="document?.biodiversityCountryProfile?.statusAndTrends">
@@ -300,7 +302,7 @@
             </section>  
             <!-- section VII end -->   
         
-            <section v-if="document.relevantInformation || document.relevantDocuments">
+            <section v-if="document?.relevantInformation || document?.relevantDocuments">
                 <legend>{{ t("additionalInformation") }}</legend>                
                 <view-relevant-information :relevant-information="document.relevantInformation" :relevant-documents="document.relevantDocuments" :locale="locale"> 
                 </view-relevant-information> 
@@ -316,31 +318,39 @@
     </div>
 </template>
 <script setup>
-    import { computed, onMounted,ref, resolveComponent} from 'vue'; 
+    import { computed, onMounted,ref } from 'vue'; 
     import '~/components/scbd-angularjs-controls/form-control-directives/km-value-ml.js'  
     import '~/components/scbd-angularjs-controls/form-control-directives/km-link-list.js'
     import '~/views/forms/view/directives/view-record-reference.directive.js'  
+    import '~/views/forms/directives/view-terms-hierarchy.js'  
     import viewRelevantInformation from '~/views/forms/view/directives/view-relevant-information.vue';
+    import nationalAssessment from '~/views/forms/view/chm/national-assessment.vue' 
+    import nationalTarget from '~/views/forms/view/chm/national-target.vue' 
+    import KmDocumentApi from "~/api/km-document";
     import kmTerm from '~/components/km/KmTerm.vue';
     import messages from '~/app-text/views/reports/chm/national-report-6.json';
     import { useI18n } from 'vue-i18n';    
-    import { lstring } from '~/components/kb/filters';
-    import '~/views/forms/directives/view-terms-hierarchy.js'   
-    import KmDocumentApi from "~/api/km-document";
-    import { useAuth } from "@scbd/angular-vue/src/index.js";
-    import nationalTarget from '~/views/forms/view/chm/national-target.vue' 
-    import nationalAssessment from '~/views/forms/view/chm/national-assessment.vue' 
+    import { lstring } from '~/components/kb/filters';  
+    import { useAuth } from "@scbd/angular-vue/src/index.js"; 
+  
+
     const auth = useAuth();
     const kmDocumentApi = new KmDocumentApi({tokenReader:()=>auth.token()});    
-    const { t } = useI18n({ messages });
+    const { t } = useI18n({ messages });        
+    const nationalTargets = ref([]); 
+    const progressAssessments = ref([]);
+
     const props = defineProps({
         documentInfo: { type:Object, required: true },
         locale      : { type:String, required: true },
     })
     const document = computed(()=>props.documentInfo?.body);
 
-    const nationalTargets = ref([]); 
-    const progressAssessments = ref([]);
+    const hasSectionIData = computed(()=>{
+        return document?.value?.government || document?.value?.notReportingOnNationalTargets || 
+               (document?.value?.targetPursued && document?.value?.nationalTargets.length >0)
+        }
+    );
 
     const loadNationalTargets = async function() {
         document.value.nationalTargets?.map(async (item) => {
