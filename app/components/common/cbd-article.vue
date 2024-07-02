6dc048281b4b14b55603da1d48dd3672bdcf9592
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-07-02 16:09:28 +0500

    update cbd-article component

--- app/components/common/cbd-article.vue
+++ app/components/common/cbd-article.vue
@@ -4,16 +4,12 @@
             <i class="fa fa-cog fa-spin fa-lg"></i> 
             {{ t("loading") }}...
         </div>
-        <div v-if="!loading">
-            <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@scbd/ckeditor5-build-inline-full@35.0.0/build/content-style.css">
-            <div v-if="!hideCoverImage && viewArticle?.coverImage?.url">
-                <cbd-article-cover-image :cover-image="viewArticle.coverImage" :cover-image-size="coverImageSize"></cbd-article-cover-image>
-            </div> 
+        <div v-if="!loading && viewArticle">
+            <cbd-view-article :article="viewArticle" :hide-cover-image="hideCoverImage" :cover-image-size="coverImageSize"></cbd-view-article>
             <cbd-add-new-view-article v-if="showEdit && hasEditRights" 
                 :tags="tags" :admin-tags="adminTags" :custom-tags="customTags" :id="(viewArticle||{})._id" :target="target"
                 class="btn btn-secondary float-end">
-            </cbd-add-new-view-article>
-            <div v-if="viewArticle" v-html="lstring(viewArticle.content, $locale)" class="ck-content"></div>          
+            </cbd-add-new-view-article>         
             <div v-if="!viewArticle" class="article-not-found-section">
                 <slot name="missingArticle">
                 </slot>
@@ -23,22 +19,18 @@
 </template>
 
 <script setup>
-    import { computed, onMounted, ref, nextTick, watch } from 'vue';
-    import { useI18n } from 'vue-i18n';
-    import messages from '../../app-text/components/kb.json';
-    import { lstring } from '../../components/kb/filters';
+    import { computed, onMounted, ref } from 'vue';
     import { useAuth } from '@scbd/angular-vue/src/index.js';
     import ArticlesApi from '../../components/kb/article-api';
     import  cbdAddNewViewArticle  from '../../components/common/cbd-add-new-view-article.vue';
-    import  cbdArticleCoverImage  from '../../components/common/cbd-article-cover-image.vue';
-    // import 'ck-editor-css'; // not sure
-
+    import  cbdViewArticle  from '../../components/common/cbd-view-article.vue';
+    import messages from '../../app-text/components/kb.json';
+    import { useI18n } from 'vue-i18n';
+    const { t } = useI18n({ messages });
     const auth = useAuth();
-    const { t, locale } = useI18n({ messages });
     const articlesApi = new ArticlesApi({tokenReader:()=>auth.token()});
     const loading = ref(false);
     const remoteArticle = ref(null);
-    const error = ref(null);
     
     const emit = defineEmits(['onArticleLoad']);
 
@@ -60,7 +52,6 @@
     const getArticle = async (query) => {
         try{
             loading.value = true;
-            error.value = undefined;
             const articleResult = await articlesApi.queryArticles(query);
            
             if(articleResult.length > 0){
@@ -73,54 +64,25 @@
                     }
                 remoteArticle.value = articleData;
                 emit('onArticleLoad', articleData);
-                setTimeout(() => {                        
-                        preProcessOEmbed();
-                    }, 1000);
             } else {
                   emit('onArticleLoad');
             }
         } 
         catch (err) {
             console.log(err);
-            error.value =  err;
         } 
         finally {
             loading.value = false;
         }
     }
- 
-    const preProcessOEmbed = async () => {
-           nextTick( async() => {
-
-                document.querySelectorAll( 'oembed[url]' ) //All <oembed> elements in the document that have a url attribute are selected
-                .forEach(async function(element) {
-                    var url = element.attributes.url.value;
-                    var params = {
-                        url : encodeURI(url)                        
-                    }
-
-                    if(/app\.tango\.us\/app\/workflow\/.*/.test(url)){
-                        params.height = '1500px'
-                    }
-
-                    const response = await useAPIFetch('/api/v2020/oembed', {method:'GET', query:params});   
-                    console.log("useAPIFetch response",response);
-                    //Inserting Embed HTML into the Document                
-                    var embedHtml = '<div class="ck-media__wrapper" style="width:100%">' + response.html +'</div>'
-                    element.insertAdjacentHTML("afterend", embedHtml);
-                });
-
-            })
-        }
-
-        watch(() => props.query, async (newQuery) => {
-            await getArticle(newQuery)  
-        });
-        
 
     onMounted( async ()=>{
-        if(!viewArticle.value)
+        if(!viewArticle.value && props.query)
             await getArticle(props.query);
     })
+
+    defineExpose({
+        getArticle
+    });
 
 </script>
--- app/views/portals/article-id.vue
+++ app/views/portals/article-id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <cbd-article :query="articleQuery" v-if="articleQuery" :hide-cover-image="true" :show-edit="true">
+    <cbd-article ref="refCbdArticle" :query="articleQuery" v-if="articleQuery" :hide-cover-image="true" :show-edit="true">
       <template #article-empty>&nbsp;</template>
     </cbd-article>
   </div>
@@ -9,8 +9,8 @@
 <script setup>
   import { mapObjectId, isObjectId } from '~/api/api-base.js';
   import cbdArticle from '../../components/common/cbd-article.vue';
-  import { computed } from 'vue';
-
+  import { computed, onMounted, ref, watch } from 'vue';
+  const refCbdArticle = ref(null);
   const props = defineProps({
         articleId: {
           type: String,
@@ -26,4 +26,21 @@
     return { ag: JSON.stringify(ag) };
   });
 
+  const fetchArticle = async () => {
+    if (refCbdArticle.value && articleQuery.value) {
+      try {
+        await refCbdArticle.value.getArticle(articleQuery.value);
+      } catch (error) {
+        console.error('Failed to fetch article:', error);
+      }
+    }
+  };
+
+  watch(() => props.articleId, () => {
+    fetchArticle();
+  });
+
+  onMounted(() => {
+    fetchArticle();
+  });
 </script>
