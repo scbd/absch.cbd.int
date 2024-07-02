1cbfccc8e281c1be51354682593741f27dbb56d4
Yuan Li <liyuan.montreal@gmail.com>
2024-07-02 09:58:31 -0400

    add marine ebsa edit page

--- app/routes/chm.js
+++ app/routes/chm.js
@@ -10,13 +10,17 @@ import { mergeTranslationKeys } from '../services/translation-merge';
 const routesLabels = mergeTranslationKeys(routesLabelsTranslations);
 const chmRouteUrls = {
   theChm,
- 
+  register_NR_edit                    : { component: ()=>asyncLogError(import('~/views/forms/edit/chm/edit-chm-national-report')) },
+  register_marineEbsa_edit                  : { component: ()=>asyncLogError(import('~/views/forms/edit/chm/edit-chm-marine-ebsa')) }
 };
 
 app.config(["$routeProvider", function ($routeProvider) {
   $routeProvider.
-  whenAsync('/',                                                  { ...mapView(chmRouteUrls.theChm),                   "label":routesLabels.theChm}).
- 
+  whenAsync('/',                    { ...mapView(chmRouteUrls.theChm),                 "label":routesLabels.theChm}).
+  whenAsync('/register/NR/new',   { ...mapView(angularViewWrapper),   "label":routesLabels.new,"param":"true","resolveController":true,"documentType":"NR","resolve":{ ...chmRouteUrls.register_NR_edit,   "securized":securize(null,true,true)}}).
+
+  whenAsync('/register/marineEbsa/new',   { ...mapView(angularViewWrapper),   "label":routesLabels.new,"param":"true","resolveController":true,"documentType":"EBSA","resolve":{ ...chmRouteUrls.register_marineEbsa_edit,   "securized":securize(null,true,true)}}).
+
   otherwise({
     templateUrl: commonRoutes.baseUrl + "views/shared/404.html",
     label: routesLabels.pageNotFound
