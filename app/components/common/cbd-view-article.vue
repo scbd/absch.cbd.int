2e378a7268a5f6b98e88d8e2a756dcb09d393dfe
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-07-02 15:55:19 +0500

    create cbd-view-article component

---
+++ app/components/common/cbd-view-article.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="border-0 mt-1">
+        <div v-if="loading" class="loading">
+            <i class="fa fa-cog fa-spin fa-lg"></i> 
+            {{ t("loading") }}...
+        </div>
+        <div v-if="!loading && viewArticle">
+            <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@scbd/ckeditor5-build-inline-full@35.0.0/build/content-style.css">
+            <div v-if="!hideCoverImage && viewArticle?.coverImage?.url">
+                <cbd-article-cover-image :cover-image="viewArticle.coverImage" :cover-image-size="coverImageSize"></cbd-article-cover-image>
+            </div> 
+            <div v-html="lstring(viewArticle.content, $locale)" class="ck-content"></div>          
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { computed, onMounted, ref, nextTick } from 'vue';
+    import { useI18n } from 'vue-i18n';
+    // import DOMPurify from 'dompurify';
+    import messages from '../../app-text/components/kb.json';
+    import { lstring } from '../../components/kb/filters';
+    import cbdArticleCoverImage from '../../components/common/cbd-article-cover-image.vue';
+
+    const { t, locale } = useI18n({ messages });
+    const loading = ref(false);
+
+    const props = defineProps({
+        hideCoverImage: { type: Boolean, required: false, default: false },
+        article: { type: Object, required: false, default: undefined },
+        coverImageSize: { type: String, required: false, default: '800x800' }
+    });
+
+    // const viewArticle = computed(() => DOMPurify.sanitize(props.article));
+    const viewArticle = computed(() => props.article);
+
+    const preProcessOEmbed = async () => {
+        nextTick(async () => {
+            document.querySelectorAll('oembed[url]').forEach(async (element) => {
+                const url = element.attributes.url.value;
+                const params = { url: encodeURI(url) };
+
+                if (/app\.tango\.us\/app\/workflow\/.*/.test(url)) {
+                    params.height = '1500px';
+                }
+
+                try {
+                    const response = await useAPIFetch('/api/v2020/oembed', { method: 'GET', query: params });
+                    console.log("useAPIFetch response", response);
+                    const embedHtml = `<div class="ck-media__wrapper" style="width:100%">${response.html}</div>`;
+                    element.insertAdjacentHTML("afterend", embedHtml);
+                } catch (error) {
+                    console.error("Failed to fetch oEmbed data:", error);
+                }
+            });
+        });
+    };
+
+    onMounted(() => {
+        if (viewArticle.value) {
+           setTimeout(() => {
+               preProcessOEmbed();
+           }, 1000);
+            loading.value = false;
+        }
+    });
+</script>
