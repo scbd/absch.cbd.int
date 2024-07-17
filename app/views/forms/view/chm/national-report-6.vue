127253c0664476bdade53a13eaf941e5bb13b468
Yuan Li <liyuan.montreal@gmail.com>
2024-07-17 17:11:10 -0400

    fix identifier can't find issue; add keyword to search page (need fix domain name issue)

--- app/views/forms/view/chm/national-report-6.vue
+++ app/views/forms/view/chm/national-report-6.vue
@@ -388,10 +388,11 @@
     const loadProgressAssessments = async function() {
         isLoadingAssessments.value = true;   
         await Promise.all(document.value.progressAssessments?.map(async (item) => {
-            const result = await kmDocumentApi.getDocument((item.assessment.identifier)); 
-            result.government=undefined;       
-                  
-            progressAssessments.value[item.assessment.identifier]={body:result};      
+            if (item?.assessment?.identifier){
+                const result = await kmDocumentApi.getDocument((item.assessment.identifier)); 
+                result.government=undefined;                           
+                progressAssessments.value[item.assessment.identifier]={body:result};   
+            }
         }));  
         isLoadingAssessments.value = false;   
     }
@@ -399,6 +400,12 @@
     onMounted(() => {
         loadNationalTargets();  
         loadProgressAssessments();    
+        console.log("nationalTarget",nationalTargets.value);
+        console.log("nationalTarget.length",nationalTargets.value.length);
+        console.log("nationalTarget[14]",nationalTargets.value[14]);
+        console.log("progressAssessments",progressAssessments.value);
+        console.log("progressAssessments.length",progressAssessments.value.length);
+        console.log("progressAssessments[14]",progressAssessments.value[14]);
    })
  
 </script>
