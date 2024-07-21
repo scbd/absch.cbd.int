de0641716f71cfeafd62c00b66f682991b3d5329
Yuan Li <liyuan.montreal@gmail.com>
2024-07-21 06:54:30 -0400

    add financial reporting analyzer code

--- app/routes/chm.js
+++ app/routes/chm.js
@@ -10,14 +10,17 @@ import { mergeTranslationKeys } from '../services/translation-merge';
 const routesLabels = mergeTranslationKeys(routesLabelsTranslations);
 const chmRouteUrls = {
   theChm,  
-  online_Reporting  : { component: ()=>asyncLogError(import('~/views/reports/chm/legacy/online-reporting')) },
+  online_Reporting     : { component: ()=>asyncLogError(import('~/views/reports/chm/legacy/online-reporting')) },
+  financial_Analyzer   : { component: ()=>asyncLogError(import('~/views/reports/chm/legacy/financial-reporting')) },
+
 };
 
 
 app.config(["$routeProvider", function ($routeProvider) {
   $routeProvider.
-    whenAsync('/',                         { ...mapView(chmRouteUrls.theChm),    "label":routesLabels.theChm}).
-    whenAsync('/search/reporting-map',     { ...mapView(angularViewWrapper),   "label":routesLabels.onlineReporting,"resolve":{ ...chmRouteUrls.online_Reporting,       },"param":"true","resolveController":true,"reloadOnSearch":false}).
+    whenAsync('/',                              { ...mapView(chmRouteUrls.theChm),    "label":routesLabels.theChm}).
+    whenAsync('/search/reporting-map',          { ...mapView(angularViewWrapper),   "label":routesLabels.onlineReporting,  "resolve":{ ...chmRouteUrls.online_Reporting,       },"param":"true","resolveController":true,"reloadOnSearch":false}).
+    whenAsync('/search/financial-analyzer',     { ...mapView(angularViewWrapper),   "label":routesLabels.financialAnalyzer,"resolve":{ ...chmRouteUrls.financial_Analyzer,     },"param":"true","resolveController":true,"reloadOnSearch":false}).
     
  
   otherwise({
