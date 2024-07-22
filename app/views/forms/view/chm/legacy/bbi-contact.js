89a4e7e61cbb8739700734031627ebcd4df2f532
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2024-07-22 15:10:59 -0400

    Chm- legacy views rebase (#1217)
    * add bbi-contact file
    
    * Legacy bbi-contact view
    
    * add change for bbi-contact ( add translation; format UI)
    
    * add bbi-opportunity file
    
    * add bbi-profile; change to  lowercase characters in json files for bbi-contact and bbi-opportunity
    
    * add bbi-request json file
    
    * add capacity-building-resource
    
    * fix issues in capacity-building-resource
    
    * add national indicator
    
    * add national-support-tool
    
    * add bbiRequest file
    
    ---------
    
    Co-authored-by: Blaise Fonseca <blaise.fonseca@un.org>

---
+++ app/views/forms/view/chm/legacy/bbi-contact.js
@@ -0,0 +1,125 @@
+import app from '~/app';
+import _ from 'lodash';
+import template from "text!./bbi-contact.html";
+import '~/components/scbd-angularjs-controls/main';
+import bbiContact from '~/app-text/views/reports/chm/bbi-contact.json';
+
+app.directive('viewBbiContact', ['$http', "$rootScope", "$filter", "$q", "$location", 'IStorage', '$route', '$timeout', 'locale','translationService',
+	function($http, $rootScope, $filter, $q, $location, storage, $route, $timeout, locale,translationService) {
+	return {
+		restrict: 'E',
+		template: template,
+		replace: true,
+		transclude: false,
+		scope: {
+			document: '=ngModel',
+			locale: '=',
+			loading: "=?",
+			user: "=?",
+			header: "=?"
+		},
+		link: function($scope) {
+			translationService.set('bbiContact', bbiContact);
+			if (typeof $scope.header === 'undefined') $scope.header = true;
+			//====================
+			//
+			//====================
+			var killWatchConOrg = $scope.$watch("document.contactOrganization", function() {
+				if (!$scope.document || ($scope.document && !$scope.document.contactOrganization)) return;
+
+				$scope.contactOrganization = angular.fromJson(angular.toJson($scope.document.contactOrganization));
+
+				if ($scope.contactOrganization)
+					$scope.loadReferences($scope.contactOrganization).then(function(data) {
+						Object.assign($scope.contactOrganization, data[0]);
+						delete(data[0].websites);
+						delete(data[0].phones);
+						delete(data[0].faxes);
+						delete(data[0].emails);
+						delete(data[0].header);
+						delete(data[0].libraries);
+						delete(data[0].name);
+						delete(data[0].acronym);
+						delete(data[0].profileLink);
+						delete(data[0].organizationTypes);
+						delete(data[0].relevantDocuments);
+						delete(data[0].absThematicAreas);
+						delete(data[0].aichiTargets);
+						delete(data[0].bchSubjects);
+						delete(data[0].operationalLanguages);
+						delete(data[0].thematicAreas);
+						delete(data[0].coordinates);
+						delete(data[0].establishmentGooglePlaceId);
+						delete(data[0].zip);
+						Object.assign($scope.document, data[0]);
+						killWatchConOrg();
+					});
+			});
+			//====================
+			//
+			//====================
+			$scope.isReview = function() {
+				return !!($location.url().indexOf('/view') > -1);
+			};
+			//====================
+			//
+			//====================
+			$scope.isAdmin = function() {
+				if ($scope.user)
+					return !!_.intersection($scope.user.roles, ["Administrator", "BBiAdministrator"]).length;
+			};
+			//====================
+			//
+			//====================
+			$scope.getLogo = function() {
+
+				if (!$scope.contactOrganization || !$scope.contactOrganization.relevantDocuments) return false;
+				return _.find($scope.contactOrganization.relevantDocuments, {
+					name: 'logo'
+				});
+			};
+			//====================
+			//
+			//====================
+			$scope.loadReferences = function(ref, index) {
+
+				if (Number.isInteger(index) && Array.isArray(ref)) ref = ref[index];
+				return storage.documents.get(ref.identifier, {
+						cache: true
+					})
+					.then(function(data) {
+						ref = [];
+						ref[0] = data.data;
+						ref[1] = index;
+						return ref;
+					})
+					.catch(function(error, code) {
+						if (error.status == 404) {
+
+							return storage.drafts.get(ref.identifier, {
+									cache: true
+								})
+								.then(function(data) {
+									ref = [];
+									ref[0] = data.data;
+									ref[1] = index;
+									return ref;
+								})
+								.catch(function(error) {
+									return {
+										error: error,
+										errorCode: code
+									};
+								});
+						} else {
+							return {
+								error: error,
+								errorCode: code
+							};
+						}
+					});
+			};
+
+		}
+	};
+}]);
