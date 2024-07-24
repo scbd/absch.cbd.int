cb87ed22bee3a33fdd4013da363b18c399dc6bb3
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-07-24 14:15:30 +0500

    update KB home page

--- app/components/kb/article-categories.vue
+++ app/components/kb/article-categories.vue
@@ -2,11 +2,11 @@
     <div>
         <div class="card shadow-sm mb-3" v-for="category in KbCategories">
             <div class="card-body">
-                <h4 class="pb-2"><a :href="`${tagUrl(category)}`">{{category.title}}</a></h4>
+                <h4 class="pb-2"><a :href="`${tagUrl(category)}`">{{lstring(category.title, locale)}}</a></h4>
                 <ul v-if="category.articles.length > 0" >
                     <li v-for="article in category.articles" class="mb-1">
-                        <a v-if="article.identifier" class="link-dark fs-6" :href="`${articleUrl(article, category.adminTags[0])}`">{{article.title}}</a>
-                        <a v-if="article.url" :href="article.url" class="link-dark fs-6" target="_blank">{{article.title}}</a>
+                        <a v-if="article.identifier" class="link-dark fs-6" :href="`${articleUrl(article, category.adminTags[0])}`"> {{lstring(article.title, locale)}}</a>
+                        <a v-if="article.url" :href="article.url" class="link-dark fs-6" target="_blank">{{lstring(article.urlTitle, locale)}}</a>
                     </li>
                 </ul>
                 <div v-if="category.articles.length == 0">
@@ -23,21 +23,50 @@
 </template>
 
 <script setup>
+    import { ref, onMounted } from "vue";
     import relevantArticles from './relevant-articles.vue';
     import { useI18n } from 'vue-i18n';
     import messages from '../../app-text/components/kb.json';
     import { useRealm } from '../../services/composables/realm.js';
-    import { loadKbCategories, getUrl } from '../../services/composables/articles.js';
-    import { ref, onMounted } from "vue";
-    const { t } = useI18n({ messages });
+    import { loadKbCategories, getUrl, getRealmArticleTag } from '../../services/composables/articles.js';
+    import { lstring } from "./filters";
+    import ArticlesApi from "./article-api";
+    import {  useAuth } from "@scbd/angular-vue/src/index.js";
+    const realmArticleTag = getRealmArticleTag();
+    const auth = useAuth();
+    const { t, locale } = useI18n({ messages });
     const realm = useRealm();
     const KbCategories = ref([]);
+    const realmTag = ref('');
+
+    const articlesApi = new ArticlesApi({tokenReader:()=>auth.token()});
 
     onMounted(async ()=>{
+        realmTag.value = realmArticleTag;
         const categories = await loadKbCategories();
-        KbCategories.value = categories.filter(tag => tag.adminTags[0] != "faq");
+        let articleCategories  = categories.filter(tag => tag.adminTags[0] != "faq");
+        const articles = await articlesApi.queryArticles({ q: { adminTags : { $all: [realmTag.value] } }}); // ToDo: need to verify the query.
+
+        const result = articleCategories.map(category => {
+            const newCategory = { ...category, articles: [] };
+            category.articles.forEach(catArticle => {
+                const matchingArticle = articles.find(article => article._id === catArticle.identifier);
+                    if (matchingArticle) {
+                        newCategory.articles.push({
+                            identifier: catArticle.identifier,
+                            title: matchingArticle.title
+                        });
+                    } 
+                    else {
+                        newCategory.articles.push(catArticle);
+                    }
+                });
+
+                return newCategory;
+        });
+        KbCategories.value = result;
     })
-    
+
     const tagUrl = function (category) {
             return getUrl(category.title, undefined, category.adminTags[0]);
     }
@@ -46,7 +75,7 @@
             if (article.url) {
                 return article.url
             } else {
-                return getUrl(article.title, article.identifier, tag);
+                return getUrl(lstring(article.title, locale), article.identifier, tag);
             }
     }
 </script>
