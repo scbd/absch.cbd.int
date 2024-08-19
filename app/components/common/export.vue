c0ec871e9a71f2cb124f7de84da362f6ecfcb124
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-08-19 16:41:11 +0500

    add custom fields option in export component

--- app/components/common/export.vue
+++ app/components/common/export.vue
@@ -23,7 +23,7 @@
                         </div>
                         
                         <div class="row">
-                            <div class="col-6">
+                            <div class="col-9">
                                 <div class="form-group color-black">
                                     {{t('downloadFormat')}}
                                     <span class="radio" style="display: initial;">
@@ -31,19 +31,47 @@
                                         <label class="radio-inline"><input type="radio" name="downloadFormatOption" value="xlsx" v-model="downloadFormat" /> {{t('xlsx')}}</label>                   
                                         <label class="radio-inline"><input type="radio" name="downloadFormatOption" value="csv"  v-model="downloadFormat" /> {{t('csv')}}</label>
                                     </span>
+                                </div> 
+                                <div v-if="!isGeneric">
+                                <div class="modal fade" ref="optionsModal" tabindex="-1" aria-labelledby="customizedFieldsModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered modal-lg">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="customizedFieldsModalLabel"> {{t('customFields')}} </h5>
+                                                <button type="button" data-bs-dismiss="modal" class="border-0 close" @click="closeModal()"
+                                                    aria-label="Close"><i class="bi bi-x-circle-fill icon-lg"></i></button>
+                                            </div>
+                                            <div class="modal-body custom-fields-modal-body">
+                                                <div class="d-flex align-items-center float-end mb-3">
+                                                    <button type="button" class="btn btn-primary" @click="selectAll" >{{t('selectAll')}}</button>
+                                                    <button type="button" class="btn btn-secondary ms-1" @click="clearAll" >{{t('clearAll')}}</button>
+                                                </div>
+                                                <div v-for="(value, key) in optionFields" :key="key" class="form-check">
+                                                    <input type="checkbox" :id="key" :value="key" v-model="selectedFields" class="form-check-input">
+                                                    <label :for="key" style="font-weight: normal !important;">{{ value }}</label>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="closeModal()" aria-label="Close"> {{t('cancel')}}</button>
+                                                <button type="button" class="btn btn-primary" @click="updateFields">{{t('apply')}}</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-6">
+                           
+                            <div class="col-3">
                                 <span class="float-end" style="padding-top: 20px;">
                                     {{t('recordsFound')}}: <strong>{{numFound}}</strong>
                                 </span>
                             </div>
                         </div>
 
-                        <div class="row">
+                        <div class="row" v-if="loading">
                             <div class="col-12">
-                                <div v-if="loading" class="alert alert-info">
-                                    <i class="fa fa-spin fa-spinner" v-if="loading" ></i> {{t('processing')}}
+                                <div class="alert alert-info">
+                                    <i class="fa fa-spin fa-spinner"></i> {{t('processing')}}
                                 </div>
                             </div>
                         </div>
@@ -70,8 +98,8 @@
                                                         
                                                         <td class="tableexport-string">{{row.rec_schema}}</td>
                                                         <td class="tableexport-string">
-                                                            <a v-if="row.rec_uniqueIdentifier" rel="noopener" target="_blank" :href="`database/${encodeURIComponent((row.rec_uniqueIdentifier).toUpperCase())}`">
-                                                                {{ (row.rec_uniqueIdentifier).toUpperCase() }}
+                                                            <a rel="noopener" target="_blank" :href="`database/row.rec_uniqueIdentifier||''`">
+                                                                {{capitalize(row.rec_uniqueIdentifier)}}
                                                             </a>
                                                         </td>
                                                         <td class="tableexport-string">{{row.rec_government}}</td>
@@ -80,7 +108,7 @@
                                                         <td class="tableexport-string">{{(row.rec_meta2||[]).join(', ')}}</td>
                                                         <td class="tableexport-string">{{(row.rec_meta3||[]).join(', ')}}</td>
                                                         <td class="tableexport-string">{{(row.rec_meta4||[]).join(', ')}}</td>  
-                                                        <td class="tableexport-string">{{row.rec_date|formatDate}}</td>                          
+                                                        <td class="tableexport-string">{{formatDate(row.rec_date, 'DD MMM YYYY')}}</td>                          
                                                     </tr>
                                                 </tbody>
                                             </table>
@@ -127,11 +155,16 @@
                             </div>
                         </div>
                     </div>
-                    <div class="modal-footer ">
-                        <button type="button" class="btn btn-secondary float-end" aria-label="Close" @click="closeDialog()" :disabled="loading">{{t('cancel')}}</button>
-                        <button type="button" class="btn btn-primary float-start" aria-label="Download" @click="exportRecords(downloadFormat)" :disabled="loading" id="downloadDataFile">
-                            {{t('download')}}
-                        </button>    
+                    <div class="modal-footer d-flex justify-content-between">
+                        <div class="float-start">
+                            <button v-if="!isGeneric" @click="openModal" class="btn btn-primary">{{t('customFields')}}</button>
+                        </div> 
+                        <div class="float-end">
+                            <button type="button" class="btn btn-secondary" aria-label="Close" @click="closeDialog()" :disabled="loading">{{t('cancel')}}</button>
+                            <button type="button" class="btn btn-primary ms-2" aria-label="Download" @click="exportRecords(downloadFormat)" :disabled="loading" id="downloadDataFile">
+                                {{t('download')}}
+                            </button>    
+                        </div>
                     </div>
                 </div>
             </div>
@@ -140,14 +173,33 @@
 </template>
 
 <script setup>
-    import { ref, shallowRef, onMounted, inject } from "vue";
+    import { ref, shallowRef, onMounted, inject, watch } from "vue";
     import { Modal } from "bootstrap";
-    import '../kb/filters';
+    import  { formatDate, capitalize } from '../kb/filters';
     import { useRealm } from '../../services/composables/realm.js';
     import { useI18n } from 'vue-i18n';
     import messages from '../../app-text/components/export.json';
     const { t } = useI18n({ messages });
     const realm = useRealm();
+    const exportModal = shallowRef(null);
+    const optionsModal = shallowRef(null);
+    const downloadDocs =ref([]);
+    const numFound = ref(0);
+    const loading = ref(false);
+    const isGeneric = ref(true);
+    const schemaFields = ref([]); // get updated and pass the updated fields to api call.
+    const optionFields = ref([]); // to show the list of fields in the dialog. { "uniqueId": "UID", "government": "Country",  ...
+    const selectedFields = ref([]); // v-modal values for selected optionsFields.  [ "government", "uniqueId" ....
+    const downloadFormat  = ref('xlsx');
+    const getDownloadRecords = inject('getDownloadRecords');
+
+    const props = defineProps({
+        fileName: {type: String},
+    })
+
+    let schema = undefined ;
+    let modal = null;
+    let customizeFieldsModal = null;
     let fields = [
         'rec_schema:schema_EN_s',
         'rec_uniqueIdentifier:uniqueIdentifier_s',
@@ -159,99 +211,118 @@
         'rec_meta4:meta4_EN_txt',
         'rec_date:updatedDate_dt',
     ];
-    const exportModal = shallowRef(null);
-    const downloadDocs =ref([]);
-    const numFound = ref(0);
-    const loading = ref(false);
-    const isGeneric = ref(true);
-    const schemaFields = ref([]);
-    let schema = undefined ;
-    let modal = null;
-    const downloadFormat  = ref('xlsx');
-    const getDownloadRecords = inject('getDownloadRecords');
 
-    onMounted( async ()=>{
-        modal = new Modal(exportModal.value)
+    onMounted(()=>{
+        modal = new Modal(exportModal.value);
     })
 
-    const props = defineProps({
-        fileName: {type: String},
-    })
+    const openModal =() =>  
+    { 
+        customizeFieldsModal = new Modal(optionsModal.value);
+        customizeFieldsModal.show();
+        selectAll(); // select all optionsFields
+    };
+    const closeModal =() => {  
+        customizeFieldsModal.hide();
+    };
+   
+    const selectAll =() => {
+        console.log('i am called select all')
+        selectedFields.value = Object.keys(optionFields.value);
+    };
+    const clearAll =() => {  
+        selectedFields.value = [];
+    };
+    const updateFields =() => { 
+        if(selectedFields.value && selectedFields.value.length>0) { 
+            let uiFields = {};
+            selectedFields.value.forEach((field) => {
+            if (optionFields.value[field] !== undefined)
+                uiFields[field] = optionFields.value[field];
+            });
+            schemaFields.value = uiFields; 
+            closeModal();
+        }
+    };
 
     const openDialog = async () => {
-                modal.show('static');
-                loading.value = true; 
-                try{
-                        const responseData = await getDownloadRecords({fields, listType:'initial', format:'json'});
-                        downloadDocs.value = responseData.docs
-                        numFound.value     = responseData.numFound;
-                        isGeneric.value    = responseData.isGeneric;
-                        schemaFields.value = responseData.schemaFields;
-                        schema             = responseData.schema;             
-                }
-                finally{
-                    loading.value = false;
-                }
+        modal.show('static');
+        loading.value = true;
+        try{
+            selectedFields.value = [];
+            const responseData = await getDownloadRecords({fields, listType:'initial', format:'json'});
+            downloadDocs.value = responseData.docs
+            numFound.value     = responseData.numFound;
+            isGeneric.value    = responseData.isGeneric;
+            schemaFields.value = optionFields.value = responseData.schemaFields ? responseData.schemaFields:[];
+            schema             = responseData.schema;
+        }
+        catch(e){
+                console.error(e)
+        }
+        finally{
+            loading.value = false;
+        }
     }
     const exportRecords = async () => {
-                let info = undefined;
-                
-                loading.value      = true; 
-                let fileName    = props.fileName||`${realm.uIdPrefix}-${schema?schema:'all'}-${new Date().getTime().toString(36)}.${downloadFormat.value}`
+        loading.value   = true; 
+        let fileName    = props.fileName||`${realm.uIdPrefix}-${schema}-${new Date().getTime().toString(36)}.${downloadFormat.value}`
+        try
+        {
+            if(isGeneric.value){
+                await import('tableexport');
+                const info        = await getDownloadRecords({fields, listType:'all', fileName, format : downloadFormat.value});
+                if(info){
+                    downloadDocs.value = info.docs;
+                    numFound.value     = info.numFound;
+                    setTimeout(()=>{
+                        $('#datatable').tableExport({
+                            formats: ["xlsx", "xls", "csv"],
+                            filename: fileName,
+                        });
+                        $(`.${downloadFormat.value}`).click(); 
 
-                try{
-                    if(isGeneric){
-                        await import('tableexport');
-                        info        = await getDownloadRecords({fields, listType:'all', fileName, format : downloadFormat.value});
-                        downloadDocs.value = info.docs;
-                        numFound.value     = info.numFound;
-                        setTimeout(()=>{
-                            $('#datatable').tableExport({
-                                formats: ["xlsx", "xls", "csv"],
-                                filename: fileName,
-                            });
-                            $(`.${downloadFormat.value}`).click(); 
+                        downloadDocs.value = downloadDocs.value.slice(0, 24);
+                    }, 500);
+                }
+            }
+            else{
+                await getDownloadRecords({fields:schemaFields.value, listType:'all', fileName, format : downloadFormat.value});
+            }
+        }
+        catch(e){
+            console.error(e)
+        }
+        finally{
+            loading.value = false;
+        }          
+    }
 
-                            downloadDocs.value = downloadDocs.slice(0, 24);
-                        }, 500);
-                    }
-                    else{
-                         info = await getDownloadRecords({fields, listType:'all', fileName, format : downloadFormat.value});
-                    }
-                }
-                catch(e){
-                    console.error(e)
-                }
-                finally{
-                    loading.value = false;
-                }
-                
-            };
     const closeDialog = () => {
-                modal.hide();
-                downloadDocs.value   =  [];
-				numFound.value       =  0;
-				loading.value        =  false;
-                isGeneric.value      =  true;
-                schemaFields.value   =  [];
-            };
+        modal.hide();
+        downloadDocs.value   =  [];
+        numFound.value       =  0;
+        loading.value        =  false;
+        isGeneric.value      =  true;
+        schemaFields.value   =  [];
+    };
 
     const formatString = (text) => {
         if(!text)
-                    return;
-                    
-                if(text.startsWith('http')){
-                    if(text.length > 35)
-                        return `<a target="_blank" href="${text}">${text.substr(0, 35)}...</a>`
-                    
-                    return `<a target="_blank" href="${text}">${text}...</a>`
-                }
+            return;
+            
+        if(text.startsWith('http')){
+            if(text.length > 35)
+                return `<a target="_blank" href="${text}">${text.substr(0, 35)}...</a>`
+            
+            return `<a target="_blank" href="${text}">${text}...</a>`
+        }
 
-                if(text.length > 50 && !text.startsWith('<a'))
-                    return text.substr(0, 50)+'...';
+        if(text.length > 50 && !text.startsWith('<a'))
+            return text.substr(0, 50)+'...';
 
-                return text;
-            }
+        return text;
+    }
 </script>
 
 
@@ -268,6 +339,14 @@
     }
     #datatable .tableexport-caption{
         display: none!important;
-    } 
+    }
+    /* replace template.css property */
+    #government {
+        float: left;
+    }
+    .custom-fields-modal-body {
+    height: 25rem; /* Fix the height in rem */
+    overflow-y: auto; /* Allow scrolling only when content overflows */
+    }
         
 </style>
