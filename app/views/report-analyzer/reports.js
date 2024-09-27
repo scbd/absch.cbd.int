48d07560fcd62b9cdd7dc6cdd44a64fe01657024
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-09-27 11:16:58 +0500

    pass report-type in url

--- app/views/report-analyzer/reports.js
+++ app/views/report-analyzer/reports.js
@@ -29,7 +29,7 @@ export default ['$scope', '$location', 'commonjs', '$q', '$http', 'realm', 'tran
                 components:{cbdArticle} 
             }
             $scope.$on('onReportTypeChanged', function(event, reportType) {
-               
+               $scope.reportType = reportType;
                 $scope.adminTags    = undefined;
                 $scope.articleQuery = undefined;
                 $timeout(()=>{ 
@@ -59,7 +59,7 @@ export default ['$scope', '$location', 'commonjs', '$q', '$http', 'realm', 'tran
                 };
                 sessionStorage.setItem('nrAnalyzerData', JSON.stringify(data));
 
-                $location.url(_.trimEnd($location.path(), '/') + '/analyzer');
+                $location.url(_.trimEnd($location.path(), '/') + '/analyzer/' + $scope.reportType);
             };
            
             var DefaultRegions = [
