a50900753dec38d6d32cff627e04547b043eb149
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-10-02 12:11:09 +0500

    update view-record-reference at document-selector

--- app/views/forms/edit/document-selector.js
+++ app/views/forms/edit/document-selector.js
@@ -708,24 +708,8 @@ app.directive("documentSelector", ["$timeout", 'locale', "$filter", "$q", "searc
                     $(`#${dialogId} .modal-body .tab-content`).css('max-height', height + 'px');  
                 }
 			};
-
-
-
-			$scope.isContact = function(document){
-				return document && _.some($scope.selectedDocuments, function(doc){
-						return (doc.identifier_s||doc.header.identifier)==removeRevisionNumber(document.identifier) && (doc.schema_s||(document.header||{}).schema) == "contact"
-				});
-			}
-			$scope.isAuthority = function(document){
-				return document && _.some($scope.selectedDocuments, function(doc){
-						return (doc.identifier_s||doc.header.identifier)==removeRevisionNumber(document.identifier) && (doc.schema_s||(document.header||{}).schema) == "authority"
-				});
-			}
-
-			$scope.isFocalPoint = function(document){
-				return document && focalPointRegex.test(document.identifier)
-			}
-
+					
+            // ToDo: Not in use, needs to be removed.
 			$scope.isMeasure = function(document){
 				return document && _.some($scope.selectedDocuments, function(doc){
 						return (doc.identifier_s||doc.header.identifier)==removeRevisionNumber(document.identifier) && (doc.schema_s||(document.header||{}).schema) == "measure"
