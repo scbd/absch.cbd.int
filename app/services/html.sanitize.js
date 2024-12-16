caae82c3f675be3daf866cecb31daa8c6c324e31
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-12-16 16:15:40 +0500

    add 'oembed' tag to the allowed list

--- app/services/html.sanitize.js
+++ app/services/html.sanitize.js
@@ -2,7 +2,10 @@ import dompurify from 'dompurify';
 
 export function sanitizeHtml(unsafeHtml) {
     if (unsafeHtml) {
-        unsafeHtml = dompurify.sanitize(unsafeHtml)
+        unsafeHtml = dompurify.sanitize(unsafeHtml, {
+            ADD_TAGS: ['oembed'], // Add 'oembed' to the default tags
+            ADD_ATTR: ['url'],
+        });
     }
     return unsafeHtml;
 }
