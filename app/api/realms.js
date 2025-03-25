ac3a099f4c5cf026a6c9f53ea72b4fdf210c8071
Blaise Fonseca <blaise.fonseca@un.org>
2025-03-25 13:14:50 -0400

    When realm does not match show uId prefix from other realm

--- app/api/realms.js
+++ app/api/realms.js
@@ -10,4 +10,16 @@ export default class RealmsApi extends ApiBase
     return this.http.get(`api/v2018/realm-configurations`,{ params }).then(res => res.data).catch(tryCastToApiError);
   }
 
+  async getRealmConfiguration(realm)  {
+    const params = {
+      q : { realm }
+    }
+
+    return this.http.get(`api/v2018/realm-configurations`, { params })
+               .then(res => {
+                return res.data?.length ? res.data[0] : null
+              })  
+               .catch(tryCastToApiError);
+  }
+
 }
