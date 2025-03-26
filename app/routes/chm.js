7987fd0a4dae95c8597f9b0f626801286fff0e2a
Blaise Fonseca <blaise.fonseca@un.org>
2025-03-26 16:07:35 -0400

    Handle legacy CHM routes (#1335)
    * Handle legacy CHM routes
    
    * Handle submit route
    
    * Redirect legacy url to new url format

--- app/routes/chm.js
+++ app/routes/chm.js
@@ -26,9 +26,64 @@ app.config(["$routeProvider", function ($routeProvider) {
   whenAsync('/register/EBSA/:identifier/edit',   { ...mapView(angularViewWrapper),   "label":routesLabels.new,"param":"true","resolveController":true,"documentType":"EBSA","resolve":{ ...chmRouteUrls.register_marineEbsa_edit,   "securized":securize(null,true,true)}}).
   whenAsync('/register/NBSAP/:identifier/edit',    { ...mapView(angularViewWrapper),   "label":routesLabels.new,"param":"true","resolveController":true,"documentType":"NBSAP","resolve":{ ...chmRouteUrls.register_NR_edit,"securized":securize(null,true,true), "routePrams":injectRouteParams({ "documentType":"NBSAP"})}}).
 
+  whenAsync('/submit/marineEbsa/new',                    {"redirectTo":"/register/EBSA/new","resolve":{}}). 
+  whenAsync('/submit/marineEbsa/:uid',                   {"redirectTo":"/register/EBSA/:uid/edit","resolve":{}}). 
+  whenAsync('/submit/organization/new',                  {"redirectTo":"/register/ORG/new","resolve":{}}). 
+  whenAsync('/submit/organization/:uid',                 {"redirectTo":"/register/ORG/:uid/edit","resolve":{}}). 
+  whenAsync('/submit/contact/new',                       {"redirectTo":"/register/CON/new","resolve":{}}). 
+  whenAsync('/submit/contact/:uid',                      {"redirectTo":"/register/CON/:uid/edit","resolve":{}}). 
+  whenAsync('/submit/resource/new',                      {"redirectTo":"/register/VLR/new","resolve":{}}). 
+  whenAsync('/submit/resource/:uid',                     {"redirectTo":"/register/VLR/:uid/edit","resolve":{}}). 
+  
+  
+  whenAsync('/submit/capacityBuildingInitiative/new',    {"redirectTo":"/register/CDI/new","resolve":{}}). 
+  whenAsync('/submit/capacityBuildingInitiative/:uid',   {"redirectTo":"/register/CDI/:uid/edit","resolve":{}}). 
+  // whenAsync('/submit/capacityBuildingResource/new',      {"redirectTo":"/register/CBR/new","resolve":{}}). 
+  // whenAsync('/submit/capacityBuildingResource/:uid',     {"redirectTo":"/register/CBR/:uid/edit","resolve":{}}).
+  whenAsync('/submit/submission/new' ,              {"redirectTo":"/register/SUB/new","resolve":{}}). 
+  whenAsync('/submit/submission/:uid',              {"redirectTo":"/register/SUB/:uid/edit","resolve":{}}).  
+  
+  whenAsync('/submit',                                   {"redirectTo":"/register","resolve":{}}). 
+
+  whenAsync('/management/requests',                      {"redirectTo":"/register/requests","resolve":{}}). 
+  whenAsync('/management/requests/:id',                  {"redirectTo":"/register/requests/:id","resolve":{}}). 
+  whenAsync('/management/requests/:id/:activity',        {"redirectTo":"/register/requests/:id","resolve":{}}). 
+  whenAsync('/management/user-preferences',              {"redirectTo":"/register/user-preferences","resolve":{}}). 
+  whenAsync('/management/national-users',                {"redirectTo":"/register/national-users","resolve":{}}). 
+  
+  whenAsync('/submit/undbAction/:uid',  { redirectTo: redirectUndbActions } ).
+  whenAsync('/submit/undbPartner/:uid', { redirectTo: redirectUndbPartner } ).
+  
   otherwise({
     templateUrl: commonRoutes.baseUrl + "views/shared/404.html",
     label: routesLabels.pageNotFound
   });
     
 }]);
+
+
+const redirectUndbActions = (routeParams, path, search) => {
+  
+  var id = routeParams.uid||'';
+  if(id=='new')
+      id = undefined;
+  var workflowId = search.workflowId ? '?workflowId=' +search.workflowId : '';
+  
+  if(id || workflowId)
+      window.location.href = 'https://www.cbd.int/undb-new/actions/submit-form/' + id + workflowId;
+  
+}
+
+const redirectUndbPartner = (routeParams, path, search) => {
+  
+  var id = routeParams.uid||'';
+  if(id=='new')
+      id = undefined;
+  var workflowId = search.workflowId ? '?workflowId=' +search.workflowId : '';
+
+  if(id || workflowId)
+      window.location.href = 'https://www.cbd.int/undb-new/actors/partners/edit/'+id + workflowId;
+  else
+      window.location.href = 'https://www.cbd.int/undb-new/actors/partners/register';
+
+}
