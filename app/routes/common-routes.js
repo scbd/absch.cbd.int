fae5bff1b5b11b3311bfb33cb3fdb7c2b10c02a1
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-05-21 12:31:24 +0500

    fix Broken link to Notifications

--- app/routes/common-routes.js
+++ app/routes/common-routes.js
@@ -167,6 +167,7 @@ app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $lo
   whenAsync('/share/email/:type/:accessKey',                      { ...mapView(angularViewWrapper),                    "label":routesLabels.embed,           "resolve":{ ...commonRouteUrls.embed,                                         },"param":"true","resolveController":true}).
 
   whenAsync('/submissions-to-notifications',                      { ...mapView(angularViewWrapper),                    "label":routesLabels.submissionsOnNotifications, "resolve":{ ...commonRouteUrls.submissionsOnNotifications,         },"param":"true","resolveController":true,"reloadOnSearch":false}).
+ whenAsync('/notifications/:notificationId',                      {"redirectTo": function(routeParams) { return `/submissions-to-notifications?notification=${routeParams.notificationId}`; }, label: routesLabels.submissionsOnNotifications, "resolve":{}  }).
   whenAsync('/database',                                          {"redirectTo":"/search","label":routesLabels.search,"resolve":{}}).
 
   //whenAsync('/portals',                                           { ...mapView(vueViewWrapper),                        "label":routesLabels.portals,       "resolve":{ ...commonRouteUrls.portal, },  "param":"true","resolveController":true}).
