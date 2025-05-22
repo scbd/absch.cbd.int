8b062a72b690344f6ad9020fc8d468cdfd4f8e8b
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-05-22 21:01:19 +0500

    update km-link use temp files API chm-16 (#1349)
    * Implement API calls for managing Temporary Attachment Slots
    
    * Update attachment logic
    
    * Display loading spinner during file upload
    
    * move code to km-document-attachment
    
    * update api-base for external api calls
    
    * commit suggestions
    
    * add args[0] to all http methods
    
    * update error messages text and use  $location
    
    * small fix, update names

---
+++ app/api/km-document-attachment.js
@@ -0,0 +1,23 @@
+import ApiBase, { tryCastToApiError } from './api-base';
+
+export default class KmDocumentAttachmentApi extends ApiBase
+{
+    constructor(options) {
+        super(options);
+    }
+
+    async createTempAttachmentSlot(fileName, contentType){
+        return this.http.post(`api/v2015/temporary-files`,{filename: fileName,contentType: contentType}).then(res => res.data).catch(tryCastToApiError);
+    }
+
+    async uploadToTempSlot(slotUrl, file, contentType)  { 
+        return this.http.put(slotUrl, file, {headers: { 'Content-Type': contentType }})
+                        .then(res => res.data).catch(tryCastToApiError);
+    }
+
+    async persistTemporaryAttachment(identifier, guid, fileName)  {
+        return this.http.post(`/api/v2013/documents/${encodeURIComponent(identifier)}/attachments/persist-temporary/${encodeURIComponent(guid)}`, {fileName})
+            .then(res => res.data).catch(tryCastToApiError);
+    }
+
+}
