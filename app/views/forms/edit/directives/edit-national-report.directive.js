9cc6b8be7b61ce48933808d0f84b06e6d88778b3
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-07-14 18:42:39 +0500

    remove error from list once question is answered

--- app/views/forms/edit/directives/edit-national-report.directive.js
+++ app/views/forms/edit/directives/edit-national-report.directive.js
@@ -132,6 +132,12 @@ app.directive("editNationalReport", ["$controller", "$http", 'IStorage', '$route
                             }
                         })
                     }
+                    // remove error from list once question is answered
+                   if($scope.validationReport?.errors?.length>0) { 
+                        $scope.validationReport.errors = $scope.validationReport.errors.filter(
+                        (error) =>  error.property !== question.key
+                        );
+                   }
                     ngModelController.$setViewValue(_.pickBy($scope.binding, _.identity), 'change'); //ToDo
                 }
                 $scope.onOptionSelected = function ($element, value, question){
