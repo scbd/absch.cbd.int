94b1b7623bbbe75c2bed9bf308b81b86d1eb0072
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-13 16:27:02 +0500

    WIP: sort the list by dates

--- app/components/common/country-party-status.vue
+++ app/components/common/country-party-status.vue
@@ -1,50 +1,17 @@
 <template>
-    <div v-if="country">
-        <!-- Party to the Convention -->
-        <div v-if="country.isChmParty" class="mb-2 text-chm">
-            <strong>{{ t("partyToConvention") }}</strong><br>
-            <span v-if="country.partyToChmDate">
-              ({{ t("since") }} {{ formatDate(country.partyToChmDate , 'DD MMM YYYY') }})
-            </span>
-        </div>
-        <div v-else class="mb-2 color-non-party">
-            <strong>{{ t("notPartyToConvention") }}</strong>
-        </div>
-
-        <!-- Party to the Nagoya Protocol -->
-        <div v-if="country.isNagoyaParty" class="mb-2 text-abs">
-            <strong>{{ t("partyToNagoya") }}</strong><br>
-            <span v-if="country.partyToNagoyaDate">
-              ({{ t("since") }} {{ formatDate(country.partyToNagoyaDate, 'DD MMM YYYY') }})
-            </span>
-        </div>
-        <div v-else class="mb-2 color-non-party">
-            <strong>{{ t("notPartyToNagoya") }}</strong>
-        </div>
-
-        <!-- Party to the Cartagena Protocol -->
-        <div v-if="country.isCartagenaParty" class="mb-2 text-bch">
-            <strong>{{ t("partyToCartagena") }}</strong><br>
-            <span v-if="country.partyToCartagenaDate">
-              ({{ t("since") }} {{ formatDate(country.partyToCartagenaDate, 'DD MMM YYYY') }})
-            </span>
-        </div>
-        <div v-else class="mb-2 color-non-party">
-            <strong>{{ t("notPartyToCartagena") }}</strong>
-        </div>
-
-        <!-- Party to the Cartagena Protocol -->
-        <div v-if="country.isNKLSParty" class="mb-2 color-NKLSP">
-            <strong>{{ t("partyToSuppProtocol") }}</strong><br>
-            <span v-if="country.partyToNKLSPDate">
-              ({{ t("since") }} {{ formatDate(country.partyToNKLSPDate, 'DD MMM YYYY') }})
-            </span>
-        </div>
-        <div v-else class="mb-2 color-non-party">
-            <strong>{{ t("notPartyToSuppProtocol") }}</strong>
-        </div>
-
+  <div v-if="country">
+    <div v-for="party in partyStatuses" :key="party.key">
+      <div v-if="country[party.key]" :class="['mb-2', party.class]">
+        <strong>{{ party.label }}</strong><br>
+        <span v-if="party.date">
+          ({{ t("since") }} {{ formatDate(party.date, 'DD MMM YYYY') }})
+        </span>
+      </div>
+      <div v-else class="mb-2 color-non-party">
+        <strong>{{ party.notLabel }}</strong>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -61,4 +28,42 @@ import messages from '../../app-text/views/countries/country-profile.json';
     }
   })
 
+  // Define party statuses with their properties
+  const partyStatuses = [
+    {
+      key: 'isChmParty',
+      label: t("partyToConvention"),
+      notLabel: t("notPartyToConvention"),
+      date: props.country.partyToChmDate,
+      class: 'text-chm'
+    },
+    {
+      key: 'isNagoyaParty',
+      label: t("partyToNagoya"),
+      notLabel: t("notPartyToNagoya"),
+      date: props.country.partyToNagoyaDate,
+      class: 'text-abs'
+    },
+    {
+      key: 'isCartagenaParty',
+      label: t("partyToCartagena"),
+      notLabel: t("notPartyToCartagena"),
+      date: props.country.partyToCartagenaDate,
+      class: 'text-bch'
+    },
+    {
+      key: 'isNKLSParty',
+      label: t("partyToSuppProtocol"),
+      notLabel: t("notPartyToSuppProtocol"),
+      date: props.country.partyToNKLSPDate,
+      class: 'color-NKLSP'
+    }
+  ];
+
+  // Sort by date (new first, nulls last)
+  partyStatuses.sort((a, b) => {
+    if (!a.date) return 1;
+    if (!b.date) return -1;
+    return new Date(b.date) - new Date(a.date);
+  });
 </script>
