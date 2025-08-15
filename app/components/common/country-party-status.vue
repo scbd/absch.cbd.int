9af1155fa8859ed9c48823cacc70080baebd1ef9
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-16 00:05:27 +0500

    update for  isInbetweenParty

--- app/components/common/country-party-status.vue
+++ app/components/common/country-party-status.vue
@@ -1,15 +1,33 @@
 <template>
   <div v-if="country">
-    <div v-for="party in partyStatuses" :key="party.key">
-      <div v-if="country[party.key]" :class="['mb-2', party.class]">
-        <strong>{{ party.label }}</strong><br>
-        <span v-if="party.date">
-          ({{ t("since") }} {{ formatDate(party.date, 'DD MMM YYYY') }})
+    <div v-for="realm in partyStatuses" :key="realm.key">
+
+      <!-- Party -->
+      <div v-if="realm.isParty" :class="[realm.class]">
+        <strong>{{ realm.label }}</strong><br>
+      </div>
+
+      <!-- Not a party, don't show in chm -->
+      <div v-else-if="!realm.isInbetweenParty && !realm.isParty && realm.realm=='chm'" class="mb-2 color-non-party">
+        <strong>{{ realm.notLabel }}</strong>
+      </div>
+
+      <!-- In-between / Entry date -->
+      <div v-if="realm.isParty || realm.isInbetweenParty">
+        <span 
+          class="bold"
+          :class="[
+            realm.isParty ? realm.class : '',
+            realm.isInbetweenParty ? 'color-inbetween' : ''
+          ]"
+        >
+          (
+            <span v-if="realm.isInbetweenParty">{{ t('entry') }} {{ formatDate(realm.entryIntoForce, 'DD MMM YYYY') }}</span>
+            <span v-else>{{ t('since') }} {{ formatDate(realm.partyToDate, 'DD MMM YYYY') }}</span>
+          )
         </span>
       </div>
-      <div v-else class="mb-2 color-non-party">
-        <strong>{{ party.notLabel }}</strong>
-      </div>
+
     </div>
   </div>
 </template>
@@ -19,65 +37,34 @@ import { formatDate } from '~/components/kb/filters';
 import { useI18n } from 'vue-i18n';
 import messages from '../../app-text/views/countries/country-profile.json';
 
-  const { t } = useI18n({ messages });
+const { t } = useI18n({ messages });
 
-  const props = defineProps({
-    country: {
-      type: Object,
-      required: true
-    },
-    visiblePartyRealms: {
-      type: Array,
-      default: () => []
-    }
-  })
+const props = defineProps({
+  country: { type: Object, required: true },
+  visiblePartyRealms: { type: Array, default: () => [] }
+});
 
-  // Define party statuses with their properties
-  const partyStatuses = [
-    {
-      key: 'isCBDParty',
-      realm: 'chm',
-      label: t("partyToConvention"),
-      notLabel: t("notPartyToConvention"),
-      date: props.country.partyToCBDDate,
-      class: 'text-chm'
-    },
-    {
-      key: 'isNagoyaParty',
-      realm: 'abs',
-      label: t("partyToNagoya"),
-      notLabel: t("notPartyToNagoya"),
-      date: props.country.partyToNagoyaDate,
-      class: 'text-abs'
-    },
-    {
-      key: 'isCartagenaParty',
-      realm: 'bch',
-      label: t("partyToCartagena"),
-      notLabel: t("notPartyToCartagena"),
-      date: props.country.partyToCartagenaDate,
-      class: 'text-bch'
-    },
-    {
-      key: 'isNKLSParty',
-      realm: 'bch',
-      label: t("partyToSuppProtocol"),
-      notLabel: t("notPartyToSuppProtocol"),
-      date: props.country.partyToNKLSPDate,
-      class: 'color-NKLSP'
-    }
-  ].filter(party => {
-    // Filter parties based on the visiblePartyRealms prop, visible-party-realms="['chm']" for chm only
-    if (props.visiblePartyRealms.length > 0) {
-      return props.visiblePartyRealms.includes(party.realm);
-    }
-    // If no filter is applied, include all parties
-    return true;
-  });
-
-  partyStatuses.sort((a, b) => {
-    if (!a.date) return 1;
-    if (!b.date) return -1;
-    return new Date(b.date) - new Date(a.date);
-  });
+const partyStatuses = [
+  {
+    key: 'isCBDParty', realm: 'chm', label: t("partyToConvention"), notLabel: t("notPartyToConvention"),
+    ...props.country.chm, class: 'text-chm'
+  },
+  {
+    key: 'isNagoyaParty', realm: 'abs', label: t("partyToNagoya"), notLabel: t("notPartyToNagoya"),
+    ...props.country.abs, class: 'text-abs'
+  },
+  {
+    key: 'isCartagenaParty', realm: 'bch', label: t("partyToCartagena"), notLabel: t("notPartyToCartagena"),
+    ...props.country.bch, class: 'text-bch'
+  },
+  {
+    key: 'isNKLSParty', realm: 'bch', label: t("partyToSuppProtocol"), notLabel: t("notPartyToSuppProtocol"),
+    isParty: props.country.isNKLSParty, partyToDate: props.country.partyToNKLSPDate, class: 'color-NKLSP'
+  }
+].filter(p => props.visiblePartyRealms.length === 0 || props.visiblePartyRealms.includes(p.realm))
+ .sort((a, b) => {
+    if (!a.entryIntoForce) return 1;
+    if (!b.entryIntoForce) return -1;
+    return new Date(b.entryIntoForce) - new Date(a.entryIntoForce);
+ });
 </script>
