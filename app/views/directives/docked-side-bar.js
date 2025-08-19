7cc2cd296414a3a8ca5fdc937a3ab6d36d04c801
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-19 11:34:49 +0500

    replace Mongo aggregation with Solr in contextual-help

--- app/views/directives/docked-side-bar.js
+++ app/views/directives/docked-side-bar.js
@@ -2,11 +2,15 @@ import app from '~/app';
 import _ from 'lodash';
 import '~/services/main';
 import template from './docked-side-bar.html';
+import SolrApi from "~/api/solr.js";
 import 'ck-editor-css';
 import dockedSideBarT from '~/app-text/views/directives/docked-side-bar.json';
+import { escape } from '../../services/solr/queries.js';
+import { OASIS_REALM } from '~/services/filters/constant';
+import ArticlesApi from '~/components/kb/article-api';
 
-app.directive('dockedSideBar', ['realm', '$rootScope', '$route', '$location', 'articlesService', '$q', 'translationService', 'locale',
-    function (realm, $rootScope, $route, $location, articlesService, $q, translationService, locale) {
+app.directive('dockedSideBar', ['realm', '$rootScope', '$route', '$location', 'translationService', 'locale',
+    function (realm, $rootScope, $route, $location, translationService, locale) {
     return {
         restrict: 'AE',
         replace: true,
@@ -15,35 +19,36 @@ app.directive('dockedSideBar', ['realm', '$rootScope', '$route', '$location', 'a
             tags: '@?',
             type: '='
         },
-        link: function($scope, $element){
-                let canceler = null;
+        
+        link: async function($scope, $element){
+                $scope.error = false;
+                const articlesApi = new ArticlesApi();
+                $scope.articles = [];
                 translationService.set('dockedSideBarT', dockedSideBarT);
-                $scope.search = (text)=>{                    
-                    loadArticles({queryTags:$scope.type == 'announcements' ? 'announcement' : 'context-help', searchText:text, skipRefetch:true});
+                $scope.search = async (text)=>{ 
+                    await loadArticles({queryTags:$scope.type == 'announcements' ? 'announcement' : 'context-help', searchText:text, skipRefetch:true});
                 }
 
-                $scope.showTagArticles = (tag)=>{
+                $scope.showTagArticles = async (tag)=>{
                     $scope.back();
-                    loadArticles({queryTags:tag, skipRefetch:true});
+                    await loadArticles({queryTags:tag, skipRefetch:true});
                 }
 
                 //---------------------------------------------------------------------
-                function loadArticles({queryTags, skipRefetch, searchText}){
+                async function loadArticles({queryTags, skipRefetch, searchText}){
                     $scope.loading = true;
-                    $scope.articles = undefined;
-
-                    var ag        = [];
-                    var agLimit   = [];
-                    var tags      = [];
-                        queryTags = queryTags || $scope.tags;
+                    const localeKey = locale.toUpperCase(); 
+                    let query = [`realm_ss:${OASIS_REALM}`];
+                    let tags = [];
+                    queryTags = queryTags || $scope.tags;
                     
                     if(queryTags)
                         tags = _(queryTags.split(',')).compact().map(function(tag){
-                                    return {"adminTags":encodeURIComponent(_.trim(tag))}
+                                    return _.trim(tag)
                                 }).value();
                     else{
                         if($scope.type == 'announcements')
-                            tags =  [{"adminTags":encodeURIComponent("announcement")}];
+                            tags =  ["announcement"];
                         else{
                             let paths = $route.current.originalPath.split('/')
                             if($location.path() == '/')
@@ -65,62 +70,55 @@ app.directive('dockedSideBar', ['realm', '$rootScope', '$route', '$location', 'a
 
                             if(paths){
                                 tags =  _(paths).compact().map(function(path){
-                                    return {"adminTags":encodeURIComponent(path.trim())}
+                                    return path.trim()
                                 }).value();
                             }
                         }
                     }
 
-                    tags.push({"adminTags":encodeURIComponent(realm.value.toLowerCase().replace(/-.*/,''))});
+                    tags.push(realm.value.toLowerCase().replace(/-.*/,'')); 
 
                     if(tags.length)
-                        ag.push({"$match":{"$and":tags}});
+                        query.push(`(${tags.map(t => `adminTags_ss:${t}`).join(' AND ')})`);
 
                     if ((searchText || '').trim() != '') {
-                        ag.push({
-                            "$match": {
-                                "$or": [
-                                    { [`title.${locale}`]: { "$$contains": searchText } },
-                                    { [`content.${locale}`]: { "$$contains": searchText } }
-                                ]
-                            }
-                        });
+                        const escapedSearchText = escape(searchText);
+                        query.push(`(title_${localeKey}_txt:${escapedSearchText} OR text_${localeKey}_txt:${escapedSearchText} OR summary_${localeKey}_txt:${escapedSearchText} OR content_${localeKey}_txt:${escapedSearchText})`); 
+                    }
+                    
+                    const solrAPI = new SolrApi();
+                    try {
+                        const result = await solrAPI.query({
+                        query: query.join(' AND '),
+                        fields: `title:title_${localeKey}_s,summary:summary_${localeKey}_s, content:content_${localeKey}_s, coverImage:coverImage_ss,adminTags:adminTags_ss,createdDate:createdDate_dt,id`,
+                        rowsPerPage: 20
+                    }); 
+ 
+
+                    if(result?.response?.docs.length>0){
+                            $scope.$applyAsync(() => {
+                            $scope.articles = result.response.docs;
+                        }); 
+                    }
+                    else if(!skipRefetch){ 
+                        return await loadArticles({queryTags:'context-help', skipRefetch:true});
                     }
 
-                    ag.push({"$project" : {"title":1, "coverImage":1, "meta":1, "content":1, "summary":1, "adminTags":1}});
-                    ag.push({"$sort" : {"meta.modifiedOn":-1}});
-
-                    agLimit = JSON.parse(JSON.stringify(ag))
-                    agLimit.push({"$limit" : 20});
-
-                    var qs = {
-                        "ag" : JSON.stringify(agLimit)
-                    };
-
-
-                    if (canceler) {
-                        canceler.resolve(true);
-                    }
-                    canceler = $q.defer();
-
-                    return articlesService.getArticles(qs, false, canceler.promise)
-                    .then(function(data){
-                        canceler = null;
-                        if((data||[]).length)
-                            $scope.articles = data;
-                        else if(!skipRefetch){
-                            return loadArticles({queryTags:'context-help', skipRefetch:true});
+                    } catch (e) {
+                            console.error("error in load article",e);
+                            $scope.error = true;
                         }
-                    })
-                    .finally(function(){
-                        $scope.loading=false
-                    });
-                }
+                        finally {
+                            $scope.$applyAsync(() => {
+                                $scope.loading = false;
+                            });
+                        };
+                } 
 
-                var evtRouteChangeSuccess = $rootScope.$on('$routeChangeSuccess', function (evt, current) {
+                var evtRouteChangeSuccess = $rootScope.$on('$routeChangeSuccess', async function (evt, current) {
                     $scope.freeText = '';
                     if($scope.type == 'help')
-                        loadArticles({});
+                       await loadArticles({});
                 });
 
                 var scrollHandlerfn = function() {                               
@@ -139,11 +137,25 @@ app.directive('dockedSideBar', ['realm', '$rootScope', '$route', '$location', 'a
                     $(window).off('scroll', scrollHandlerfn)
                 });
 
-                $scope.details =function(article)
-                {
-                    $scope.showArticle = true;
-                    $scope.articleData = article;
+                $scope.details = async function(article) {
+                    try {
+                        $scope.loading = true;
+                        $scope.showArticle = true;
+                        
+                        const articleData = await articlesApi.getArticleById(article.id);
+                        $scope.$applyAsync(() => {
+                            $scope.articleData = articleData;
+                        });
+                    } catch (err) {
+                        console.error("Error loading article", err);
+                        $scope.error = true;
+                    } finally {
+                        $scope.$applyAsync(() => {
+                            $scope.loading = false;
+                        });
+                    }
                 }
+
                 $scope.back =function()
                 {
                     $scope.showArticle = false;
@@ -164,7 +176,7 @@ app.directive('dockedSideBar', ['realm', '$rootScope', '$route', '$location', 'a
                 }
 
                 if($scope.type)
-                    loadArticles({});
+                    await loadArticles({});
 
         }                
     };
