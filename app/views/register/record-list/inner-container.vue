b47969c321917a4f3e093b6bfa99e6134d38c413
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-25 22:11:50 +0500

    create 'inner-container' vue3 component

---
+++ app/views/register/record-list/inner-container.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="innerPage"class="card card-1-notrans bg-white mx-1 mx-lg-3 mt-3 mb-5">
+        <!-- Header -->
+        <div class="p-0 mb-1" :class="headerClass">
+            <div class="d-flex align-items-center justify-content-between p-1 w-100">
+                <!-- Slot for header title -->
+                <slot name="header-title"></slot>
+
+                <!-- Slot for right button group -->
+                <slot name="right-buttons"></slot>
+            </div>
+        </div>
+        <!-- Content -->
+        <div class="register-content-content">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+  const props = defineProps({
+    headerClass: { type: String, default: "bg-primary" },
+  });
+  
+</script>
