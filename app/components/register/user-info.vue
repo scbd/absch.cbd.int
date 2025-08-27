06f573def994629eb28cdf404641965c65a3715c
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-27 19:17:28 +0500

    remove validator

--- app/components/register/user-info.vue
+++ app/components/register/user-info.vue
@@ -1,24 +1,20 @@
 <template>
-        <a
-            class="text-decoration-none"
-            :href="`mailto:${user.email}`"
-        >
-            <strong>{{ user.firstName }} {{ user.lastName }}</strong>
-        </a>
+    <a
+        v-if="user"
+        class="text-decoration-none"
+        :href="`mailto:${user.email}`"
+    >
+        <strong>{{ user.firstName }} {{ user.lastName }}</strong>
+    </a>
 </template>
 
 <script setup>
 
-        const props = defineProps({
-            user: {
-                type: Object,
-                required: true,
-                validator: (u) =>
-                typeof u.userID === "number" &&
-                typeof u.firstName === "string" &&
-                typeof u.lastName === "string" &&
-                typeof u.email === "string"
-            }
-        })
-
+    const props = defineProps({
+        user: {
+            type: Object,
+            required: true
+        }
+    })
+    
 </script>
