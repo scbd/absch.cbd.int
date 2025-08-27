81b905cbf28c68b9ee909f6acacb99973aa22e7d
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-27 19:10:48 +0500

    create 'user-info' Vue3 component

---
+++ app/components/register/user-info.vue
@@ -0,0 +1,21 @@
+<template>
+        <a class="text-decoration-none" :href="`mailto:${user.email}`">
+            <strong>{{ user.firstName }} {{ user.lastName }}</strong>
+        </a>
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        user: {
+            type: Object,
+            required: true,
+            validator: (u) =>
+            typeof u.userID === "number" &&
+            typeof u.firstName === "string" &&
+            typeof u.lastName === "string" &&
+            typeof u.email === "string"
+        }
+    })
+
+</script>
