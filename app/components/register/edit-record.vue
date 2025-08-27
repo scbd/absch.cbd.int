35cd7c5f5b59ef9f7cee8b0f1d696e42960ee648
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-27 17:08:51 +0500

    create edit-record Vue component

--- app/components/register/edit-record.vue
+++ app/components/register/edit-record.vue
@@ -1,44 +1,30 @@
 <template>
-      <a
-        id="editRecord"
-        class="btn btn-outline-secondary text-decoration-none"
-        rel="noopener"
-        :title="t('edit')"
-        :href="`/en/register/${schemaCode}/${identifier}/edit`"
-      >
-        <i class="bi bi-pencil-square"></i>
-      </a>
+    <a
+      id="editRecord"
+      class="btn btn-outline-secondary text-decoration-none"
+      :title="t('edit')"
+      :href="`/en/register/${schemaCode}/${identifier}/edit`"
+    >
+      <i class="bi bi-pencil-square"></i>
+    </a>
 </template>
 
 <script setup>
-    import { ref, onMounted } from "vue"
     import { useI18n } from "vue-i18n"
-    import KmDocumentApi from "~/api/km-document"
-    import { useAuth } from "@scbd/angular-vue/src/index.js"
-    import { useSchema } from "~/services/composables/schema.js"
     import messages from "~/app-text/views/register/record-list.json"
 
+    const { t } = useI18n({ messages })
+
+    // --- Props ---
     const props = defineProps({
+      schemaCode: {
+        type: String,
+        required: true
+      },
       identifier: {
         type: String,
         required: true
       }
     })
 
-    // --- Composables ---
-    const { t } = useI18n({ messages })
-    const { schemaShortName } = useSchema()
-    const auth = useAuth()
-    const kmDocumentApi = new KmDocumentApi({ tokenReader: () => auth.token() })
-
-    // --- Reactive state ---
-    const schemaCode = ref("")
-
-    // --- Lifecycle ---
-    onMounted(async () => {
-      const result = await kmDocumentApi.getDocument(props.identifier)
-      console.log("result", result)
-      schemaCode.value = schemaShortName(result.header.schema)
-      console.log("schemaCode", schemaCode.value)
-    })
 </script>
