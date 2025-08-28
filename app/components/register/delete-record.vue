a24919eb69b11f8318828e7a0cdc9c043e7cec75
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-08-28 15:34:09 +0500

    Delete record component

---
+++ app/components/register/delete-record.vue
@@ -0,0 +1,29 @@
+<template>
+    <button 
+      class="btn btn-outline-secondary" 
+      :title="t('delete')" 
+      type="button" 
+      @click="handleClick"
+    >
+      <i class="bi bi-trash"></i>
+    </button>
+</template>
+
+<script setup>
+  import { defineEmits, inject } from "vue"
+  import { useI18n } from "vue-i18n"
+  import messages from "~/app-text/views/register/record-list.json"
+
+  const { t } = useI18n({ messages })
+  const emit = defineEmits(["refresh"])
+  const askDelete = inject("askDelete")
+
+  const props = defineProps({
+    record: { type: Object, required: true }
+  })
+
+  const handleClick =  () => {
+      askDelete(props.record) // call Angular-provided delete
+      emit("refresh", props.record) // tell parent to refresh
+  }
+</script>
