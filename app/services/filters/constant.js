e3787f67b9ea993fe1676fffd590433793e202e3
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2025-09-11 19:24:27 +0500

    add 'show all' option in pagination chm-771 (#1389)
    * add 'show all' option in pagination
    
    * apply on all reference records
    
    * pass array of options and rename variables
    
    * move to const file
    
    * Store last selected page size in Search pagination
    
    * Renames cache keys for page size preferences
    
    Updates the names of the cache keys used to store user preferences for pagination page sizes.
    
    This change improves the clarity and consistency of the cache keys, making it easier to identify and manage user preference data.
    
    ---------
    
    Co-authored-by: Blaise Fonseca <blaise.fonseca@un.org>

--- app/services/filters/constant.js
+++ app/services/filters/constant.js
@@ -1,5 +1,6 @@
-// export * from './thesaurus.js'
-
+import numbers from '~/app-text/common/numbers.json'; 
+import { mergeTranslationKeys } from '../../services/translation-merge';
+const numbersText = mergeTranslationKeys(numbers);
 export const THESAURUS = {
     COUNTRIES                         : "countries", 
     JURISDICTIONS                     : "50AC1489-92B8-4D99-965A-AAE97A80F38E",  
@@ -8,3 +9,19 @@ export const THESAURUS = {
     REPORT_STATUS                     : "7F0D898A-6BF1-4CE6-AA77-7FEAED3429C6",  
     REPORT_TYPES                      : "2FD0C77B-D30B-42BC-8049-8C62D898A193"  
 }
+
+export const PAGINATION_OPTIONS_WITH_ALL = [
+    { value: 25, label: numbersText.twentyFive },
+    { value: 50, label: numbersText.fifty },
+    { value: 100, label: numbersText.oneHundred },
+    { value: 10000, label: numbersText.allNumbers }];
+
+export const PAGINATION_OPTIONS_DEFAULT = [
+    { value: 25, label: numbersText.twentyFive },
+    { value: 50, label: numbersText.fifty },
+    { value: 100, label: numbersText.oneHundred },
+    { value: 300, label: numbersText.threeHundred }];
+
+export const CACHE_PAGINATION_DOCUMENT_SELECTOR_PAGE_SIZE = 'userPreferenceDocumentSelectorPageSize';
+export const CACHE_PAGINATION_REFERENCED_RECORDS_PAGE_SIZE = 'userPreferenceReferenceRecordsPageSize';
+export const CACHE_PAGINATION_SEARCH_PAGE_SIZE = 'userPreferenceSearchResultPageSize';
