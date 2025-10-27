a2a81fb8c24f664e5ad8b74135b3a7e488931179
Mitch1000 <mwdrohan@gmail.com>
2025-10-27 09:48:49 -0400

    coverts the arrays filter file from javascript to typescript

---
+++ app/services/filters/arrays.ts
@@ -0,0 +1,21 @@
+interface FilterItem {
+  __value: string;
+  [key: string]: any;
+}
+
+export function genericFilter ($query: string, items: FilterItem[] | undefined): FilterItem[] | undefined {
+  if (!items) {
+    return
+  }
+  const lowerCaseQuery = $query.toLowerCase()
+  return items.filter(item => item.__value.toLowerCase().indexOf(lowerCaseQuery) !== -1)
+}
+
+interface MappingItem {
+  identifier: any;
+  [key: string]: any;
+}
+
+export function genericMapping (item: MappingItem) {
+  return { identifier: item.identifier }
+}
