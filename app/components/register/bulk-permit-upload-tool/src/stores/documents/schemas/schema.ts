4bd1b0f8e3bb48397d267ae4b042696122b4ffec
Mitch1000 <mwdrohan@gmail.com>
2025-11-06 10:28:09 -0500

    migrates logic for parsing xlsx sheet data to named attributes into its own function

--- app/components/register/bulk-permit-upload-tool/src/stores/documents/schemas/schema.ts
+++ app/components/register/bulk-permit-upload-tool/src/stores/documents/schemas/schema.ts
@@ -1,11 +1,7 @@
 import type { LanguageCode, LanguageType } from '../../../mappings/types'
-import type {
-  IDocumentMap, SubjectMatter, IContactFields,
-  IFileData, IMapData, FileReference
-} from './types'
+import type { SubjectMatter, FileReference } from './types'
 
 import languageMapping from '../../../mappings/languageMapping'
-type DocumentKeys = keyof IDocumentMap
 
 export default class Schema {
   language: LanguageCode = 'en'
@@ -16,14 +12,7 @@ export default class Schema {
     this.xlsxFileData = xlsxData
   }
 
-  getColumnValue (col: string): string {
-    const location = `${col}${this.documentNumber + 2}`
-    return (this.xlsxFileData[location] || {}).w || ''
-  }
-
-  getSubjectMatter (col: string): SubjectMatter {
-    const subjectMatter: string = this.getColumnValue(col) || ''
-
+  getSubjectMatter (subjectMatter: string): SubjectMatter {
     if (subjectMatter.trim() === '') { return { [this.language]: '' } as SubjectMatter }
 
     return {
@@ -31,8 +20,7 @@ export default class Schema {
     } as SubjectMatter
   }
 
-  getLanguageCode (col: string): LanguageCode {
-    const langValue: string = this.getColumnValue(col)
+  getLanguageCode (langValue: string): LanguageCode {
     const lang: string = langValue.toLowerCase()
     return languageMapping[lang as LanguageType]
   }
@@ -67,72 +55,5 @@ export default class Schema {
       name: this.xlsxFileData[col] || '',
       [this.language]: this.xlsxFileData[col] || 'en'
     } as FileReference
-  }
-
-  getProviderMap (): IContactFields {
-    return {
-      type: 'G',
-      existing: 'H',
-      orgName_firstName: 'I',
-      acronym_lastName: 'J',
-      address: 'K',
-      city: 'L',
-      country: 'M',
-      email: 'N',
-    }
-  }
-
-  getPICMap (): IContactFields {
-    return {
-      consent: 'O',
-      type: 'P',
-      existing: 'Q',
-      orgName_firstName: 'R',
-      acronym_lastName: 'S',
-      address: 'T',
-      city: 'U',
-      country: 'V',
-      email: 'W',
-    }
-  }
-
-  getDocumentMap (): IDocumentMap {
-    return {
-      language: 'A',
-      title: 'D',
-      dateOfIssuance: 'E',
-      dateOfExpiry: 'F',
-      provider: this.getProviderMap(),
-      pic: this.getPICMap(),
-      matEstablished: 'X',
-      subjectMatter: 'Y',
-      keywords: 'Z',
-      specimens: 'AA',
-      taxonomies: 'AB',
-      usage: 'AC',
-      usageDescription: 'AD',
-      conditionsThirdPartyTransfer: 'AE',
-      permitFiles: 'AF',
-      additionalInformation: 'AG',
-    }
-  }
-
-  mapXLSXFileDataToAttributeNames (documentMap: IDocumentMap): IDocumentMap {
-    const mapKeyToColumn = (map: IContactFields | IDocumentMap): IDocumentMap => {
-      const mappedObject: IDocumentMap = this.getDocumentMap()
-      Object.entries(map).forEach(([k, value]) => {
-        const key: DocumentKeys = k as DocumentKeys
-        if (typeof value === 'string') {
-          mappedObject[key] = this.getColumnValue(value)
-          return
-        }
-        if (typeof value === 'object') {
-          mappedObject[key] = mapKeyToColumn(value)
-        }
-      })
-      return mappedObject
-    }
-
-    return mapKeyToColumn(documentMap)
   }
 }
