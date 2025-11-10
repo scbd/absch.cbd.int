e646bc9787991118e1ddbbb30a422a7deb5fd4f4
Mitch1000 <mwdrohan@gmail.com>
2025-11-10 15:16:07 -0500

    fixing rollup build temp

--- app/components/common/modal.vue
+++ app/components/common/modal.vue
@@ -1,62 +1,88 @@
-
 <template>
-    <div ref="modalRef" class="modal fade"  aria-hidden="true">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
+  <div
+    ref="modalRef"
+    class="modal fade"
+    aria-hidden="true"
+  >
+    <div
+      class="modal-dialog modal-xl"
+    >
+      <div class="modal-content">
         <div class="modal-header">
-            <slot name="header">
-                <h5 class="modal-title" id="staticBackdropLabel">
-                    {{ lstring(title, locale) }}
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </slot>
+          <slot name="header">
+            <h5
+              id="staticBackdropLabel"
+              class="modal-title"
+            >
+              {{ title }}
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            />
+          </slot>
         </div>
         <div class="modal-body">
-            <slot name="default">
-            </slot>
+          <slot name="default" />
         </div>
         <div class="modal-footer">
-            <slot name="footer">
-                <div class="row w-100">
-                    <div class="col">
-                    </div>
-                    <div class="col-auto">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ t("closeButton") }}</button>
-                    </div>
-                </div>
-            </slot>
+          <slot name="footer">
+            <div class="row w-100">
+              <div class="col" />
+              <div class="col-auto">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  {{ $t("closeButton") }}
+                </button>
+              </div>
+            </div>
+          </slot>
         </div>
-        </div>
+      </div>
     </div>
-    </div>
-
+  </div>
 </template>
-<script setup>
-    import { shallowRef, onMounted } from "vue";
-    import { Modal } from "bootstrap";
-    import { useI18n } from 'vue-i18n';
-    import messages from '../../app-text/components/common/modal.json';
-    import { lstring } from '../../components/kb/filters';
-    const { t, locale } = useI18n({ messages });
-    const modalRef = shallowRef(null);
-    let modal = null;
-    
-    const props = defineProps({
-        title: [String, Object], 
-        required: true,
-    });
 
-    onMounted(() => {
-        modal = new Modal(modalRef.value);
-    });
+<script lang="ts" setup>
 
-    const show = () => {
-        modal.show('static');
-    };
+import { shallowRef, onMounted, watch } from 'vue'
+import { Modal } from 'bootstrap'
+const modalRef = shallowRef(null)
+let modal = null
 
-    const close = () => {
-        modal.hide();
-    };
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  isShown: {
+    type: Boolean,
+    default: true,
+  }
+})
 
-    defineExpose({show, close})
+const show = () => {
+  modal.show('static')
+}
+
+const close = () => {
+  modal.hide()
+}
+
+watch(() => props.isShown, (newValue) => {
+  if (newValue) { return show() }
+  close()
+})
+
+onMounted(() => {
+  modal = new Modal(modalRef.value)
+  if (props.isShown) { show() }
+})
+
+defineExpose({ show, close })
 </script>
