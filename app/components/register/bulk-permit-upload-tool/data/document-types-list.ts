7172a6ea9ddf87730bd598e1b420e6b6049963cc
Mitch1000 <mwdrohan@gmail.com>
2025-11-10 11:54:42 -0500

    removes files that will not be included in the final PR to ensure it is easier to discuss what changes are being made

---
+++ app/components/register/bulk-permit-upload-tool/data/document-types-list.ts
@@ -0,0 +1,23 @@
+import IrccSchema from '../stores/documents/schemas/ircc-schema/index'
+import Schema from '../stores/documents/schemas/schema'
+import irccAttributesMap from '../utilities/xlsx-file-to-document-attributes/maps/ircc-document.json'
+
+
+type AttributesMap = {
+  [key: string]: string | AttributesMap
+}
+
+export type DocumentTypes = 'ircc' // | 'cpc' | 'contact'
+
+type DocumentInfo = {
+  ApiSchema: typeof Schema
+  attributesMap: AttributesMap
+}
+
+export type DocumentsList = {
+  [key: string]: DocumentInfo
+}
+
+export const documentsList: DocumentsList = {
+  ircc: { ApiSchema: IrccSchema, attributesMap: irccAttributesMap }
+}
