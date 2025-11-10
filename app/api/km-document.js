e206de6f0be93a84f6d0119d9009c283e5696a5c
Mitch1000 <mwdrohan@gmail.com>
2025-11-10 12:35:51 -0500

    removes unused code from km documents file

--- app/api/km-document.js
+++ app/api/km-document.js
@@ -14,26 +14,8 @@ export default class KmDocumentApi extends ApiBase
         return this.http.get(`api/v2013/documents/${identifier}`).then(res => res.data).catch(tryCastToApiError);
     }
 
-    async queryCountries()  {
-        return this.http.get(`https://accounts.cbd.int/api/v2013/countries`).then(res => res.data).catch(tryCastToApiError);
-    }
-
-    validateDocument(element){
-        return this.http.put(`api/v2013/documents/x/validate`, {schema: element.header.schema, data: element})
-            .then(res => res.data).catch(tryCastToApiError);
-    }
-
-    createNationalRecord(identifier, schema, isDraft){
-        const url = isDraft ? `api/v2013/documents/${identifier}/versions/draft` : `api/v2013/documents/${identifier}`;
-        return this.http.put(url, {schema}).then(res => res.data).catch(tryCastToApiError);;
-    }
-
-    async paramDocuments(params){
-        return this.http.get(`api/v2013/documents/${params}`).then(res => res.data).catch(tryCastToApiError);
-    }
-
-    async queryCountries()  {
-        return this.http.get(`https://accounts.cbd.int/api/v2013/countries`).then(res => res.data).catch(tryCastToApiError);
+    async getDocument(identifier)  {
+        return this.http.get(`api/v2013/documents/${identifier}`).then(res => res.data).catch(tryCastToApiError);
     }
 
 }
