05113d410a7487c3677c89590c54be90d0c04fcc
Mitch1000 <mwdrohan@gmail.com>
2025-11-10 22:53:14 -0500

    modifies files to allow component to build with rollup build

--- app/components/register/bulk-permit-upload-tool/components/app.vue
+++ app/components/register/bulk-permit-upload-tool/components/app.vue
@@ -1,18 +1,20 @@
 <template>
-  <Modal :is-shown="true">
+  <Modal>
     <template #header>
       <BulkUploaderHeader />
     </template>
+
     <UploadButton
       :is-loading="false"
-          @on-file-change="() => []"
+      @on-file-change="onFileChange"
     />
+
     <template #footer>
-      <ModalFooter
-        v-if="true"
+      <BulkUploaderFooter
+        v-if="hasParsedFiles()"
         :is-loading="false"
-        @handle-confirm="() => {} "
-        @handle-clear="() => {} "
+        @handle-confirm="handleConfirm"
+        @handle-clear="handleClearFile"
       />
       <div v-else />
     </template>
@@ -24,7 +26,8 @@ import ModalFooter from './bulk-uploader-footer.vue'
 import BulkUploaderHeader from './bulk-uploader-header.vue'
 import UploadButton from './upload-button.vue'
 import Modal from '../../../common/modal.vue'
-import { useXlSXSheetStore } from '../stores/xlsx-sheet/index'
+// import { useXlSXSheetStore } from '../stores/xlsx-sheet/index'
+import { readFile } from '../stores/xlsx-sheet/index'
 import xlsxFileToDocumentAttributes from '../utilities/xlsx-file-to-document-attributes'
 import mapDocumentAttributesToAPIJSON from '../utilities/document-attributes-to-api-json'
 import { type DocumentAttributesMap } from '../utilities/xlsx-file-to-document-attributes/types'
@@ -34,25 +37,38 @@ const props = defineProps<{
   documentType: DocumentTypes;
 }>()
 
-const xlsxSheetStore = useXlSXSheetStore()
+// const xlsxSheetStore = useXlSXSheetStore()
 
 const handleConfirm = () => {
   // POST to API to create Document Draft
 }
-// 
-// const onFileChange = async (changeEvent: Event) => {
-//   const docType :DocumentTypes = props.documentType
-//   // Read File
-//   const workbook = await xlsxSheetStore.readFile(changeEvent)
-//   const sheet: XLSX.WorkSheet | Array<string> = workbook.Sheets['Sheet3'] || []
-//   // Parse File to JSON matching the attributes of a given document
-//   const documentAttributesList :DocumentAttributesMap = xlsxFileToDocumentAttributes(docType, sheet)
-//   console.log('documentAttributesList', documentAttributesList)
-// 
-//   // Match document attributes to the API Schema
-//   const apiJson = await mapDocumentAttributesToAPIJSON(documentAttributesList, docType)
-//   console.log('apiJson', apiJson)
-// 
-//   return apiJson
-// }
+
+const handleClearFile = () => {
+  // POST to API to create Document Draft
+}
+const hasParsedFiles = () => {
+  return true
+}
+
+
+
+const onFileChange = async (changeEvent: Event) => {
+  const docType :DocumentTypes = props.documentType
+  // Read File
+  // const readFile = xlsxSheetStore.readFile
+  const workbook = await readFile(changeEvent)
+  const sheet: XLSX.WorkSheet | Array<string> = workbook.Sheets['Sheet3'] || []
+  console.log('sheet', sheet)
+
+  // // Parse File to JSON matching the attributes of a given document
+  // const documentAttributesList :DocumentAttributesMap = xlsxFileToDocumentAttributes(docType, sheet)
+  const documentAttributesList :DocumentAttributesMap = xlsxFileToDocumentAttributes(docType, sheet)
+  console.log('documentAttributesList', documentAttributesList)
+
+  // // Match document attributes to the API Schema
+  const apiJson = await mapDocumentAttributesToAPIJSON(documentAttributesList, docType)
+  console.log('apiJson', apiJson)
+
+  // return apiJson
+}
 </script>
