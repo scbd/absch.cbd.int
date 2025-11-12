ba827ddadebdc25aa278e2fbc366ebf1fa1664f9
Mitch1000 <mwdrohan@gmail.com>
2025-11-12 11:34:37 -0500

    converts the popular-tags.vue file to typescript to test if typescript works inside of vue files

--- app/components/kb/popular-tags.vue
+++ app/components/kb/popular-tags.vue
@@ -4,31 +4,47 @@
         <h4>{{ t("popularTags") }}</h4>
 				<hr>
         <div v-if="!loading">
-            <button v-for="tag in popularTags" type="button"  class="btn btn-sm btn-outline-secondary m-1 me-2" @click="goToAdminTag(tag.adminTags[0])">
-               {{tag.title}}
-            </button>
+          <button
+            v-for="tag in popularTags"
+            type="button"
+            class="btn btn-sm
+            btn-outline-secondary m-1 me-2"
+            @click="goToAdminTag(tag.adminTags[0] || '')"
+          >
+            {{tag.title}}
+          </button>
         </div>
     </div>
   </template>
   
-<script setup>
-    import { ref, onMounted } from "vue";
+<script setup lang="ts">
+    import { type Ref, ref, onMounted } from "vue";
+    // @ts-ignore
     import { useRealm } from '../../services/composables/realm.js';
+    // @ts-ignore
     import { useI18n } from 'vue-i18n';
     import messages from '../../app-text/components/kb.json';
+    // @ts-ignore
     import { loadKbCategories } from '../../services/composables/articles.js';
+    // @ts-ignore
     import {  useRouter } from "@scbd/angular-vue/src/index.js";
+
+    type Tag = {
+      title: string;
+      adminTags: Array<string>;
+    }
+
     const { t, locale } = useI18n({ messages });
     const realm = useRealm();
     const router = useRouter();
-    const popularTags = ref([]);
-    const loading = ref(true);
+    const popularTags :Ref<Array<Tag>> = ref([]);
+    const loading :Ref<boolean> = ref(true);
   
-    const goToAdminTag = (tag) => {
-        if (tag === 'faq') {
+    const goToAdminTag = (adminTag: string) => {
+        if (adminTag === 'faq') {
         router.push({ path: 'kb/faqs' });
         } else {
-        router.push({ path: `kb/tags/${encodeURIComponent(tag)}` });
+        router.push({ path: `kb/tags/${encodeURIComponent(adminTag)}` });
         }
     };
   
@@ -42,5 +58,4 @@
         loading.value = false;
       }
   });
-
 </script>
