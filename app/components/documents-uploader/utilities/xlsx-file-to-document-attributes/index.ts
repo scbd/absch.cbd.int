c7033495c1932002880fd34deeccd1c78af5ad6c
Mitch1000 <mwdrohan@gmail.com>
2025-11-14 17:28:17 -0500

    handles an list of documents instead of just a single document

--- app/components/documents-uploader/utilities/xlsx-file-to-document-attributes/index.ts
+++ app/components/documents-uploader/utilities/xlsx-file-to-document-attributes/index.ts
@@ -5,17 +5,21 @@ import type {
 } from './types'
 import { type DocumentTypes, documentsList } from '../../data/document-types-list'
 
-function getColumnValue (sheet: XLSX.WorkSheet | IFileData, col: string, documentNumber: number): string {
-  const location = `${col}${documentNumber + 2}`
+type MapParams = {
+  documentMap: DocumentAttributesMap
+  sheet: XLSX.WorkSheet | IFileData
+  rowNumber: number
+}
+
+function getColumnValue (sheet: XLSX.WorkSheet | IFileData, col: string, rowNumber: number): string {
+  const location = `${col}${rowNumber}`
   return (sheet[location] || {}).w || ''
 }
 
-export function mapXLSXFileToAttributeNames (documentMap: DocumentAttributesMap, sheet: XLSX.WorkSheet | IFileData): DocumentAttributesMap {
-  const documentNumber :number = 1
-
+export function mapXLSXFileToAttributeNames ({ documentMap, sheet, rowNumber }: MapParams): DocumentAttributesMap {
   const getColumnString = (value: string | undefined): string => {
     if (typeof value === 'string') {
-      return getColumnValue(sheet, value, documentNumber) as string
+      return getColumnValue(sheet, value, rowNumber) as string
     }
     return 'Column not readable'
   }
@@ -36,15 +40,35 @@ export function mapXLSXFileToAttributeNames (documentMap: DocumentAttributesMap,
   return mapColumnToDocumentAttribute(documentMap)
 }
 
-export default function mapXLSXFileToDocumentAttributes (documentType: DocumentTypes, sheet: XLSX.WorkSheet | Array<string>): DocumentAttributesMap {
+function isRowEmpty (rowNumber: number, map: DocumentAttributesMap, sheet: XLSX.Sheet) {
+  return !Object.values(map)
+    .some((col) => {
+      if (typeof col === 'object') { return false }
+      return getColumnValue(sheet, col as string, rowNumber).length > 0
+    })
+}
+
+export default function mapXLSXFileToDocumentAttributes (documentType: DocumentTypes, sheet: XLSX.WorkSheet | Array<string>): Array<DocumentAttributesMap> {
   if (!documentType) {
     console.warn('No document type defined')
-    return {}
+    return [{}]
   }
-  const documentAttributesMap = documentsList[documentType]?.attributesMap
+  const documentMap = documentsList[documentType]?.attributesMap as DocumentAttributesMap
 
+  if (documentMap === undefined) { return [{}] }
 
-  if (documentAttributesMap === undefined) { return {} }
+  const range = sheet['!ref']
+  const headerCount: number = 3
+  const rangeEnd = XLSX.utils.decode_range(range).e.r
+  const rangeStart = XLSX.utils.decode_range(range).s.r + headerCount
 
-  return mapXLSXFileToAttributeNames(documentAttributesMap as DocumentAttributesMap, sheet)
+  const documents = []
+
+  for (let i = rangeStart; i < rangeEnd; i += 1) {
+    if (isRowEmpty(i, documentMap, sheet)) { continue }
+
+    documents.push(mapXLSXFileToAttributeNames({ documentMap, sheet, rowNumber: i }))
+  }
+
+  return documents
 }
