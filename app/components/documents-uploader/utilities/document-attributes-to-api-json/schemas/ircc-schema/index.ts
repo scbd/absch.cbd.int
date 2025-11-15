b231f17c1ba2212b6f237863769e758b9f2b22c1
Mitch1000 <mwdrohan@gmail.com>
2025-11-14 20:53:42 -0500

    removes check comments from schema as they are unnecessary

--- app/components/documents-uploader/utilities/document-attributes-to-api-json/schemas/ircc-schema/index.ts
+++ app/components/documents-uploader/utilities/document-attributes-to-api-json/schemas/ircc-schema/index.ts
@@ -8,44 +8,45 @@ export default class IrccSchema extends Schema {
     const Schema = IrccSchema
 
     return {
+      type: 'absPermit',
       header: {
-        identifier: Schema.generateUID(), // ✅
-        schema: 'absPermit', // ✅
-        languages: [Schema.getLanguageCode(sheet.language)] // ✅
+        identifier: Schema.generateUID(),
+        schema: 'absPermit',
+        languages: [Schema.getLanguageCode(sheet.language)]
       },
       absCNA: {
-        identifier: await Schema.getDocumentIdentifierByUid(sheet.absCNAId) // ✅
+        identifier: await Schema.getDocumentIdentifierByUid(sheet.absCNAId)
       },
       title: {
-        [this.language]: sheet.permitEquivalent // ✅
+        [this.language]: sheet.permitEquivalent
       },
-      dateOfIssuance: Schema.parseDate(sheet.dateOfIssuance), // ✅
-      dateOfExpiry: Schema.parseDate(sheet.dateOfExpiry), // ✅
-      providers: await Schema.findOrCreateContact(sheet.provider.existing), // ✅
-      providersConfidential: Schema.getIsConfidential(sheet.provider.type), // ✅
-      entitiesToWhomPICGranted: await Schema.findOrCreateContact(sheet.pic.existing), // ✅
-      entitiesToWhomPICGrantedConfidential: Schema.getIsConfidential(sheet.pic.type), // ✅
-      picGranted: Schema.parseTextToBoolean(sheet.pic.consent), // ✅
+      dateOfIssuance: Schema.parseDate(sheet.dateOfIssuance),
+      dateOfExpiry: Schema.parseDate(sheet.dateOfExpiry),
+      providers: await Schema.findOrCreateContact(sheet.provider.existing),
+      providersConfidential: Schema.getIsConfidential(sheet.provider.type),
+      entitiesToWhomPICGranted: await Schema.findOrCreateContact(sheet.pic.existing),
+      entitiesToWhomPICGrantedConfidential: Schema.getIsConfidential(sheet.pic.type),
+      picGranted: Schema.parseTextToBoolean(sheet.pic.consent),
       subjectMatter: {
-        [this.language]: Schema.getAsHtmlElement(sheet.subjectMatter) // ✅
+        [this.language]: Schema.getAsHtmlElement(sheet.subjectMatter)
       },
-      keywords: Schema.getKeywords(sheet.keywords).processedKeywords, // ✅
-      matEstablished: Schema.parseTextToBoolean(sheet.matEstablished), // ✅
+      keywords: Schema.getKeywords(sheet.keywords).processedKeywords,
+      matEstablished: Schema.parseTextToBoolean(sheet.matEstablished),
       usages: [
         {
-          identifier: Schema.getUsageMapping(sheet.usage) // ✅
+          identifier: Schema.getUsageMapping(sheet.usage)
         }
       ],
-      usagesConfidential: Schema.getIsConfidential(sheet.usage), // ✅
+      usagesConfidential: Schema.getIsConfidential(sheet.usage),
       usagesDescription: {
-        [this.language]: Schema.getAsHtmlElement(sheet.usageDescription) // ✅
+        [this.language]: Schema.getAsHtmlElement(sheet.usageDescription)
       },
       thirdPartyTransferCondition: {
-        [this.language]: Schema.getAsHtmlElement(sheet.conditionsThirdPartyTransfer) // ✅
+        [this.language]: Schema.getAsHtmlElement(sheet.conditionsThirdPartyTransfer)
       },
-      specimens: Schema.getELinkData(sheet.specimens), // ✅
-      taxonomies: Schema.getELinkData(sheet.taxonomies), // ✅
-      relevantInformation: sheet.additionalInformation // ✅
+      specimens: Schema.getELinkData(sheet.specimens),
+      taxonomies: Schema.getELinkData(sheet.taxonomies),
+      relevantInformation: sheet.additionalInformation
     }
   }
 }
