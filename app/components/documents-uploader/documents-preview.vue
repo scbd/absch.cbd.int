057146a4b208c987d91884a654b0be3857dc9697
Mitch1000 <mwdrohan@gmail.com>
2025-11-17 18:27:41 -0500

    includes documents preview file to break away logic for viewing documents from the main modal component

---
+++ app/components/documents-uploader/documents-preview.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- TODO: display parsed documents in a way that
+  will be easy for th document creators to understand. -->
+  <div
+    class="preview-list ps-3 pe-3 overflow-auto"
+  >
+    <div
+      v-for="documentJson in apiJson"
+      :key="documentJson.header.identifier"
+      class="mt-2 "
+    >
+      <h6> {{ documentJson.header.identifier }} </h6>
+      <div
+        class="preview overflow-scroll h-50 p-2 mt-2 bg-gray-200 border border-tertiary border-2"
+      >
+        <div> <strong>Data: </strong> </div>
+        <pre> {{ apiJson }} </pre>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  apiJson: {
+    type: Array,
+    default: () => [{ header: { identifier: '' } }]
+  }
+})
+</script>
+<style scoped>
+  .preview {
+    max-height: 160px;
+  }
+  .preview-list  {
+    max-height: 450px
+  }
+</style>
