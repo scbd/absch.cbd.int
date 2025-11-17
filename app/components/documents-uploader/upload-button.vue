59880e708e0c0600228a1b0812fa9725a3707a5b
Mitch1000 <mwdrohan@gmail.com>
2025-11-17 18:20:58 -0500

    adds loader indicator next to button to show the modal is loading without flasing a fullscreen loader

--- app/components/documents-uploader/upload-button.vue
+++ app/components/documents-uploader/upload-button.vue
@@ -1,33 +1,43 @@
 <template>
-  <button
-    class="btn btn-primary position-relative p-10"
-    type="button"
-    :disabled="props.isLoading"
-  >
-    {{ $t("browse") }}
-    <input
-      type="file"
-      name="file"
-      accept=".xlsx, .xls"
-      class="position-absolute fs-1 opacity-0 top-0 start-0 w-100 h-100"
-      @change="onChange"
-      @click="onFileInputClick"
+  <div class="d-flex">
+    <button
+      class="btn btn-primary position-relative p-10"
+      type="button"
+      :disabled="props.isLoading"
     >
-  </button>
+      {{ $t("browse") }}
+      <input
+        type="file"
+        name="file"
+        accept=".xlsx, .xls"
+        class="position-absolute fs-1 opacity-0 top-0 start-0 w-100 h-100"
+        @change="onChange"
+        @click="onFileInputClick"
+      >
+    </button>
+    <div
+      v-if="isLoading"
+      class="ms-3 align-content-center"
+    >
+      <CircleLoader
+        :size="22"
+      />
+    </div>
+  </div>
 </template>
 <script setup>
+import CircleLoader from './circle-loader.vue'
 const props = defineProps({
   isLoading: {
     type: Boolean,
-    default: false,
-  },
+    default: false
+  }
 })
 const $emit = defineEmits(['onFileChange'])
 
 const onChange = (changeEvent) => {
   $emit('onFileChange', changeEvent)
 }
-
 
 const onFileInputClick = (event) => {
   const { target } = event
