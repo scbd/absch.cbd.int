6de885d4a34d84d26da6476dfbfdc0bea10a5c9d
Mitch1000 <mwdrohan@gmail.com>
2025-11-17 10:27:37 -0500

    replaces progress bar with loader

---
+++ app/components/documents-uploader/circle-loader.vue
@@ -0,0 +1,54 @@
+<template>
+  <svg
+    class="circle-loader"
+    :width="size"
+    :height="size"
+    viewBox="0 0 74 74"
+  >
+    <circle
+      class="loader-circle"
+      cx="37"
+      cy="37"
+      r="32"
+      fill="none"
+      :stroke="color"
+      stroke-width="8"
+      transform="rotate(270 37 37)"
+    />
+  </svg>
+</template>
+<script setup>
+// TODO: Check project for existing Vue loader and use that.
+defineProps({
+  color: {
+    default: '#C0C0C0',
+    type: String
+  },
+
+  size: {
+    default: 70,
+    type: Number
+  }
+})
+</script>
+
+<style scoped>
+  .loader-circle {
+    stroke-dasharray: 201px;
+    stroke-dashoffset: 402px;
+    animation: base_modal_load_mask 2.62s cubic-bezier(0.645, 0.045, 0.355, 1.000) infinite;
+    pointer-events: none;
+  }
+
+  @keyframes base_modal_load_mask {
+    0% {
+      stroke-dashoffset: 402px;
+    }
+    50% {
+      stroke-dashoffset: 201px;
+    }
+    100% {
+      stroke-dashoffset: 0px;
+    }
+  }
+</style>
---
+++ app/components/documents-uploader/loader-overly.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="loader-overlay"
+    :style="{ backgroundColor: backgroundColor }"
+  >
+    <circle-loader
+      :color="color"
+      :size="size"
+    />
+  </div>
+</template>
+
+<script setup>
+import CircleLoader from './circle-loader.vue'
+defineProps({
+  backgroundColor: {
+    default: 'rgba(255, 255, 255, 0.72)',
+    type: String
+  },
+
+  color: {
+    default: '#C0C0C0',
+    type: String
+  },
+
+  size: {
+    default: 76,
+    type: Number
+  }
+})
+</script>
+
+<style scoped>
+  .loader-overlay {
+    display: flex;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    align-items: center;
+    justify-content: center;
+    z-index: 10;
+  }
+</style>
