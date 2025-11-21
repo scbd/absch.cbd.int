c318893b847950738c9cd0b3a5226f7895930029
Mitch1000 <mwdrohan@gmail.com>
2025-11-20 21:09:44 -0500

    creates types file used for parsing excel sheets to document attributes CHM-113

---
+++ app/types/components/documents-uploader/xlsx-file-to-document-attributes.ts
@@ -0,0 +1,46 @@
+import * as XLSX from 'xlsx'
+import { DocumentJsonType } from '~/types/components/documents-uploader/document-schema'
+import { StandardError } from '../../errors'
+
+export type DocumentAttributesMap = { [key: string]: string | DocumentAttributesMap }
+
+export type CellValue = {
+  w: string;
+}
+
+export interface IFileData {
+  [key: string]: CellValue;
+}
+
+export type DocError = {
+  message: string
+  index: number
+  value?: string
+  column?: number
+}
+
+export type DocumentsJson = {
+  documentsJson: Array<DocumentJsonType>
+  errors: Array<DocError>
+}
+
+export type KeywordType = {
+  name: string
+  identifier: string
+}
+
+export type MapParams = {
+  documentMap: DocumentAttributesMap
+  sheet: XLSX.WorkSheet | IFileData
+  rowNumber: number
+}
+
+export type DocumentsMapResult = {
+  documents: Array<DocumentAttributesMap>
+  errors: Array<StandardError>
+}
+
+export type MapResult = {
+  document: DocumentAttributesMap
+  errors: Array<StandardError>
+}
