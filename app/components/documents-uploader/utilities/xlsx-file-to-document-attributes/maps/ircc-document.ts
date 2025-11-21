71a5ab9fcc305511b58c0384eb8909964e54c495
Mitch1000 <mwdrohan@gmail.com>
2025-11-20 21:11:03 -0500

    creates map file that maps each excel column to a named document attribute CHM-113

---
+++ app/components/documents-uploader/utilities/xlsx-file-to-document-attributes/maps/ircc-document.ts
@@ -0,0 +1,40 @@
+export default {
+  language: 'A',
+  country: 'B',
+  absCNAId: 'C',
+  permitEquivalent: 'D',
+  dateOfIssuance: 'E',
+  dateOfExpiry: 'F',
+  provider: {
+    consent: 'ZZ',
+    type: 'G',
+    existing: 'H',
+    orgName: 'I',
+    acronym: 'J',
+    address: 'K',
+    city: 'L',
+    country: 'M',
+    email: 'N'
+  },
+  pic: {
+    consent: 'O',
+    type: 'P',
+    existing: 'Q',
+    orgName: 'R',
+    acronym: 'S',
+    address: 'T',
+    city: 'U',
+    country: 'V',
+    email: 'W'
+  },
+  matEstablished: 'X',
+  subjectMatter: 'Y',
+  keywords: 'Z',
+  specimens: 'AA',
+  taxonomies: 'AB',
+  usage: 'AC',
+  usageDescription: 'AD',
+  conditionsThirdPartyTransfer: 'AE',
+  permitFiles: 'AF',
+  additionalInformation: 'AG'
+}
