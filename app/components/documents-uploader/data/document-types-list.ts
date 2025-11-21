8469f3bd3852b3a344c7ca3f6447774e695f0a21
Mitch1000 <mwdrohan@gmail.com>
2025-11-20 21:07:06 -0500

    creates list of differnt kinds of documents handle by our system CHM-113

---
+++ app/components/documents-uploader/data/document-types-list.ts
@@ -0,0 +1,11 @@
+import IrccSchema from '../utilities/document-attributes-to-schema-json/schemas/ircc-schema'
+import irccAttributesMap from '../utilities/xlsx-file-to-document-attributes/maps/ircc-document'
+import { DocumentsList } from '~/types/components/documents-uploader/document-types-list'
+
+export const documentsList: DocumentsList = {
+  ircc: {
+    DocumentSchema: IrccSchema,
+    attributesMap: irccAttributesMap,
+    keywordDomains: ['1A22EAAB-9BBC-4543-890E-DEF913F59E98'] // TODO: Remove magic string by fetching the ABS Permit Keyword category. Possible fetch from env variable
+  }
+}
