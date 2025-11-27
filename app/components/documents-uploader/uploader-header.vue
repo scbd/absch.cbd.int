759dc6a86a150aef39c64ab04cd1c049f261119f
Mitch1000 <mwdrohan@gmail.com>
2025-11-27 12:27:14 -0500

    simplifies modal close button

--- app/components/documents-uploader/uploader-header.vue
+++ app/components/documents-uploader/uploader-header.vue
@@ -12,7 +12,7 @@
     </div>
     <button
       type="button"
-      class="close-button"
+      class="btn-close btn-secondary"
       aria-label="Dismiss"
       data-bs-dismiss="modal"
       @click="$emit('closeModal')"
@@ -22,30 +22,3 @@
 <script setup>
 const $emit = defineEmits(['closeModal'])
 </script>
-<style scoped>
-.close-button {
-  border: none;
-  background: 0 0;
-  cursor: pointer;
-  position: absolute;
-  right: 0;
-  top: 0;
-}
-
-.close-button::before {
-  display: block;
-  padding: 3px;
-  color: #bbb;
-  content: '\00D7';
-  font-size: 26px;
-  font-weight: 400;
-  line-height: 14px;
-  text-align: center;
-  border-radius: 20px;
-  background: #000;
-  width: 25px;
-  height: 25px;
-  margin: 10px;
-
-}
-</style>
