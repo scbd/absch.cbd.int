67736a77ccf9e94714129fbb63c5f5388650d5eb
Mitch1000 <mwdrohan@gmail.com>
2025-11-27 12:03:06 -0500

    finds document attributes by index instead of column letter in order to simplify code

--- app/components/documents-uploader/data/maps/ircc-document.ts
+++ app/components/documents-uploader/data/maps/ircc-document.ts
@@ -1,43 +1,43 @@
 export default {
-  language: { column: 'A', required: true },
-  country: { column: 'B', required: true },
-  absCNAId: { column: 'C', required: true },
-  permitEquivalent: { column: 'D' },
-  dateOfIssuance: { column: 'E', required: true },
-  dateOfExpiry: { column: 'F', required: false },
+  language: { column: '0', required: true }, // A
+  country: { column: '1', required: true }, // B
+  absCNAId: { column: '2', required: true }, // C
+  permitEquivalent: { column: '3' }, // D
+  dateOfIssuance: { column: '4', required: true }, // E
+  dateOfExpiry: { column: '5', required: false }, // F
   provider: {
     schema: {
-      type: { column: 'G' },
-      existing: { column: 'H' },
-      orgName: { column: 'I' },
-      acronym: { column: 'J' },
-      address: { column: 'K' },
-      city: { column: 'L' },
-      country: { column: 'M' },
-      email: { column: 'N' }
+      type: { column: '6' }, // G
+      existing: { column: '7' }, // H
+      orgName: { column: '8' }, // I
+      acronym: { column: '9' }, // J
+      address: { column: '10' }, // K
+      city: { column: '11' }, // L
+      country: { column: '12' }, // M
+      email: { column: '13' } // N
     }
   },
   pic: {
     schema: {
-      consent: { column: 'O' },
-      type: { column: 'P' },
-      existing: { column: 'Q' },
-      orgName: { column: 'R' },
-      acronym: { column: 'S' },
-      address: { column: 'T' },
-      city: { column: 'U' },
-      country: { column: 'V' },
-      email: { column: 'W' }
+      consent: { column: '14' }, // O
+      type: { column: '15' }, // P
+      existing: { column: '16' }, // Q
+      orgName: { column: '17' }, // R
+      acronym: { column: '18' }, // S
+      address: { column: '19' }, // T
+      city: { column: '20' }, // U
+      country: { column: '21' }, // V
+      email: { column: '22' } // W
     }
   },
-  matEstablished: { column: 'X' },
-  subjectMatter: { column: 'Y' },
-  keywords: { column: 'Z' },
-  specimens: { column: 'AA' },
-  taxonomies: { column: 'AB' },
-  usage: { column: 'AC' },
-  usageDescription: { column: 'AD' },
-  conditionsThirdPartyTransfer: { column: 'AE' },
-  permitFiles: { column: 'AF' },
-  additionalInformation: { column: 'AG' }
+  matEstablished: { column: '23' }, // X
+  subjectMatter: { column: '24' }, // Y
+  keywords: { column: '25' }, // Z
+  specimens: { column: '26' }, // AA
+  taxonomies: { column: '27' }, // AB
+  usage: { column: '28' }, // AC
+  usageDescription: { column: '29' }, // AD
+  conditionsThirdPartyTransfer: { column: '30' }, // AE
+  permitFiles: { column: '31' }, // AF
+  additionalInformation: { column: '32' } // AG
 }
