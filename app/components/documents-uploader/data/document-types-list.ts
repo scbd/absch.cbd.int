b43dbb472a4619d7fd391967bbbe535a79ace1f7
Mitch1000 <mwdrohan@gmail.com>
2025-12-01 16:32:53 -0500

    fetches abs permit keywords from thesaurus constants file

--- app/components/documents-uploader/data/document-types-list.ts
+++ app/components/documents-uploader/data/document-types-list.ts
@@ -1,12 +1,13 @@
 import IrccSchema from '../utilities/document-attributes-to-schema-json/schemas/ircc-schema'
 import irccAttributesMap from './maps/ircc-document'
 import { DocumentsList } from '~/types/components/documents-uploader/document-types-list'
+import { THESAURUS_DOMAINS } from '~/constants/thesaurus'
 
 export const documentsList: DocumentsList = {
   ircc: {
     DocumentSchema: IrccSchema,
     attributesMap: irccAttributesMap,
-    keywordDomains: ['1A22EAAB-9BBC-4543-890E-DEF913F59E98'], // TODO: Remove magic string by fetching the ABS Permit Keyword category. Possible fetch from env variable
+    keywordDomains: [THESAURUS_DOMAINS.ABS_PERMIT_KEYWORD],
     headerRows: [0, 1]
   }
 }
