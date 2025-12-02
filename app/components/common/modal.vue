00040c4b7aad82354411cb614f72f3f23c2bc917
Mitch1000 <mwdrohan@gmail.com>
2025-12-02 10:27:04 -0500

    updates modal component to be more extensible in order to support to future documents upload modal

--- app/components/common/modal.vue
+++ app/components/common/modal.vue
@@ -1,62 +1,105 @@
-
 <template>
-    <div ref="modalRef" class="modal fade"  aria-hidden="true">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-        <div class="modal-header">
-            <slot name="header">
-                <h5 class="modal-title" id="staticBackdropLabel">
-                    {{ lstring(title, locale) }}
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </slot>
+  <div
+    ref="modalRef"
+    class="modal fade overflow-hidden pt-5 pb-5"
+    aria-hidden="true"
+    @click.self="close"
+  >
+    <div
+      class="modal-dialog"
+      :class="`modal-${modalSize}`"
+    >
+      <div class="modal-content">
+        <div
+          class="modal-header"
+          :class="headerClass"
+        >
+          <slot name="header">
+            <h5
+              id="staticBackdropLabel"
+              class="modal-title"
+            >
+              {{ title }}
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              @click="close"
+            />
+          </slot>
         </div>
         <div class="modal-body">
-            <slot name="default">
-            </slot>
+          <slot name="default" />
         </div>
-        <div class="modal-footer">
-            <slot name="footer">
-                <div class="row w-100">
-                    <div class="col">
-                    </div>
-                    <div class="col-auto">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ t("closeButton") }}</button>
-                    </div>
-                </div>
-            </slot>
+        <div
+          class="modal-footer"
+          :class="footerClass"
+        >
+          <slot name="footer">
+            <div class="row w-100">
+              <div class="col" />
+              <div class="col-auto">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                  @click="close"
+                >
+                  {{ $t("closeButton") }}
+                </button>
+              </div>
+            </div>
+          </slot>
         </div>
-        </div>
+      </div>
     </div>
-    </div>
-
+  </div>
 </template>
+
 <script setup>
-    import { shallowRef, onMounted } from "vue";
-    import { Modal } from "bootstrap";
-    import { useI18n } from 'vue-i18n';
-    import messages from '../../app-text/components/common/modal.json';
-    import { lstring } from '../../components/kb/filters';
-    const { t, locale } = useI18n({ messages });
-    const modalRef = shallowRef(null);
-    let modal = null;
-    
-    const props = defineProps({
-        title: [String, Object], 
-        required: true,
-    });
+import { shallowRef, onMounted } from 'vue'
+import { Modal } from 'bootstrap'
+const modalRef = shallowRef(null)
 
-    onMounted(() => {
-        modal = new Modal(modalRef.value);
-    });
+let modal = { show: () => {}, hide: () => {} } 
 
-    const show = () => {
-        modal.show('static');
-    };
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  modalSize: {
+    type: String,
+    default: 'xl',
+  },
+  headerClass: {
+    type: String,
+    default: '',
+  },
+  footerClass: {
+    type: String,
+    default: '',
+  },
+})
 
-    const close = () => {
-        modal.hide();
-    };
+const $emit = defineEmits(['onClose'])
 
-    defineExpose({show, close})
+const show = () => {
+  modal.show('static')
+}
+
+const close = () => {
+  modal.hide()
+  $emit('onClose')
+}
+
+onMounted(() => {
+  const value = modalRef.value
+
+  modal = new Modal(value)
+})
+
+defineExpose({ show, close })
 </script>
