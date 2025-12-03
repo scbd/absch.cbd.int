7644ef9d006a1c1a100bfb4276caa09c79ab406d
Mitch1000 <mwdrohan@gmail.com>
2025-12-02 23:47:29 -0500

    adds button for uploading loading and parsing excel files

---
+++ app/components/documents-uploader/upload-button.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="d-flex">
+    <button
+      class="btn btn-primary position-relative p-10"
+      type="button"
+      :disabled="isLoading"
+    >
+      {{ $t('browseFiles') }}
+      <input
+        type="file"
+        name="file"
+        accept=".xlsx, .xls"
+        class="position-absolute fs-1 opacity-0 top-0 start-0 w-100 h-100"
+        @change="onChange"
+        @click="onFileInputClick"
+      >
+    </button>
+    <div
+      v-if="isLoading"
+      class="ms-3 align-content-center"
+    >
+      <Loader
+        :caption="$t('uploadingLoader')"
+      />
+    </div>
+  </div>
+</template>
+<script setup>
+import Loader from '../common/loading.vue'
+defineProps({
+  isLoading: {
+    type: Boolean,
+    default: false
+  }
+})
+const $emit = defineEmits(['onFileChange'])
+
+const onChange = (changeEvent) => {
+  $emit('onFileChange', changeEvent)
+}
+
+const onFileInputClick = (event) => {
+  const { target } = event
+  if (target === null) { return }
+
+  target.value = ''
+}
+</script>
