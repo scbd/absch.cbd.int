4b6dcd5afce9393ed7314abfe10af517c758bf1e
Blaise Fonseca <blaise.fonseca@un.org>
2025-12-04 16:12:21 -0500

    Update submission to include new fields (#1461)
    * add gbfGoalsTargets in thesaurus
    
    * move text to translation file
    
    * update submission edit form form CHM
    
    * WIP
    
    * wip
    
    * wip submission view page
    
    * wip, remove the BD-SUBJECT-CROSS-CUTTING
    
    * Adds functionality to retrieve narrower terms
    
    Adds a new method to retrieve narrower terms from the thesaurus,
    including options for immediate and domain code filtering. This
    enhances the thesaurus API by providing more flexible term retrieval
    capabilities.
    
    * Adds thesaurus terms for BETI finance
    
    Adds thesaurus terms and domains related to BETI finance and mainstreaming, enabling the selection of relevant information within the system.
    
    Updates the `gbfGoalsTargets` typo to `gbfGoalsAndTargets`.
    
    Fixes CHM-830
    
    * Updates submission form thematics and keywords (BETI)
    
    Updates submission form to use controlled vocabulary
    for thematic areas and keywords.
    
    Adds finance and mainstreaming keywords.
    Removes cross-cutting issues.
    
    Related to CHM-830
    
    * Improves display of tabular term checks
    
    Updates the tabular term check display for resource types and CBD thematic areas.
    This change ensures the correct number of columns are rendered for these elements.
    Adds CSS class to handle different column layouts.
    
    Relates to CHM-830
    
    * Fixes missing thesaurus domain term
    
    Ensures thesaurus domain terms are properly handled even when a specific term identifier is missing, preventing errors.
    
    The change also extends search filters by adding filters related to GBF goals/targets, CBD thematic areas, and BETI keywords.
    
    Updates translation keys in chm-left-menu-filters.json
    
    * Adds CHM left menu filters
    
    Adds new filters to the CHM left menu, including those for CBD thematic areas, resource types, GBF goals and targets, and BETI finance and mainstreaming keywords.
    
    These filters enhance the search functionality by allowing users to refine results based on these criteria.
    
    ---------
    
    Co-authored-by: Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>

--- app/services/thesaurus-service.js
+++ app/services/thesaurus-service.js
@@ -35,7 +35,7 @@ import _ from 'lodash';
                     throw "Domain term is missing";
 
                     options = options || {};
-                var url     = '/api/v2013/thesaurus/domains/' + encodeURIComponent(domainTerms[termIdentifier]) + '/terms';
+                var url     = '/api/v2013/thesaurus/domains/' + encodeURIComponent(domainTerms[termIdentifier]||termIdentifier) + '/terms';
 
                 if(options.other){
                     var urlOther = '/api/v2013/thesaurus/terms/' + encodeURIComponent(domainTerms['other']);
@@ -146,6 +146,7 @@ import _ from 'lodash';
                 languages              : "ISO639-2",
                 aichiTargets           : 'AICHI-TARGETS',
                 gbfTargets             : 'GBF-TARGETS',
+                gbfGoalsAndTargets     : 'GBF-GOALS-TARGETS',
                 legislationAgreementTypes : 'Legislation And Agreement Types',
                 bchRaAuthorAffiliation :   'Organization Types',
                 transboundaryMovementTypes : 'TransboundaryMovementTypes',
