b8897dd9133671a38c82803e9935697426f08442
Mitch1000 <mwdrohan@gmail.com>
2025-12-11 16:57:37 -0500

    generates forum portal link from the portal slug in order to simplify portal url generation CHM-865

--- app/views/portals/index.vue
+++ app/views/portals/index.vue
@@ -64,31 +64,27 @@ import { useI18n } from 'vue-i18n'
 // @ts-expect-error importing js file
 import { sanitizeHtml } from '~/services/html.sanitize'
 
-
 const { realm } = useRealm()
-
 const { locale } = useI18n()
 
-type ContentSchema = {
-  article: { articleId: string }
-}
+type LocalizedValue = { en: string }
 type PortalSchema = {
-  [key: string]: string | boolean | number | ContentSchema
+  _id: string
+  title: LocalizedValue
+  slug: string
+  content: { article: { articleId: string } }
 }
 type Image = { position: string, size: string, url: string }
 
 type Article = {
   _id: string
   coverImage: Image
-  summary: { en: string }
-  title: { en: string }
-  content: { en: string }
+  summary: LocalizedValue
+  title: LocalizedValue
+  content: LocalizedValue
   meta: { createdOn: string }
   adminTags?: string[]
   url?: string
-}
-type URLS_LIST = {
-  [key: string]: 'bch-iac' | 'capacity-building'
 }
 
 const auth = useAuth();
@@ -96,13 +92,6 @@ const articlesApi = new ArticlesApi({tokenReader:()=>auth.token()});
 const portalsApi = new PortalsApi();
 const articles :Ref<Article[]> = ref([])
 
-// TODO: Set article adminTag values for the below bch portal articles in the database to avoid
-// hard-coding these URLs.
-const FORUM_URLS :URLS_LIST = {
-  '64d54b6b8e4a59a51e799916': 'bch-iac', // Set article.adminTags = [portal:bch-iac] in the DB
-  '6866bc79b7c31f956fd986b9': 'capacity-building' // Set article.adminTags = [portal:capacity-building] in the DB
-}
-const PORTAL_TAGS_STRING = 'portal:'
 const PORTALS_URL = 'portals'
 
 onMounted(async () => {
@@ -110,8 +99,8 @@ onMounted(async () => {
   const portals = await portalsApi.queryPortals({ q: { realms: realm } })
 
   const articleOidQueries = portals
-    .filter((article: PortalSchema) => isObjectId(article['_id']))
-    .map((portalSchema: PortalSchema) => ({ $oid: mapObjectId((portalSchema['content'] as ContentSchema).article.articleId) }))
+    .filter((portalSchema: PortalSchema) => isObjectId(portalSchema['_id']))
+    .map((portalSchema: PortalSchema) => ({ $oid: mapObjectId(portalSchema.content.article.articleId) }))
 
   const query = [{ $match: {_id: { $in: articleOidQueries } } }]
 
@@ -119,14 +108,11 @@ onMounted(async () => {
 
   articles.value = articleData.map((article: Article) => {
     article.content = sanitizeHtml(lstring(article.content, locale))
-    if (!article.adminTags) {
-      article.url = `${PORTALS_URL}/${FORUM_URLS[article._id]}`
-      return article
-    }
+    const portalSlug = portals
+      .find((portalSchema: PortalSchema) => portalSchema.content.article.articleId === article._id)
+      .slug
 
-    const portalTag = (article.adminTags || [])
-      .find(tag => tag.includes(PORTAL_TAGS_STRING)) || ''
-    article.url = `${PORTALS_URL}/${portalTag.replace(PORTAL_TAGS_STRING, '')}`
+    article.url = `${PORTALS_URL}/${encodeURIComponent(portalSlug)}`
     return article
   })
 })
