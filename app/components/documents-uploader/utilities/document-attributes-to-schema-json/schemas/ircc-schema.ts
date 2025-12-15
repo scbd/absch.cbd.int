e548be4ea727cbcc9a3d57bdafcc3c5007e4c0f5
Mitch1000 <mwdrohan@gmail.com>
2025-12-15 12:14:34 -0500

    including warning indicating that the ircc schema parseJson method is not implemented

--- app/components/documents-uploader/utilities/document-attributes-to-schema-json/schemas/ircc-schema.ts
+++ app/components/documents-uploader/utilities/document-attributes-to-schema-json/schemas/ircc-schema.ts
@@ -1,5 +1,7 @@
 import Schema from './schema'
 
 export default class IrccSchema extends Schema {
-  override async parseXLSXFileToDocumentJson () {}
+  override async parseXLSXFileToDocumentJson () {
+    console.warn('Not implemented.')
+  }
 }
