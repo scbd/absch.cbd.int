1dd044370b4c0144dfbc902a52c0ef104a1aa8b2
Mitch1000 <mwdrohan@gmail.com>
2025-12-16 13:34:54 -0500

    makes type improvements for stricter type checking CHM-113

--- app/components/documents-uploader/utilities/document-attributes-to-schema-json/index.ts
+++ app/components/documents-uploader/utilities/document-attributes-to-schema-json/index.ts
@@ -1,17 +1,19 @@
-import {
+import type {
   MapToJsonParams, DocError, DocumentsJson
 } from '~/types/components/documents-uploader/document-schema'
-import { DocumentRequest } from '~/types/common/documents'
+import type { DocumentRequest } from '~/types/common/documents'
 import { documentsList } from '../../data/document-types-list'
 
 const defaultJson: DocumentRequest = { header: { identifier: '' } }
-function getError () :DocumentsJson {
+function getError (): DocumentsJson {
   const error: DocError = {
     reason: 'noDocumentParser',
-    row: 1,
-    value: ''
+    row: -1,
+    value: '',
+    message: '',
+    name: ''
   }
-  const errors: Array<DocError> = [error]
+  const errors: DocError[] = [error]
   return { documentsJson: [defaultJson], errors }
 }
 
@@ -19,30 +21,37 @@ export async function mapDocumentAttributesToSchemaJson ({
   attributesList,
   documentType,
   keywordsMap
-}: MapToJsonParams) :Promise<DocumentsJson> {
+}: MapToJsonParams): Promise<DocumentsJson> {
   // Handle the file type not exixting
   if (typeof documentsList[documentType] !== 'object') {
     getError()
   }
 
-  const errors: Array<DocError> = []
+  const errors: DocError[] = []
 
   // Iterate over each document in XLSX file and generate
   // the Schema JSON that representing a draft document.
-  const parsePromises = attributesList.map(async (attributes, index) :Promise<DocumentRequest> => {
+  const parsePromises = attributesList.map(async (attributes, index): Promise<DocumentRequest> => {
     const schema = new documentsList[documentType].DocumentSchema(attributes, keywordsMap)
 
     const json = await schema.parseXLSXFileToDocumentJson()
-      .catch((error) => {
+      .catch((e: unknown) => {
+        const error: DocError = Object.assign({ row: -1, reason: '', name: 'Parse Error', message: '' }, e)
         error.row = index
+        error.level = 'warning'
 
-        console.warn(error)
         errors.push(error)
+        console.warn(error) // eslint-disable-line no-console -- show error in console
+        const data: DocumentRequest = { header: { identifier: '' } }
+        const parseError = Object.assign({ data }, e)
+
+        return parseError.data
       })
 
-    return json === undefined ? defaultJson : json as DocumentRequest
+    return json
   })
 
   const documentsJson = await Promise.all(parsePromises)
+
   return { documentsJson, errors }
 }
