bea3f3c4ed0de4b5b4dcd85e736dbd2fcc30eb21
Mitch1000 <mwdrohan@gmail.com>
2025-12-16 00:31:43 -0500

    includes eslint config files

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import globals from 'globals'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { globalIgnores, defineConfig } from 'eslint/config'
+import pluginVue from 'eslint-plugin-vue'
+import eslintMinimalFilesList from './.config/eslintminimal.json' with { type: 'json' }
+import standard from 'neostandard'
+import love from 'eslint-config-love'
+
+const standardStyles = standard({ ts: true, filesTs: ['**/*.{ts,mts,tsx,vue,js}'] })
+  .find((config) => config.name === 'neostandard/style') || {}
+
+const vueTs = defineConfigWithVueTs([ 
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+])
+
+standardStyles.rules['@stylistic/type-annotation-spacing'] = ["error", { "before": false, "after": true }]
+love.rules['@typescript-eslint/no-magic-numbers'] = ["error", { "ignoreArrayIndexes": true, "ignore": [0, 1] }]
+
+export default defineConfig([ 
+  globalIgnores(['.config/', 'dist/', 'node_modules/']),
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  ...standard({
+    files: ['**/*.{ts,mts,tsx,vue,js}'],
+    noStyle: true 
+  }),
+  {
+    name: 'include styles for new files',
+    files: ['**/*.{ts,mts,tsx,vue,js}'],
+    rules: standardStyles.rules,
+    plugins: standardStyles.plugins,
+    ignores: eslintMinimalFilesList
+  },
+  vueTs,
+  {
+    ...love,
+    files: ['**/*.{ts,js}'],
+    ignores: eslintMinimalFilesList
+  },
+])
