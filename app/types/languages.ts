f11b5f64041de0d7b0ac0b6cdb6ea91f9ef67962
Mitch1000 <mwdrohan@gmail.com>
2025-12-19 19:23:07 -0500

    removes unuses types from languages types CHM-113

--- app/types/languages.ts
+++ app/types/languages.ts
@@ -6,18 +6,13 @@ export type EnglishLanguageType = 'Arabic' | 'English' | 'Spanish' | 'French'
 // TODO: Fix Francai typo
 export type LanguageType = 'العربية' | '中文' | 'Español' | 'Françai' | 'Русский' | 'English'
 
-export type GetKeys = ()=> LanguageCode[]
+export type LanguagesType = Record<LanguageCode, LanguageType>
 
-export interface ILanguagesType extends Record<LanguageCode, LanguageType> {
-  keys?: GetKeys
-}
-
-export interface IEnglishLanguages extends Record<LanguageCode, EnglishLanguageType> {
-  keys?: GetKeys
-}
+export type EnglishLanguages = Record<LanguageCode, EnglishLanguageType>
 
 export interface LanguageMapType {
   identifier: string
   name: LanguageType
 }
+
 export type Translations = (arg: string)=> string
