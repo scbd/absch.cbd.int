f1d07bdc5127de4aa1b1d017f170710140718535
Mitch1000 <mwdrohan@gmail.com>
2025-12-23 13:18:04 -0500

    ensures draft creation confirmation modal opens even if there are no errors in the parsed documents CHM-113

--- app/components/documents-uploader/uploader-footer.vue
+++ app/components/documents-uploader/uploader-footer.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="w-100 d-flex gap-2 justify-content-end">
-    <button
-      class="btn btn-secondary"
-      type="button"
-      @click="$emit('handleClear')"
+  <div class="w-100 d-flex mx-3">
+    <div
+      class="flex-fill"
     >
-      {{ $t("clearImportedRecords") }}
-    </button>
-    <button
-      class="btn btn-primary"
-      type="button"
-      @click="$emit('handleConfirm')"
-    >
-      {{ $t("confirmCreateDraftRecords") }}
-    </button>
+      <button
+        class="btn btn-secondary"
+        data-bs-dismiss="modal"
+        type="button"
+        @click="$emit('closeModal')"
+      >
+        {{ $t("close") }}
+      </button>
+    </div>
+    <div class="d-flex gap-2">
+      <button
+        class="btn btn-secondary"
+        type="button"
+        @click="$emit('handleClear')"
+      >
+        {{ $t("clearImportedRecords") }}
+      </button>
+      <button
+        class="btn btn-primary"
+        type="button"
+        @click="$emit('handleConfirm')"
+      >
+        {{ $t("confirmCreateDraftRecords") }}
+      </button>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -21,5 +35,5 @@ defineProps<{
   hasErrors: boolean
 }>()
 
-const $emit = defineEmits(['handleConfirm', 'handleClear'])
+const $emit = defineEmits(['handleConfirm', 'handleClear', 'closeModal'])
 </script>
