cd930e677183dbbf6e8dffe117f92e645776b716
Mitch1000 <mwdrohan@gmail.com>
2026-01-09 11:16:58 -0500

    reverts changes to view-national-report directive CHM-859

--- app/views/forms/view/view-national-report.directive.js
+++ app/views/forms/view/view-national-report.directive.js
@@ -1,9 +1,8 @@
 import app from '~/app';
-import { reactive } from 'vue'
 import template from "text!./view-national-report.directive.html";
-import NationalReportSection from '~/components/common/document-report/national-report-section.vue'
+import viewNationalReportT from '~/app-text/views/forms/view/view-national-report.json';
 
-app.directive("viewNationalReport", [function () {
+app.directive("viewNationalReport", ['translationService', function (translationService) {
 	return {
 		restrict: "EA",
 		template: template,
@@ -15,10 +14,28 @@ app.directive("viewNationalReport", [function () {
 			target: "@linkTarget",
 		},
 		link: function ($scope) {
-      $scope.vueComponent = {
-        components: { NationalReportSection },
-      }
-      $scope.makeReactive = (value) => reactive(value)
+
+			translationService.set('viewNationalReportT', viewNationalReportT);
+			$scope.display = function (field) {
+
+				if (!$scope.hide) return true; //show all fields
+
+				return ($scope.hide.indexOf(field) >= 0 ? false : true);
+			}
+
+			$scope.spaceSubQuestion = function (number) {
+				if ((number || '') == '') return '';
+				return number.replace(/([0-9]{1,3})([a-z])/, '$1 $2') + '. '
+
+			}
+
+			$scope.displayText = function (answer, question) {
+				if (question.options && answer) {
+					var option = _.find(question.options, { value: answer });
+					return option && option.title || answer;
+				}
+				return answer;
+			}
 		}
 	};
 }]);
