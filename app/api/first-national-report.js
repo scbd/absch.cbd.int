cfdf29eb6df5f202a1954c5266ab5c9075853bbf
Mitch1000 <mwdrohan@gmail.com>
2026-01-12 15:22:28 -0500

    moves code making the request for first national report data to the api folder CHM-859

---
+++ app/api/first-national-report.js
@@ -0,0 +1,38 @@
+import ApiBase, { tryCastToApiError } from './api-base'
+import { analyzerMapping } from '~/app-data/report-analyzer-mapping.js'
+import axios from 'axios'
+
+export default class KmDocumentApi extends ApiBase {
+  /**
+   * fetchReportData - For getting an array of question answers from the first national report.
+   * Makes GET request to https://absch.cbddev.xyz/api/v2019/report-analyzer/abs-national-report-1
+   *
+   * @param {string} countryCode
+   * @param {Realm} realm -> For determining the url and which realm the NR1 belongs to.
+   * @param {string[]} questionsList -> What questions are relevant to the
+   * @return {Promise} - Http request
+   */
+  async fetchReportData (countryCode, realm, questionsList) {
+    const q = { government_REL: { $in: [countryCode.toLowerCase()] } }
+
+    const realmKey = realm.notificationTemplateFolder.toLowerCase()
+
+    const map = analyzerMapping[realmKey]
+      .find(map => map.type === 'absNationalReport1')
+    // Params to fetch NR1 for the current country from the server.
+    const params = { q, realm: realm.realm }
+
+    if ((questionsList || []).length > 0 && Array.isArray(questionsList)) {
+      // Only get fetch relevant questions from the NR1 data on the server.
+      params.f = questionsList.reduce((acc, value) => {
+        const obj = acc
+        obj[value] = 1
+        return obj
+      }, {})
+    }
+    // https://absch.cbddev.xyz/api/v2019/report-analyzer/abs-national-report-1
+    return await axios.get(map.dataUrl, { params })
+      .then(res => res.data)
+      .catch(tryCastToApiError)
+  }
+}
