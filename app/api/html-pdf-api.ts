1a9db30ecfcd4937f7cb4529f3f882a81bee17ec
Blaise Fonseca <blaise.fonseca@un.org>
2026-02-17 10:43:22 -0500

    Adds HTML to PDF generation API endpoint
    Implements an API endpoint to generate PDF files from HTML content.
    This endpoint handles the conversion, sets the download file name,
    and includes captcha validation.

---
+++ app/api/html-pdf-api.ts
@@ -0,0 +1,32 @@
+import ApiBase, { tryCastToApiError } from './api-base';
+
+export default class HtmlPdfApi extends ApiBase
+{
+    constructor() {
+        super();
+    }
+
+    async generatePdf(html: string, downloadFileName: string, baseUrl: string, captchaToken: string) {
+        try {
+            const response = await this.http.post('/api/v2017/generate-pdf', {
+                html
+            }, { 
+                params : {
+                    'attachment-name' : downloadFileName,
+                    baseurl:baseUrl
+                },
+                responseType: "blob", 
+                headers:{
+                    'x-captcha-v2-badge-token' : captchaToken
+                }
+            })
+
+            // Create a Blob from the response data
+            const buffer = await response?.data?.arrayBuffer();
+            const blob = new Blob([buffer], { type: 'application/pdf' });
+            return blob;
+        } catch (error) {
+            throw tryCastToApiError(error);
+        }
+    }
+}
