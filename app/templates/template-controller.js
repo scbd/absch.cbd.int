e410a22f701609223fdf519f6ef3fb645948d27c
Blaise Fonseca <blaise.fonseca@un.org>
2026-02-27 11:56:37 -0500

    Disables Slaask chat integration
    Comments out the definition of the Slaask module and all related calls to initialize the chat service and identify users. This prevents Slaask from loading and running in the application.

--- app/templates/template-controller.js
+++ app/templates/template-controller.js
@@ -146,28 +146,28 @@ export default ['$rootScope', '$location', '$window', '$scope', 'locale', 'realm
                 return;
             var queryString = $location.search();
             
-            if(!queryString.print && !queryString.embed && !window.scbdApp.isPrerender){
+            // if(!queryString.print && !queryString.embed && !window.scbdApp.isPrerender){
                 
-                require(["slaask"], function (_slaask) {
+            //     require(["slaask"], function (_slaask) {
 
-                    if (user.isAuthenticated && _slaask) {
-                        _slaask.identify(user.name, {
-                            'user-id': user.userID,
-                            'name': user.name,
-                            'email': user.email,
-                        });
+            //         if (user.isAuthenticated && _slaask) {
+            //             _slaask.identify(user.name, {
+            //                 'user-id': user.userID,
+            //                 'name': user.name,
+            //                 'email': user.email,
+            //             });
 
-                        if (_slaask.initialized) {
-                            if (_slaask.slaaskSendUserInfos)
-                                _slaask.slaaskSendUserInfos();
-                        }
-                    }
+            //             if (_slaask.initialized) {
+            //                 if (_slaask.slaaskSendUserInfos)
+            //                     _slaask.slaaskSendUserInfos();
+            //             }
+            //         }
 
-                    if (_slaask && !_slaask.initialized) {
-                        _slaask.init('8b989bd6ee0cf49384761d4f86ddd945');
-                    }
-                });
-            }
+            //         if (_slaask && !_slaask.initialized) {
+            //             _slaask.init('8b989bd6ee0cf49384761d4f86ddd945');
+            //         }
+            //     });
+            // }
 
             var fields = logglyLogger.fields()
             fields.user = user.userID;
