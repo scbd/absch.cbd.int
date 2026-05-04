130d6f478809197ad2f3f3884f3cd0998c295eb0
Blaise Fonseca <blaise.fonseca@un.org>
2026-05-04 10:45:13 -0400

    concurrency

--- eslint.config.js
+++ eslint.config.js
@@ -17,17 +17,38 @@ const vueTs = defineConfigWithVueTs([
   pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
   {
-    rules: { ...love.rules, ...{ 'no-useless-assignment': ['off'] } },
+    rules: {
+      ...love.rules,
+      'no-useless-assignment': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off'
+    },
     plugins: love.plugins,
     files: ['**/*.vue']
   }
 ])
 
 export default defineConfig([
-  globalIgnores(['.config/', 'dist/', 'node_modules/']),
+  globalIgnores(['.config/', 'dist/', 'node_modules/', 'app/views/pdf-viewer/pdfjs/']),
   {
     languageOptions: {
-      globals: globals.browser
+      globals: {
+        ...globals.browser,
+        angular: 'readonly',
+        $: 'readonly',
+        jQuery: 'readonly',
+        moment: 'readonly',
+        _: 'readonly',
+        L: 'readonly',
+        Chart: 'readonly',
+        AmCharts: 'readonly',
+        Trix: 'readonly',
+        YT: 'readonly'
+      }
+    },
+    rules: {
+      'import-x/no-webpack-loader-syntax': 'off'
     }
   },
   ...standard({
@@ -51,6 +72,30 @@ export default defineConfig([
       'vue-i18n': {
         localeDir: './app/app-text/*.{json,json5,yaml,yml}'
       }
+    }
+  },
+  {
+    // Legacy JS files: disable/downgrade rules inappropriate for old AngularJS code
+    files: ['**/*.js'],
+    rules: {
+      'import-x/no-webpack-loader-syntax': 'off',
+      eqeqeq: 'warn',
+      camelcase: 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-type-assertion': 'off',
+      '@typescript-eslint/init-declarations': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      'new-cap': 'off'
     }
   }
 ])
