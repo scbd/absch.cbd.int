574e7e8aeb54adb77574632a7e85aa71fc0f3192
Blaise Fonseca <blaise.fonseca@un.org>
2026-05-05 16:40:53 -0400

    Oembed resources

--- app/components/common/cbd-view-article.vue
+++ app/components/common/cbd-view-article.vue
@@ -49,12 +49,44 @@
                     const response = await articlesApi.replaceOembed(params);
                     const embedHtml = `<div class="ck-media__wrapper" style="width:100%">${response.html}</div>`;
                     element.insertAdjacentHTML("afterend", embedHtml);
+                    loadResources(response.resources);
                 } catch (error) {
                     console.error("Failed to fetch oEmbed data:", error);
                 }
             });
         });
     };
+
+    async function loadResources(resources) {
+
+        if(resources?.length){
+            resources.forEach(resource=>{
+
+                const parsedUrl = new URL(resource.src);
+                
+                if(/cbd.int$/.test(parsedUrl.hostname) || /cbddev.xyz$/.test(parsedUrl.hostname)){
+                    if(['css', 'javascript'].includes(resource.type)){       
+                        const elementType =  resource.type == 'css' ? 'link' : 'script'                                    
+                        const remoteResource = document.createElement(elementType); 
+                        const head = document.getElementsByTagName('HEAD')[0];
+
+                        remoteResource.type = `text/${resource.type}`; 
+
+                        if(resource.type == 'javascript'){
+                            remoteResource.async = true;
+                            remoteResource.src = resource.src;
+                        }
+                        else{
+                            remoteResource.rel = 'stylesheet';
+                            link.href = resource.src
+                        }
+
+                        head.appendChild(remoteResource);
+                    }
+                }
+            });                                    
+        }
+    }
 
     onMounted(() => {
         if (props.article) {
