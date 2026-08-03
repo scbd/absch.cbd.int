62d0ef97d6d44f36f3137859dc46765f23a7d237
Blaise Fonseca <blaise.fonseca@un.org>
2026-08-03 11:17:58 -0400

    Remove unused search sort mappings for ABS and CHM
    The specific search sort mappings for the Access and Benefit-sharing (ABS) and Clearing-House Mechanism (CHM) modules are no longer required. This change empties the respective mapping objects and removes their associated translation imports. (CHM-901)

--- app/app-data/abs/search-sort-mapping.js
+++ app/app-data/abs/search-sort-mapping.js
@@ -1,8 +1,2 @@
-import fieldsTranslations from '~/app-text/app-data/abs/search-sort-mapping.json';
-import { mergeTranslationKeys } from '~/services/translation-merge';
-
-const fieldsT 		= mergeTranslationKeys(fieldsTranslations);
-
-
-export const searchSortMapping = {	    
+export const searchSortMapping = {
 }
